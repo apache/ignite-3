3c1d2b5d2fd415b8b7833802a28d277faddac748
ibessonov <bessonov.ip@gmail.com>
2021-04-02 13:33:54 +0300

    Update modules/configuration/src/main/java/org/apache/ignite/configuration/internal/util/ConfigurationUtil.java
    Co-authored-by: Alexander Polovtcev <alex.polovtcev@gmail.com>

--- modules/configuration/src/main/java/org/apache/ignite/configuration/internal/util/ConfigurationUtil.java
+++ modules/configuration/src/main/java/org/apache/ignite/configuration/internal/util/ConfigurationUtil.java
@@ -511,7 +511,7 @@ public class ConfigurationUtil {
 
     /**
      * Nullifies leaves in {@code newNode} node that are equal to the corresponding leaf values in {@code curNode}.
-     * In this context we view {@code curNode} as full configuration node with all the data, while {@code newNode}
+     * In this context we view {@code curNode} as a full configuration node with all the data, while {@code newNode}
      * contains only updates that we plan to apply to the {@code curNode} in the future.
      * @param curNode Node to look for definitive values.
      * @param newNode Node to nullify matching values.
