656a1efd249f34db675b829c41d38a50bd067b8e
tledkov <tledkov@gridgain.com>
2021-05-13 12:52:52 +0300

    fix style

--- modules/schema/src/main/java/org/apache/ignite/internal/schema/SchemaDescriptor.java
+++ modules/schema/src/main/java/org/apache/ignite/internal/schema/SchemaDescriptor.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import org.apache.ignite.internal.tostring.S;
 import org.jetbrains.annotations.NotNull;
