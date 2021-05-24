c7b4e51c491e61dc31b5279b0e6e6c0052f08dec
Aleksandr Polovtsev <alex.polovtcev@gmail.com>
2021-05-24 18:04:39 +0300

    add README

--- modules/network-api/src/test/java/org/apache/ignite/network/serialization/MessageSerializationRegistryTest.java
+++ modules/network-api/src/test/java/org/apache/ignite/network/serialization/MessageSerializationRegistryTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@
 
 package org.apache.ignite.network.messages;
 
+import org.apache.ignite.network.NetworkConfigurationException;
 import org.apache.ignite.network.NetworkMessage;
 import org.apache.ignite.network.serialization.MessageDeserializer;
 import org.apache.ignite.network.serialization.MessageSerializationFactory;
@@ -52,7 +53,7 @@ public class MessageSerializationRegistryTest {
 
         registry.registerFactory(Msg.TYPE, new MsgSerializationFactory());
 
-        assertThrows(IllegalArgumentException.class, () -> registry.registerFactory(Msg.TYPE, new MsgSerializationFactory()));
+        assertThrows(NetworkConfigurationException.class, () -> registry.registerFactory(Msg.TYPE, new MsgSerializationFactory()));
     }
 
     /**
