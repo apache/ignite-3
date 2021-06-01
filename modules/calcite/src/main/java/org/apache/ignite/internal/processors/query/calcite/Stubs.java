07f86cdf713b953955faa44b0df7710452ebd5d6
korlov42 <korlov@gridgain.com>
2021-06-01 17:12:01 +0300

    IGNITE-14822 Move planner related code to Ignite 3.0

---
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/Stubs.java
@@ -0,0 +1,16 @@
+package org.apache.ignite.internal.processors.query.calcite;
+
+/** Stubs */
+public class Stubs {
+    public static int intFoo(Object... args) {
+        return 0;
+    }
+
+    public static boolean boolFoo(Object... args) {
+        return false;
+    }
+
+    public static String stringFoo(Object... args) {
+        return "";
+    }
+}
