5bbdaf3e5e0bed4d75280f4b10ed8ef7af1b3994
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2021-06-09 10:44:30 +0300

    IGNITE-13885 Import.

--- modules/raft/src/test/java/org/apache/ignite/raft/jraft/core/TestCluster.java
+++ modules/raft/src/test/java/org/apache/ignite/raft/jraft/core/TestCluster.java
@@ -25,7 +25,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
