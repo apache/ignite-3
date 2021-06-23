4e3ea361af947bad4103677ac2d92ebff8dec6ec
Aleksandr Polovtsev <alex.polovtcev@gmail.com>
2021-06-23 15:04:10 +0300

    m

--- modules/metastorage/src/main/java/org/apache/ignite/internal/metastorage/MetaStorageManager.java
+++ modules/metastorage/src/main/java/org/apache/ignite/internal/metastorage/MetaStorageManager.java
@@ -542,10 +542,10 @@ public class MetaStorageManager {
 
             long appliedRevision = appliedRevisionBytes == null ? 0L : bytesToLong(appliedRevisionBytes);
 
-            if (revision < appliedRevision) {
+            if (revision <= appliedRevision) {
                 throw new IgniteInternalException(String.format(
-                    "Revision number in the Vault (%d) is greater than the current revision (%d)",
-                    appliedRevision, revision
+                    "Current revision (%d) must be greater than the revision in the Vault (%d)",
+                    revision, appliedRevision
                 ));
             }
 
