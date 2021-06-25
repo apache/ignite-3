a38a297cab5b2b6cb363942b387d9271936e5dec
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-06-25 15:06:58 +0300

    wip message decoder - variable message size decoding

--- modules/client-connector/src/main/java/org/apache/ignite/clientconnector/ClientMessageDecoder.java
+++ modules/client-connector/src/main/java/org/apache/ignite/clientconnector/ClientMessageDecoder.java
@@ -22,7 +22,10 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.util.CharsetUtil;
 import org.apache.ignite.lang.IgniteException;
+import org.msgpack.core.MessageFormat;
+import org.msgpack.core.MessagePack;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -44,8 +47,12 @@ class ClientMessageDecoder extends ByteToMessageDecoder {
     /** */
     private boolean magicDecoded;
 
+    /** */
+    private MessageFormat sizeFormat = null;
+
     @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) {
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list)
+            throws IOException {
         if (!readMagic(byteBuf))
             return;
 
@@ -83,14 +90,49 @@ class ClientMessageDecoder extends ByteToMessageDecoder {
      * @param buf Buffer.
      * @return True when a complete message has been received; false otherwise.
      */
-    private boolean read(ByteBuf buf) {
+    private boolean read(ByteBuf buf) throws IOException {
         if (cnt < 0) {
-            for (; cnt < 0 && buf.readableBytes() > 0; cnt++)
-                msgSize |= (buf.readByte() & 0xFF) << (8 * (4 + cnt));
+            if (sizeFormat == null) {
+                byte firstByte = buf.readByte();
+                sizeFormat = MessageFormat.valueOf(firstByte);
+
+                switch (sizeFormat) {
+                    case POSFIXINT:
+                        cnt = 0;
+                        msgSize = firstByte;
+                        break;
+
+                    case INT8:
+                    case UINT8:
+                        cnt = -1;
+                        data = new byte[] {firstByte, 0};
+                        break;
+
+                    case INT16:
+                    case UINT16:
+                        cnt = -2;
+                        data = new byte[] {firstByte, 0, 0};
+                        break;
+
+                    case INT32:
+                    case UINT32:
+                        cnt = -4;
+                        data = new byte[] {firstByte, 0, 0, 0, 0};
+                        break;
+
+                    default:
+                        throw new IgniteException("Unexpected message length format: " + sizeFormat);
+                }
+            }
+
+            for (; cnt < 0 && buf.readableBytes() > 0; cnt++) {
+                data[data.length + cnt] = buf.readByte();
+            }
 
             if (cnt < 0)
                 return false;
 
+            msgSize = MessagePack.newDefaultUnpacker(data).unpackInt(); // TODO: Cache unpacker.
             data = new byte[msgSize];
         }
 
@@ -113,9 +155,9 @@ class ClientMessageDecoder extends ByteToMessageDecoder {
         }
 
         if (cnt == msgSize) {
-            // TODO: MsgPack size can be variable.
             cnt = -1;
             msgSize = 0;
+            sizeFormat = null;
 
             return true;
         }
