0faae0f4c7f8e4f34e700d6645df879820cacb1c
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-07-16 17:43:51 +0300

    wip ClientSchema, ClientTuple

---
+++ modules/client/src/main/java/org/apache/ignite/client/internal/ClientTuple.java
@@ -0,0 +1,69 @@
+package org.apache.ignite.client.internal;
+
+import org.apache.ignite.binary.BinaryObject;
+import org.apache.ignite.table.Tuple;
+
+import java.util.BitSet;
+import java.util.UUID;
+
+public class ClientTuple implements Tuple {
+    @Override
+    public boolean contains(String colName) {
+        return false;
+    }
+
+    @Override
+    public <T> T value(String colName) {
+        return null;
+    }
+
+    @Override
+    public BinaryObject binaryObjectField(String colName) {
+        return null;
+    }
+
+    @Override
+    public byte byteValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public short shortValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public int intValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public long longValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public float floatValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue(String colName) {
+        return 0;
+    }
+
+    @Override
+    public String stringValue(String colName) {
+        return null;
+    }
+
+    @Override
+    public UUID uuidValue(String colName) {
+        return null;
+    }
+
+    @Override
+    public BitSet bitmaskValue(String colName) {
+        return null;
+    }
+}
