4195bb8f675018550f1a687cdaa623db37849ab0
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-07-31 15:13:06 +0300

    Fix ClientMessageDecoderTest

--- modules/client-common/src/test/java/org/apache/ignite/client/proto/ClientMessageDecoderTest.java
+++ modules/client-common/src/test/java/org/apache/ignite/client/proto/ClientMessageDecoderTest.java
@@ -49,43 +49,16 @@ public class ClientMessageDecoderTest {
         assertArrayEquals(new byte[]{33, 44}, bytes);
     }
 
-//
-//    @Test
-//    void testInvalidMagicThrowsException() {
-//        byte[] buf = {66, 69, 69, 70, 1, 2, 3};
-//
-//        var t = assertThrows(IgniteException.class,
-//                () -> new ClientMessageDecoder().decode(null, Unpooled.wrappedBuffer(buf), new ArrayList<>()));
-//
-//        assertEquals("Invalid magic header in thin client connection. Expected 'IGNI', but was 'BEEF'.",
-//                t.getMessage());
-//    }
-//
-//    /**
-//     * Tests multipart buffer arrival: socket can split incoming stream into arbitrary chunks.
-//     */
-//    @Test
-//    void testMultipartValidMagicAndMessageReturnsPayload() throws Exception {
-//        var decoder = new ClientMessageDecoder();
-//        var res = new ArrayList<>();
-//
-//        byte[] data = getMagicWithPayload();
-//
-//        decoder.decode(null, Unpooled.wrappedBuffer(data, 0, 4), res);
-//        assertEquals(0, res.size());
-//
-//        decoder.decode(null, Unpooled.wrappedBuffer(data, 4, 4), res);
-//        assertEquals(0, res.size());
-//
-//        decoder.decode(null, Unpooled.wrappedBuffer(data, 8, 1), res);
-//        assertEquals(0, res.size());
-//
-//        decoder.decode(null, Unpooled.wrappedBuffer(data, 9, 1), res);
-//        assertEquals(1, res.size());
-//
-//        var resBuf = (ByteBuffer) res.get(0);
-//        assertArrayEquals(new byte[]{33, 44}, resBuf.array());
-//    }
+
+    @Test
+    void testInvalidMagicThrowsException() {
+        byte[] buf = {66, 69, 69, 70, 1, 2, 3};
+
+        var t = assertThrows(IgniteException.class, () -> decode(buf));
+
+        assertEquals("Invalid magic header in thin client connection. Expected 'IGNI', but was 'BEEF'.",
+                t.getMessage());
+    }
 
     private static byte[] getMagicWithPayload() {
         var buf = new byte[10];
