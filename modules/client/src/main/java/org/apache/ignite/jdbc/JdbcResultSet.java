5af1df3aacb6ae34b8a0af7e1833e4121700472a
vermakov <vermakov@gridgain.com>
2021-08-16 13:45:16 +0300

    IGNITE-15186 Fixed licenses

--- modules/client/src/main/java/org/apache/ignite/jdbc/JdbcResultSet.java
+++ modules/client/src/main/java/org/apache/ignite/jdbc/JdbcResultSet.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.jdbc;
 
 import java.io.InputStream;
@@ -120,7 +137,7 @@ public class JdbcResultSet implements ResultSet {
      * @param closeStmt Close statement on the result set close.
      */
     JdbcResultSet(JdbcStatement stmt, long cursorId, int fetchSize, boolean finished,
-        List<List<Object>> rows, boolean isQry, boolean autoClose, long updCnt, boolean closeStmt, QueryEventHandler processor) {
+        List<List<Object>> rows, boolean isQuery, boolean autoClose, long updCnt, boolean closeStmt, QueryEventHandler processor) {
         assert stmt != null;
         assert fetchSize > 0;
 
@@ -129,11 +146,11 @@ public class JdbcResultSet implements ResultSet {
         this.cursorId = cursorId;
         this.fetchSize = fetchSize;
         this.finished = finished;
-        this.isQuery = isQry;
+        this.isQuery = isQuery;
         this.autoClose = autoClose;
         this.closeStmt = closeStmt;
 
-        if (isQry) {
+        if (isQuery) {
             this.rows = rows;
 
             rowsIter = rows != null ? rows.iterator() : null;
