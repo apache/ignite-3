6b15a341fc0556832c8bf544dc49fe000c8d4442
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-08-22 21:07:04 +0300

    wip TestHandshake

--- modules/platforms/dotnet/Apache.Ignite.Tests/ConnectionTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/ConnectionTests.cs
@@ -17,9 +17,12 @@
 
 namespace Apache.Ignite.Tests
 {
+    using System;
+    using System.Buffers;
     using System.Net;
     using System.Net.Sockets;
     using System.Threading.Tasks;
+    using MessagePack;
     using NUnit.Framework;
 
     public class ConnectionTests
@@ -36,18 +39,50 @@ namespace Apache.Ignite.Tests
                 await socket.ConnectAsync(IPAddress.Loopback, 10800);
                 var stream = new NetworkStream(socket, ownsSocket: true);
 
-                stream.WriteByte((byte)'I');
-                stream.WriteByte((byte)'G');
-                stream.WriteByte((byte)'N');
-                stream.WriteByte((byte)'I');
+                WriteMagic(stream);
+                WriteHandshake(stream);
 
                 await stream.FlushAsync();
+
+                var response = new byte[6];
+                await stream.ReadAsync(response);
             }
             catch
             {
                 socket.Dispose();
                 throw;
             }
+        }
+
+        private static void WriteMagic(NetworkStream stream)
+        {
+            // TODO: Async, in one call.
+            stream.WriteByte((byte)'I');
+            stream.WriteByte((byte)'G');
+            stream.WriteByte((byte)'N');
+            stream.WriteByte((byte)'I');
+        }
+
+        private static unsafe void WriteHandshake(NetworkStream stream)
+        {
+            var bufferWriter = new ArrayBufferWriter<byte>();
+            var writer = new MessagePackWriter(bufferWriter);
+
+            // Version.
+            writer.Write(3);
+            writer.Write(0);
+            writer.Write(0);
+
+            writer.Write(2); // Client type: general purpose.
+
+            writer.WriteBinHeader(0); // Features.
+            writer.WriteMapHeader(0); // Extensions.
+
+            writer.Flush();
+
+            // Write little-endian message size (TODO: check endianness).
+            var msgSize = (uint)bufferWriter.WrittenCount;
+            stream.Write(new ReadOnlySpan<byte>(&msgSize, 4)); // TODO: Async
         }
     }
 }
