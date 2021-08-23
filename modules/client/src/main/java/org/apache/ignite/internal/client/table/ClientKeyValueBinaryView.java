b3e231a2a816e9fe1ac86442f9be8990d9755d9c
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-08-23 13:07:15 +0300

    wip ClientKeyValueBinaryView

--- modules/client/src/main/java/org/apache/ignite/internal/client/table/ClientKeyValueBinaryView.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/table/ClientKeyValueBinaryView.java
@@ -17,16 +17,220 @@
 
 package org.apache.ignite.internal.client.table;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import org.apache.ignite.table.InvokeProcessor;
 import org.apache.ignite.table.KeyValueBinaryView;
 import org.apache.ignite.table.Table;
+import org.apache.ignite.table.Tuple;
+import org.apache.ignite.table.TupleBuilder;
+import org.apache.ignite.tx.Transaction;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Client key-value view implementation for binary user-object representation.
  */
 public class ClientKeyValueBinaryView implements KeyValueBinaryView {
+    /** Underlying table. */
     private final Table tbl;
 
+    /**
+     * Constructor.
+     *
+     * @param tbl Table.
+     */
     public ClientKeyValueBinaryView(Table tbl) {
+        assert tbl != null;
+
         this.tbl = tbl;
+    }
+
+    /** {@inheritDoc} */
+    @Override public TupleBuilder tupleBuilder() {
+        return tbl.tupleBuilder();
+    }
+
+    /** {@inheritDoc} */
+    @Override public Tuple get(@NotNull Tuple key) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Tuple> getAsync(@NotNull Tuple key) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Map<Tuple, Tuple> getAll(@NotNull Collection<Tuple> keys) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Map<Tuple, Tuple>> getAllAsync(@NotNull Collection<Tuple> keys) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean contains(@NotNull Tuple key) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void put(@NotNull Tuple key, Tuple val) {
+
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Void> putAsync(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public void putAll(@NotNull Map<Tuple, Tuple> pairs) {
+
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Void> putAllAsync(@NotNull Map<Tuple, Tuple> pairs) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Tuple getAndPut(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Tuple> getAndPutAsync(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean putIfAbsent(@NotNull Tuple key, @NotNull Tuple val) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Boolean> putIfAbsentAsync(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean remove(@NotNull Tuple key) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Boolean> removeAsync(@NotNull Tuple key) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean remove(@NotNull Tuple key, @NotNull Tuple val) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Boolean> removeAsync(@NotNull Tuple key, @NotNull Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Collection<Tuple> removeAll(@NotNull Collection<Tuple> keys) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Collection<Tuple>> removeAllAsync(@NotNull Collection<Tuple> keys) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Tuple getAndRemove(@NotNull Tuple key) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Tuple> getAndRemoveAsync(@NotNull Tuple key) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean replace(@NotNull Tuple key, Tuple val) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Boolean> replaceAsync(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public boolean replace(@NotNull Tuple key, Tuple oldVal, Tuple newVal) {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Boolean> replaceAsync(@NotNull Tuple key, Tuple oldVal, Tuple newVal) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Tuple getAndReplace(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull CompletableFuture<Tuple> getAndReplaceAsync(@NotNull Tuple key, Tuple val) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public <R extends Serializable> R invoke(
+            @NotNull Tuple key,
+            InvokeProcessor<Tuple, Tuple, R> proc,
+            Serializable... args
+    ) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull <R extends Serializable> CompletableFuture<R> invokeAsync(
+            @NotNull Tuple key,
+            InvokeProcessor<Tuple, Tuple, R> proc,
+            Serializable... args
+    ) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public <R extends Serializable> Map<Tuple, R> invokeAll(
+            @NotNull Collection<Tuple> keys,
+            InvokeProcessor<Tuple, Tuple, R> proc,
+            Serializable... args
+    ) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @NotNull <R extends Serializable> CompletableFuture<Map<Tuple, R>> invokeAllAsync(
+            @NotNull Collection<Tuple> keys,
+            InvokeProcessor<Tuple, Tuple, R> proc,
+            Serializable... args
+    ) {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public @Nullable Transaction transaction() {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override public KeyValueBinaryView withTransaction(Transaction tx) {
+        return null;
     }
 }
