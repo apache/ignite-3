ee0d51bb4056f2c5afe2d2318efb372677a25ee5
Kirill Tkalenko <tkalkirill@yandex.ru>
2021-08-27 15:07:27 +0300

    IGNITE-15047 modify javadoc

--- modules/configuration-api/src/main/java/org/apache/ignite/configuration/annotation/InternalConfiguration.java
+++ modules/configuration-api/src/main/java/org/apache/ignite/configuration/annotation/InternalConfiguration.java
@@ -26,8 +26,16 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * This annotation can only be applied to a class that is either marked with {@link ConfigurationRoot}
  * or the superclass is marked with {@link ConfigurationRoot}, {@link Config}.
+ * <br><br/>
  * It indicates that this is an internal configuration that should be hidden from the end user.
  * Any extensions are allowed for any configuration.
+ * <br><br/>
+ * NOTE:
+ * <ul>
+ * <li>Extension and superclass fields cannot have the same name.</li>
+ * <li>Extensions of the same superclass can have the same fields, they must be semantically equal,
+ * then they will share it among themselves.</li>
+ * </ul>
  *
  * @see ConfigurationRoot
  * @see Config
