35ed31553c1b8e87a18e3fb1a75106e2bd7195a8
Kirill Gusakov <kgusakov@gmail.com>
2021-09-21 18:16:54 +0300

    fixed test

--- modules/table/src/test/java/org/apache/ignite/internal/table/TableManagerTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/TableManagerTest.java
@@ -338,7 +338,7 @@ public class TableManagerTest {
         CompletableFuture<TableManager> tblManagerFut,
         Phaser phaser
     ) {
-        when(rm.prepareRaftGroup(any(), any(), any())).thenReturn(CompletableFuture.completedFuture(null));
+        when(rm.prepareRaftGroup(any(), any(), any(), any())).thenReturn(CompletableFuture.completedFuture(null));
 
         AtomicBoolean tableCreatedFlag = new AtomicBoolean();
 
