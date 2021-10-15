abb59b29cee5c0770dcd2dc25e674ec8965fc32d
tledkov <tledkov@gridgain.com>
2021-10-15 15:21:52 +0300

    fix imports

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/ITTablesApiTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/ITTablesApiTest.java
@@ -18,7 +18,6 @@
 package org.apache.ignite.internal.runner.app;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -26,6 +25,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 import com.google.common.collect.Lists;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgnitionManager;
