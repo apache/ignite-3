cccc0348e7d09fd0337c7c238f7883da679272c8
Vladislav Pyatkov <vldpyatkov@gmail.com>
2021-11-15 15:51:30 +0300

    IGNITE-15711 Implement Loza stop

--- modules/raft/src/main/java/org/apache/ignite/internal/raft/Loza.java
+++ modules/raft/src/main/java/org/apache/ignite/internal/raft/Loza.java
@@ -25,15 +25,19 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.apache.ignite.internal.manager.IgniteComponent;
 import org.apache.ignite.internal.raft.server.RaftServer;
 import org.apache.ignite.internal.raft.server.impl.JraftServerImpl;
 import org.apache.ignite.internal.thread.NamedThreadFactory;
+import org.apache.ignite.internal.util.IgniteSpinBusyLock;
 import org.apache.ignite.internal.util.IgniteUtils;
+import org.apache.ignite.lang.IgniteException;
 import org.apache.ignite.lang.IgniteInternalException;
 import org.apache.ignite.lang.LoggerMessageHelper;
+import org.apache.ignite.lang.NodeStoppingException;
 import org.apache.ignite.network.ClusterNode;
 import org.apache.ignite.network.ClusterService;
 import org.apache.ignite.raft.client.Peer;
@@ -77,6 +81,12 @@ public class Loza implements IgniteComponent {
 
     /** Executor for raft group services. */
     private final ScheduledExecutorService executor;
+
+    /** Busy lock to stop synchronously. */
+    private final IgniteSpinBusyLock busyLock = new IgniteSpinBusyLock();
+
+    /** Prevents double stopping the component. */
+    private final AtomicBoolean stopGuard = new AtomicBoolean();
 
     /**
      * The constructor.
@@ -123,7 +133,12 @@ public class Loza implements IgniteComponent {
     /** {@inheritDoc} */
     @Override
     public void stop() throws Exception {
-        // TODO: IGNITE-15161 Implement component's stop.
+        if (!stopGuard.compareAndSet(false, true)) {
+            return;
+        }
+
+        busyLock.block();
+
         IgniteUtils.shutdownAndAwaitTermination(executor, 10, TimeUnit.SECONDS);
 
         raftServer.stop();
@@ -147,6 +162,27 @@ public class Loza implements IgniteComponent {
             List<ClusterNode> nodes,
             Supplier<RaftGroupListener> lsnrSupplier
     ) {
+        if (!busyLock.enterBusy()) {
+            throw new IgniteException(new NodeStoppingException());
+        }
+
+        try {
+            return prepareRaftGroupInternal(groupId, nodes, lsnrSupplier);
+        } finally {
+            busyLock.leaveBusy();
+        }
+    }
+
+    /**
+     * Internal method for creates a RAFT group.
+     *
+     * @param groupId      Raft group id.
+     * @param nodes        Raft group nodes.
+     * @param lsnrSupplier Raft group listener supplier.
+     * @return Future representing pending completion of the operation.
+     */
+    private CompletableFuture<RaftGroupService> prepareRaftGroupInternal(String groupId, List<ClusterNode> nodes,
+            Supplier<RaftGroupListener> lsnrSupplier) {
         assert !nodes.isEmpty();
 
         List<Peer> peers = nodes.stream().map(n -> new Peer(n.address())).collect(Collectors.toList());
@@ -195,6 +231,28 @@ public class Loza implements IgniteComponent {
             Collection<ClusterNode> deltaNodes,
             Supplier<RaftGroupListener> lsnrSupplier
     ) {
+        if (!busyLock.enterBusy()) {
+            throw new IgniteException(new NodeStoppingException());
+        }
+
+        try {
+            return updateRaftGroupInternal(groupId, nodes, deltaNodes, lsnrSupplier);
+        } finally {
+            busyLock.leaveBusy();
+        }
+    }
+
+    /**
+     * Internal mthod for creates a raft group.
+     *
+     * @param groupId      Raft group id.
+     * @param nodes        Full set of raft group nodes.
+     * @param deltaNodes   New raft group nodes.
+     * @param lsnrSupplier Raft group listener supplier.
+     * @return Future representing pending completion of the operation.
+     */
+    private CompletableFuture<RaftGroupService> updateRaftGroupInternal(String groupId, Collection<ClusterNode> nodes,
+            Collection<ClusterNode> deltaNodes, Supplier<RaftGroupListener> lsnrSupplier) {
         assert !nodes.isEmpty();
 
         List<Peer> peers = nodes.stream().map(n -> new Peer(n.address())).collect(Collectors.toList());
@@ -232,6 +290,26 @@ public class Loza implements IgniteComponent {
      * @return Future which will complete when peers change.
      */
     public CompletableFuture<Void> changePeers(String groupId, List<ClusterNode> expectedNodes, List<ClusterNode> changedNodes) {
+        if (!busyLock.enterBusy()) {
+            throw new IgniteException(new NodeStoppingException());
+        }
+
+        try {
+            return changePeersInternal(groupId, expectedNodes, changedNodes);
+        } finally {
+            busyLock.leaveBusy();
+        }
+    }
+
+    /**
+     * Internal method for change peers for a RAFT group.
+     *
+     * @param groupId       Raft group id.
+     * @param expectedNodes List of nodes that contains the raft group peers.
+     * @param changedNodes  List of nodes that will contain the raft group peers after.
+     * @return Future which will complete when peers change.
+     */
+    private CompletableFuture<Void> changePeersInternal(String groupId, List<ClusterNode> expectedNodes, List<ClusterNode> changedNodes) {
         List<Peer> expectedPeers = expectedNodes.stream().map(n -> new Peer(n.address())).collect(Collectors.toList());
         List<Peer> changedPeers = changedNodes.stream().map(n -> new Peer(n.address())).collect(Collectors.toList());
 
@@ -255,7 +333,15 @@ public class Loza implements IgniteComponent {
      * @param groupId Raft group id.
      */
     public void stopRaftGroup(String groupId) {
-        raftServer.stopRaftGroup(groupId);
+        if (!busyLock.enterBusy()) {
+            throw new IgniteException(new NodeStoppingException());
+        }
+
+        try {
+            raftServer.stopRaftGroup(groupId);
+        } finally {
+            busyLock.leaveBusy();
+        }
     }
 
     /**
