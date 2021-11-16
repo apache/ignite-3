ca4c4d49bdde77e89aad6a8d2f70aa5e11004753
Andrew Mashenkov <andrey.mashenkov@gmail.com>
2021-11-16 18:16:10 +0300

    Remove indents in empty lines.

--- modules/table/src/test/java/org/apache/ignite/internal/table/SchemaValidationTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/SchemaValidationTest.java
@@ -45,12 +45,12 @@ public class SchemaValidationTest {
                 new Column[]{new Column("id", NativeTypes.INT64, false)},
                 new Column[]{new Column("val", NativeTypes.INT64, true)}
         );
-        
+
         RecordView<Tuple> recView = createTableImpl(schema).recordView();
-        
+
         assertThrowsWithCause(SchemaMismatchException.class, () -> recView.insert(Tuple.create().set("id", 0L).set("invalidCol", 0)));
     }
-    
+
     @Test
     public void schemaMismatch() {
         SchemaDescriptor schema = new SchemaDescriptor(
@@ -61,24 +61,24 @@ public class SchemaValidationTest {
                 },
                 new Column[]{new Column("val", NativeTypes.INT64, true)}
         );
-        
+
         Table tbl = createTableImpl(schema);
-        
+
         assertThrowsWithCause(SchemaMismatchException.class,
                 () -> tbl.recordView().get(Tuple.create().set("id", 0L).set("affId", 1L).set("val", 0L)));
         assertThrowsWithCause(SchemaMismatchException.class, () -> tbl.recordView().get(Tuple.create().set("id", 0L)));
-        
+
         assertThrowsWithCause(SchemaMismatchException.class, () -> tbl.keyValueView().get(Tuple.create().set("id", 0L)));
         assertThrowsWithCause(SchemaMismatchException.class,
                 () -> tbl.keyValueView().get(Tuple.create().set("id", 0L).set("affId", 1L).set("val", 0L)));
-        
+
         assertThrowsWithCause(SchemaMismatchException.class, () -> tbl.keyValueView().put(Tuple.create().set("id", 0L), Tuple.create()));
         assertThrowsWithCause(SchemaMismatchException.class,
                 () -> tbl.keyValueView().put(Tuple.create().set("id", 0L).set("affId", 1L).set("val", 0L), Tuple.create()));
         assertThrowsWithCause(SchemaMismatchException.class, () -> tbl.keyValueView().put(Tuple.create().set("id", 0L).set("affId", 1L),
                 Tuple.create().set("id", 0L).set("val", 0L)));
     }
-    
+
     @Test
     public void typeMismatch() {
         SchemaDescriptor schema = new SchemaDescriptor(
@@ -89,20 +89,20 @@ public class SchemaValidationTest {
                         new Column("valBytes", NativeTypes.blobOf(3), true)
                 }
         );
-        
+
         RecordView<Tuple> tbl = createTableImpl(schema).recordView();
-        
+
         // Check not-nullable column.
         assertThrowsWithCause(IllegalArgumentException.class, () -> tbl.insert(Tuple.create().set("id", null)));
-        
+
         // Check length of the string column
         assertThrowsWithCause(InvalidTypeException.class, () -> tbl.insert(Tuple.create().set("id", 0L).set("valString", "qweqwe")));
-        
+
         // Check length of the string column
         assertThrowsWithCause(InvalidTypeException.class,
                 () -> tbl.insert(Tuple.create().set("id", 0L).set("valBytes", new byte[]{0, 1, 2, 3})));
     }
-    
+
     @Test
     public void stringTypeMatch() {
         SchemaDescriptor schema = new SchemaDescriptor(
@@ -112,21 +112,21 @@ public class SchemaValidationTest {
                         new Column("valString", NativeTypes.stringOf(3), true)
                 }
         );
-        
+
         RecordView<Tuple> tbl = createTableImpl(schema).recordView();
-        
+
         Tuple tuple = Tuple.create().set("id", 1L);
-        
+
         tbl.insert(tuple.set("valString", "qwe"));
         tbl.insert(tuple.set("valString", "qw"));
         tbl.insert(tuple.set("valString", "q"));
         tbl.insert(tuple.set("valString", ""));
         tbl.insert(tuple.set("valString", null));
-        
+
         // Check string 3 char length and 9 bytes.
         tbl.insert(tuple.set("valString", "我是谁"));
     }
-    
+
     @Test
     public void bytesTypeMatch() {
         SchemaDescriptor schema = new SchemaDescriptor(
@@ -136,36 +136,36 @@ public class SchemaValidationTest {
                         new Column("valUnlimited", NativeTypes.BYTES, true),
                         new Column("valLimited", NativeTypes.blobOf(2), true)
                 });
-        
+
         RecordView<Tuple> tbl = createTableImpl(schema).recordView();
-        
+
         Tuple tuple = Tuple.create().set("id", 1L);
-        
+
         tbl.insert(tuple.set("valUnlimited", null));
         tbl.insert(tuple.set("valLimited", null));
         tbl.insert(tuple.set("valUnlimited", new byte[2]));
         tbl.insert(tuple.set("valLimited", new byte[2]));
         tbl.insert(tuple.set("valUnlimited", new byte[3]));
-        
+
         assertThrowsWithCause(InvalidTypeException.class, () -> tbl.insert(tuple.set("valLimited", new byte[3])));
-        
+
     }
-    
+
     private TableImpl createTableImpl(SchemaDescriptor schema) {
         return new TableImpl(new DummyInternalTableImpl(), new DummySchemaManagerImpl(schema), null);
     }
-    
+
     private <T extends Throwable> void assertThrowsWithCause(Class<T> expectedType, Executable executable) {
         Throwable ex = assertThrows(IgniteException.class, executable);
-        
+
         while (ex.getCause() != null) {
             if (expectedType.isInstance(ex.getCause())) {
                 return;
             }
-            
+
             ex = ex.getCause();
         }
-        
+
         fail("Expected cause wasn't found.");
     }
 }
