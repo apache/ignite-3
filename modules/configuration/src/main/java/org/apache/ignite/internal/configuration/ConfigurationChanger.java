bedbb18a56fa0dfa063372fab4555cd79c5607de
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2021-11-24 18:15:47 +0400

    IGNITE-15971 - inline a couple of methods

--- modules/configuration/src/main/java/org/apache/ignite/internal/configuration/ConfigurationChanger.java
+++ modules/configuration/src/main/java/org/apache/ignite/internal/configuration/ConfigurationChanger.java
@@ -383,65 +383,58 @@ public abstract class ConfigurationChanger implements DynamicConfigurationChange
         StorageRoots localRoots = storageRoots;
         
         return CompletableFuture
-                .supplyAsync(() -> applyAndValidateChangesAndCalcDiff(src, localRoots), pool)
-                .thenCompose(allChanges -> persistChanges(src, localRoots, allChanges));
-    }
+                .supplyAsync(() -> {
+                    SuperRoot curRoots = localRoots.roots;
 
-    @Nullable
-    private Map<String, Serializable> applyAndValidateChangesAndCalcDiff(ConfigurationSource src,
-                                                                         StorageRoots localRoots) {
-        SuperRoot curRoots = localRoots.roots;
+                    SuperRoot changes = curRoots.copy();
 
-        SuperRoot changes = curRoots.copy();
+                    src.reset();
 
-        src.reset();
+                    src.descend(changes);
 
-        src.descend(changes);
+                    addDefaults(changes);
 
-        addDefaults(changes);
+                    Map<String, Serializable> allChanges = createFlattenedUpdatesMap(curRoots, changes);
 
-        Map<String, Serializable> allChanges = createFlattenedUpdatesMap(curRoots, changes);
-
-        // Unlikely but still possible.
-        if (allChanges.isEmpty()) {
-            return null;
-        }
-
-        dropNulls(changes);
-
-        List<ValidationIssue> validationIssues = ValidationUtil.validate(
-                curRoots,
-                changes,
-                this::getRootNode,
-                cachedAnnotations,
-                validators
-        );
-
-        if (!validationIssues.isEmpty()) {
-            throw new ConfigurationValidationException(validationIssues);
-        }
-
-        return allChanges;
-    }
-
-    private CompletableFuture<Void> persistChanges(ConfigurationSource src, StorageRoots localRoots,
-                                                   Map<String, Serializable> allChanges) {
-        if (allChanges == null) {
-            return completedFuture(null);
-        }
-
-        return storage.write(allChanges, localRoots.version)
-                .thenCompose(casWroteSuccessfully -> {
-                    if (casWroteSuccessfully) {
-                        return localRoots.changeFuture;
-                    } else {
-                        // Here we go to next iteration of an implicit spin loop; we have to do it via recursion
-                        // because we work with async code (futures).
-                        return localRoots.changeFuture.thenCompose(v -> changeInternally(src));
+                    // Unlikely but still possible.
+                    if (allChanges.isEmpty()) {
+                        return null;
                     }
-                })
-                .exceptionally(throwable -> {
-                    throw new ConfigurationChangeException("Failed to change configuration", throwable);
+
+                    dropNulls(changes);
+
+                    List<ValidationIssue> validationIssues = ValidationUtil.validate(
+                            curRoots,
+                            changes,
+                            this::getRootNode,
+                            cachedAnnotations,
+                            validators
+                    );
+
+                    if (!validationIssues.isEmpty()) {
+                        throw new ConfigurationValidationException(validationIssues);
+                    }
+
+                    return allChanges;
+                }, pool)
+                .thenCompose(allChanges -> {
+                    if (allChanges == null) {
+                        return completedFuture(null);
+                    }
+
+                    return storage.write(allChanges, localRoots.version)
+                            .thenCompose(casWroteSuccessfully -> {
+                                if (casWroteSuccessfully) {
+                                    return localRoots.changeFuture;
+                                } else {
+                                    // Here we go to next iteration of an implicit spin loop; we have to do it via recursion
+                                    // because we work with async code (futures).
+                                    return localRoots.changeFuture.thenCompose(v -> changeInternally(src));
+                                }
+                            })
+                            .exceptionally(throwable -> {
+                                throw new ConfigurationChangeException("Failed to change configuration", throwable);
+                            });
                 });
     }
 
