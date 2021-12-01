ccb8dfec8c10c2c327a2141944b21a0ef53da4f5
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2021-12-01 18:56:46 +0400

    IGNITE-15897 - add debug print

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -249,6 +249,7 @@ public class IgniteImpl implements Ignite {
      *            {@code workDir} specified by the user.
      */
     public void start(@Nullable String cfg) {
+        System.out.println("XXX the config: " + cfg);
         List<IgniteComponent> startedComponents = new ArrayList<>();
 
         try {
