dd5ce93e17719d9197a2451fc464bdbed9070608
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-12-07 19:42:57 +0300

    add MarshallerColumn

---
+++ modules/schema/src/main/java/org/apache/ignite/internal/schema/marshaller/reflection/MarshallerColumn.java
@@ -0,0 +1,40 @@
+package org.apache.ignite.internal.schema.marshaller.reflection;
+
+import java.util.function.Supplier;
+import org.apache.ignite.internal.schema.marshaller.BinaryMode;
+import org.apache.ignite.internal.tostring.IgniteToStringExclude;
+import org.jetbrains.annotations.Nullable;
+
+public class MarshallerColumn {
+    /** Default "default value supplier". */
+    private static final Supplier<Object> NULL_SUPPLIER = () -> null;
+
+    /**
+     * Column name.
+     */
+    private final String name;
+
+    /**
+     * Column type.
+     */
+    private final BinaryMode type;
+
+    /**
+     * Default value supplier.
+     */
+    @IgniteToStringExclude
+    private final Supplier<Object> defValSup;
+
+    /**
+     * Constructor.
+     *
+     * @param name      Column name.
+     * @param type      An instance of column data type.
+     * @param defValSup Default value supplier.
+     */
+    public MarshallerColumn(String name, BinaryMode type, @Nullable Supplier<Object> defValSup) {
+        this.name = name;
+        this.type = type;
+        this.defValSup = defValSup == null ? NULL_SUPPLIER : defValSup;
+    }
+}
