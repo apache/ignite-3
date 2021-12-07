66ad66d444c353dc75283ba5a57d93b399eeeef8
Pavel Tupitsyn <ptupitsyn@apache.org>
2021-12-07 14:38:12 +0300

    IGNITE-14971 Java thin: RecordView

---
+++ modules/client/src/main/java/org/apache/ignite/internal/client/table/ClientRecordView.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.internal.client.table;
+
+/**
+ * TODO:
+ * - Reuse PojoMarshaller from the server.
+ * - It depends on Column class (only name and index? do we need types?)
+ * - - add an interface? ColumnCommon
+ * - - or pass an array of strings?
+ *
+ */
+public class ClientRecordView {
+}
