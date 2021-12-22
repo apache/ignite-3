b245457c2c0be7a42a96959288e3d75bfd41d2b9
Vladislav Pyatkov <vldpyatkov@gmail.com>
2021-12-22 15:16:59 +0300

    Fixed tests.

--- modules/client/src/test/java/org/apache/ignite/client/AbstractClientTableTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/AbstractClientTableTest.java
@@ -95,13 +95,9 @@ public class AbstractClientTableTest extends AbstractClientTest {
     }
 
     protected Table tableWithDefaultValues() {
-        Table clientTbl = client.tables().table(TABLE_WITH_DEFAULT_VALUES);
-
-        if (clientTbl != null) {
-            return clientTbl;
+        if (server.tables().table(TABLE_WITH_DEFAULT_VALUES) == null) {
+            server.tables().createTable(TABLE_WITH_DEFAULT_VALUES, tbl -> tbl.changeReplicas(1));
         }
-
-        server.tables().createTable(TABLE_WITH_DEFAULT_VALUES, tbl -> tbl.changeReplicas(1));
 
         return client.tables().table(TABLE_WITH_DEFAULT_VALUES);
     }
@@ -132,13 +128,9 @@ public class AbstractClientTableTest extends AbstractClientTest {
     }
 
     protected Table fullTable() {
-        Table clientTbl = client.tables().table(TABLE_ALL_COLUMNS);
-
-        if (clientTbl != null) {
-            return clientTbl;
+        if (server.tables().table(TABLE_ALL_COLUMNS) == null) {
+            server.tables().createTable(TABLE_ALL_COLUMNS, tbl -> tbl.changeReplicas(1));
         }
-
-        server.tables().createTable(TABLE_ALL_COLUMNS, tbl -> tbl.changeReplicas(1));
 
         return client.tables().table(TABLE_ALL_COLUMNS);
     }
@@ -148,13 +140,9 @@ public class AbstractClientTableTest extends AbstractClientTest {
     }
 
     protected Table oneColumnTable() {
-        Table clientTbl = client.tables().table(TABLE_ONE_COLUMN);
-
-        if (clientTbl != null) {
-            return clientTbl;
+        if (server.tables().table(TABLE_ONE_COLUMN) == null) {
+            server.tables().createTable(TABLE_ONE_COLUMN, tbl -> tbl.changeReplicas(1));
         }
-
-        server.tables().createTable(TABLE_ONE_COLUMN, tbl -> tbl.changeReplicas(1));
 
         return client.tables().table(TABLE_ONE_COLUMN);
     }
