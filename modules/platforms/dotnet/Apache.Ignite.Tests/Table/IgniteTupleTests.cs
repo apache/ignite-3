f8ba26f5a5f389ace69950a62353b88e2cf8679d
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-01-10 12:03:50 +0300

    wip TestEquality

--- modules/platforms/dotnet/Apache.Ignite.Tests/Table/IgniteTupleTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/IgniteTupleTests.cs
@@ -88,7 +88,25 @@ namespace Apache.Ignite.Tests.Table
         [Test]
         public void TestEquality()
         {
-            Assert.Fail("TODO");
+            var t1 = new IgniteTuple(2) { ["k"] = 1, ["v"] = "2" };
+            var t2 = new IgniteTuple(3) { ["k"] = 1, ["v"] = "2" };
+            var t3 = new IgniteTuple(4) { ["k"] = 1, ["v"] = null };
+
+            Assert.AreEqual(t1, t2);
+            Assert.AreEqual(t2, t1);
+            Assert.AreEqual(t1.GetHashCode(), t2.GetHashCode());
+
+            Assert.AreNotEqual(t1, t3);
+            Assert.AreNotEqual(t1.GetHashCode(), t3.GetHashCode());
+
+            Assert.AreNotEqual(t2, t3);
+            Assert.AreNotEqual(t2.GetHashCode(), t3.GetHashCode());
+        }
+
+        [Test]
+        public void TestCustomTupleEquality()
+        {
+
         }
     }
 }
