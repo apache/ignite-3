c523310467015413cc7260b2a0ced13bd7ddb740
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-01-24 21:07:07 +0300

    wip RecordViewDefaultMappingTest

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/RecordViewDefaultMappingTest.cs
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Table
+{
+    using Ignite.Table;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests the default user type mapping behavior in <see cref="IRecordView{T}"/>.
+    /// </summary>
+    public class RecordViewDefaultMappingTest
+    {
+        [Test]
+        public void TestPropertyMapping()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestFieldMapping()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestRecordMapping()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestAnonymousTypeMapping()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestValueTupleMapping()
+        {
+            Assert.Fail("TODO");
+        }
+    }
+}
