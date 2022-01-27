854e56b1732bb5ccde0fd7adfd5a3166c5a0f419
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-01-27 15:22:07 +0300

    ip

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/Program.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Program.cs
@@ -24,7 +24,7 @@ namespace Apache.Ignite.Benchmarks
     {
         private static void Main()
         {
-            BenchmarkRunner.Run<SerializerHandlerWriteBenchmarks>();
+            BenchmarkRunner.Run<SerializerHandlerReadBenchmarks>();
         }
     }
 }
