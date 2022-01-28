74c2285e40c6e78a68a6793ac5ca9f0c6ae0b5b8
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-01-28 10:42:37 +0300

    ClientDataTypeExtensions

---
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Proto/ClientDataTypeExtensions.cs
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Internal.Proto
+{
+    using System;
+    using System.Collections;
+
+    /// <summary>
+    /// Extension methods for <see cref="ClientDataType"/>.
+    /// </summary>
+    internal static class ClientDataTypeExtensions
+    {
+        /// <summary>
+        /// Converts client data type to <see cref="Type"/>.
+        /// </summary>
+        /// <param name="clientDataType">Client data type.</param>
+        /// <returns>Corresponding CLR type.</returns>
+        public static Type ToType(this ClientDataType clientDataType)
+        {
+            return clientDataType switch
+            {
+                ClientDataType.Int8 => typeof(byte),
+                ClientDataType.Int16 => typeof(short),
+                ClientDataType.Int32 => typeof(int),
+                ClientDataType.Int64 => typeof(long),
+                ClientDataType.Float => typeof(float),
+                ClientDataType.Double => typeof(double),
+                ClientDataType.Decimal => typeof(decimal),
+                ClientDataType.Uuid => typeof(Guid),
+                ClientDataType.String => typeof(string),
+                ClientDataType.Bytes => typeof(byte[]),
+                ClientDataType.BitMask => typeof(BitArray),
+                _ => throw new ArgumentOutOfRangeException(nameof(clientDataType), clientDataType, null)
+            };
+        }
+    }
+}
