edcad9d2a445d2c8a7a41c32fa06d23bd359b2ef
Vladislav Pyatkov <vldpyatkov@gmail.com>
2022-02-04 08:59:09 +0300

    IGNITE-16368 Implement a futures for causality tokens

---
+++ modules/core/src/main/java/org/apache/ignite/internal/causality/VersionedValue.java
@@ -0,0 +1,195 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.causality;
+
+import java.util.Map.Entry;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.BiConsumer;
+import org.apache.ignite.lang.IgniteLogger;
+import org.apache.ignite.lang.IgniteStringFormatter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Parametrized type to store several versions of responses.
+ * A value can be available through the causality token, which is represented by long.
+ *
+ * @param <T> Type of real value.
+ */
+public class VersionedValue<T> {
+    /** The logger. */
+    private static final IgniteLogger LOG = IgniteLogger.forClass(VersionedValue.class);
+
+    /** Last applied casualty token. */
+    private long actualToken;
+
+    /** Size of stored history. */
+    private int historySize;
+
+    /** Closure applied on storage revision update. */
+    private BiConsumer<VersionedValue<T>, Long> onStorageRevisionUpdate;
+
+    /** Bersoned value storage. */
+    private final ConcurrentNavigableMap<Long, CompletableFuture<T>> history = new ConcurrentSkipListMap<>();
+
+    /**
+     * Constructor.
+     *
+     * @param onStorageRevisionUpdate Closure applied on storage revision update (see {@link #onStorageRevisionUpdate(long)}.
+     * @param historySize             Size of the history of changes to store, including last applied token.
+     */
+    public VersionedValue(@Nullable BiConsumer<VersionedValue<T>, Long> onStorageRevisionUpdate, int historySize) {
+        this.onStorageRevisionUpdate = onStorageRevisionUpdate == null ? (versionedValue, token) -> {
+            Entry<Long, CompletableFuture<T>> entry = history.floorEntry(token);
+
+            assert entry != null : IgniteStringFormatter.format("No future by token [token={}]", token);
+
+            if (!entry.getValue().isDone()) {
+                Entry<Long, CompletableFuture<T>> entryBefore = history.headMap(token).lastEntry();
+
+                assert entryBefore != null && entryBefore.getValue().isDone() : IgniteStringFormatter.format(
+                        "No future by token [token={}]", token);
+
+                entryBefore.getValue().whenComplete((t, throwable) -> {
+                    if (throwable != null) {
+                        entry.getValue().completeExceptionally(throwable);
+                    } else {
+                        entry.getValue().complete(t);
+                    }
+                });
+            }
+        } : onStorageRevisionUpdate;
+
+        this.historySize = historySize;
+    }
+
+    /**
+     * Constructor with default history size that equals 2. See {@link #VersionedValue(BiConsumer, int)}.
+     *
+     * @param onStorageRevisionUpdate Closure applied on storage revision update (see {@link #onStorageRevisionUpdate(long)}.
+     */
+    public VersionedValue(@Nullable BiConsumer<VersionedValue<T>, Long> onStorageRevisionUpdate) {
+        this(null, 2);
+    }
+
+    /**
+     * Constructor with default history size that equals 2 and no closure.
+     */
+    public VersionedValue() {
+        this(null);
+    }
+
+    /**
+     * Creates a future for this value and causality token, or returns it if it already exists.
+     * <p>
+     * The returned future is associated with an update having the given causality token and completes when this update is finished
+     * applying.
+     *
+     * @param causalityToken Causality token. Let's assume that the update associated with token N is already applied to this value. Then,
+     *                       if token N is given as an argument, a completed future will be returned. If token N - 1 is given, this method
+     *                       returns the result in the state that is actual for the given token. If the token is strongly outdated, {@link
+     *                       OutdatedTokenException} is thrown. If token N + 1 is given, this method will return a future that will be
+     *                       completed when the update associated with token N + 1 will have been applied. Tokens that greater than N by
+     *                       more than 1 should never be passed.
+     * @return The future.
+     * @throws OutdatedTokenException If outdated token is passed as an argument.
+     */
+    public CompletableFuture<T> get(long causalityToken) throws OutdatedTokenException {
+        if (causalityToken <= actualToken) {
+            Entry<Long, CompletableFuture<T>> histEntry = history.floorEntry(causalityToken);
+
+            if (histEntry == null) {
+                throw new OutdatedTokenException(causalityToken);
+            }
+
+            return histEntry.getValue();
+        } else {
+            var fut = new CompletableFuture<T>();
+
+            CompletableFuture<T> previousFut = history.putIfAbsent(causalityToken, fut);
+
+            return previousFut == null ? fut : previousFut;
+        }
+    }
+
+    /**
+     * Save the version of the value associated with the given causality token. If someone has got a future to await the value associated
+     * with the given causality token (see {@link #get(long)}, then the future will be completed.
+     *
+     * @param causalityToken Causality token.
+     * @param value          Current value.
+     */
+    public void set(long causalityToken, T value) {
+        assert causalityToken > actualToken : IgniteStringFormatter.format("Token earlier than actual [token={}, actual={}]",
+                causalityToken, actualToken);
+
+        assert actualToken + 1 == causalityToken : IgniteStringFormatter.format(
+                "Previous token did not complete [token={}, previous={}]", causalityToken, causalityToken -1);
+
+        CompletableFuture<T> res = history.putIfAbsent(causalityToken, CompletableFuture.completedFuture(value));
+
+        if (res == null || res.isCompletedExceptionally()) {
+            return;
+        }
+
+        assert !res.isDone() : IgniteStringFormatter.format("Different values asosiated with the token "
+                + "[token={}, value={}, prevValeu={}]", causalityToken, value, res.join());
+
+        res.complete(value);
+    }
+
+    /**
+     * Should be called on a storage revision update. This also triggers completion of a future created for the given causality token. It
+     * implies that all possible updates associated with this token have been already applied to the component.
+     *
+     * @param causalityToken Causality token.
+     */
+    public void onStorageRevisionUpdate(long causalityToken) {
+        assert causalityToken > actualToken : IgniteStringFormatter.format(
+                "New token shoul be more than current [current={}, new={}]", actualToken, causalityToken);
+
+        onStorageRevisionUpdate.accept(this, causalityToken);
+
+        if (history.size() > 1 && causalityToken - history.firstKey() >= historySize) {
+            trimToSize(causalityToken);
+        }
+
+        Entry<Long, CompletableFuture<T>> entry = history.floorEntry(causalityToken);
+
+        assert entry != null && entry.getValue().isDone() : IgniteStringFormatter.format(
+                "Future for the token is not completed [token={}]", causalityToken);
+
+        actualToken = causalityToken;
+    }
+
+    /**
+     * Trims the storage to history size.
+     *
+     * @param causalityToken Last token which was happend.
+     */
+    private void trimToSize(long causalityToken) {
+        Long lastToken = history.lastKey();
+
+        for (Long token : history.keySet()) {
+            if (token != lastToken && causalityToken - token >= historySize) {
+                history.remove(token);
+            }
+        }
+    }
+}
---
+++ modules/core/src/test/java/org/apache/ignite/internal/causality/VersionValueTest.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.causality;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.CompletableFuture;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests of causality token implementation based on versioned value.
+ * {@link VersionedValue}
+ */
+public class VersionValueTest {
+    /** Test value. */
+    public static final int TEST_VALUE = 1;
+
+    /**
+     * @throws OutdatedTokenException If failed.
+     */
+    @Test
+    public void testGetValueBeforeReady() throws OutdatedTokenException {
+        VersionedValue<Integer> longVersionedValue = new VersionedValue<>((integerVersionedValue, token) -> {
+            integerVersionedValue.set(token, TEST_VALUE);
+        }, 2);
+
+        CompletableFuture<Integer> fut = longVersionedValue.get(1);
+
+        assertFalse(fut.isDone());
+
+        longVersionedValue.onStorageRevisionUpdate(1);
+
+        assertTrue(fut.isDone());
+
+        assertEquals(TEST_VALUE, fut.join());
+
+        assertSame(fut, longVersionedValue.get(1));
+    }
+
+    /**
+     * @throws OutdatedTokenException If failed.
+     */
+    @Test
+    public void testExplicitlySetValue() throws OutdatedTokenException {
+        VersionedValue<Integer> longVersionedValue = new VersionedValue<>();
+
+        CompletableFuture<Integer> fut = longVersionedValue.get(1);
+
+        assertFalse(fut.isDone());
+
+        longVersionedValue.set(1, TEST_VALUE);
+
+        assertTrue(fut.isDone());
+
+        assertEquals(TEST_VALUE, fut.join());
+
+        assertSame(fut, longVersionedValue.get(1));
+    }
+
+    /**
+     * @throws OutdatedTokenException If failed.
+     */
+    @Test
+    public void testMissValueUpdateBeforeReady() throws OutdatedTokenException {
+        VersionedValue<Integer> longVersionedValue = new VersionedValue<>();
+
+        longVersionedValue.set(1, TEST_VALUE);
+
+        longVersionedValue.onStorageRevisionUpdate(1);
+
+        CompletableFuture<Integer> fut = longVersionedValue.get(2);
+
+        assertFalse(fut.isDone());
+
+        longVersionedValue.onStorageRevisionUpdate(2);
+
+        assertTrue(fut.isDone());
+
+        assertEquals(TEST_VALUE, fut.join());
+
+        assertSame(fut.join(), longVersionedValue.get(1).join());
+    }
+
+    /**
+     * Checks that the value
+     *
+     * @throws OutdatedTokenException If failed.
+     */
+    @Test
+    public void testMissValueUpdate() throws OutdatedTokenException {
+        VersionedValue<Integer> longVersionedValue = new VersionedValue<>();
+
+        longVersionedValue.set(1, TEST_VALUE);
+
+        longVersionedValue.onStorageRevisionUpdate(1);
+        longVersionedValue.onStorageRevisionUpdate(2);
+
+        CompletableFuture<Integer> fut = longVersionedValue.get(2);
+
+        assertTrue(fut.isDone());
+
+        assertEquals(TEST_VALUE, fut.join());
+
+        assertSame(fut, longVersionedValue.get(1));
+    }
+
+    /**
+     * Test checks token history size.
+     */
+    @Test()
+    public void testAbsoleteToken() {
+        VersionedValue<Integer> longVersionedValue = new VersionedValue<>();
+
+        longVersionedValue.set(1, TEST_VALUE);
+
+        longVersionedValue.onStorageRevisionUpdate(1);
+
+        longVersionedValue.set(2, TEST_VALUE);
+
+        longVersionedValue.onStorageRevisionUpdate(2);
+        longVersionedValue.onStorageRevisionUpdate(3);
+
+        assertThrowsExactly(OutdatedTokenException.class, () -> longVersionedValue.get(1));
+    }
+}
