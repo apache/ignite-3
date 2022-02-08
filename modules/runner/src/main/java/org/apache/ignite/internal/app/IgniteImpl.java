4604fce8188479127711ec4648bc822c64362bda
denis-chudov <moonglloom@gmail.com>
2022-02-08 19:37:48 +0300

    IGNITE-16363 Provide a guarantee of completeness of pre-recovery actions

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -308,6 +308,10 @@ public class IgniteImpl implements Ignite {
                 nodeCfgMgr.configurationRegistry().initializeDefaults();
             }
 
+            if (!standaloneMode()) {
+                waitForJoinPermission();
+            }
+
             // Start the remaining components.
             List<IgniteComponent> otherComponents = List.of(
                     nettyBootstrapFactory,
@@ -344,6 +348,21 @@ public class IgniteImpl implements Ignite {
 
             throw new IgniteException(errMsg, e);
         }
+    }
+
+    /**
+     * @return Whether the node had started in standalone mode (for example, maintenance mode).
+     */
+    private boolean standaloneMode() {
+        return false;
+    }
+
+    /**
+     * Awaits for a permission to join the cluster, i.e. node join response from Cluster Management group.
+     * After the completion of this method, the node is considered as validated.
+     */
+    private void waitForJoinPermission() {
+        // TODO https://issues.apache.org/jira/browse/IGNITE-15114
     }
 
     /**
