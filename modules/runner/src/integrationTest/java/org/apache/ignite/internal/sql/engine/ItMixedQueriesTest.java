b4315f2cd71a2ace687f5c8986bad53ed34b5200
Mirza Aliev <alievmirza@gmail.com>
2022-02-08 17:51:05 +0300

    IGNITE-16406 disable tests

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItMixedQueriesTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItMixedQueriesTest.java
@@ -40,6 +40,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Group of tests that still has not been sorted out. It’s better to avoid extending this class with new tests.
  */
+@Disabled("https://issues.apache.org/jira/browse/IGNITE-16502")
 public class ItMixedQueriesTest extends AbstractBasicIntegrationTest {
     /**
      * Before all.
