96fa23119785cea578d2a64b5eaab793ed1018d4
Mirza Aliev <alievmirza@gmail.com>
2022-02-22 10:25:57 +0300

    IGNITE-15554 add changing assignments mock on replica changing

--- modules/table/src/test/java/org/apache/ignite/internal/table/TableManagerTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/TableManagerTest.java
@@ -149,6 +149,10 @@ public class TableManagerTest extends IgniteAbstractTest {
     @Mock
     MetaStorageManager msm;
 
+    /** Meta storage manager. */
+    @Mock
+    MetaStorageManager msm;
+
     /** Tables configuration. */
     @InjectConfiguration(
             internalExtensions = ExtendedTableConfigurationSchema.class,
