f07f9eb352cba1b6fb8b9fb70b3f2d79488856c4
denis-chudov <moonglloom@gmail.com>
2022-02-24 14:29:57 +0300

    Revert "IGNITE-16363 Provide a guarantee of completeness of pre-recovery actions"
    This reverts commit 7d53cae8792255c88fc087adeef97b71d2848d52.
    
    # Conflicts:
    #	modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -336,10 +336,6 @@ public class IgniteImpl implements Ignite {
                 nodeCfgMgr.configurationRegistry().initializeDefaults();
             }
 
-            if (!standaloneMode()) {
-                waitForJoinPermission();
-            }
-
             // Start the remaining components.
             List<IgniteComponent> otherComponents = List.of(
                     nettyBootstrapFactory,
@@ -377,24 +373,6 @@ public class IgniteImpl implements Ignite {
             throw new IgniteException(errMsg, e);
         }
     }
-
-    /**
-     * Whether the node had started in standalone mode (for example, maintenance mode).
-     *
-     * @return Whether the node had started in standalone mode.
-     */
-    private boolean standaloneMode() {
-        return false;
-    }
-
-    /**
-     * Awaits for a permission to join the cluster, i.e. node join response from Cluster Management group.
-     * After the completion of this method, the node is considered as validated.
-     */
-    private void waitForJoinPermission() {
-        // TODO https://issues.apache.org/jira/browse/IGNITE-15114
-    }
-
     /**
      * Stops ignite node.
      */
