67f6cc9f945c460bf9308941c1cb3d99c1539478
denis-chudov <moonglloom@gmail.com>
2022-02-24 20:19:33 +0300

    enabled examples tests

--- examples/src/test/java/org/apache/ignite/example/sql/jdbc/SqlExamplesTest.java
+++ examples/src/test/java/org/apache/ignite/example/sql/jdbc/SqlExamplesTest.java
@@ -27,7 +27,6 @@ import org.apache.ignite.internal.testframework.WorkDirectoryExtension;
 import org.apache.ignite.internal.util.IgniteUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -35,7 +34,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * These tests check that all SQL JDBC examples pass correctly.
  */
 @ExtendWith(WorkDirectoryExtension.class)
-@Disabled("https://issues.apache.org/jira/browse/IGNITE-16543")
 public class SqlExamplesTest {
     /** Empty argument to invoke an example. */
     protected static final String[] EMPTY_ARGS = new String[0];
--- examples/src/test/java/org/apache/ignite/example/table/TableExamplesTest.java
+++ examples/src/test/java/org/apache/ignite/example/table/TableExamplesTest.java
@@ -27,7 +27,6 @@ import org.apache.ignite.internal.testframework.WorkDirectoryExtension;
 import org.apache.ignite.internal.util.IgniteUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -35,7 +34,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * These tests check that all table examples pass correctly.
  */
 @ExtendWith(WorkDirectoryExtension.class)
-@Disabled("https://issues.apache.org/jira/browse/IGNITE-16543")
 public class TableExamplesTest {
     /** Empty argument to invoke an example. */
     protected static final String[] EMPTY_ARGS = new String[0];
