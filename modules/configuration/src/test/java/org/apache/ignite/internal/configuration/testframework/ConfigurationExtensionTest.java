6b9070a1f1ea5a949ba4bdad465c407b8bc2d9b7
Kirill Tkalenko <tkalkirill@yandex.ru>
2022-02-28 18:07:13 +0300

    IGNITE-16545 impl

--- modules/configuration/src/test/java/org/apache/ignite/internal/configuration/testframework/ConfigurationExtensionTest.java
+++ modules/configuration/src/test/java/org/apache/ignite/internal/configuration/testframework/ConfigurationExtensionTest.java
@@ -18,14 +18,19 @@
 package org.apache.ignite.internal.configuration.testframework;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.apache.ignite.internal.configuration.notifications.ConfigurationStorageRevisionListenerHolder;
 import org.apache.ignite.internal.configuration.sample.DiscoveryConfiguration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,6 +44,9 @@ class ConfigurationExtensionTest {
     @InjectConfiguration
     private DiscoveryConfiguration fieldCfg;
 
+    @InjectRevisionListenerHolder
+    private ConfigurationStorageRevisionListenerHolder fieldRevisionListenerHolder;
+
     /** Test that contains injected parameter. */
     @Test
     public void injectConfiguration(
@@ -48,11 +56,11 @@ class ConfigurationExtensionTest {
 
         assertEquals(100, paramCfg.joinTimeout().value());
 
-        paramCfg.change(d -> d.changeJoinTimeout(200)).get(1, TimeUnit.SECONDS);
+        paramCfg.change(d -> d.changeJoinTimeout(200)).get(1, SECONDS);
 
         assertEquals(200, paramCfg.joinTimeout().value());
 
-        paramCfg.joinTimeout().update(300).get(1, TimeUnit.SECONDS);
+        paramCfg.joinTimeout().update(300).get(1, SECONDS);
 
         assertEquals(300, paramCfg.joinTimeout().value());
     }
@@ -80,13 +88,13 @@ class ConfigurationExtensionTest {
             return completedFuture(null);
         });
 
-        fieldCfg.change(change -> change.changeJoinTimeout(1000_000)).get(1, TimeUnit.SECONDS);
+        fieldCfg.change(change -> change.changeJoinTimeout(1000_000)).get(1, SECONDS);
 
         assertEquals(List.of("update", "join"), log);
 
         log.clear();
 
-        fieldCfg.failureDetectionTimeout().update(2000_000).get(1, TimeUnit.SECONDS);
+        fieldCfg.failureDetectionTimeout().update(2000_000).get(1, SECONDS);
 
         assertEquals(List.of("update", "failure"), log);
     }
@@ -108,10 +116,57 @@ class ConfigurationExtensionTest {
             change.changeVisible(3);
 
             ((ExtendedChange) change).changeInvisible(4);
-        }).get(1, TimeUnit.SECONDS);
+        }).get(1, SECONDS);
 
         assertEquals(3, cfg.visible().value());
 
         assertEquals(4, ((ExtendedConfiguration) cfg).invisible().value());
+    }
+
+    @Test
+    void testFieldConfigurationStorageRevisionListenerHolder() throws Exception {
+        assertNotNull(fieldRevisionListenerHolder);
+
+        List<Long> revisions = new CopyOnWriteArrayList<>();
+
+        fieldRevisionListenerHolder.listenUpdateStorageRevision(revision -> {
+            revisions.add(revision);
+
+            return completedFuture(null);
+        });
+
+        fieldCfg.joinTimeout().update(1_000_000).get(1, SECONDS);
+
+        fieldCfg.joinTimeout().update(2_000_000).get(1, SECONDS);
+
+        assertEquals(2, revisions.size(), revisions::toString);
+
+        assertTrue(revisions.get(0) < revisions.get(1), revisions::toString);
+    }
+
+    @Test
+    void testParamConfigurationStorageRevisionListenerHolder(
+            @InjectConfiguration("mock.joinTimeout=100") DiscoveryConfiguration paramCfg,
+            @InjectRevisionListenerHolder ConfigurationStorageRevisionListenerHolder paramRevisionListenerHolder
+    ) throws Exception {
+        assertNotNull(paramRevisionListenerHolder);
+
+        assertSame(fieldRevisionListenerHolder, paramRevisionListenerHolder);
+
+        List<Long> revisions = new CopyOnWriteArrayList<>();
+
+        paramRevisionListenerHolder.listenUpdateStorageRevision(revision -> {
+            revisions.add(revision);
+
+            return completedFuture(null);
+        });
+
+        paramCfg.joinTimeout().update(1_000_000).get(1, SECONDS);
+
+        paramCfg.joinTimeout().update(2_000_000).get(1, SECONDS);
+
+        assertEquals(2, revisions.size(), revisions::toString);
+
+        assertTrue(revisions.get(0) < revisions.get(1), revisions::toString);
     }
 }
