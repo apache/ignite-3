6659e4a3deb55f200f2575d7ad40b8fbc50bdc81
zstan <stanilovsky@gmail.com>
2022-03-04 12:18:44 +0300

    IGNITE-16645 Adoption of a bunch of calcite related tickets from Ignite-2 part2 - Fixes #702.
    IGNITE-16228 SetOp execution node cannot be rewinded.
    IGNITE-16191 Unexpected result of COUNT with multiple parameters.
    IGNITE-16354 Memory leak in RelOptCluster during mapping.
    
    Signed-off-by: zstan <stanilovsky@gmail.com>

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/Commons.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/Commons.java
@@ -834,8 +834,4 @@ public final class Commons {
     public static RelOptCluster cluster() {
         return CLUSTER;
     }
-
-    public static RelOptCluster createCluster() {
-        return BaseQueryContext.createCluster();
-    }
 }
