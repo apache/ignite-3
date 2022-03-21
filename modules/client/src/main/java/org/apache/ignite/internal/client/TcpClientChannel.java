bbab5269c133dd17a8381b6ac505e3b26be7113c
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-03-21 10:16:45 +0300

    Replace IdleStateHandler with a timer to adjust interval after the connection is established - comments

--- modules/client/src/main/java/org/apache/ignite/internal/client/TcpClientChannel.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/TcpClientChannel.java
@@ -108,6 +108,8 @@ class TcpClientChannel implements ClientChannel, ClientMessageHandler, ClientCon
 
         handshake(DEFAULT_VERSION);
 
+        // Netty has a built-in IdleStateHandler to detect idle connections (used on the server side).
+        // However, to adjust the heartbeat interval dynamically, we have to use a timer here.
         heartbeatTimer = initHeartbeat(cfg.clientConfiguration().heartbeatInterval());
     }
 
