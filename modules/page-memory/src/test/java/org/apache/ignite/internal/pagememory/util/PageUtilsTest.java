32b3139d2c1d146e27cc54647113fa8f594dd9f4
Kirill Tkalenko <tkalkirill@yandex.ru>
2022-03-30 15:39:41 +0300

    IGNITE-16280 after review #3.3

--- modules/page-memory/src/test/java/org/apache/ignite/internal/pagememory/util/PageUtilsTest.java
+++ modules/page-memory/src/test/java/org/apache/ignite/internal/pagememory/util/PageUtilsTest.java
@@ -20,134 +20,78 @@ package org.apache.ignite.internal.pagememory.util;
 import static org.apache.ignite.internal.pagememory.util.PageUtils.getBytes;
 import static org.apache.ignite.internal.pagememory.util.PageUtils.putByteBuffer;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Consumer;
 import java.util.function.LongConsumer;
 import org.apache.ignite.internal.util.GridUnsafe;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * For {@link PageUtils} testing.
  */
 public class PageUtilsTest {
-    @Test
-    void testPutByteBufferIndirect() {
+    @ParameterizedTest(name = "position = {0}")
+    @ValueSource(ints = {0, 128})
+    void testPutByteBufferIndirect(int position) {
         byte[] bytes = randomBytes(1024);
 
-        ByteBuffer buf = ByteBuffer.wrap(bytes);
+        ByteBuffer buf = ByteBuffer.wrap(bytes).position(position);
 
         assertFalse(buf.isDirect());
-
-        // Checking that the entire buffer will be written.
-
-        assertEquals(0, buf.position());
-
-        allocateNativeMemory(2048, addr -> {
-            putByteBuffer(addr, 256, buf);
-
-            assertArrayEquals(bytes, getBytes(addr, 256, bytes.length));
-        });
-
-        // Checking that the buffer will be written from a certain position.
-
-        int pos = 128;
-
-        buf.position(pos);
 
         allocateNativeMemory(2048, addr -> {
             putByteBuffer(addr, 256, buf);
 
             assertArrayEquals(
-                    Arrays.copyOfRange(bytes, pos, bytes.length),
-                    getBytes(addr, 256, bytes.length - pos));
+                    Arrays.copyOfRange(bytes, position, bytes.length),
+                    getBytes(addr, 256, bytes.length - position)
+            );
         });
     }
 
-    @Test
-    void testPutByteBufferIndirectSlice() {
+    @ParameterizedTest(name = "newPosition = {0}")
+    @ValueSource(ints = {0, 128})
+    void testPutByteBufferIndirectSlice(int newPosition) {
         byte[] bytes = randomBytes(1024);
 
         int pos = 256;
         int len = 512;
 
-        ByteBuffer buf = ByteBuffer.wrap(bytes).position(pos).limit(pos + len).slice();
+        ByteBuffer buf = ByteBuffer.wrap(bytes).position(pos).limit(pos + len).slice().position(newPosition);
 
         assertFalse(buf.isDirect());
 
-        // Checking that the entire buffer will be written.
-
-        assertEquals(0, buf.position());
-
-        // Checking that the entire buffer will be written.
-
-        assertEquals(0, buf.position());
-
         allocateNativeMemory(2048, addr -> {
             putByteBuffer(addr, 256, buf);
 
             assertArrayEquals(
-                    Arrays.copyOfRange(bytes, pos, pos + len),
-                    getBytes(addr, 256, len));
-        });
-
-        // Checking that the buffer will be written from a certain position.
-
-        int newPos = 128;
-
-        buf.position(newPos);
-
-        allocateNativeMemory(2048, addr -> {
-            putByteBuffer(addr, 256, buf);
-
-            assertArrayEquals(
-                    Arrays.copyOfRange(bytes, pos + newPos, pos + len),
-                    getBytes(addr, 256, len - newPos));
+                    Arrays.copyOfRange(bytes, pos + newPosition, pos + len),
+                    getBytes(addr, 256, len - newPosition)
+            );
         });
     }
 
-    @Test
-    void testPutByteBufferDirect() {
+    @ParameterizedTest(name = "position = {0}")
+    @ValueSource(ints = {0, 128})
+    void testPutByteBufferDirect(int position) {
         final byte[] bytes = randomBytes(1024);
 
-        // Checking that the entire buffer will be written.
+        ByteBuffer buf = ByteBuffer.allocateDirect(1024).put(bytes).position(position);
 
-        allocateDirectBuffer(1024, buf -> {
-            assertTrue(buf.isDirect());
+        assertTrue(buf.isDirect());
 
-            buf.put(bytes).rewind();
+        allocateNativeMemory(2048, addr -> {
+            putByteBuffer(addr, 256, buf);
 
-            assertEquals(0, buf.position());
-
-            allocateNativeMemory(2048, addr -> {
-                putByteBuffer(addr, 256, buf);
-
-                assertArrayEquals(bytes, getBytes(addr, 256, bytes.length));
-            });
-        });
-
-        // Checking that the buffer will be written from a certain position.
-
-        int pos = 128;
-
-        allocateDirectBuffer(1024, buf -> {
-            assertTrue(buf.isDirect());
-
-            buf.put(bytes).position(pos);
-
-            allocateNativeMemory(2048, addr -> {
-                putByteBuffer(addr, 256, buf);
-
-                byte[] exp = new byte[bytes.length - pos];
-                System.arraycopy(bytes, pos, exp, 0, exp.length);
-
-                assertArrayEquals(exp, getBytes(addr, 256, exp.length));
-            });
+            assertArrayEquals(
+                    Arrays.copyOfRange(bytes, position, bytes.length),
+                    getBytes(addr, 256, bytes.length - position)
+            );
         });
     }
 
@@ -166,16 +110,6 @@ public class PageUtilsTest {
             adderConsumer.accept(addr);
         } finally {
             GridUnsafe.freeMemory(addr);
-        }
-    }
-
-    private void allocateDirectBuffer(int size, Consumer<ByteBuffer> directBufConsumer) {
-        ByteBuffer buf = GridUnsafe.allocateBuffer(size);
-
-        try {
-            directBufConsumer.accept(buf);
-        } finally {
-            GridUnsafe.freeBuffer(buf);
         }
     }
 }
