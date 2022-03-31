d32888ddc61bb824437b0081c5fead9e70784622
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-03-31 10:43:01 +0300

    wip ItThinClientComputeTest

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/client/ItThinClientComputeTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/client/ItThinClientComputeTest.java
@@ -21,8 +21,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.ignite.compute.ComputeJob;
+import org.apache.ignite.compute.JobExecutionContext;
 import org.apache.ignite.network.ClusterNode;
 import org.junit.jupiter.api.Test;
 
@@ -45,5 +48,36 @@ public class ItThinClientComputeTest extends ItAbstractThinClientTest {
         assertEquals("ItThinClientComputeTest_null_3345", nodes.get(1).name());
         assertEquals(3345, nodes.get(1).address().port());
         assertTrue(nodes.get(1).id().length() > 10);
+    }
+
+    @Test
+    void testExecute() {
+        var nodes = new HashSet<>(client().clusterNodes());
+        String res = client().compute().execute(nodes, NodeNameJob.class).join();
+
+        assertEquals("ItThinClientComputeTest_null_334", res);
+    }
+
+    @Test
+    void testExecuteWithArgs() {
+        var nodes = new HashSet<>(client().clusterNodes());
+        String res = client().compute().execute(nodes, ConcatJob.class, 1, "2", 3.3).join();
+
+        assertEquals("1_2_3.3", res);
+    }
+
+    private static class NodeNameJob implements ComputeJob<String> {
+        @Override
+        public String execute(JobExecutionContext context, Object... args) {
+            return context.ignite().name();
+        }
+    }
+
+    private static class ConcatJob implements ComputeJob<String> {
+        @Override
+        public String execute(JobExecutionContext context, Object... args) {
+            // TODO: Concat args.
+            return null;
+        }
     }
 }
