5349bdd79cd33ebcbe190519801a565e28fd3b7f
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-04-11 15:06:22 +0300

    Implement node-aware compute call

--- modules/platforms/dotnet/Apache.Ignite/Internal/Compute/Compute.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Compute/Compute.cs
@@ -89,25 +89,45 @@ namespace Apache.Ignite.Internal.Compute
         {
             IgniteArgumentCheck.NotNull(node, nameof(node));
 
-            // TODO:
-            // 1. Locate matching active connection in ClientFailoverSocket
-            // 2. If there is one, write without node details, send request.
-            // 3. If failed, fallback to default socket with retry policy.
-            using var writer = new PooledArrayBufferWriter();
-            Write();
+            // Try direct connection to the specified node.
+            if (_socket.GetEndpoint(node.Name) is { } endpoint)
+            {
+                using var writerWithoutNode = new PooledArrayBufferWriter();
+                Write(writerWithoutNode, writeNode: false);
 
-            using var resBuf = await _socket.DoOutInOpAsync(ClientOp.ComputeExecute, writer).ConfigureAwait(false);
+                using var res1 = await _socket.TryDoOutInOpAsync(endpoint, ClientOp.ComputeExecute, writerWithoutNode)
+                    .ConfigureAwait(false);
 
-            return Read();
+                // Result is null when there was a connection issue, but retry policy allows another try.
+                if (res1 != null)
+                {
+                    return Read(res1.Value);
+                }
+            }
 
-            void Write()
+            // When direct connection is not available, use default connection and pass target node info to the server.
+            using var writerWithNode = new PooledArrayBufferWriter();
+            Write(writerWithNode, writeNode: true);
+
+            using var res2 = await _socket.DoOutInOpAsync(ClientOp.ComputeExecute, writerWithNode).ConfigureAwait(false);
+
+            return Read(res2);
+
+            void Write(PooledArrayBufferWriter writer, bool writeNode)
             {
                 var w = writer.GetMessageWriter();
 
-                w.Write(node.Id);
-                w.Write(node.Name);
-                w.Write(node.Address.Address.ToString());
-                w.Write(node.Address.Port);
+                if (writeNode)
+                {
+                    w.Write(node.Id);
+                    w.Write(node.Name);
+                    w.Write(node.Address.Address.ToString());
+                    w.Write(node.Address.Port);
+                }
+                else
+                {
+                    w.WriteNil();
+                }
 
                 w.Write(jobClassName);
                 w.WriteObjectArrayWithTypes(args);
@@ -115,9 +135,9 @@ namespace Apache.Ignite.Internal.Compute
                 w.Flush();
             }
 
-            T Read()
+            static T Read(in PooledBuffer buf)
             {
-                var reader = resBuf.GetReader();
+                var reader = buf.GetReader();
 
                 return (T)reader.ReadObjectWithType()!;
             }
