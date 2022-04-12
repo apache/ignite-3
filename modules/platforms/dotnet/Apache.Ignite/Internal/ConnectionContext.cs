78bbd829afc25d7ab163cb26e0e7618496af5d61
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-04-12 10:36:39 +0300

    Fix build

--- modules/platforms/dotnet/Apache.Ignite/Internal/ConnectionContext.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/ConnectionContext.cs
@@ -15,6 +15,9 @@
  * limitations under the License.
  */
 
+// XMLDoc check fails on older SDKs: https://github.com/dotnet/roslyn/issues/44571.
+#pragma warning disable CS1572
+#pragma warning disable CS1573
 namespace Apache.Ignite.Internal
 {
     using System;
