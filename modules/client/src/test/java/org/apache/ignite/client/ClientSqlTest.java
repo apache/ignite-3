b6eef923f7158e6c1e2b5c4129d2cfa3c9057478
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-05-31 09:48:51 +0300

    testStatementPropertiesOverrideSessionProperties done

--- modules/client/src/test/java/org/apache/ignite/client/ClientSqlTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/ClientSqlTest.java
@@ -27,6 +27,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.apache.ignite.sql.Session;
 import org.apache.ignite.sql.SqlRow;
+import org.apache.ignite.sql.Statement;
 import org.apache.ignite.sql.async.AsyncResultSet;
 import org.junit.jupiter.api.Test;
 
@@ -71,6 +72,33 @@ public class ClientSqlTest extends AbstractClientTableTest {
 
     @Test
     public void testStatementPropertiesOverrideSessionProperties() {
+        Session session = client.sql().sessionBuilder()
+                .defaultSchema("SCHEMA1")
+                .defaultTimeout(123, TimeUnit.SECONDS)
+                .defaultPageSize(234)
+                .property("prop1", 1)
+                .property("prop2", 2)
+                .build();
 
+        Statement statement = client.sql().statementBuilder()
+                .query("SELECT PROPS")
+                .defaultSchema("SCHEMA2")
+                .queryTimeout(124, TimeUnit.SECONDS)
+                .pageSize(235)
+                .property("prop2", 22)
+                .property("prop3", 3)
+                .build();
+
+        AsyncResultSet resultSet = session.executeAsync(null, statement).join();
+
+        Map<String, Object> props = StreamSupport.stream(resultSet.currentPage().spliterator(), false)
+                .collect(Collectors.toMap(x -> x.stringValue(0), x -> x.value(1)));
+
+        assertEquals("SCHEMA2", props.get("schema"));
+        assertEquals(124000L, props.get("timeout"));
+        assertEquals(235, props.get("pageSize"));
+        assertEquals(1, props.get("prop1"));
+        assertEquals(22, props.get("prop2"));
+        assertEquals(3, props.get("prop3"));
     }
 }
