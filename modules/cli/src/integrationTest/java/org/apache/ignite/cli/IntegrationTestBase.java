0824097aecc54f1e6924ec7711061a579d63058e
Mikhail Pochatkin <mpochatkin@unison.team>
2022-06-03 12:46:16 +0300

    Update copyright

--- modules/cli/src/integrationTest/java/org/apache/ignite/cli/IntegrationTestBase.java
+++ modules/cli/src/integrationTest/java/org/apache/ignite/cli/IntegrationTestBase.java
@@ -1,5 +1,3 @@
-package org.apache.ignite.cli;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.ignite.cli;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.ignite.cli;
 
 import static java.util.stream.Collectors.toList;
 import static org.apache.ignite.internal.testframework.IgniteTestUtils.await;
--- modules/cli/src/integrationTest/java/org/apache/ignite/cli/commands/CliCommandTestIntegrationBase.java
+++ modules/cli/src/integrationTest/java/org/apache/ignite/cli/commands/CliCommandTestIntegrationBase.java
@@ -1,5 +1,3 @@
-package org.apache.ignite.cli.commands;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.ignite.cli.commands;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.ignite.cli.commands;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
