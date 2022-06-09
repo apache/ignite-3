30a2d9fffe91b60fd8a73f82c9dbfa6c04c33989
Alexander Polovtcev <alex.polovtcev@gmail.com>
2022-06-09 15:28:27 +0300

    IGNITE-17144 Fix parsing of alpha versions of Ignite (#873)

--- modules/core/src/main/java/org/apache/ignite/internal/properties/IgniteProductVersion.java
+++ modules/core/src/main/java/org/apache/ignite/internal/properties/IgniteProductVersion.java
@@ -22,13 +22,14 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.ignite.internal.util.StringUtils;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Class representing an Ignite version.
  */
 public class IgniteProductVersion implements Serializable {
     private static final Pattern VERSION_PATTERN =
-            Pattern.compile("(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<maintenance>\\d+)(?<snapshot>-SNAPSHOT)?");
+            Pattern.compile("(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<maintenance>\\d+)((?<snapshot>-SNAPSHOT)|-(?<alpha>alpha\\d+))?");
 
     /**
      * Version of the current node.
@@ -47,11 +48,16 @@ public class IgniteProductVersion implements Serializable {
     /** Flag indicating if this is a snapshot release. */
     private final boolean isSnapshot;
 
-    private IgniteProductVersion(byte major, byte minor, byte maintenance, boolean isSnapshot) {
+    /** Alpha version part or an empty string if this is not an alpha release. */
+    // TODO: IGNITE-17146 Fix and add support for beta and other releases
+    private final String alphaVersion;
+
+    private IgniteProductVersion(byte major, byte minor, byte maintenance, boolean isSnapshot, @Nullable String alphaVersion) {
         this.major = major;
         this.minor = minor;
         this.maintenance = maintenance;
         this.isSnapshot = isSnapshot;
+        this.alphaVersion = alphaVersion == null ? "" : alphaVersion;
     }
 
     /**
@@ -76,24 +82,44 @@ public class IgniteProductVersion implements Serializable {
                 Byte.parseByte(matcher.group("major")),
                 Byte.parseByte(matcher.group("minor")),
                 Byte.parseByte(matcher.group("maintenance")),
-                matcher.group("snapshot") != null
+                matcher.group("snapshot") != null,
+                matcher.group("alpha")
         );
     }
 
+    /**
+     * Returns the major version number.
+     */
     public byte major() {
         return major;
     }
 
+    /**
+     * Returns the minor version number.
+     */
     public byte minor() {
         return minor;
     }
 
+    /**
+     * Returns the maintenance version number.
+     */
     public byte maintenance() {
         return maintenance;
     }
 
+    /**
+     * Returns {@code true} if this is a snapshot release, {@code false} otherwise.
+     */
     public boolean snapshot() {
         return isSnapshot;
+    }
+
+    /**
+     * Returns the alpha version of this release or an empty string if this is not an alpha release.
+     */
+    public String alphaVersion() {
+        return alphaVersion;
     }
 
     @Override
@@ -105,18 +131,19 @@ public class IgniteProductVersion implements Serializable {
             return false;
         }
         IgniteProductVersion that = (IgniteProductVersion) o;
-        return major == that.major && minor == that.minor && maintenance == that.maintenance && isSnapshot == that.isSnapshot;
+        return major == that.major && minor == that.minor && maintenance == that.maintenance && isSnapshot == that.isSnapshot
+                && alphaVersion.equals(that.alphaVersion);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(major, minor, maintenance, isSnapshot);
+        return Objects.hash(major, minor, maintenance, isSnapshot, alphaVersion);
     }
 
     @Override
     public String toString() {
         String version = String.join(".", String.valueOf(major), String.valueOf(minor), String.valueOf(maintenance));
 
-        return version + (isSnapshot ? "-SNAPSHOT" : "");
+        return version + (alphaVersion.isEmpty() ? "" : "-" + alphaVersion) + (isSnapshot ? "-SNAPSHOT" : "");
     }
 }
