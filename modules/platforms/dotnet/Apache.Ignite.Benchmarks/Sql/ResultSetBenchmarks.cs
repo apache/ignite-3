b13a3168642842c37f6433a78ed9ffe8f13275ce
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-07-10 11:22:58 +0300

    Enable server GC for benchmarks

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/Sql/ResultSetBenchmarks.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Sql/ResultSetBenchmarks.cs
@@ -21,21 +21,24 @@ namespace Apache.Ignite.Benchmarks.Sql
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Jobs;
     using Ignite.Table;
     using Tests;
 
     /// <summary>
     /// Measures SQL result set enumeration with two pages of data (two network requests per enumeration).
     /// <para />
-    /// Results on Intel Core i7-9700K, GC = Concurrent Workstation, Ubuntu 20.04:
-    /// |          Method |       Runtime |     Mean |    Error |   StdDev | Ratio |   Gen 0 |   Gen 1 | Allocated |
-    /// |---------------- |-------------- |---------:|---------:|---------:|------:|--------:|--------:|----------:|
-    /// |     ToListAsync |      .NET 6.0 | 259.1 us |  5.16 us |  4.03 us |  0.64 | 64.4531 | 23.9258 |    391 KB |
-    /// | AsyncEnumerable |      .NET 6.0 | 361.2 us |  7.22 us |  7.73 us |  0.89 | 64.4531 | 25.3906 |    392 KB |
-    /// |     ToListAsync | .NET Core 3.1 | 405.7 us |  8.07 us | 10.50 us |  1.00 | 62.5000 | 23.4375 |    383 KB |
-    /// | AsyncEnumerable | .NET Core 3.1 | 543.1 us | 10.82 us | 15.17 us |  1.34 | 62.5000 | 22.4609 |    384 KB |.
+    /// Results on Intel Core i7-9700K, GC = Concurrent Server, Ubuntu 20.04:
+    /// |          Method |       Runtime |     Mean |   Error |   StdDev | Ratio |  Gen 0 |  Gen 1 | Allocated |
+    /// |---------------- |-------------- |---------:|--------:|---------:|------:|-------:|-------:|----------:|
+    /// |     ToListAsync |      .NET 6.0 | 218.4 us | 2.14 us |  2.01 us |  0.59 | 8.3008 | 1.7090 |    391 KB |
+    /// | AsyncEnumerable |      .NET 6.0 | 325.8 us | 6.14 us |  6.03 us |  0.88 | 8.7891 | 0.4883 |    392 KB |
+    /// |     ToListAsync | .NET Core 3.1 | 368.6 us | 7.14 us |  7.01 us |  1.00 | 7.3242 | 1.4648 |    383 KB |
+    /// | AsyncEnumerable | .NET Core 3.1 | 497.9 us | 9.59 us | 12.12 us |  1.35 | 7.8125 | 2.9297 |    384 KB |.
     /// </summary>
     [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: true)]
+    /* [SimpleJob(RuntimeMoniker.Net60)] */
     public class ResultSetBenchmarks
     {
         private FakeServer? _server;
