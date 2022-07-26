258ed0da97266575886391198d978b2f4327105a
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-07-26 16:04:20 +0300

    IgniteException.wrap: support IgniteCheckedException

--- modules/api/src/main/java/org/apache/ignite/lang/IgniteException.java
+++ modules/api/src/main/java/org/apache/ignite/lang/IgniteException.java
@@ -278,6 +278,19 @@ public class IgniteException extends RuntimeException {
             }
         }
 
+        if (e instanceof IgniteCheckedException) {
+            IgniteCheckedException iex = (IgniteCheckedException) e;
+
+            try {
+                Constructor<?> ctor = e.getClass().getDeclaredConstructor(UUID.class, int.class, String.class, Throwable.class);
+
+                return (IgniteException) ctor.newInstance(iex.traceId(), iex.code(), e.getMessage(), e);
+            } catch (Exception ex) {
+                throw new RuntimeException("IgniteCheckedException-derived class does not have required constructor: " +
+                        e.getClass().getName());
+            }
+        }
+
         return new IgniteException(UNKNOWN_ERR, e.getMessage(), e);
     }
 }
