ed5b54a9acafbcebb78c4187b3be2e8932ee8114
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-08-30 17:54:22 +0300

    BinaryTupleCommon ported

--- modules/platforms/dotnet/Apache.Ignite/Internal/Proto/BinaryTuple/BinaryTupleCommon.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Proto/BinaryTuple/BinaryTupleCommon.cs
@@ -17,8 +17,89 @@
 
 namespace Apache.Ignite.Internal.Proto.BinaryTuple
 {
+    /// <summary>
+    /// Common binary tuple constants and utils.
+    /// </summary>
     internal class BinaryTupleCommon
     {
+        /// <summary>
+        /// Size of a tuple header, in bytes.
+        /// </summary>
+        public const int HeaderSize = 1;
 
+        /// <summary>
+        /// Mask for size of entries in variable-length offset table.
+        /// </summary>
+        public const int VarsizeMask = 0b011;
+
+        /// <summary>
+        /// Flag that indicates null map presence.
+        /// </summary>
+        public const int NullmapFlag = 0b100;
+
+        /// <summary>
+        /// Calculates flags for a given size of variable-length area.
+        /// </summary>
+        /// <param name="size">Variable-length area size.</param>
+        /// <returns>Flags value.</returns>
+        public static byte ValueSizeToFlags(long size)
+        {
+            if (size <= 0xff)
+            {
+                return 0b00;
+            }
+
+            if (size <= 0xffff)
+            {
+                return 0b01;
+            }
+
+            if (size <= int.MaxValue)
+            {
+                return 0b10;
+            }
+
+            throw new IgniteClientException("Too big binary tuple size");
+        }
+
+        /// <summary>
+        /// Calculates the size of entry in variable-length offset table for given flags.
+        /// </summary>
+        /// <param name="flags">Flags.</param>
+        /// <returns>Size of entry in variable-length offset table.</returns>
+        public static int FlagsToEntrySize(byte flags)
+        {
+            return 1 << (flags & VarsizeMask);
+        }
+
+        /// <summary>
+        /// Calculates the null map size.
+        /// </summary>
+        /// <param name="numElements">Number of tuple elements.</param>
+        /// <returns>Null map size in bytes.</returns>
+        public static int NullMapSize(int numElements)
+        {
+            return (numElements + 7) / 8;
+        }
+
+        /// <summary>
+        /// Returns offset of the byte that contains null-bit of a given tuple element.
+        /// </summary>
+        /// <param name="index">Tuple element index.</param>
+        /// <returns>Offset of the required byte relative to the tuple start.</returns>
+        public static int NullOffset(int index)
+        {
+            return HeaderSize + index / 8;
+        }
+
+        /// <summary>
+        /// Returns a null-bit mask corresponding to a given tuple element.
+        /// </summary>
+        /// <param name="index">Tuple element index.</param>
+        /// <returns>Mask to extract the required null-bit.</returns>
+        public static byte NullMask(int index)
+        {
+            return (byte)(1 << (index % 8));
+        }
     }
 }
