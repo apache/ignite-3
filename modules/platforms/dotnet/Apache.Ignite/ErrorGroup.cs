afb7ccfd0c49c2e3194ec102783c889d864b1dcb
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-09-08 13:50:03 +0300

    IGNITE-17314 .NET: Thin 3.0: Unified exception handling

---
+++ modules/platforms/dotnet/Apache.Ignite/ErrorGroup.cs
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite
+{
+    /// <summary>
+    /// Represents a concept of error group. Error group defines a collection of errors that belong to a single semantic component.
+    /// Each group can be identified by a name and an integer number that both must be unique across all error groups.
+    /// </summary>
+    public class ErrorGroup
+    {
+        /// <summary>
+        /// Ignite error prefix.
+        /// </summary>
+        public const string ErrPrefix = "IGN-";
+
+        /// <summary>
+        /// Returns error code extracted from the given full error code.
+        /// </summary>
+        /// <param name="code">Full error code.</param>
+        /// <returns>Error code.</returns>
+        public static int ExtractErrorCode(int code) => code & 0xFFFF;
+    }
+}
