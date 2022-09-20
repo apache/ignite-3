f92788a9dbc9cd09cc7e501da2d94dae5840cd70
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-09-20 18:05:21 +0300

    wip PartitionAwarenessTest

---
+++ modules/client/src/test/java/org/apache/ignite/client/PartitionAwarenessTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.client;
+
+import io.netty.util.ResourceLeakDetector;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.client.fakes.FakeIgnite;
+import org.apache.ignite.table.RecordView;
+import org.apache.ignite.table.Tuple;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests partition awareness.
+ */
+public class PartitionAwarenessTest extends AbstractClientTest {
+    protected static TestServer testServer2;
+
+    protected static Ignite server2;
+
+    protected static IgniteClient client2;
+
+    protected static int serverPort2;
+
+    /**
+     * Before all.
+     */
+    @BeforeAll
+    public static void beforeAll() {
+        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+
+        server2 = new FakeIgnite();
+        testServer2 = startServer(10800, 10, 0, server2);
+        serverPort2 = testServer2.port();
+
+        client2 = startClient("127.0.0.1:" + serverPort, "127.0.0.1:" + serverPort2);
+    }
+
+    /**
+     * After all.
+     */
+    @AfterAll
+    public static void afterAll() throws Exception {
+        testServer2.close();
+    }
+
+    @Test
+    public void testGetRoutesRequestToPrimaryNode() {
+        RecordView<Tuple> recordView = client2.tables().table(DEFAULT_TABLE).recordView();
+
+        recordView.get(null, Tuple.create().set("key", 1));
+        recordView.get(null, Tuple.create().set("key", 2));
+    }
+}
