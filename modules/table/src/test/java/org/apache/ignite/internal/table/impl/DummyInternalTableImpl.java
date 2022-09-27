7178e535723c3cfc26a0c92bb3c08b08b51bdec8
Vladislav Pyatkov <vldpyatkov@gmail.com>
2022-09-27 22:11:34 +0300

    Code style.

--- modules/table/src/test/java/org/apache/ignite/internal/table/impl/DummyInternalTableImpl.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/impl/DummyInternalTableImpl.java
@@ -207,11 +207,11 @@ public class DummyInternalTableImpl extends InternalTableImpl {
         return super.get(keyRow, tx);
     }
 
-//    /** {@inheritDoc} */
-//    @Override
-//    public Flow.Publisher<BinaryRow> scan(int p, InternalTransaction tx) {
-//        throw new IgniteInternalException(new OperationNotSupportedException());
-//    }
+    ///** {@inheritDoc} */
+    //@Override
+    //public Flow.Publisher<BinaryRow> scan(int p, InternalTransaction tx) {
+    //    throw new IgniteInternalException(new OperationNotSupportedException());
+    //}
 
     /** {@inheritDoc} */
     @Override
