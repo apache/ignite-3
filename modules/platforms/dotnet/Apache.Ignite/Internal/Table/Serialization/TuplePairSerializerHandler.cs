48e08a2fa314d357189a6219f868897a1d33a947
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-10-13 14:32:57 +0300

    TuplePairSerializerHandler done

--- modules/platforms/dotnet/Apache.Ignite/Internal/Table/Serialization/TuplePairSerializerHandler.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Table/Serialization/TuplePairSerializerHandler.cs
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace Apache.Ignite.Internal.Table.Serialization;
 
 using Ignite.Table;
@@ -26,7 +43,6 @@ internal class TuplePairSerializerHandler : IRecordSerializerHandler<KvPair<IIgn
     /// <inheritdoc/>
     public KvPair<IIgniteTuple, IIgniteTuple> Read(ref MessagePackReader reader, Schema schema, bool keyOnly = false)
     {
-        // TODO: Deal with value nullability properly. Maybe use Tuple.Empty? Or keep it as is for perf?
         var columns = schema.Columns;
         var count = keyOnly ? schema.KeyColumnCount : columns.Count;
         var keyTuple = new IgniteTuple(count);
@@ -74,11 +90,12 @@ internal class TuplePairSerializerHandler : IRecordSerializerHandler<KvPair<IIgn
             for (var index = 0; index < count; index++)
             {
                 var col = columns[index];
-                var colIdx = record.GetOrdinal(col.Name);
+                var rec = index < schema.KeyColumnCount ? record.Key : record.Val;
+                var colIdx = rec.GetOrdinal(col.Name);
 
                 if (colIdx >= 0)
                 {
-                    tupleBuilder.AppendObject(record[colIdx], col.Type, col.Scale);
+                    tupleBuilder.AppendObject(rec[colIdx], col.Type, col.Scale);
                 }
                 else
                 {
