34bb53129a1562465b021d171ae50b0ef811ad20
amashenkov <andrey.mashenkov@gmail.com>
2022-10-14 14:38:27 +0300

    WIP. Fix incorrect dependencies.

--- modules/storage-api/src/testFixtures/java/org/apache/ignite/internal/storage/impl/TestStorageEngineDistributedConfigurationModule.java
+++ modules/storage-api/src/testFixtures/java/org/apache/ignite/internal/storage/impl/TestStorageEngineDistributedConfigurationModule.java
@@ -20,6 +20,8 @@ package org.apache.ignite.internal.storage.impl;
 import java.util.Collection;
 import java.util.List;
 import org.apache.ignite.configuration.annotation.ConfigurationType;
+import org.apache.ignite.configuration.schemas.table.HashIndexConfigurationSchema;
+import org.apache.ignite.configuration.schemas.table.SortedIndexConfigurationSchema;
 import org.apache.ignite.internal.configuration.ConfigurationModule;
 import org.apache.ignite.internal.schema.configuration.index.HashIndexConfigurationSchema;
 import org.apache.ignite.internal.schema.configuration.index.SortedIndexConfigurationSchema;
@@ -38,6 +40,10 @@ public class TestStorageEngineDistributedConfigurationModule implements Configur
     /** {@inheritDoc} */
     @Override
     public Collection<Class<?>> polymorphicSchemaExtensions() {
-        return List.of(TestDataStorageConfigurationSchema.class);
+        return List.of(
+                TestDataStorageConfigurationSchema.class,
+                HashIndexConfigurationSchema.class,
+                SortedIndexConfigurationSchema.class
+        );
     }
 }
