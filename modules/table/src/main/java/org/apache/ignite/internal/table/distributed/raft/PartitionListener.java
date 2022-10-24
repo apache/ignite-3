d079c6f1153ee7e261d0d8171b12c24ddfe4a82a
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2022-10-24 14:13:56 +0400

    IGNITE-17893 / add a TODO

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/raft/PartitionListener.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/raft/PartitionListener.java
@@ -353,6 +353,7 @@ public class PartitionListener implements RaftGroupListener {
     /** {@inheritDoc} */
     @Override
     public void onShutdown() {
+        // TODO: IGNITE-17958 - probably, we should not close the storage here as PartitionListener did not create the storage.
         try {
             storage.close();
         } catch (Exception e) {
