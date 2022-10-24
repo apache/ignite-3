14b5eb126644f54f0d5b9aa708d2ef1364f6d529
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-10-24 14:54:20 +0300

    Require ConfigureAwait - fix code

--- modules/platforms/dotnet/Apache.Ignite/IgniteClient.cs
+++ modules/platforms/dotnet/Apache.Ignite/IgniteClient.cs
@@ -35,7 +35,7 @@ namespace Apache.Ignite
         {
             IgniteArgumentCheck.NotNull(configuration, nameof(configuration));
 
-            var socket = await ClientFailoverSocket.ConnectAsync(configuration);
+            var socket = await ClientFailoverSocket.ConnectAsync(configuration).ConfigureAwait(false);
 
             return new IgniteClientInternal(socket);
         }
