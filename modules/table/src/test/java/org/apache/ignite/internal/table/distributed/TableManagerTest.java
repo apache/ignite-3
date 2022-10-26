3a9963e7a6ae68a76ad58e1901239f102248ec23
denis-chudov <moonglloom@gmail.com>
2022-10-26 15:40:22 +0300

    fixed TableManagerTest

--- modules/table/src/test/java/org/apache/ignite/internal/table/distributed/TableManagerTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/distributed/TableManagerTest.java
@@ -72,6 +72,7 @@ import org.apache.ignite.internal.configuration.notifications.ConfigurationStora
 import org.apache.ignite.internal.configuration.testframework.ConfigurationExtension;
 import org.apache.ignite.internal.configuration.testframework.InjectConfiguration;
 import org.apache.ignite.internal.configuration.testframework.InjectRevisionListenerHolder;
+import org.apache.ignite.internal.hlc.HybridClockImpl;
 import org.apache.ignite.internal.logger.IgniteLogger;
 import org.apache.ignite.internal.logger.Loggers;
 import org.apache.ignite.internal.metastorage.MetaStorageManager;
@@ -769,7 +770,7 @@ public class TableManagerTest extends IgniteAbstractTest {
                 msm,
                 sm = new SchemaManager(revisionUpdater, tblsCfg, msm),
                 budgetView -> new LocalLogStorageFactory(),
-                null,
+                new HybridClockImpl(),
                 new OutgoingSnapshotsManager(messagingService)
         );
 
