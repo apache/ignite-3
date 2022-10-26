044a5f22a6c122a2c5c96ed117f375e3301971b8
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-10-26 08:58:08 +0300

    wip ColocationHashTests

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Proto/ColocationHashTests.cs
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Proto;
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+/// <summary>
+/// Tests that colocation hash calculation is consistent with server logic.
+/// </summary>
+public class ColocationHashTests : IgniteTestsBase
+{
+    private const string ColocationHashJob = "org.apache.ignite.internal.runner.app.PlatformTestNodeRunner$ColocationHashJob";
+
+    [Test]
+    public async Task TestColocationHashIsSameOnServerAndClient()
+    {
+        var nodes = await Client.GetClusterNodesAsync();
+        var serverHash = await Client.Compute.ExecuteAsync<int>(nodes, ColocationHashJob);
+
+        Assert.AreEqual(123, serverHash);
+    }
+}
