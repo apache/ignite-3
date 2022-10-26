b84c24a1bd8509d6c2c8e5496a34f11fba14e42e
Semyon Danilov <samvimes@yandex.ru>
2022-10-26 19:38:17 +0400

    IGNITE-17968 Review fixes

--- modules/storage-api/src/testFixtures/java/org/apache/ignite/internal/storage/AbstractMvPartitionStorageTest.java
+++ modules/storage-api/src/testFixtures/java/org/apache/ignite/internal/storage/AbstractMvPartitionStorageTest.java
@@ -1290,23 +1290,13 @@ public abstract class AbstractMvPartitionStorageTest extends BaseMvStoragesTest 
     @ParameterizedTest
     @EnumSource(ScanTimestampProvider.class)
     public void scanDoesNotSeeTombstonesWhenTombstoneIsCommitted(ScanTimestampProvider tsProvider) throws Exception {
-        testScanDoesNotSeeTombstones(tsProvider, true);
-    }
-
-    private void testScanDoesNotSeeTombstones(ScanTimestampProvider scantsProvider, boolean commitRemoval) throws Exception {
         RowId rowId = insert(binaryRow, txId);
         commitWrite(rowId, clock.now());
 
         addWrite(rowId, null, newTransactionId());
-        if (commitRemoval) {
-            commitWrite(rowId, clock.now());
-        }
+        commitWrite(rowId, clock.now());
 
-        assertScanSeesNothing(scantsProvider);
-    }
-
-    private void assertScanSeesNothing(ScanTimestampProvider scanTsProvider) throws Exception {
-        try (PartitionTimestampCursor cursor = scan(scanTsProvider.scanTimestamp(clock))) {
+        try (PartitionTimestampCursor cursor = scan(tsProvider.scanTimestamp(clock))) {
             assertFalse(cursor.hasNext());
         }
     }
