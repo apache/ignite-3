8fda1b6a1c523ef24974c27843005e7ca0568c70
Semyon Danilov <samvimes@yandex.ru>
2022-11-01 20:12:30 +0300

    IGNITE-18007 MacOS support (#1272)

--- modules/platforms/cpp/ignite/network/network.cpp
+++ modules/platforms/cpp/ignite/network/network.cpp
@@ -23,15 +23,17 @@
 
 #ifdef _WIN32
 # include "detail/win/win_async_client_pool.h"
-#else // Other. Assume Linux
+#else
 # include "detail/linux/linux_async_client_pool.h"
 #endif
 
 namespace ignite::network {
 
 std::shared_ptr<async_client_pool> make_async_client_pool(data_filters filters) {
-    auto pool =
-        std::make_shared<IGNITE_SWITCH_WIN_OTHER(detail::win_async_client_pool, detail::linux_async_client_pool)>();
+    auto pool = std::make_shared<IGNITE_SWITCH_WIN_OTHER(
+        detail::win_async_client_pool,
+        detail::linux_async_client_pool
+    )>();
 
     return std::make_shared<async_client_pool_adapter>(std::move(filters), std::move(pool));
 }
