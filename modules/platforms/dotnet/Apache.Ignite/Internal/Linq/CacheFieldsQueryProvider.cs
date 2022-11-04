85a5bb212fd05a0882b0f95bfa8ad4cd0875fcdc
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-04 18:05:11 +0200

    Import linq implementation from Ignite 2.x - fixing compilation

--- modules/platforms/dotnet/Apache.Ignite/Internal/Linq/CacheFieldsQueryProvider.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Linq/CacheFieldsQueryProvider.cs
@@ -48,35 +48,26 @@ internal class CacheFieldsQueryProvider : IQueryProvider
     private readonly IIgnite _ignite;
 
     /** */
-    private readonly CacheConfiguration _cacheConfiguration;
-
-    /** */
     private readonly string _tableName;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CacheFieldsQueryProvider"/> class.
     /// </summary>
-    public CacheFieldsQueryProvider(IQueryParser queryParser, CacheFieldsQueryExecutor executor, IIgnite ignite,
-        CacheConfiguration cacheConfiguration, string tableName, Type cacheValueType)
+    public CacheFieldsQueryProvider(
+        IQueryParser queryParser,
+        CacheFieldsQueryExecutor executor,
+        IIgnite ignite,
+        string tableName,
+        Type cacheValueType)
     {
         Debug.Assert(queryParser != null);
         Debug.Assert(executor != null);
-        Debug.Assert(cacheConfiguration != null);
         Debug.Assert(cacheValueType != null);
 
         _parser = queryParser;
         _executor = executor;
         _ignite = ignite;
-        _cacheConfiguration = cacheConfiguration;
-
-        if (tableName != null)
-        {
-            _tableName = tableName;
-
-            ValidateTableName();
-        }
-        else
-            _tableName = InferTableName(cacheValueType);
+        _tableName = tableName;
     }
 
     /// <summary>
@@ -86,14 +77,6 @@ internal class CacheFieldsQueryProvider : IQueryProvider
     public IIgnite Ignite
     {
         get { return _ignite; }
-    }
-
-    /// <summary>
-    /// Gets the name of the cache.
-    /// </summary>
-    public CacheConfiguration CacheConfiguration
-    {
-        get { return _cacheConfiguration; }
     }
 
     /// <summary>
@@ -121,16 +104,15 @@ internal class CacheFieldsQueryProvider : IQueryProvider
     }
 
     /** <inheritdoc /> */
-    [SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0")]
     public IQueryable CreateQuery(Expression expression)
     {
         Debug.Assert(expression != null);
 
         var elementType = GetItemTypeOfClosedGenericIEnumerable(expression.Type, "expression");
 
-        // Slow, but this method is never called during normal LINQ usage with generics
-        return (IQueryable) GenericCreateQueryMethod.MakeGenericMethod(elementType)
-            .Invoke(this, new object[] {expression});
+        // Slow, but this method is never called during normal LINQ usage with generics.
+        // TODO: When is this called?
+        return (IQueryable) GenericCreateQueryMethod.MakeGenericMethod(elementType).Invoke(this, new object[] {expression})!;
     }
 
     /** <inheritdoc /> */
@@ -152,95 +134,6 @@ internal class CacheFieldsQueryProvider : IQueryProvider
     }
 
     /// <summary>
-    /// Executes the specified expression.
-    /// </summary>
-    private IStreamedData Execute(Expression expression)
-    {
-        var model = GenerateQueryModel(expression);
-
-        return model.Execute(_executor);
-    }
-
-    /// <summary>
-    /// Validates the name of the table.
-    /// </summary>
-    private void ValidateTableName()
-    {
-        var validTableNames = GetValidTableNames().Select(x => EscapeTableName(x)).ToArray();
-
-        if (!validTableNames.Contains(_tableName, StringComparer.OrdinalIgnoreCase))
-        {
-            throw new CacheException(string.Format("Invalid table name specified for CacheQueryable: '{0}'; " +
-                                                   "configured table names are: {1}",
-                _tableName,
-                validTableNames.Aggregate((x, y) => x + ", " + y)));
-        }
-    }
-
-    /// <summary>
-    /// Gets the valid table names for current cache.
-    /// </summary>
-    private string[] GetValidTableNames()
-    {
-        // Split on '.' to throw away Java type namespace
-        var validTableNames = _cacheConfiguration.QueryEntities == null
-            ? null
-            : _cacheConfiguration.QueryEntities.Select(GetTableName).ToArray();
-
-        if (validTableNames == null || !validTableNames.Any())
-            throw new CacheException(string.Format("Queries are not configured for cache '{0}'",
-                _cacheConfiguration.Name ?? "null"));
-
-        return validTableNames;
-    }
-
-    /// <summary>
-    /// Gets the name of the SQL table.
-    /// </summary>
-    private static string GetTableName(QueryEntity e)
-    {
-        return e.TableName ?? e.ValueTypeName;
-    }
-
-    /// <summary>
-    /// Infers the name of the table from cache configuration.
-    /// </summary>
-    /// <param name="cacheValueType"></param>
-    private string InferTableName(Type cacheValueType)
-    {
-        var validTableNames = GetValidTableNames();
-
-        if (validTableNames.Length == 1)
-        {
-            return EscapeTableName(validTableNames[0]);
-        }
-
-        // Try with full type name (this works when TableName is not set).
-        var valueTypeName = cacheValueType.FullName;
-
-        if (valueTypeName != null)
-        {
-            if (validTableNames.Contains(valueTypeName, StringComparer.OrdinalIgnoreCase))
-            {
-                return EscapeTableName(valueTypeName);
-            }
-
-            // Remove namespace and nested class qualification and try again.
-            valueTypeName = EscapeTableName(valueTypeName);
-
-            if (validTableNames.Contains(valueTypeName, StringComparer.OrdinalIgnoreCase))
-            {
-                return valueTypeName;
-            }
-        }
-
-        throw new CacheException(string.Format("Table name cannot be inferred for cache '{0}', " +
-                                               "please use AsCacheQueryable overload with tableName parameter. " +
-                                               "Valid table names: {1}", _cacheConfiguration.Name ?? "null",
-            validTableNames.Aggregate((x, y) => x + ", " + y)));
-    }
-
-    /// <summary>
     /// Escapes the name of the table: strips namespace and nested class qualifiers.
     /// </summary>
     private static string EscapeTableName(string valueTypeName)
@@ -255,16 +148,23 @@ internal class CacheFieldsQueryProvider : IQueryProvider
     /// </summary>
     private static Type GetItemTypeOfClosedGenericIEnumerable(Type enumerableType, string argumentName)
     {
-        Type itemType;
-
-        if (!ItemTypeReflectionUtility.TryGetItemTypeOfClosedGenericIEnumerable(enumerableType, out itemType))
+        if (!ItemTypeReflectionUtility.TryGetItemTypeOfClosedGenericIEnumerable(enumerableType, out var itemType))
         {
-            var message = string.Format("Expected a closed generic type implementing IEnumerable<T>, " +
-                                        "but found '{0}'.", enumerableType);
+            var message = "Expected a closed generic type implementing IEnumerable<T>, " + $"but found '{enumerableType}'.";
 
             throw new ArgumentException(message, argumentName);
         }
 
         return itemType;
+    }
+
+    /// <summary>
+    /// Executes the specified expression.
+    /// </summary>
+    private IStreamedData Execute(Expression expression)
+    {
+        var model = GenerateQueryModel(expression);
+
+        return model.Execute(_executor);
     }
 }
