78409697ab168eeb16d0516591bb500b59b536bd
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-11 16:52:11 +0200

    exclude Gradle dir

--- modules/platforms/dotnet/Apache.Ignite.Internal.Generators/ExceptionsGenerator.cs
+++ modules/platforms/dotnet/Apache.Ignite.Internal.Generators/ExceptionsGenerator.cs
@@ -42,7 +42,8 @@ namespace Apache.Ignite.Internal.Generators
             var exclude = new[]
             {
                 "internal",
-                string.Format(CultureInfo.InvariantCulture, "{0}target{0}", Path.DirectorySeparatorChar),
+                string.Format(CultureInfo.InvariantCulture, "{0}target{0}", Path.DirectorySeparatorChar), // Maven
+                string.Format(CultureInfo.InvariantCulture, "{0}build{0}", Path.DirectorySeparatorChar), // Gradle
                 Path.Combine("modules", "cli")
             };
 
