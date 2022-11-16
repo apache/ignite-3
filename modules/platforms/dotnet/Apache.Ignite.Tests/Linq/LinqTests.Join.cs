4ece73a4d9604c7951d65dbadda0172d7cb0a954
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-16 11:22:03 +0200

    tests done

--- modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Join.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Join.cs
@@ -23,9 +23,6 @@ using NUnit.Framework;
 
 /// <summary>
 /// Linq JOINs tests.
-/// TODO:
-/// TestLocalCollectionJoin
-/// TestInvalidJoin.
 /// </summary>
 public partial class LinqTests
 {
@@ -258,12 +255,6 @@ public partial class LinqTests
             "left outer join (select * from PUBLIC.TBL_INT16 as _T2 ) as _T1 " +
             "on (_T1.KEY = _T0.KEY)",
             joinQuery.ToString());
-    }
-
-    [Test]
-    public void TestInvalidJoin()
-    {
-        Assert.Fail("TODO");
     }
 
     [Test]
