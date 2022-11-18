e7bcb0d64e54400e839b7a692a7d7fe9b2c762d1
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-18 11:03:34 +0200

    IGNITE-18083 .NET: Thin 3.0: LINQ: Groupings

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.GroupBy.cs
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Linq;
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+/// <summary>
+/// Linq GROUP BY tests.
+/// </summary>
+public partial class LinqTests
+{
+    [Test]
+    public void TestGroupBy()
+    {
+    }
+
+    [Test]
+    public void TestGroupBySubQuery()
+    {
+        Assert.Fail("TODO");
+    }
+
+    [Test]
+    public void TestGroupByWithJoin()
+    {
+        Assert.Fail("TODO");
+    }
+
+    [Test]
+    public void TestGroupByWithReverseJoin()
+    {
+        Assert.Fail("TODO");
+    }
+
+    [Test]
+    public void TestGroupByWithReverseJoinAndProjection()
+    {
+        Assert.Fail("TODO");
+    }
+}
