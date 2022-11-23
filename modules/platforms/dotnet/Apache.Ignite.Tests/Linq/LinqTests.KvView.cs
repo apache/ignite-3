c6fcfdeda74168079779ce3ca976094486458340
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-23 17:54:21 +0200

    wip LinqTests.KvView

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.KvView.cs
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Linq;
+
+using System.Linq;
+using Ignite.Table;
+using NUnit.Framework;
+
+/// <summary>
+/// Linq KvView tests.
+/// </summary>
+public partial class LinqTests
+{
+    private IKeyValueView<KeyPoco, ValPoco> KvView { get; set; } = null!;
+
+    [Test]
+    public void TestSelectKeyValuePair()
+    {
+        var query = KvView.AsQueryable()
+            .Where(x => x.Key.Key > 3 && x.Value.Val != null)
+            .OrderBy(x => x.Key.Key);
+
+        var res = query.ToList();
+
+        Assert.AreEqual(4, res[0].Key.Key);
+        Assert.AreEqual("v-4", res[0].Value.Val);
+
+        StringAssert.Contains("TODO", query.ToString());
+    }
+
+    [OneTimeSetUp]
+    private void InitKvView()
+    {
+        KvView = Table.GetKeyValueView<KeyPoco, ValPoco>();
+    }
+
+    private record KeyPoco(long Key);
+
+    private record ValPoco(string? Val);
+}
