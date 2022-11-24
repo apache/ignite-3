e27d7d62117068c81aef15e6c4fd1f560f970f9a
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-24 20:23:32 +0200

    wip LinqTests.Cast

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Cast.cs
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Linq;
+
+using System.Linq;
+using NUnit.Framework;
+
+/// <summary>
+/// Linq type cast tests.
+/// </summary>
+public partial class LinqTests
+{
+    [Test]
+    public void TestCastProjectWithCastIntoAnonymousType()
+    {
+        var query = PocoIntView.AsQueryable()
+            .Select(x => new
+            {
+                Byte = (sbyte)x.Key,
+                Short = (short)x.Key,
+                Long = (long)x.Key,
+                Float = (float)x.Key / 1000,
+                Double = (double)x.Key / 2000,
+                Decimal = (decimal)x.Key / 3000
+            })
+            .OrderByDescending(x => x.Long);
+
+        var res = query.ToList();
+
+        Assert.AreEqual(100, res[0].Byte);
+        Assert.AreEqual(100, res[0].Short);
+        Assert.AreEqual(100, res[0].Long);
+        Assert.AreEqual(100, res[0].Float);
+        Assert.AreEqual(100, res[0].Double);
+        Assert.AreEqual(100, res[0].Decimal);
+
+        StringAssert.Contains("TODO", query.ToString());
+    }
+
+    [Test]
+    public void TestCastProjectWithCastIntoRecord()
+    {
+    }
+}
