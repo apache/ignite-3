7c964791db93a0cf74dabc32c1db7c398e38ed3c
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-29 18:50:59 +0200

    wip TestNumericFunctions - logarithms

--- modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
@@ -64,6 +64,11 @@ public partial class LinqTests
         TestOpDouble(x => Math.Tan(x.Key), 1.5574077246549023d, "select Tan(_T0.KEY) from");
         TestOpDouble(x => Math.Tanh(x.Key / 10), 0.71629787019902447d, "select Tanh((_T0.KEY / ?)) from");
         TestOpDouble(x => Math.Atan(x.Key), 1.4601391056210009d, "select Atan(_T0.KEY) from");
+        TestOpDouble(x => Math.Ceiling(x.Key / 3), 3.0d, "select Ceiling((_T0.KEY / ?)) from");
+        TestOpDouble(x => Math.Exp(x.Key), 8103.0839275753842d, "select Exp(_T0.KEY) from");
+        TestOpDouble(x => Math.Log(x.Key), 2.1972245773362196d, "select Ln(_T0.KEY) from");
+        TestOpDouble(x => Math.Log10(x.Key), 0.95424250943932487d, "select Log10(_T0.KEY) from");
+        TestOpDouble(x => Math.Log2(x.Key), 8103.0839275753842d, "select Log2(_T0.KEY) from");
 
         // TODO: Ceiling, Exp, Floor, Exp, Log, Log10, Pow, Round, Sign, Sqrt, Truncate
         TestOpInt(x => Math.Abs(-x.Key), 9, "select Abs((-_T0.KEY)) from");
