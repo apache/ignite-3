960cc0e9edb9128090ba7fda02edb51938659768
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-29 10:52:00 +0200

    Ignore TestModulus with ticket

--- modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
@@ -43,10 +43,9 @@ public partial class LinqTests
     }
 
     [Test]
+    [Ignore("IGNITE-18274")]
     public void TestModulus()
     {
-        // TODO: Calcite throws errors - asked in #sql_team.
-        // Alternatively, emit MOD(X, Y) instead of X % Y.
         TestOpDouble(x => x.Key % 3d, 1.0d, "select (_T0.KEY % ?) from");
         TestOpInt(x => x.Key % 4, 2, "select (_T0.KEY % ?) from");
     }
