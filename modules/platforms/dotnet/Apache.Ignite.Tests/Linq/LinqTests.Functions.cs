7ed098401edb7d8baf3d9d28531c068f97c3b01e
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-29 09:35:53 +0200

    wip TestNumericOperators

--- modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Linq/LinqTests.Functions.cs
@@ -17,6 +17,10 @@
 
 namespace Apache.Ignite.Tests.Linq;
 
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Ignite.Table;
 using NUnit.Framework;
 
 /// <summary>
@@ -27,7 +31,18 @@ public partial class LinqTests
     [Test]
     public void TestNumericOperators()
     {
-        Assert.Fail("TODO");
+        TestOpDouble(x => x.Key + 1.234d, 10.234d, "select (_T0.KEY + ?) from");
+        TestOpDouble(x => x.Key - 1.234d, 7.766d, "select (_T0.KEY - ?) from");
+        TestOpDouble(x => x.Key * 2.5d, 22.5d, "select (_T0.KEY * ?) from");
+        TestOpDouble(x => x.Key / 3d, 3.0d, "select (_T0.KEY / ?) from");
+
+        TestOpInt(x => x.Key + 1.234d, 10.234d, "select (cast(_T0.KEY as double) + ?) from");
+        TestOpInt(x => x.Key - 1.234d, 7.766d, "select (cast(_T0.KEY as double) - ?) from");
+        TestOpInt(x => x.Key * 2.5d, 22.5d, "select (cast(_T0.KEY as double) * ?) from");
+        TestOpInt(x => x.Key / 3d, 3.0d, "select (cast(_T0.KEY as double) / ?) from");
+
+        // TODO:
+        // TestOpDouble(x => x.Key % 3d, 3.0d, "select (_T0.KEY % ?) from");
     }
 
     [Test]
@@ -53,4 +68,20 @@ public partial class LinqTests
     {
         Assert.Fail("TODO");
     }
+
+    private static void TestOp<T>(IRecordView<T> view, Expression<Func<T, double>> expr, double expectedRes, string expectedQuery)
+        where T : notnull
+    {
+        var query = view.AsQueryable().Select(expr);
+        var res = query.Max();
+
+        Assert.AreEqual(expectedRes, res);
+        StringAssert.Contains(expectedQuery, query.ToString());
+    }
+
+    private void TestOpDouble(Expression<Func<PocoDouble, double>> expr, double expectedRes, string expectedQuery) =>
+        TestOp(PocoDoubleView, expr, expectedRes, expectedQuery);
+
+    private void TestOpInt(Expression<Func<PocoInt, double>> expr, double expectedRes, string expectedQuery) =>
+        TestOp(PocoIntView, expr, expectedRes, expectedQuery);
 }
