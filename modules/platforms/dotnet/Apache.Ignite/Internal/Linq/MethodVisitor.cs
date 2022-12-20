95d1bd6adaa920f22e2e5dc8c8e47cb07570c6e1
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-12-20 10:07:23 +0200

    wip TestStringFunctions

--- modules/platforms/dotnet/Apache.Ignite/Internal/Linq/MethodVisitor.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Linq/MethodVisitor.cs
@@ -272,7 +272,7 @@ internal static class MethodVisitor
                 constExpr.Type.IsPrimitive &&
                 constExpr.Type != typeof(char))
             {
-                // TODO IGNITE-18282 Remove this logic, we should be able to pass args as SQL params for all functions.
+                // TODO Remove this logic, we should be able to pass args as SQL params for all functions.
                 // We only allow inline for numeric types. Other types can lead to SQL injections.
                 visitor.ResultBuilder.Append(constExpr.Value);
             }
