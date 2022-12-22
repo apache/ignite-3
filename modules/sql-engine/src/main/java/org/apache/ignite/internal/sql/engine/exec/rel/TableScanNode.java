7f6662531c57391f07e0c0882e81d681e611a241
Yury Gerzhedovich <ygerzhedovich@gridgain.com>
2022-12-22 18:39:32 +0200

    IGNITE-18227: refactoring scan nodes and add support RO index scans.

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/rel/TableScanNode.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/rel/TableScanNode.java
@@ -19,66 +19,40 @@ package org.apache.ignite.internal.sql.engine.exec.rel;
 
 import static org.apache.ignite.internal.util.ArrayUtils.nullOrEmpty;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
-import java.util.Queue;
 import java.util.concurrent.Flow;
-import java.util.concurrent.Flow.Subscription;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Flow.Publisher;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import org.apache.ignite.internal.schema.BinaryRow;
 import org.apache.ignite.internal.sql.engine.exec.ExecutionContext;
 import org.apache.ignite.internal.sql.engine.exec.RowHandler;
 import org.apache.ignite.internal.sql.engine.schema.InternalIgniteTable;
+import org.apache.ignite.internal.sql.engine.util.CompositePublisher;
 import org.apache.ignite.internal.table.InternalTable;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Scan node.
+ * Execution node for table scan. Provide result of table scan by given table and partitions.
  */
-public class TableScanNode<RowT> extends AbstractNode<RowT> {
-    /** Special value to highlights that all row were received and we are not waiting any more. */
-    private static final int NOT_WAITING = -1;
+public class TableScanNode<RowT> extends StorageScanNode<RowT> {
 
     /** Table that provides access to underlying data. */
     private final InternalTable physTable;
 
-    /** Table that is an object in SQL schema. */
-    private final InternalIgniteTable schemaTable;
-
-    private final RowHandler.RowFactory<RowT> factory;
-
     private final int[] parts;
-
-    private final Queue<RowT> inBuff = new LinkedBlockingQueue<>(inBufSize);
-
-    private final @Nullable Predicate<RowT> filters;
-
-    private final @Nullable Function<RowT, RowT> rowTransformer;
-
-    /** Participating columns. */
-    private final @Nullable BitSet requiredColumns;
-
-    private int requested;
-
-    private int waiting;
-
-    private boolean inLoop;
-
-    private Subscription activeSubscription;
-
-    private int curPartIdx;
 
     /**
      * Constructor.
      *
-     * @param ctx             Execution context.
-     * @param rowFactory      Row factory.
-     * @param schemaTable     The table this node should scan.
-     * @param parts           Partition numbers to scan.
-     * @param filters         Optional filter to filter out rows.
-     * @param rowTransformer  Optional projection function.
+     * @param ctx Execution context.
+     * @param rowFactory Row factory.
+     * @param schemaTable The table this node should scan.
+     * @param parts Partition numbers to scan.
+     * @param filters Optional filter to filter out rows.
+     * @param rowTransformer Optional projection function.
      * @param requiredColumns Optional set of column of interest.
      */
     public TableScanNode(
@@ -90,190 +64,33 @@ public class TableScanNode<RowT> extends AbstractNode<RowT> {
             @Nullable Function<RowT, RowT> rowTransformer,
             @Nullable BitSet requiredColumns
     ) {
-        super(ctx);
+        super(ctx, rowFactory, schemaTable, filters, rowTransformer, requiredColumns);
 
         assert !nullOrEmpty(parts);
 
-        assert context().transaction() != null || context().transactionTime() != null : "Transaction not initialized.";
-
         this.physTable = schemaTable.table();
-        this.schemaTable = schemaTable;
         this.parts = parts;
-        this.filters = filters;
-        this.rowTransformer = rowTransformer;
-        this.requiredColumns = requiredColumns;
-        this.factory = rowFactory;
     }
 
     /** {@inheritDoc} */
     @Override
-    public void request(int rowsCnt) throws Exception {
-        assert rowsCnt > 0 && requested == 0 : "rowsCnt=" + rowsCnt + ", requested=" + requested;
+    protected Publisher<RowT> scan() {
+        List<Flow.Publisher<BinaryRow>> partPublishers = new ArrayList<>(parts.length);
 
-        checkState();
+        boolean roTx = context().transactionTime() != null;
 
-        requested = rowsCnt;
-
-        if (!inLoop) {
-            context().execute(this::push, this::onError);
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void closeInternal() {
-        super.closeInternal();
-
-        if (activeSubscription != null) {
-            activeSubscription.cancel();
-
-            activeSubscription = null;
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    protected void rewindInternal() {
-        requested = 0;
-        waiting = 0;
-        curPartIdx = 0;
-
-        if (activeSubscription != null) {
-            activeSubscription.cancel();
-
-            activeSubscription = null;
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void register(List<Node<RowT>> sources) {
-        throw new UnsupportedOperationException();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    protected Downstream<RowT> requestDownstream(int idx) {
-        throw new UnsupportedOperationException();
-    }
-
-    private void push() throws Exception {
-        if (isClosed()) {
-            return;
-        }
-
-        checkState();
-
-        if (requested > 0 && !inBuff.isEmpty()) {
-            inLoop = true;
-            try {
-                while (requested > 0 && !inBuff.isEmpty()) {
-                    checkState();
-
-                    RowT row = inBuff.poll();
-
-                    if (filters != null && !filters.test(row)) {
-                        continue;
-                    }
-
-                    if (rowTransformer != null) {
-                        row = rowTransformer.apply(row);
-                    }
-
-                    requested--;
-                    downstream().push(row);
-                }
-            } finally {
-                inLoop = false;
-            }
-        }
-
-        if (requested > 0) {
-            if (waiting == 0 || activeSubscription == null) {
-                requestNextBatch();
-            }
-        }
-
-        if (requested > 0 && waiting == NOT_WAITING) {
-            if (inBuff.isEmpty()) {
-                requested = 0;
-                downstream().end();
+        for (int p : parts) {
+            Publisher<BinaryRow> pub;
+            if (roTx) {
+                pub = physTable.scan(p, context().transactionTime(), context().localNode());
             } else {
-                context().execute(this::push, this::onError);
+                pub = physTable.scan(p, context().transaction());
             }
+
+            partPublishers.add(pub);
         }
+
+        return convertPublisher(new CompositePublisher<>(partPublishers));
     }
 
-    private void requestNextBatch() {
-        if (waiting == NOT_WAITING) {
-            return;
-        }
-
-        if (waiting == 0) {
-            // we must not request rows more than inBufSize
-            waiting = inBufSize - inBuff.size();
-        }
-
-        Subscription subscription = this.activeSubscription;
-        if (subscription != null) {
-            subscription.request(waiting);
-        } else if (curPartIdx < parts.length) {
-            if (context().transactionTime() != null) {
-                physTable.scan(parts[curPartIdx++], context().transactionTime(), context().localNode()).subscribe(new SubscriberImpl());
-            } else {
-                physTable.scan(parts[curPartIdx++], context().transaction()).subscribe(new SubscriberImpl());
-            }
-        } else {
-            waiting = NOT_WAITING;
-        }
-    }
-
-    private class SubscriberImpl implements Flow.Subscriber<BinaryRow> {
-        /** {@inheritDoc} */
-        @Override
-        public void onSubscribe(Subscription subscription) {
-            assert TableScanNode.this.activeSubscription == null;
-
-            TableScanNode.this.activeSubscription = subscription;
-            subscription.request(waiting);
-        }
-
-        /** {@inheritDoc} */
-        @Override
-        public void onNext(BinaryRow binRow) {
-            RowT row = convert(binRow);
-
-            inBuff.add(row);
-
-            if (inBuff.size() == inBufSize) {
-                context().execute(() -> {
-                    waiting = 0;
-                    push();
-                }, TableScanNode.this::onError);
-            }
-        }
-
-        /** {@inheritDoc} */
-        @Override
-        public void onError(Throwable throwable) {
-            context().execute(() -> {
-                throw throwable;
-            }, TableScanNode.this::onError);
-        }
-
-        /** {@inheritDoc} */
-        @Override
-        public void onComplete() {
-            context().execute(() -> {
-                activeSubscription = null;
-                waiting = 0;
-
-                push();
-            }, TableScanNode.this::onError);
-        }
-    }
-
-    private RowT convert(BinaryRow binRow) {
-        return schemaTable.toRow(context(), binRow, factory, requiredColumns);
-    }
 }
