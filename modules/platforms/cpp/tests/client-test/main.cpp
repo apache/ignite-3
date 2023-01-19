807e256cf5dc8c3db892da0b212d84fe31443468
Igor Sapego <isapego@apache.org>
2023-01-19 15:15:28 +0400

    IGNITE-18583 Remove node startup from tests

--- modules/platforms/cpp/tests/client-test/main.cpp
+++ modules/platforms/cpp/tests/client-test/main.cpp
@@ -15,79 +15,14 @@
  * limitations under the License.
  */
 
-#include "ignite_runner.h"
-
 #include <ignite/common/ignite_error.h>
 
 #include <gtest/gtest.h>
 
-#include <chrono>
-#include <csignal>
-#include <thread>
-
-namespace {
-/** Shutdown handler that cleans up resources. */
-std::function<void(int)> shutdown_handler;
-
-/**
- * Receives OS signal and handles it.
- *
- * @param signum Signal value.
- */
-void signal_handler(int signum) {
-    shutdown_handler(signum);
-
-    signal(signum, SIG_DFL);
-
-    raise(signum);
-}
-} // namespace
-
-/**
- * Sets process abortion (SIGABRT, SIGINT, SIGSEGV signals) handler.
- *
- * @param handler Abortion handler.
- */
-void set_process_abort_handler(std::function<void(int)> handler) {
-    shutdown_handler = std::move(handler);
-
-    // Install signal handlers to clean up resources on early exit.
-    signal(SIGABRT, signal_handler);
-    signal(SIGINT, signal_handler);
-    signal(SIGSEGV, signal_handler);
-}
-
-/**
- * Run prior to any other tests.
- */
-void before_all() {
-    ignite::IgniteRunner runner;
-
-    runner.start();
-
-    // Five minutes should be enough but feel free to increase.
-    runner.join(std::chrono::minutes(5));
-    runner.stop();
-}
-
 int main(int argc, char **argv) {
     int res = 0;
-    before_all();
-
-    ignite::IgniteRunner runner;
-
-    set_process_abort_handler([&](int signal) {
-        std::cout << "Caught signal " << signal << " during tests" << std::endl;
-
-        runner.stop();
-    });
 
     try {
-        runner.start();
-
-        // TODO: Implement node startup await
-        std::this_thread::sleep_for(std::chrono::seconds(20));
-
         ::testing::InitGoogleTest(&argc, argv);
         res = RUN_ALL_TESTS();
     } catch (const std::exception &err) {
@@ -95,7 +30,6 @@ int main(int argc, char **argv) {
     } catch (...) {
         std::cout << "Unknown uncaught error" << std::endl;
     }
-    runner.stop();
 
     return res;
 }
