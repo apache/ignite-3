647d465fffa533531be7e9f78ff813b419b64434
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-01-24 10:48:16 +0200

    Add ResultSetBenchmarks.DbDataReader

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/Sql/ResultSetBenchmarks.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Sql/ResultSetBenchmarks.cs
@@ -37,6 +37,15 @@ namespace Apache.Ignite.Benchmarks.Sql
     /// |                AsyncEnumerable | 1,172.7 us | 23.41 us | 27.87 us |  1.21 |    0.03 | 15.6250 | 1.9531 |    393 KB |
     /// |                LinqToListAsync |   786.1 us | 14.94 us | 15.34 us |  0.81 |    0.02 |  2.9297 |      - |     80 KB |
     /// | LinqSelectOneColumnToListAsync |   677.0 us | 13.42 us | 11.21 us |  0.70 |    0.02 |  1.9531 |      - |     57 KB |.
+    ///
+    /// NEW (TODO DESC):
+    /// |                         Method |     Mean |   Error |  StdDev | Ratio | RatioSD |  Gen 0 |  Gen 1 | Allocated |
+    /// |------------------------------- |---------:|--------:|--------:|------:|--------:|-------:|-------:|----------:|
+    /// |                    ToListAsync | 222.2 us | 2.66 us | 2.49 us |  1.00 |    0.00 | 1.4648 | 0.2441 |    392 KB |
+    /// |                AsyncEnumerable | 264.1 us | 4.10 us | 3.83 us |  1.19 |    0.02 | 1.4648 | 0.4883 |    393 KB |
+    /// |                LinqToListAsync | 181.3 us | 2.48 us | 2.32 us |  0.82 |    0.01 | 0.2441 |      - |     80 KB |
+    /// | LinqSelectOneColumnToListAsync | 159.6 us | 2.70 us | 3.41 us |  0.72 |    0.02 | 0.2441 |      - |     57 KB |
+    /// |                   DbDataReader | 188.2 us | 1.96 us | 1.83 us |  0.85 |    0.01 |      - |      - |     51 KB |.
     /// </summary>
     [MemoryDiagnoser]
     [SimpleJob(RuntimeMoniker.Net60)]
