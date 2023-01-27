0361e499ef83286da4dfbe7ce8215a74e205aa5a
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-01-27 11:04:57 +0200

    IGNITE-17992 .NET: Thin 3.0: Incorrect colocation hash due to ignored precision for LocalTime, LocalDateTime, Timestamp
    https://issues.apache.org/jira/browse/IGNITE-17992

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/PlatformTestNodeRunner.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/PlatformTestNodeRunner.java
@@ -47,6 +47,7 @@ import org.apache.ignite.internal.schema.row.Row;
 import org.apache.ignite.internal.schema.testutils.SchemaConfigurationConverter;
 import org.apache.ignite.internal.schema.testutils.builder.SchemaBuilders;
 import org.apache.ignite.internal.schema.testutils.definition.ColumnType;
+import org.apache.ignite.internal.schema.testutils.definition.ColumnType.TemporalColumnType;
 import org.apache.ignite.internal.schema.testutils.definition.TableDefinition;
 import org.apache.ignite.internal.table.distributed.TableManager;
 import org.apache.ignite.internal.util.IgniteUtils;
@@ -179,6 +180,8 @@ public class PlatformTestNodeRunner {
                         .changePartitions(10)
         ));
 
+        int maxTimePrecision = TemporalColumnType.MAX_TIME_PRECISION;
+
         TableDefinition schTblAll = SchemaBuilders.tableBuilder(SCHEMA_NAME, TABLE_NAME_ALL_COLUMNS).columns(
                 SchemaBuilders.column(keyCol, ColumnType.INT64).build(),
                 SchemaBuilders.column("str", ColumnType.string()).asNullable(true).build(),
@@ -191,12 +194,12 @@ public class PlatformTestNodeRunner {
                 SchemaBuilders.column("uuid", ColumnType.UUID).asNullable(true).build(),
                 SchemaBuilders.column("date", ColumnType.DATE).asNullable(true).build(),
                 SchemaBuilders.column("bitmask", ColumnType.bitmaskOf(64)).asNullable(true).build(),
-                SchemaBuilders.column("time", ColumnType.time(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
-                SchemaBuilders.column("datetime", ColumnType.datetime(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
-                SchemaBuilders.column("timestamp", ColumnType.timestamp(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
+                SchemaBuilders.column("time", ColumnType.time(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("time2", ColumnType.time(2)).asNullable(true).build(),
+                SchemaBuilders.column("datetime", ColumnType.datetime(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("datetime2", ColumnType.datetime(3)).asNullable(true).build(),
+                SchemaBuilders.column("timestamp", ColumnType.timestamp(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("timestamp2", ColumnType.timestamp(4)).asNullable(true).build(),
                 SchemaBuilders.column("blob", ColumnType.blob()).asNullable(true).build(),
                 SchemaBuilders.column("decimal", ColumnType.decimal()).asNullable(true).build()
         ).withPrimaryKey(keyCol).build();
@@ -218,12 +221,12 @@ public class PlatformTestNodeRunner {
                 SchemaBuilders.column("float", ColumnType.FLOAT).asNullable(true).build(),
                 SchemaBuilders.column("double", ColumnType.DOUBLE).asNullable(true).build(),
                 SchemaBuilders.column("date", ColumnType.DATE).asNullable(true).build(),
-                SchemaBuilders.column("time", ColumnType.time(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
-                SchemaBuilders.column("datetime", ColumnType.datetime(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
-                SchemaBuilders.column("timestamp", ColumnType.timestamp(ColumnType.TemporalColumnType.MAX_TIME_PRECISION))
-                        .asNullable(true).build(),
+                SchemaBuilders.column("time", ColumnType.time(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("time2", ColumnType.time(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("datetime", ColumnType.datetime(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("datetime2", ColumnType.datetime(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("timestamp", ColumnType.timestamp(maxTimePrecision)).asNullable(true).build(),
+                SchemaBuilders.column("timestamp2", ColumnType.timestamp(maxTimePrecision)).asNullable(true).build(),
                 SchemaBuilders.column("blob", ColumnType.blob()).asNullable(true).build(),
                 SchemaBuilders.column("decimal", ColumnType.decimal()).asNullable(true).build()
         ).withPrimaryKey(keyCol).build();
