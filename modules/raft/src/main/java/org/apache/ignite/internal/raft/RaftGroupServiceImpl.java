3a622190ec39807fc0d096f63fefb7f8de6440cf
Vladislav Pyatkov <vldpyatkov@gmail.com>
2023-02-07 22:11:14 +0300

    Revert "NodeStoppingException throws when Raft service is stopping and try to handle a command."
    This reverts commit eceace1e0a889b05677c7734811e6b733184b9ae.

--- modules/raft/src/main/java/org/apache/ignite/internal/raft/RaftGroupServiceImpl.java
+++ modules/raft/src/main/java/org/apache/ignite/internal/raft/RaftGroupServiceImpl.java
@@ -61,7 +61,6 @@ import org.apache.ignite.internal.replicator.ReplicationGroupId;
 import org.apache.ignite.internal.tostring.S;
 import org.apache.ignite.internal.util.IgniteSpinBusyLock;
 import org.apache.ignite.lang.IgniteException;
-import org.apache.ignite.lang.NodeStoppingException;
 import org.apache.ignite.network.ClusterNode;
 import org.apache.ignite.network.ClusterService;
 import org.apache.ignite.network.NetworkMessage;
@@ -489,8 +488,7 @@ public class RaftGroupServiceImpl implements RaftGroupService {
             Peer peer, Function<Peer, ? extends NetworkMessage> requestFactory, long stopTime, CompletableFuture<R> fut
     ) {
         if (!busyLock.enterBusy()) {
-            //fut.cancel(true);
-            fut.completeExceptionally(new NodeStoppingException());
+            fut.cancel(true);
 
             return;
         }
