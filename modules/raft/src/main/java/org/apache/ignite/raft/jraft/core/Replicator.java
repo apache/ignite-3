6227666003b4f5c3ebc20fda111302d1d66a90bc
Mirza Aliev <alievmirza@gmail.com>
2023-02-16 00:43:25 +0400

    IGNITE-17465 Rre check connection before send install snapshot request https://github.com/sofastack/sofa-jraft/commit/3054d55920ceb162040804ced2466d15fd007650

--- modules/raft/src/main/java/org/apache/ignite/raft/jraft/core/Replicator.java
+++ modules/raft/src/main/java/org/apache/ignite/raft/jraft/core/Replicator.java
@@ -595,6 +595,11 @@ public class Replicator implements ThreadId.OnError {
             return;
         }
         boolean doUnlock = true;
+        if (!rpcService.connect(options.getPeerId())) {
+            LOG.error("Fail to check install snapshot connection to peer={}, give up to send install snapshot request.", options.getPeerId());
+            block(Utils.nowMs(), RaftError.EHOSTDOWN.getNumber());
+            return;
+        }
         try {
             Requires.requireTrue(this.reader == null,
                 "Replicator %s already has a snapshot reader, current state is %s", this.options.getPeerId(),
