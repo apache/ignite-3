e660692212475b0f611b69cfa5a25952979720e3
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-02-21 14:11:11 +0200

    wip PartitionAwarenessProvider

--- modules/client/src/main/java/org/apache/ignite/internal/client/table/PartitionAwarenessProvider.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/table/PartitionAwarenessProvider.java
@@ -26,12 +26,27 @@ import org.jetbrains.annotations.Nullable;
  * Represents 3 use cases:
  * 1. Partition awareness is enabled. Use hashFunc to determine partition.
  * 2. Transaction is used. Use specific channel.
- * 3. No partition awareness and no transaction. Use any channel.
+ * 3. Null instance = No partition awareness and no transaction. Use any channel.
  */
 class PartitionAwarenessProvider {
     private final @Nullable ClientChannel channel;
 
     private final @Nullable Function<ClientSchema, Integer> hashFunc;
+
+    private PartitionAwarenessProvider(@Nullable ClientChannel channel, @Nullable Function<ClientSchema, Integer> hashFunc) {
+        assert (channel == null) ^ (hashFunc == null) : "One must be null, another not null: channel, hashFunc";
+
+        this.channel = channel;
+        this.hashFunc = hashFunc;
+    }
+
+    public static PartitionAwarenessProvider of(ClientChannel channel) {
+        return new PartitionAwarenessProvider(channel, null);
+    }
+
+    public static PartitionAwarenessProvider of(Function<ClientSchema, Integer> hashFunc) {
+        return new PartitionAwarenessProvider(null, hashFunc);
+    }
 
     @Nullable ClientChannel channel() {
         return channel;
@@ -39,9 +54,9 @@ class PartitionAwarenessProvider {
 
     int getObjectHashCode(ClientSchema schema) {
         if (hashFunc == null) {
-            throw new IllegalStateException("Partition awareness is not enabled.");
+            throw new IllegalStateException("Partition awareness is not enabled. Check channel() first.");
         }
 
-        return hashFunc != null ? hashFunc.apply(schema) : 0;
+        return hashFunc.apply(schema);
     }
 }
