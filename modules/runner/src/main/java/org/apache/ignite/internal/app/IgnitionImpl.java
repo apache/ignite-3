95f0790b581b95988bd01aff5622960df97effd7
Ivan Gagarkin <igagarkin@gridgain.com>
2023-03-03 15:03:56 +0400

    fix checkstyle

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgnitionImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgnitionImpl.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
