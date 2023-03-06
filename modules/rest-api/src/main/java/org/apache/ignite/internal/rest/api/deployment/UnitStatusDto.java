65a7162885097522512fc692f25d3d3b25ed1d6a
Mikhail <Pochatkin@users.noreply.github.com>
2023-03-06 11:34:45 +0300

    IGNITE-18730 REST API for Deployment Code API
    Unused import

--- modules/rest-api/src/main/java/org/apache/ignite/internal/rest/api/deployment/UnitStatusDto.java
+++ modules/rest-api/src/main/java/org/apache/ignite/internal/rest/api/deployment/UnitStatusDto.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import org.apache.ignite.deployment.UnitStatus;
 import org.apache.ignite.deployment.version.Version;
 
