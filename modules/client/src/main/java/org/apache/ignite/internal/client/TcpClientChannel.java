82bbcb374d516e51d89e571a62afa011f20f9441
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-03-14 14:48:02 +0200

    Log failed requests

--- modules/client/src/main/java/org/apache/ignite/internal/client/TcpClientChannel.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/TcpClientChannel.java
@@ -268,6 +268,9 @@ class TcpClientChannel implements ClientChannel, ClientMessageHandler, ClientCon
 
             return fut;
         } catch (Throwable t) {
+            log.warn("Failed to send request [id=" + id + ", op=" + opCode + ", remoteAddress=" + cfg.getAddress() + "]: " +
+                    t.getMessage(), t);
+
             // Close buffer manually on fail. Successful write closes the buffer automatically.
             payloadCh.close();
             pendingReqs.remove(id);
