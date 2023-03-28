38f8d989b26758a036c9eb9e0843e73fae679c8f
Kirill Gusakov <kgusakov@gmail.com>
2023-03-28 18:19:38 +0300

    fix test

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/table/ItReadOnlyTransactionTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/table/ItReadOnlyTransactionTest.java
@@ -38,6 +38,7 @@ import org.apache.ignite.internal.schema.SchemaDescriptor;
 import org.apache.ignite.internal.schema.row.Row;
 import org.apache.ignite.internal.schema.row.RowAssembler;
 import org.apache.ignite.internal.sql.engine.ClusterPerClassIntegrationTest;
+import org.apache.ignite.lang.IgniteStringFormatter;
 import org.apache.ignite.network.ClusterNode;
 import org.apache.ignite.tx.Transaction;
 import org.junit.jupiter.api.BeforeAll;
@@ -55,7 +56,12 @@ public class ItReadOnlyTransactionTest extends ClusterPerClassIntegrationTest {
 
     @BeforeAll
     public void beforeTestStart() {
-        sql("CREATE TABLE " + TABLE_NAME + " (id INT PRIMARY KEY, val VARCHAR) WITH REPLICAS=" + nodes() + ", PARTITIONS=10");
+        String zoneName = "ZONE_" + TABLE_NAME.toUpperCase();
+
+        sql(IgniteStringFormatter.format("CREATE ZONE IF NOT EXISTS {} WITH REPLICAS={}, PARTITIONS={};",
+                zoneName, nodes(), 10));
+        sql(IgniteStringFormatter.format("CREATE TABLE {}(id INT PRIMARY KEY, val VARCHAR) WITH PRIMARY_ZONE='{}'",
+                TABLE_NAME, zoneName));
 
         Ignite ignite = CLUSTER_NODES.get(0);
 
@@ -90,7 +96,6 @@ public class ItReadOnlyTransactionTest extends ClusterPerClassIntegrationTest {
     }
 
     @Test
-    @Disabled("https://issues.apache.org/jira/browse/IGNITE-19136")
     public void testFutureRead() throws Exception {
         for (int i = 0; i < nodes(); i++) {
             Ignite ignite = CLUSTER_NODES.get(i);
@@ -146,7 +151,6 @@ public class ItReadOnlyTransactionTest extends ClusterPerClassIntegrationTest {
     }
 
     @Test
-    @Disabled("https://issues.apache.org/jira/browse/IGNITE-19136")
     public void testPastRead() throws Exception {
         for (int i = 0; i < nodes(); i++) {
             Ignite ignite = CLUSTER_NODES.get(i);
