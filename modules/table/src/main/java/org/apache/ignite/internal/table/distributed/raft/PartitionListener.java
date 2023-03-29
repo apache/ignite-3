2ea18297f18da0356c73730ea1ae7779b2bfba79
Alexander Lapin <lapin1702@gmail.com>
2023-03-29 17:08:12 +0300

    IGNITE-19043 ItRaftCommandLeftInLogUntilRestartTest fixed (#1857)

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/raft/PartitionListener.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/raft/PartitionListener.java
@@ -203,18 +203,6 @@ public class PartitionListener implements RaftGroupListener {
             return;
         }
 
-        TxMeta txMeta = txStateStorage.get(cmd.txId());
-
-        if (txMeta != null && (txMeta.txState() == COMMITED || txMeta.txState() == ABORTED)) {
-            storage.runConsistently(() -> {
-                storage.lastApplied(commandIndex, commandTerm);
-
-                return null;
-            });
-
-            return;
-        }
-
         storageUpdateHandler.handleUpdate(cmd.txId(), cmd.rowUuid(), cmd.tablePartitionId().asTablePartitionId(), cmd.rowBuffer(),
                 rowId -> {
                     txsPendingRowIds.computeIfAbsent(cmd.txId(), entry -> new HashSet<>()).add(rowId);
@@ -235,15 +223,6 @@ public class PartitionListener implements RaftGroupListener {
         // Skips the write command because the storage has already executed it.
         if (commandIndex <= storage.lastAppliedIndex()) {
             return;
-        }
-
-        TxMeta txMeta = txStateStorage.get(cmd.txId());
-        if (txMeta != null && (txMeta.txState() == COMMITED || txMeta.txState() == ABORTED)) {
-            storage.runConsistently(() -> {
-                storage.lastApplied(commandIndex, commandTerm);
-
-                return null;
-            });
         }
 
         storageUpdateHandler.handleUpdateAll(cmd.txId(), cmd.rowsToUpdate(), cmd.tablePartitionId().asTablePartitionId(), rowIds -> {
