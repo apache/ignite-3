ea06c06ad209d89759dbab8a0a4f438e29d525f0
Kirill Tkalenko <tkalkirill@yandex.ru>
2023-03-29 14:35:27 +0300

    IGNITE-19085 after review #1.1

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/TableManager.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/TableManager.java
@@ -1105,8 +1105,7 @@ public class TableManager extends Producer<TableEvent, TableEventParameters> imp
 
         InternalTableImpl internalTable = new InternalTableImpl(name, tblId, new Int2ObjectOpenHashMap<>(partitions),
                 partitions, clusterNodeResolver, txManager, tableStorage, txStateStorage, replicaSvc, clock);
-
-        // TODO: IGNITE-16288 directIndexIds should use async configuration API
+        
         var table = new TableImpl(internalTable, lockMgr);
 
         // TODO: IGNITE-19082 Need another way to wait for indexes
