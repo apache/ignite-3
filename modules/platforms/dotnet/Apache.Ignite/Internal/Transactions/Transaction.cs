f50bff4adf11ca9c1964c773d879a26b84a45e25
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-04-03 11:54:17 +0300

    Refactor Transaction

--- modules/platforms/dotnet/Apache.Ignite/Internal/Transactions/Transaction.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Transactions/Transaction.cs
@@ -19,6 +19,7 @@ namespace Apache.Ignite.Internal.Transactions
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using Common;
     using Ignite.Transactions;
     using Proto;
     using Proto.MsgPack;
@@ -101,7 +102,12 @@ namespace Apache.Ignite.Internal.Transactions
                 _ => "RolledBack"
             };
 
-            return $"Transaction {{ Id = {Id}, State = {state}, IsReadOnly = {IsReadOnly} }}";
+            var builder = new IgniteToStringBuilder(typeof(Transaction));
+            builder.Append(nameof(Id), Id);
+            builder.Append("State", state);
+            builder.Append(nameof(IsReadOnly), IsReadOnly);
+
+            return builder.Build();
         }
 
         /// <summary>
