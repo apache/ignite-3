8c57bbec57c9b8106f70fad94f8224125474b167
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-04-07 13:01:08 +0300

    wip MutableTupleBinaryTupleAdapter

--- modules/client-common/src/main/java/org/apache/ignite/internal/client/table/MutableTupleBinaryTupleAdapter.java
+++ modules/client-common/src/main/java/org/apache/ignite/internal/client/table/MutableTupleBinaryTupleAdapter.java
@@ -17,11 +17,259 @@
 
 package org.apache.ignite.internal.client.table;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.BitSet;
+import java.util.Iterator;
+import java.util.UUID;
+import org.apache.ignite.binary.BinaryObject;
+import org.apache.ignite.internal.binarytuple.BinaryTupleReader;
+import org.apache.ignite.table.Tuple;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * {@link org.apache.ignite.table.Tuple} implementation over {@link org.apache.ignite.internal.binarytuple.BinaryTupleReader},
  * with mutable fallback.
  */
-public class MutableTupleBinaryTupleAdapter {
+public final class MutableTupleBinaryTupleAdapter implements Tuple {
+    // TODO: SchemaAware?
+    // TODO: This class should be in client, not client-common: we need to deal with ClientSchema.
+    // BUT at the same time, this might be needed on the server side to wrap BinaryTuple.
+    // So we might abstract schema access somehow?
     // TODO: See MutableRowTupleAdapter.
     // TODO: Should this class replace ClientTuple completely?
+    /** Underlying BinaryTuple. */
+    private final BinaryTupleReader binaryTuple;
+
+    /** Tuple with overwritten data. */
+    private Tuple tuple;
+
+    /**
+     * Constructor.
+     *
+     * @param binaryTuple Binary tuple.
+     */
+    public MutableTupleBinaryTupleAdapter(BinaryTupleReader binaryTuple) {
+        this.binaryTuple = binaryTuple;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int columnCount() {
+        return tuple != null ? tuple.columnCount() : super.columnCount();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String columnName(int columnIndex) {
+        return tuple != null ? tuple.columnName(columnIndex) : super.columnName(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int columnIndex(@NotNull String columnName) {
+        return tuple != null ? tuple.columnIndex(columnName) : super.columnIndex(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T> T valueOrDefault(@NotNull String columnName, T defaultValue) {
+        return tuple != null ? tuple.valueOrDefault(columnName, defaultValue) : super.valueOrDefault(columnName, defaultValue);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T> T value(@NotNull String columnName) {
+        return tuple != null ? tuple.value(columnName) : super.value(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T> T value(int columnIndex) {
+        return tuple != null ? tuple.value(columnIndex) : super.value(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public BinaryObject binaryObjectValue(@NotNull String columnName) {
+        return tuple != null ? tuple.binaryObjectValue(columnName) : super.binaryObjectValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public BinaryObject binaryObjectValue(int columnIndex) {
+        return super.binaryObjectValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public byte byteValue(@NotNull String columnName) {
+        return tuple != null ? tuple.byteValue(columnName) : super.byteValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public byte byteValue(int columnIndex) {
+        return tuple != null ? tuple.byteValue(columnIndex) : super.byteValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public short shortValue(@NotNull String columnName) {
+        return tuple != null ? tuple.shortValue(columnName) : super.shortValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public short shortValue(int columnIndex) {
+        return tuple != null ? tuple.shortValue(columnIndex) : super.shortValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int intValue(@NotNull String columnName) {
+        return tuple != null ? tuple.intValue(columnName) : super.intValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int intValue(int columnIndex) {
+        return tuple != null ? tuple.intValue(columnIndex) : super.intValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long longValue(@NotNull String columnName) {
+        return tuple != null ? tuple.longValue(columnName) : super.longValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long longValue(int columnIndex) {
+        return tuple != null ? tuple.longValue(columnIndex) : super.longValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public float floatValue(@NotNull String columnName) {
+        return tuple != null ? tuple.floatValue(columnName) : super.floatValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public float floatValue(int columnIndex) {
+        return tuple != null ? tuple.floatValue(columnIndex) : super.floatValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double doubleValue(@NotNull String columnName) {
+        return tuple != null ? tuple.doubleValue(columnName) : super.doubleValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double doubleValue(int columnIndex) {
+        return tuple != null ? tuple.doubleValue(columnIndex) : super.doubleValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String stringValue(@NotNull String columnName) {
+        return tuple != null ? tuple.stringValue(columnName) : super.stringValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String stringValue(int columnIndex) {
+        return tuple != null ? tuple.stringValue(columnIndex) : super.stringValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public UUID uuidValue(@NotNull String columnName) {
+        return tuple != null ? tuple.uuidValue(columnName) : super.uuidValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public UUID uuidValue(int columnIndex) {
+        return tuple != null ? tuple.uuidValue(columnIndex) : super.uuidValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public BitSet bitmaskValue(@NotNull String columnName) {
+        return tuple != null ? tuple.bitmaskValue(columnName) : super.bitmaskValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public BitSet bitmaskValue(int columnIndex) {
+        return tuple != null ? tuple.bitmaskValue(columnIndex) : super.bitmaskValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalDate dateValue(String columnName) {
+        return tuple != null ? tuple.dateValue(columnName) : super.dateValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalDate dateValue(int columnIndex) {
+        return tuple != null ? tuple.dateValue(columnIndex) : super.dateValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalTime timeValue(String columnName) {
+        return tuple != null ? tuple.timeValue(columnName) : super.timeValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalTime timeValue(int columnIndex) {
+        return tuple != null ? tuple.timeValue(columnIndex) : super.timeValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalDateTime datetimeValue(String columnName) {
+        return tuple != null ? tuple.datetimeValue(columnName) : super.datetimeValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LocalDateTime datetimeValue(int columnIndex) {
+        return tuple != null ? tuple.datetimeValue(columnIndex) : super.datetimeValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Instant timestampValue(String columnName) {
+        return tuple != null ? tuple.timestampValue(columnName) : super.timestampValue(columnName);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Instant timestampValue(int columnIndex) {
+        return tuple != null ? tuple.timestampValue(columnIndex) : super.timestampValue(columnIndex);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public @NotNull Iterator<Object> iterator() {
+        return tuple != null ? tuple.iterator() : super.iterator();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Tuple set(@NotNull String columnName, Object value) {
+        tuple.set(columnName, value);
+
+        return this;
+    }
 }
