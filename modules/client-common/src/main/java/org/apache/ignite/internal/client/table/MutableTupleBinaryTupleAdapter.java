b7efc637a892734873065b60061dfee5ccc5d723
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-04-07 10:50:27 +0300

    IGNITE-18899 Thin 3.0: Refactor ClientTuple to wrap BinaryTuple similar to MutableRowTupleAdapter

---
+++ modules/client-common/src/main/java/org/apache/ignite/internal/client/table/MutableTupleBinaryTupleAdapter.java
@@ -0,0 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.client.table;
+
+/**
+ * {@link org.apache.ignite.table.Tuple} implementation over {@link org.apache.ignite.internal.binarytuple.BinaryTupleReader},
+ * with mutable fallback.
+ */
+public class MutableTupleBinaryTupleAdapter {
+    // TODO: See MutableRowTupleAdapter.
+    // TODO: Should this class replace ClientTuple completely?
+}
