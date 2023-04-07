da40ef4e9c71e4801c8b51e077cd91c0343eba74
yimengael <7570881+yimengael@users.noreply.github.com>
2023-04-06 17:14:09 -0700

    Update TestColumnDescriptor and TestSortedIndex

--- modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/engine/table/TestSortedIndex.java
+++ modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/engine/table/TestSortedIndex.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.sql.engine.table;
 
 import java.util.ArrayList;
@@ -21,6 +38,10 @@ import org.apache.ignite.internal.utils.PrimaryReplica;
 import org.apache.ignite.network.ClusterNode;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * TestSortedIndex.
+ * TODO Documentation
+ */
 public class TestSortedIndex implements SortedIndex {
     private final UUID id = UUID.randomUUID();
 
@@ -28,6 +49,10 @@ public class TestSortedIndex implements SortedIndex {
 
     private final SortedIndexDescriptor descriptor;
 
+    /**
+     * create method.
+     * TODO Documentation
+     */
     public static TestSortedIndex create(RelCollation collation, String name, IgniteTable table) {
         List<String> columns = new ArrayList<>();
         List<ColumnCollation> collations = new ArrayList<>();
@@ -46,6 +71,10 @@ public class TestSortedIndex implements SortedIndex {
         return new TestSortedIndex(descriptor);
     }
 
+    /**
+     * Constructor.
+     * TODO Documentation
+     */
     public TestSortedIndex(SortedIndexDescriptor descriptor) {
         this.descriptor = descriptor;
     }
