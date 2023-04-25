082e26cc1c4fece6272d66a66e5584b6867e2b3d
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-04-25 15:33:38 +0300

    wip SchemaSyncTest

--- modules/platforms/dotnet/Apache.Ignite.Tests/Table/SchemaSyncTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/SchemaSyncTest.cs
@@ -17,6 +17,7 @@
 
 namespace Apache.Ignite.Tests.Table;
 
+using System;
 using System.Threading.Tasks;
 using Ignite.Table;
 using NUnit.Framework;
@@ -28,24 +29,53 @@ public class SchemaSyncTest : IgniteTestsBase
 {
     private const string TempTableName = nameof(SchemaSyncTest);
 
+    private IIgniteClient _tempClient = null!;
+
     [SetUp]
     [TearDown]
     public async Task DropTempTable() => await Client.Sql.ExecuteAsync(null, $"DROP TABLE IF EXISTS {TempTableName}");
 
+    [SetUp]
+    public async Task ConnectTempClient() => _tempClient = await IgniteClient.StartAsync(GetConfig());
+
+    [TearDown]
+    public void CloseTempClient() => _tempClient.Dispose();
+
     [Test]
-    public async Task TestSchemaUpdateWithSql()
+    public async Task TestSchemaUpdateOnGet() =>
+        await TestSchemaUpdate(
+            async view =>
+            {
+                var (value, _) = await view.GetAsync(null, new IgniteTuple { ["id"] = 1 });
+                Assert.AreEqual("IgniteTuple { ID = 1, VAL = 1 }", value.ToString());
+            },
+            async view =>
+            {
+                var (value, _) = await view.GetAsync(null, new IgniteTuple { ["id"] = 1 });
+                Assert.AreEqual("IgniteTuple { ID = 1, VAL = 1, VAL2 = 2 }", value.ToString());
+            });
+
+    private async Task TestSchemaUpdate(
+        Func<IRecordView<IIgniteTuple>, Task> before,
+        Func<IRecordView<IIgniteTuple>, Task> after)
+    {
+        await CreateTempTable();
+        var table = await _tempClient.Tables.GetTableAsync(TempTableName);
+
+        await before(table!.RecordBinaryView);
+        await AddTempTableColumn();
+        await after(table.RecordBinaryView);
+    }
+
+    private async Task AddTempTableColumn()
+    {
+        await Client.Sql.ExecuteAsync(null, $"ALTER TABLE {TempTableName} ADD COLUMN val2 int");
+        await Client.Sql.ExecuteAsync(null, $"UPDATE {TempTableName} SET val2 = 2");
+    }
+
+    private async Task CreateTempTable()
     {
         await Client.Sql.ExecuteAsync(null, $"CREATE TABLE IF NOT EXISTS {TempTableName} (id int primary key, val int)");
         await Client.Sql.ExecuteAsync(null, $"INSERT INTO {TempTableName} (id, val) VALUES (1, 1)");
-
-        var table = await Client.Tables.GetTableAsync(TempTableName);
-        var (value, _) = await table!.RecordBinaryView.GetAsync(null, new IgniteTuple { ["id"] = 1 });
-        Assert.AreEqual("IgniteTuple { ID = 1, VAL = 1 }", value.ToString());
-
-        await Client.Sql.ExecuteAsync(null, $"ALTER TABLE {TempTableName} ADD COLUMN val2 int");
-        await Client.Sql.ExecuteAsync(null, $"UPDATE {TempTableName} SET val2 = 2");
-
-        var (value2, _) = await table.RecordBinaryView.GetAsync(null, new IgniteTuple { ["id"] = 1 });
-        Assert.AreEqual("IgniteTuple { ID = 1, VAL = 1, VAL2 = 2 }", value2.ToString());
     }
 }
