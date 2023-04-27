487b1abe7f9722287b98eac1d7d569f9f10a89af
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-04-27 16:43:44 +0300

    .NET: IStreamerTarget

---
+++ modules/platforms/dotnet/Apache.Ignite/Table/IStreamerTarget.cs
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Table;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Data streamer target.
+/// </summary>
+/// <typeparam name="T">Entry type.</typeparam>
+public interface IStreamerTarget<in T>
+{
+    /// <summary>
+    /// Streams data into the table.
+    /// </summary>
+    /// <param name="stream">Data stream.</param>
+    /// <param name="options">Options.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task StreamDataAsync(IAsyncEnumerable<T> stream, DataStreamerOptions? options = null);
+
+    /// <summary>
+    /// Streams data into the table with a receiver.
+    /// </summary>
+    /// <param name="stream">Data stream.</param>
+    /// <param name="keySelector">Key selector for partition awareness.</param>
+    /// <param name="receiverClassName">Java class name of the stream receiver.</param>
+    /// <param name="options">Options.</param>
+    /// <typeparam name="TItem">Item type.</typeparam>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task StreamDataAsync<TItem>(
+        IAsyncEnumerable<TItem> stream,
+        Func<TItem, T> keySelector,
+        string receiverClassName,
+        DataStreamerOptions? options = null);
+}
