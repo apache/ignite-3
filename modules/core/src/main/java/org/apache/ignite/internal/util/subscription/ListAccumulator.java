3ed333c53534cc4a50b8734b0dd047051066c6f7
Mikhail Pochatkin <Pochatkin@users.noreply.github.com>
2023-04-27 12:05:10 +0300

    Fix javadoc

--- modules/core/src/main/java/org/apache/ignite/internal/util/subscription/ListAccumulator.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/subscription/ListAccumulator.java
@@ -24,6 +24,7 @@ import java.util.function.Function;
 /**
  * Plain list accumulator.
  *
+ * @param <T> Stream elements value type.
  * @param <R> Result value type.
  */
 public class ListAccumulator<T, R> implements Accumulator<T, List<R>> {
