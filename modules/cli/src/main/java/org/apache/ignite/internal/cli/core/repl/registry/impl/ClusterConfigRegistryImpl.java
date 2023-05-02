63fc2ba5dcced13edb880d120e95487c52a50e0b
Aleksandr Pakhomov <apkhmv@gmail.com>
2023-05-02 18:49:56 +0400

    IGNITE-19316 Use LazyObjectRef in ClusterConfigRegistryImpl

--- modules/cli/src/main/java/org/apache/ignite/internal/cli/core/repl/registry/impl/ClusterConfigRegistryImpl.java
+++ modules/cli/src/main/java/org/apache/ignite/internal/cli/core/repl/registry/impl/ClusterConfigRegistryImpl.java
@@ -20,8 +20,6 @@ package org.apache.ignite.internal.cli.core.repl.registry.impl;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import jakarta.inject.Singleton;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicReference;
 import org.apache.ignite.internal.cli.call.configuration.ClusterConfigShowCall;
 import org.apache.ignite.internal.cli.call.configuration.ClusterConfigShowCallInput;
 import org.apache.ignite.internal.cli.core.repl.AsyncSessionEventListener;
@@ -34,7 +32,7 @@ public class ClusterConfigRegistryImpl implements ClusterConfigRegistry, AsyncSe
 
     private final ClusterConfigShowCall clusterConfigShowCall;
 
-    private final AtomicReference<Config> config = new AtomicReference<>(null);
+    private LazyObjectRef<Config> configRef;
 
     public ClusterConfigRegistryImpl(ClusterConfigShowCall clusterConfigShowCall) {
         this.clusterConfigShowCall = clusterConfigShowCall;
@@ -42,17 +40,13 @@ public class ClusterConfigRegistryImpl implements ClusterConfigRegistry, AsyncSe
 
     @Override
     public void onConnect(SessionInfo sessionInfo) {
-        CompletableFuture.runAsync(() -> {
-            try {
-                config.set(ConfigFactory.parseString(
+        configRef = new LazyObjectRef<>(
+                () -> ConfigFactory.parseString(
                         clusterConfigShowCall.execute(
                                 ClusterConfigShowCallInput.builder().clusterUrl(sessionInfo.nodeUrl()).build()
                         ).body().getValue()
-                ));
-            } catch (Exception ignored) {
-                // no-op
-            }
-        });
+                )
+        );
     }
 
     @Override
@@ -63,6 +57,10 @@ public class ClusterConfigRegistryImpl implements ClusterConfigRegistry, AsyncSe
     /** {@inheritDoc} */
     @Override
     public Config config() {
-        return config.get();
+        if (configRef == null) {
+            return null;
+        }
+
+        return configRef.get();
     }
 }
