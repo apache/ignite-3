758d97227377048ff381f8a4a423a9c3d72ed59c
Mirza Aliev <alievmirza@gmail.com>
2023-05-08 14:22:47 +0400

    IGNITE-18955 Add the ability to use filters when data nodes are calculated

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/distribution/zones/ItDistributionZonesFilterTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/distribution/zones/ItDistributionZonesFilterTest.java
@@ -46,15 +46,17 @@ import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 
 /**
- * Integration test for filters functionality.
+ * Integration test for data nodes' filters functionality.
  */
 public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest {
     @Language("JSON")
     private static final String NODE_ATTRIBUTES = "{region:{attribute:\"US\"},storage:{attribute:\"SSD\"}}";
 
-    protected static final String COLUMN_KEY = "key";
+    private static final String COLUMN_KEY = "key";
 
-    protected static final String COLUMN_VAL = "val";
+    private static final String COLUMN_VAL = "val";
+
+    private static final int TIMEOUT_MILLIS = 10_000;
 
     @Language("JSON")
     private static String createStartConfig(@Language("JSON") String nodeAttributes) {
@@ -81,8 +83,15 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
         return 1;
     }
 
+    /**
+     * Tests the scenario when filter was applied to data nodes and stable key for rebalance was changed to that filtered value.
+     *
+     * @throws Exception If failed.
+     */
     @Test
     void testFilteredDataNodesPropagatedToStable() throws Exception {
+        String filter = "'$[?(@.region == \"US\" && @.storage == \"SSD\")]'";
+
         // This node do not pass the filter
         @Language("JSON") String firstNodeAttributes = "{region:{attribute:\"EU\"},storage:{attribute:\"SSD\"}}";
 
@@ -93,7 +102,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
         session.execute(null, "CREATE ZONE \"TEST_ZONE\" WITH "
                 + "\"REPLICAS\" = 3, "
                 + "\"PARTITIONS\" = 2, "
-                + "\"DATA_NODES_FILTER\" = '$[?(@.storage == \"SSD\" && @.region == \"US\")]', "
+                + "\"DATA_NODES_FILTER\" = " + filter + ", "
                 + "\"DATA_NODES_AUTO_ADJUST_SCALE_UP\" = 0, "
                 + "\"DATA_NODES_AUTO_ADJUST_SCALE_DOWN\" = 0");
 
@@ -116,31 +125,34 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 stablePartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 null,
-                10_000
+                TIMEOUT_MILLIS
         );
 
         @Language("JSON") String secondNodeAttributes = "{region:{attribute:\"US\"},storage:{attribute:\"SSD\"}}";
 
+        // This node pass the filter
         startNode(2, createStartConfig(secondNodeAttributes));
 
         assertValueInStorage(
                 metaStorageManager,
                 zoneDataNodesKey(1),
-                (v) -> ((Map<String, Integer>) fromBytes(v)).size(),
+                (v) -> ((Map<NodeWithAttributes, Integer>) fromBytes(v)).size(),
                 3,
-                10_000
+                TIMEOUT_MILLIS
         );
 
         Entry dataNodesEntry = metaStorageManager.get(zoneDataNodesKey(1)).get(5_000, MILLISECONDS);
 
-        assertTrue(waitForCondition(() -> metaStorageManager.appliedRevision() >= dataNodesEntry.revision(), 10_000));
+        assertTrue(waitForCondition(() -> metaStorageManager.appliedRevision() >= dataNodesEntry.revision(), TIMEOUT_MILLIS));
+
+        // We check that two nodes that pass the filter are presented in the stable key.
 
         assertValueInStorage(
                 metaStorageManager,
                 stablePartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 2,
-                10_000
+                TIMEOUT_MILLIS
         );
 
         byte[] stableAssignments = metaStorageManager.get(stablePartAssignmentsKey(partId)).get(5_000, MILLISECONDS).value();
@@ -156,8 +168,17 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
         assertTrue(stable.contains(node(0).name()) && stable.contains(node(2).name()));
     }
 
+    /**
+     * Tests the scenario when removal node from the logical topology leads to empty data nodes, but this empty data nodes do not trigger
+     * rebalance. Data nodes become empty after applying corresponding filter.
+     *
+     * @throws Exception If failed.
+     */
     @Test
     void testFilteredEmptyDataNodesDoNotTriggerRebalance() throws Exception {
+        String filter = "'$[?(@.region == \"EU\" && @.storage == \"HDD\")]'";
+
+        // This node do not pass the filter.
         IgniteImpl node0 = node(0);
 
         // This node pass the filter
@@ -170,7 +191,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
         session.execute(null, "CREATE ZONE \"TEST_ZONE\" WITH "
                 + "\"REPLICAS\" = 1, "
                 + "\"PARTITIONS\" = 1, "
-                + "\"DATA_NODES_FILTER\" = '$[?(@.storage == \"HDD\" && @.region == \"EU\")]', "
+                + "\"DATA_NODES_FILTER\" = " + filter + ", "
                 + "\"DATA_NODES_AUTO_ADJUST_SCALE_UP\" = 0, "
                 + "\"DATA_NODES_AUTO_ADJUST_SCALE_DOWN\" = 0");
 
@@ -193,7 +214,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 stablePartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 null,
-                5_000
+                TIMEOUT_MILLIS
         );
 
         assertValueInStorage(
@@ -201,7 +222,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 zoneDataNodesKey(1),
                 (v) -> ((Map<NodeWithAttributes, Integer>) fromBytes(v)).size(),
                 2,
-                5_000
+                TIMEOUT_MILLIS
         );
 
         Entry dataNodesEntry = metaStorageManager.get(zoneDataNodesKey(1)).get(5_000, MILLISECONDS);
@@ -213,7 +234,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 stablePartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 null,
-                5_000
+                TIMEOUT_MILLIS
         );
 
         stopNode(1);
@@ -223,7 +244,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 zoneDataNodesKey(1),
                 (v) -> ((Map<NodeWithAttributes, Integer>) fromBytes(v)).size(),
                 1,
-                10_000
+                TIMEOUT_MILLIS
         );
 
         Entry newDataNodesEntry = metaStorageManager.get(zoneDataNodesKey(1)).get(5_000, MILLISECONDS);
@@ -235,7 +256,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 pendingPartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 null,
-                5_000
+                TIMEOUT_MILLIS
         );
 
         assertValueInStorage(
@@ -243,7 +264,7 @@ public class ItDistributionZonesFilterTest extends ClusterPerTestIntegrationTest
                 stablePartAssignmentsKey(partId),
                 (v) -> ((Set<Assignment>) fromBytes(v)).size(),
                 null,
-                5_000
+                TIMEOUT_MILLIS
         );
     }
 }
