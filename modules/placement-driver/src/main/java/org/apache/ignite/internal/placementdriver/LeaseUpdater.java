7a38dabeb37a5b7dbf909376c326fac5eb719cd3
denis-chudov <moonglloom@gmail.com>
2023-05-11 22:42:09 +0300

    test fix

--- modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/LeaseUpdater.java
+++ modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/LeaseUpdater.java
@@ -64,7 +64,7 @@ public class LeaseUpdater {
     private static final IgniteLogger LOG = Loggers.forClass(LeaseUpdater.class);
 
     /** Update attempts interval in milliseconds. */
-    private static final long UPDATE_LEASE_MS = 200L;
+    private static final long UPDATE_LEASE_MS = 500L;
 
     /** Lease holding interval. */
     public static final long LEASE_INTERVAL = 10 * UPDATE_LEASE_MS;
@@ -345,7 +345,7 @@ public class LeaseUpdater {
                     noop()
             ).thenAccept(isCreated -> {
                 if (isCreated) {
-                    boolean force = candidate.equals(lease.getLeaseholder());
+                    boolean force = candidate.name().equals(lease.getLeaseholder());
 
                     leaseNegotiator.negotiate(grpId, renewedLease, force);
                 }
