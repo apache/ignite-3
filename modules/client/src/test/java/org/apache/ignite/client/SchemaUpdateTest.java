5e51f4931c5414ab83d17fc3a78ce752f6bde618
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-05-16 11:36:30 +0300

    wip SchemaUpdateTest

--- modules/client/src/test/java/org/apache/ignite/client/SchemaUpdateTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/SchemaUpdateTest.java
@@ -18,9 +18,15 @@
 package org.apache.ignite.client;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.client.fakes.FakeIgnite;
+import org.apache.ignite.client.fakes.FakeIgniteTables;
 import org.apache.ignite.internal.client.ClientMetricSource;
 import org.apache.ignite.internal.client.TcpIgniteClient;
+import org.apache.ignite.table.RecordView;
+import org.apache.ignite.table.Table;
+import org.apache.ignite.table.Tuple;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +34,7 @@ import org.junit.jupiter.api.Test;
  * Tests schema update logic. See also {@link ClientTableTest#testGetReturningTupleWithUnknownSchemaRequestsNewSchema}.
  */
 public class SchemaUpdateTest {
+    private static final String DEFAULT_TABLE = "default_table";
     private TestServer server;
     private IgniteClient client;
 
@@ -44,10 +51,18 @@ public class SchemaUpdateTest {
 
     @Test
     public void testMultipleParallelOperationsRequestSchemaOnce() {
-        server = new TestServer(10800, 10, 1000, new FakeIgnite(), null, idx -> 100, "n", UUID.randomUUID(), null);
+        int responseDelay = 100;
+        server = new TestServer(10800, 10, 1000, ignite(), null, idx -> responseDelay, "n", UUID.randomUUID(), null);
         client = startClient();
+
+        RecordView<Tuple> view = client.tables().table(DEFAULT_TABLE).recordView();
+        CompletableFuture<Void> fut1 = view.upsertAsync(null, Tuple.create().set("id", 1));
+        CompletableFuture<Void> fut2 = view.upsertAsync(null, Tuple.create().set("id", 2));
+
+        CompletableFuture.allOf(fut1, fut2).join();
     }
 
+    @NotNull
     @Test
     public void testFailedSchemaLoadTaskIsRetried() {
         // TODO
@@ -62,5 +77,13 @@ public class SchemaUpdateTest {
 
     private ClientMetricSource metrics() {
         return ((TcpIgniteClient) client).metrics();
+    }
+
+    private static FakeIgnite ignite() {
+        FakeIgnite ignite = new FakeIgnite();
+
+        ((FakeIgniteTables) ignite.tables()).createTable(DEFAULT_TABLE);
+
+        return ignite;
     }
 }
