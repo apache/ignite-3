051c47b1a43dc7f856b5faf68c061e2edffd5e17
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-06-02 12:59:49 +0300

    IGNITE-19545 .NET: Add Basic Data Streamer

---
+++ modules/platforms/dotnet/Apache.Ignite/Table/DataStreamerOptions.cs
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Table;
+
+using System;
+
+/// <summary>
+/// Data streamer options.
+/// </summary>
+/// <param name="BatchSize">Batch size - the number of entries that will be sent to the cluster in one network call.</param>
+/// <param name="RetryLimit">Retry limit for a batch. If a batch fails to be sent to the cluster,
+/// the streamer will retry it a number of times.</param>
+/// <param name="PerNodeParallelOperations">Number of parallel operations per node
+/// (how many in-flight requests can be active for a given node).</param>
+/// <param name="AutoFlushFrequency">Auto flush frequency - the period of time after which the streamer
+/// will flush the per-node buffer even if it is not full.</param>
+public record DataStreamerOptions(int BatchSize, int RetryLimit, int PerNodeParallelOperations, TimeSpan AutoFlushFrequency);
---
+++ modules/platforms/dotnet/Apache.Ignite/Table/IDataStreamerTarget.cs
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// ReSharper disable TypeParameterCanBeVariant (justification: future compatibility for streamer with receiver).
+namespace Apache.Ignite.Table;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Represents an entity that can be used as a target for streaming data.
+/// </summary>
+/// <typeparam name="T">Data type.</typeparam>
+public interface IDataStreamerTarget<T>
+{
+    /// <summary>
+    /// Streams data into the underlying table.
+    /// </summary>
+    /// <param name="data">Data.</param>
+    /// <param name="options">Streamer options.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task StreamDataAsync(IAsyncEnumerable<T> data, DataStreamerOptions? options = null);
+}
