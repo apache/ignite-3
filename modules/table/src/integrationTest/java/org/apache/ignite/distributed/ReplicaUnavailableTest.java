7ce6cfc829bc31cac7004106fa441c445fbe651b
Vladislav Pyatkov <vldpyatkov@gmail.com>
2023-06-05 15:28:43 +0300

    IGNITE-19136 Handling timeout on waiting for replica readiness

--- modules/table/src/integrationTest/java/org/apache/ignite/distributed/ReplicaUnavailableTest.java
+++ modules/table/src/integrationTest/java/org/apache/ignite/distributed/ReplicaUnavailableTest.java
@@ -22,6 +22,8 @@ import static org.apache.ignite.distributed.ItTxDistributedTestSingleNode.NODE_P
 import static org.apache.ignite.distributed.ItTxDistributedTestSingleNode.startNode;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureExceptionMatcher.willThrow;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willSucceedIn;
+import static org.apache.ignite.internal.util.ExceptionUtils.unwrapCause;
+import static org.apache.ignite.lang.ErrorGroups.Replicator.REPLICA_TIMEOUT_ERR;
 import static org.apache.ignite.raft.jraft.test.TestUtils.getLocalAddress;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,7 +45,7 @@ import org.apache.ignite.internal.replicator.ReplicaManager;
 import org.apache.ignite.internal.replicator.ReplicaService;
 import org.apache.ignite.internal.replicator.TablePartitionId;
 import org.apache.ignite.internal.replicator.exception.ReplicaStoppingException;
-import org.apache.ignite.internal.replicator.exception.ReplicaUnavailableException;
+import org.apache.ignite.internal.replicator.exception.ReplicationException;
 import org.apache.ignite.internal.replicator.message.ReplicaMessageGroup;
 import org.apache.ignite.internal.replicator.message.ReplicaMessagesFactory;
 import org.apache.ignite.internal.replicator.message.ReplicaResponse;
@@ -184,7 +186,7 @@ public class ReplicaUnavailableTest extends IgniteAbstractTest {
                 .transactionId(TestTransactionIds.newTransactionId())
                 .commitPartitionId(tablePartitionId)
                 .timestampLong(clock.nowLong())
-                .binaryRow(createKeyValueRow(1L, 1L))
+                .binaryRowBytes(createKeyValueRow(1L, 1L))
                 .requestType(RequestType.RW_GET)
                 .build();
 
@@ -236,10 +238,56 @@ public class ReplicaUnavailableTest extends IgniteAbstractTest {
         }
 
         assertTrue(e0 != null);
-        assertTrue(e0.getCause() instanceof ReplicaUnavailableException, e0.toString());
+        assertTrue(unwrapCause(e0) instanceof ReplicationException, e0.toString());
 
         assertTrue(e1 != null);
-        assertTrue(e1.getCause() instanceof ReplicaUnavailableException, e1.toString());
+        assertTrue(unwrapCause(e1) instanceof ReplicationException, e1.toString());
+    }
+
+    @Test
+    public void testWithNotReadyReplica() {
+        ClusterNode clusterNode = clusterService.topologyService().localMember();
+
+        TablePartitionId tablePartitionId = new TablePartitionId(1, 1);
+
+        clusterService.messagingService().addMessageHandler(ReplicaMessageGroup.class,
+                (message, sender, correlationId) -> {
+                    try {
+                        log.info("Replica msg " + message.getClass().getSimpleName());
+
+                        replicaManager.startReplica(
+                                tablePartitionId,
+                                new CompletableFuture<>(),
+                                request0 -> completedFuture(replicaMessageFactory.replicaResponse()
+                                        .result(Integer.valueOf(5))
+                                        .build()),
+                                mock(TopologyAwareRaftGroupService.class),
+                                new PendingComparableValuesTracker<>(0L)
+                        );
+                    } catch (NodeStoppingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+
+        ReadWriteSingleRowReplicaRequest request = tableMessagesFactory.readWriteSingleRowReplicaRequest()
+                .groupId(tablePartitionId)
+                .timestampLong(clock.nowLong())
+                .binaryRowBytes(createKeyValueRow(1L, 1L))
+                .requestType(RequestType.RW_GET)
+                .build();
+
+        Exception e0 = null;
+
+        try {
+            replicaService.invoke(clusterNode, request).get(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e0 = e;
+        }
+
+        assertTrue(e0 != null);
+        assertTrue(unwrapCause(e0) instanceof ReplicationException, e0.toString());
+        assertEquals(REPLICA_TIMEOUT_ERR, ((ReplicationException) unwrapCause(e0)).code());
     }
 
     private static ByteBuffer createKeyValueRow(long id, long value) {
