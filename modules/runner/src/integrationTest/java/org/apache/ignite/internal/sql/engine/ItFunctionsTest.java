decabe677e5f6e2ba7f71b232a156d1e99b55276
zstan <stanilovsky@gmail.com>
2023-06-08 15:56:05 +0300

    forgotten comment

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItFunctionsTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItFunctionsTest.java
@@ -300,7 +300,7 @@ public class ItFunctionsTest extends ClusterPerClassIntegrationTest {
         assertQuery("SELECT SUBSTRING('test' FROM null FOR 2)").returns(null).check();
 
         // uncomment after https://issues.apache.org/jira/browse/IGNITE-19686 was implemented.
-        assertQuery("select SUBSTRING(s from i for l) from (values ('abc', null, 2)) as t (s, i, l);").returns(null).check();
+        //assertQuery("select SUBSTRING(s from i for l) from (values ('abc', null, 2)) as t (s, i, l);").returns(null).check();
 
         assertThrowsWithCause(() -> sql("SELECT SUBSTRING('abcdefg', 1, -3)"), IgniteException.class, "negative substring length");
     }
