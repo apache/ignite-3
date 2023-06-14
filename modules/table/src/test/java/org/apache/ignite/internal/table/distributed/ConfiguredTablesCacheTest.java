697b62e58b1fed132e8e9c2eb857593bebc5f49b
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2023-06-14 19:54:52 +0400

    IGNITE-19732 / fix checkstyle violations

--- modules/table/src/test/java/org/apache/ignite/internal/table/distributed/ConfiguredTablesCacheTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/distributed/ConfiguredTablesCacheTest.java
@@ -1,4 +1,4 @@
-package org.apache.ignite.internal.table.distributed;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,6 +14,8 @@ package org.apache.ignite.internal.table.distributed;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.ignite.internal.table.distributed;
 
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willCompleteSuccessfully;
 import static org.hamcrest.MatcherAssert.assertThat;
