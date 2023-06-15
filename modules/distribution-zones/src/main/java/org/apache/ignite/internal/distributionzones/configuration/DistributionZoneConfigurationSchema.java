f91a6429513629d2906e88a14b9bb35e2dc64172
Mirza Aliev <alievmirza@gmail.com>
2023-06-15 12:30:13 +0400

    IGNITE-19184 Added javadocs and md file to describe node attributes and distribution zone filters (#2188)

--- modules/distribution-zones/src/main/java/org/apache/ignite/internal/distributionzones/configuration/DistributionZoneConfigurationSchema.java
+++ modules/distribution-zones/src/main/java/org/apache/ignite/internal/distributionzones/configuration/DistributionZoneConfigurationSchema.java
@@ -80,6 +80,8 @@ public class DistributionZoneConfigurationSchema {
     /**
      * Filter to form nodes which must be included to data nodes of this zone.
      * Default value is {@code $..*}, which is a {@link com.jayway.jsonpath.JsonPath} expression for including all attributes of nodes.
+     *
+     * @see <a href="https://github.com/apache/ignite-3/blob/main/modules/distribution-zones/tech-notes/filters.md">Filter documentation</a>
      */
     @ValidFilter
     @Value(hasDefault = true)
