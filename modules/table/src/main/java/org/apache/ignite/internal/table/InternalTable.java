a39851a0e7138fde497460619ce2b589b198a28b
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-06-19 16:12:58 +0300

    javadoc cleanup

--- modules/table/src/main/java/org/apache/ignite/internal/table/InternalTable.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/InternalTable.java
@@ -133,7 +133,7 @@ public interface InternalTable extends ManuallyCloseable {
     CompletableFuture<Void> upsert(BinaryRowEx row, @Nullable InternalTransaction tx);
 
     /**
-     * Asynchronously inserts a row into the table if does not exist or replaces the existed one.
+     * Asynchronously inserts records into a table, if they do not exist, or replaces the existing ones.
      *
      * @param rows Rows to insert into the table.
      * @param tx   The transaction.
@@ -141,6 +141,13 @@ public interface InternalTable extends ManuallyCloseable {
      */
     CompletableFuture<Void> upsertAll(Collection<BinaryRowEx> rows, @Nullable InternalTransaction tx);
 
+    /**
+     * Asynchronously inserts records into a table, if they do not exist, or replaces the existing ones.
+     *
+     * @param rows Rows to insert into the table.
+     * @param partition Partition that the rows belong to.
+     * @return Future representing pending completion of the operation.
+     */
     CompletableFuture<Void> upsertAll(Collection<BinaryRowEx> rows, int partition);
 
     /**
