28a41bd344481eeea1cc6ad8c6e0e0dcf32582dc
ibessonov <bessonov.ip@gmail.com>
2023-06-28 13:33:33 +0300

    IGNITE-19828 Javadoc updated
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/metastorage-api/src/main/java/org/apache/ignite/internal/metastorage/RevisionUpdateListener.java
+++ modules/metastorage-api/src/main/java/org/apache/ignite/internal/metastorage/RevisionUpdateListener.java
@@ -22,8 +22,8 @@ import java.util.concurrent.CompletableFuture;
 /**
  * The listener which receives and handles the Meta Storage revision update. No listeners for revision {@code N+1} will be invoked until
  * all listeners for revision {@code N} are completed.
- * Also, this listeners is only triggered strictly after all {@link WatchListener#onUpdate(WatchEvent)} are executed, but not necessarily
- * completed, for the specified revision.
+ * Also, this listener is only triggered strictly after all {@link WatchListener#onUpdate(WatchEvent)} are invoked (but the returned futures
+ * might not still be completed completed), for the specified revision.
  */
 @FunctionalInterface
 public interface RevisionUpdateListener {
