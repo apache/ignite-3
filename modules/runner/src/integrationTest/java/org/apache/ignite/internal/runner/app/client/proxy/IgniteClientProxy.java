21d88c0e8e5ac417c0d88e7bdc1b36f6165ad69d
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-06-30 11:27:39 +0300

    wip IgniteClientProxy

---
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/client/proxy/IgniteClientProxy.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.runner.app.client.proxy;
+
+public final class IgniteClientProxy implements AutoCloseable {
+    private final EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    private final EventLoopGroup workerGroup = new NioEventLoopGroup();
+    private final int targetPort;
+
+    private IgniteClientProxy(int targetPort) {
+        this.targetPort = targetPort;
+    }
+
+    private void start() {
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new IgniteClientProxyInitializer(targetPort))
+                .childOption(ChannelOption.AUTO_READ, false)
+                .bind(LOCAL_PORT).sync().channel().closeFuture().sync();
+    }
+
+    public static AutoCloseable start(int serverPort) throws Exception {
+        IgniteClientProxy proxy = new IgniteClientProxy(serverPort);
+        proxy.start();
+
+        return proxy;
+    }
+
+    @Override
+    public void close() throws Exception {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+}
