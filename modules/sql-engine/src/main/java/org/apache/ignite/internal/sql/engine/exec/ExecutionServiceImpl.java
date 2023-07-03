b09780cb089b6a38127a0551b516493a50beb1e0
zstan <stanilovsky@gmail.com>
2023-07-03 10:38:00 +0300

    fix

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/ExecutionServiceImpl.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/ExecutionServiceImpl.java
@@ -841,7 +841,7 @@ public class ExecutionServiceImpl<RowT> implements ExecutionService, TopologyEve
                             if (e != null) {
                                 Throwable ex = unwrapCause(e);
 
-                                LOG.warn(format("Fragment closing processed with errors, root fragmentId={}", rootFragmentId), ex);
+                                LOG.warn("Fragment closing processed with errors, root [queryId={}]", ex, ctx.queryId());
                             }
 
                             queryManagerMap.remove(ctx.queryId());
