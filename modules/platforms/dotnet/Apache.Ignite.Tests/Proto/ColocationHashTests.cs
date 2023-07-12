e638bb6a3bd427a64d9802a829aaf2677a4db2ac
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-07-12 12:51:05 +0300

    tests cleanup

--- modules/platforms/dotnet/Apache.Ignite.Tests/Proto/ColocationHashTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Proto/ColocationHashTests.cs
@@ -169,7 +169,6 @@ public class ColocationHashTests : IgniteTestsBase
     [Test]
     public async Task TestCustomColocationColumnOrder([Values(true, false)] bool reverseColocationOrder)
     {
-        // TODO: Stop this work and rework hashing first - see discussion in #ignite-3
         var tableName = $"{nameof(TestCustomColocationColumnOrder)}_{reverseColocationOrder}";
         var sql = $"create table if not exists {tableName} " +
                   $"(id integer, id0 bigint, id1 varchar, v INTEGER, primary key(id, id0, id1)) " +
@@ -201,7 +200,7 @@ public class ColocationHashTests : IgniteTestsBase
                 tableName,
                 i);
 
-            Assert.AreEqual(serverColocationHash, clientColocationHash);
+            Assert.AreEqual(serverColocationHash, clientColocationHash, key.ToString());
         }
     }
 
