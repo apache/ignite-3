d5df07bfebe67d0ac07372539c585c1f8d9f0388
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2023-07-25 19:20:00 +0300

    IGNITE-15927 Fix formatting 2

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/replication/request/ReadWriteMultiRowReplicaRequest.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/replication/request/ReadWriteMultiRowReplicaRequest.java
@@ -36,7 +36,7 @@ public interface ReadWriteMultiRowReplicaRequest extends MultipleRowReplicaReque
     TablePartitionId commitPartitionId();
 
     /**
-     * @return {@code True} if this is a full transaction.
+     * Return {@code true} if this is a full transaction.
      */
     boolean full();
 }
