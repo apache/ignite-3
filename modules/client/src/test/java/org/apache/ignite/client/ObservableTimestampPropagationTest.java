3bd68f17369ad5f19b59ea5a6ed57bfb7849f6c3
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-07-27 16:09:05 +0300

    wip ObservableTimestampPropagationTest

--- modules/client/src/test/java/org/apache/ignite/client/ObservableTimestampPropagationTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/ObservableTimestampPropagationTest.java
@@ -1,6 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.client;
 
-import io.netty.util.ResourceLeakDetector;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.client.fakes.FakeIgnite;
 import org.apache.ignite.internal.TestHybridClock;
@@ -9,42 +27,39 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class ObservableTimestampPropagationTest extends AbstractClientTest {
-    private static TestServer testServer2;
+/**
+ * Tests that observable timestamp (causality token) is propagated from server to client and back.
+ */
+public class ObservableTimestampPropagationTest {
+    private static TestServer testServer;
 
-    private static Ignite server2;
+    private static Ignite ignite;
 
-    private static IgniteClient client2;
+    private static IgniteClient client;
 
-    /**
-     * Before all.
-     */
+    private static final AtomicLong observableTimestamp = new AtomicLong(1);
+
     @BeforeAll
     public static void startServer2() {
-        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+        TestHybridClock clock = new TestHybridClock(observableTimestamp::get);
 
-        TestHybridClock clock = new TestHybridClock(() -> 1000L);
+        ignite = new FakeIgnite("server-2");
+        testServer = new TestServer(0, ignite, unused -> false, null, "server-2", UUID.randomUUID(), null, null, clock);
 
-        server2 = new FakeIgnite("server-2");
-        testServer2 = new TestServer(0, server2, null, null, "server-2", clusterId, null, serverPort + 1, clock);
-
-        var clientBuilder = IgniteClient.builder()
-                .addresses("127.0.0.1:" + serverPort, "127.0.0.1:" + testServer2.port())
-                .heartbeatInterval(200);
-
-        client2 = clientBuilder.build();
+        client = IgniteClient.builder().addresses("127.0.0.1:" + testServer.port()).build();
     }
 
-    /**
-     * After all.
-     */
     @AfterAll
     public static void stopServer2() throws Exception {
-        IgniteUtils.closeAll(client2, testServer2, server2);
+        IgniteUtils.closeAll(client, testServer, ignite);
     }
 
     @Test
     public void testClientPropagatesLatestKnownHybridTimestamp() {
-        // TODO
+        // Perform request.
+        client.tables().tables();
+
+        // Verify current timestamp is propagated by performing TX_BEGIN and checking ts value received by server.
+        client.transactions().begin();
     }
 }
