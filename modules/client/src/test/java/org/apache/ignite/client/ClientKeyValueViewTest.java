dbb9d1a8e73836f5a43d48e93af2afc150eed427
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-08-03 12:27:02 +0300

    Fix ClientKeyValueViewTest

--- modules/client/src/test/java/org/apache/ignite/client/ClientKeyValueViewTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/ClientKeyValueViewTest.java
@@ -100,7 +100,10 @@ public class ClientKeyValueViewTest extends AbstractClientTableTest {
         key.gid = 1;
 
         IgniteException e = assertThrows(IgniteException.class, () -> pojoView.get(null, key));
-        assertEquals("Failed to deserialize server response: Fields [gid, id] are not mapped to columns.", e.getMessage());
+        assertEquals(
+                "Failed to deserialize server response: Fields [gid, id] of type "
+                        + "org.apache.ignite.client.AbstractClientTableTest$IncompletePojo are not mapped to columns.",
+                e.getMessage());
     }
 
     @Test
