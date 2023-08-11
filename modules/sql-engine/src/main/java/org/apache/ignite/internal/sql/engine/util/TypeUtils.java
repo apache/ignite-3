e4ede07974ae61bb429f074970fa88b53f31326b
Max Zhuravkov <shhwwa@gmail.com>
2023-08-11 09:06:57 +0300

    IGNITE-19893: Sql. Add plan checks to BaseIndexDataTypeTest.
    Add comments that clarifies checks for binary/char types.

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/TypeUtils.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/TypeUtils.java
@@ -529,9 +529,10 @@ public class TypeUtils {
 
     /** Checks whether cast operation is necessary in {@code SearchBound}. */
     public static boolean needCastInSearchBounds(IgniteTypeFactory typeFactory, RelDataType fromType, RelDataType toType) {
-        // Checks for both chars and binary type allow comparison
-        // between types with precision, types w/o precision, and
-        // varying non-varying length variants.
+        // Checks for character and binary types should allow comparison
+        // between types with precision, types w/o precision, and varying non-varying length variants.
+        // Otherwise the optimizer wouldn't pick an index for conditions such as
+        // col (VARCHAR(M)) = CAST(s AS VARCHAR(N) (M != N) , or col (VARCHAR) = CAST(s AS VARCHAR(N)).
 
         // No need to cast between char and varchar.
         if (SqlTypeUtil.isCharacter(toType) && SqlTypeUtil.isCharacter(fromType)) {
