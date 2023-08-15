c70c507b4b3115e1de3d5a42da63f9cd4f8d799b
Dmitry Baranov <dbaranov@unison.team>
2023-08-15 14:17:25 +0300

    IGNITE-20171 add more unit tests

--- modules/cli/src/test/java/org/apache/ignite/internal/cli/core/exception/handler/IgniteCliApiExceptionHandlerTest.java
+++ modules/cli/src/test/java/org/apache/ignite/internal/cli/core/exception/handler/IgniteCliApiExceptionHandlerTest.java
@@ -22,6 +22,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import io.micronaut.http.HttpStatus;
+import java.util.Collections;
 import org.apache.ignite.internal.cli.core.exception.ExceptionWriter;
 import org.apache.ignite.internal.cli.core.exception.IgniteCliApiException;
 import org.apache.ignite.rest.client.invoker.ApiException;
@@ -29,19 +31,56 @@ import org.junit.jupiter.api.Test;
 
 class IgniteCliApiExceptionHandlerTest {
 
+    private static final String NODE_URL = "nodeUrl";
+
+    private final IgniteCliApiExceptionHandler igniteCliApiExceptionHandler = new IgniteCliApiExceptionHandler();
+
     @Test
     void handleExceptionWithoutProblemJson() {
         // Given
-        IgniteCliApiExceptionHandler igniteCliApiExceptionHandler = new IgniteCliApiExceptionHandler();
-
         ExceptionWriter exceptionWriter = mock(ExceptionWriter.class);
         IgniteCliApiException igniteCliApiException =
-                new IgniteCliApiException(new ApiException("Missing the required parameter"), "nodeUrl");
+                new IgniteCliApiException(new ApiException("Missing the required parameter"), NODE_URL);
 
         // When
         igniteCliApiExceptionHandler.handle(exceptionWriter, igniteCliApiException);
 
         // Then
         verify(exceptionWriter, times(1)).write(eq("Missing the required parameter"));
+    }
+
+    @Test
+    void handleExceptionWithProblem() {
+        // Given
+        ExceptionWriter exceptionWriter = mock(ExceptionWriter.class);
+        String problemJson = "{\"title\":\"Bad Request\",\"status\":400,\"code\":\"IGN-ERR\",\"detail\":"
+                + "\"User value 'test_name' has not been found\",\"traceId\":\"567c9a5c-4820-46c5-9994-ff3fd6339efe\"}";
+        IgniteCliApiException igniteCliApiException =
+                new IgniteCliApiException(
+                        new ApiException(HttpStatus.BAD_REQUEST.getCode(), Collections.emptyMap(), problemJson), NODE_URL);
+
+        // When
+        igniteCliApiExceptionHandler.handle(exceptionWriter, igniteCliApiException);
+
+        // Then
+        verify(exceptionWriter, times(1)).write(
+                eq("IGN-ERR Trace ID: 567c9a5c-4820-46c5-9994-ff3fd6339efe" + System.lineSeparator()
+                + "User value 'test_name' has not been found"));
+    }
+
+    @Test
+    void handleExceptionWith404() {
+        // Given
+        ExceptionWriter exceptionWriter = mock(ExceptionWriter.class);
+        IgniteCliApiException igniteCliApiException =
+                new IgniteCliApiException(new ApiException(HttpStatus.UNAUTHORIZED.getCode(), "UNAUTHORIZED"), NODE_URL);
+
+        // When
+        igniteCliApiExceptionHandler.handle(exceptionWriter, igniteCliApiException);
+
+        // Then
+        verify(exceptionWriter, times(1)).write(eq("Authentication error" + System.lineSeparator()
+                + String.format("Could not connect to node with URL %s. ", NODE_URL)
+                + "Check authentication configuration or provided username/password"));
     }
 }
