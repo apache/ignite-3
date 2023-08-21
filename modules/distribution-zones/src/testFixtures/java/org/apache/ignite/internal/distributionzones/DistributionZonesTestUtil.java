5794d12a9f23c6812b93677ca4c75a35aaade88b
Kirill Tkalenko <tkalkirill@yandex.ru>
2023-08-21 10:45:35 +0300

    IGNITE-20249 wip

--- modules/distribution-zones/src/testFixtures/java/org/apache/ignite/internal/distributionzones/DistributionZonesTestUtil.java
+++ modules/distribution-zones/src/testFixtures/java/org/apache/ignite/internal/distributionzones/DistributionZonesTestUtil.java
@@ -90,7 +90,7 @@ public class DistributionZonesTestUtil {
             String zoneName,
             int partitions,
             int replicas,
-            Consumer<DataStorageChange> dataStorageChangeConsumer
+            @Nullable Consumer<DataStorageChange> dataStorageChangeConsumer
     ) {
         var distributionZoneCfgBuilder = new Builder(zoneName)
                 .replicas(replicas)
