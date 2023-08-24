d7111b95e2fc96d4f0fc6b37d2e6980469b330da
sanpwc <lapin1702@gmail.com>
2023-08-24 18:53:22 +0300

    IGNITE-18856 TestPlacementDriver now returns test unbounded replica meta instead of null

--- modules/placement-driver-api/src/testFixtures/java/org/apache/ignite/internal/placementdriver/TestPlacementDriver.java
+++ modules/placement-driver-api/src/testFixtures/java/org/apache/ignite/internal/placementdriver/TestPlacementDriver.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.placementdriver;
 
 import java.util.concurrent.CompletableFuture;
@@ -8,24 +25,24 @@ import org.jetbrains.annotations.TestOnly;
 @TestOnly
 public class TestPlacementDriver implements PlacementDriver {
 
-    private final String leaseholder;
+    private final TestReplicaMetaImpl primaryReplica;
 
     // TODO: sanpwc remove.
     public TestPlacementDriver() {
-        this.leaseholder = null;
+        this.primaryReplica = null;
     }
 
     public TestPlacementDriver(String leaseholder) {
-        this.leaseholder = leaseholder;
+        this.primaryReplica = new TestReplicaMetaImpl(leaseholder);
     }
 
     @Override
     public CompletableFuture<ReplicaMeta> awaitPrimaryReplica(ReplicationGroupId groupId, HybridTimestamp timestamp) {
-        return null;
+        return CompletableFuture.completedFuture(primaryReplica);
     }
 
     @Override
     public CompletableFuture<ReplicaMeta> getPrimaryReplica(ReplicationGroupId replicationGroupId, HybridTimestamp timestamp) {
-        return null;
+        return CompletableFuture.completedFuture(primaryReplica);
     }
 }
