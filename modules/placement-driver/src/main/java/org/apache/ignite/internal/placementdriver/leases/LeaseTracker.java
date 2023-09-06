5363044d2cb27c8f0e636fd910d0c9eb849a39a9
sanpwc <lapin1702@gmail.com>
2023-09-06 17:38:39 +0300

    ignite-18856 Ivestigation rollback

--- modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/leases/LeaseTracker.java
+++ modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/leases/LeaseTracker.java
@@ -216,7 +216,7 @@ public class LeaseTracker implements PlacementDriver {
 
             Lease lease = leasesMap.getOrDefault(replicationGroupId, EMPTY_LEASE);
 
-            if (lease.getExpirationTime().after(timestamp) && lease.isAccepted()) {
+            if (lease.getExpirationTime().after(timestamp)) {
                 return completedFuture(lease);
             }
 
@@ -226,7 +226,7 @@ public class LeaseTracker implements PlacementDriver {
                     .thenApply(ignored -> inBusyLock(busyLock, () -> {
                         Lease lease0 = leasesMap.getOrDefault(replicationGroupId, EMPTY_LEASE);
 
-                        if (lease0.getExpirationTime().after(timestamp) && lease.isAccepted()) {
+                        if (lease0.getExpirationTime().after(timestamp)) {
                             return lease0;
                         } else {
                             return null;
