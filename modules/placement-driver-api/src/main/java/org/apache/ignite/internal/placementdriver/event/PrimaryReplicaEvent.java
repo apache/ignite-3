aaa795fcc49146924e1b76f1c536bb1a5521e6d3
Kirill Tkalenko <tkalkirill@yandex.ru>
2023-09-15 11:03:24 +0300

    IGNITE-20422 wip

---
+++ modules/placement-driver-api/src/main/java/org/apache/ignite/internal/placementdriver/event/PrimaryReplicaEvent.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.placementdriver.event;
+
+import org.apache.ignite.internal.manager.Event;
+
+/** Primary replica management events. */
+public enum PrimaryReplicaEvent implements Event {
+    /** This event is fired when the replica becomes primary. */
+    BECOME_PRIMARY,
+
+    /** This event is fired when the replica ceases to be primary. */
+    STOP_BEING_PRIMARY;
+}
