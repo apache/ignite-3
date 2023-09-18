88a3efe1d0961d174bff4143d677cbab0bb213bc
Igor Sapego <isapego@apache.org>
2023-09-19 02:00:29 +0400

    IGNITE-19216 Ported type_info_query

---
+++ modules/platforms/cpp/ignite/odbc/query/type_info_query.h
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "ignite/odbc/query/query.h"
+
+namespace ignite
+{
+
+/**
+ * Type info query.
+ */
+class type_info_query : public query {
+public:
+    /**
+     * Constructor.
+     *
+     * @param diag Diagnostics collector.
+     * @param sql_type SQL type.
+     */
+    type_info_query(diagnosable_adapter &diag, std::int16_t sql_type);
+
+    /**
+     * Destructor.
+     */
+    virtual ~type_info_query() = default;
+
+    /**
+     * Execute query.
+     *
+     * @return True on success.
+     */
+    virtual sql_result execute();
+
+    /**
+     * Get column metadata.
+     *
+     * @return Column metadata.
+     */
+    virtual const column_meta_vector* get_meta();
+
+    /**
+     * Fetch next result row to application buffers.
+     *
+     * @return Operation result.
+     */
+    virtual sql_result fetch_next_row(column_binding_map& column_bindings);
+
+    /**
+     * Get data of the specified column in the result set.
+     *
+     * @param column_idx Column index.
+     * @param buffer Buffer to put column data to.
+     * @return Operation result.
+     */
+    virtual sql_result get_column(std::uint16_t column_idx, application_data_buffer& buffer);
+
+    /**
+     * Close query.
+     *
+     * @return True on success.
+     */
+    virtual sql_result close();
+
+    /**
+     * Check if data is available.
+     *
+     * @return True if data is available.
+     */
+    virtual bool is_data_available() const;
+
+    /**
+     * Get number of rows affected by the statement.
+     *
+     * @return Number of rows affected by the statement.
+     */
+    virtual std::int64_t affected_rows() const;
+
+    /**
+     * Move to the next result set.
+     *
+     * @return Operation result.
+     */
+    virtual sql_result next_result_set();
+
+private:
+    /** Columns metadata. */
+    column_meta_vector m_columns_meta;
+
+    /** Executed flag. */
+    bool m_executed = false;
+
+    /** Fetched flag. */
+    bool m_fetched = false;
+
+    /** Requested types. */
+    std::vector<ignite_type> m_types;
+
+    /** Query cursor. */
+    std::vector<ignite_type>::const_iterator m_cursor = m_types.end();
+};
+
+}
