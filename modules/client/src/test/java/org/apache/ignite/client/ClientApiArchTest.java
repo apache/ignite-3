1c1bedc2ca9648899d6cba49a9a3edbeb611b3b9
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-10-03 10:45:31 +0300

    ClientApiArchTest done

--- modules/client/src/test/java/org/apache/ignite/client/ClientApiArchTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/ClientApiArchTest.java
@@ -19,11 +19,12 @@ package org.apache.ignite.client;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -32,17 +33,61 @@ import org.junit.jupiter.api.Test;
  */
 public class ClientApiArchTest {
     @Test
-    public void testClientApiImportsArePublic() {
-        assert false : "TODO";
+    public void testClientApiImportsArePublic() throws IOException {
+        var code = Files.readAllLines(getSourceFilePath("IgniteClient.java"));
+
+        assertPublicImports(
+                code,
+                "internal.client.IgniteClientConfigurationImpl;",
+                "internal.client.TcpIgniteClient;",
+                "internal.client.ClientUtils.sync");
     }
 
     @Test
     public void testClientConfigurationImportsArePublic() throws IOException {
-        // Read IgniteClientConfiguration.java and check that all imports are public.
-        // Get repo root directory
-        var rootDir = getRepoRoot();
-        var clientConfigPath = Path.of(
-                rootDir,
+        var code = Files.readAllLines(getSourceFilePath("IgniteClientConfiguration.java"));
+
+        assertPublicImports(code);
+    }
+
+    private static void assertPublicImports(List<String> code, String... excludes) {
+        assertThat("Code is too short", code.size(), greaterThan(50));
+
+        int importCount = 0;
+
+        for (var line : code) {
+            if (line.startsWith("import ")) {
+                importCount++;
+                boolean excluded = false;
+
+                for (var exclude : excludes) {
+                    if (line.contains(exclude)) {
+                        excluded = true;
+                        break;
+                    }
+                }
+
+                if (excluded) {
+                    continue;
+                }
+
+                if (line.contains(".internal.") || line.contains(".impl.")) {
+                    Assertions.fail("Import is not public: " + line);
+                }
+
+                if (line.contains("*")) {
+                    Assertions.fail("Wildcard import is not allowed: " + line);
+                }
+            }
+        }
+
+        assertThat("Imports not found", importCount, greaterThan(0));
+    }
+
+    @NotNull
+    private static Path getSourceFilePath(String file) {
+        return Path.of(
+                getRepoRoot(),
                 "modules",
                 "client",
                 "src",
@@ -52,18 +97,7 @@ public class ClientApiArchTest {
                 "apache",
                 "ignite",
                 "client",
-                "IgniteClientConfiguration.java");
-
-        var code = Files.readAllLines(clientConfigPath);
-        assertThat(code.size(), greaterThan(50));
-
-        for (var line : code) {
-            if (line.startsWith("import ")) {
-                if (line.contains(".internal.") || line.contains(".impl.")) {
-                    Assertions.fail("Import is not public: " + line);
-                }
-            }
-        }
+                file);
     }
 
     private static String getRepoRoot() {
