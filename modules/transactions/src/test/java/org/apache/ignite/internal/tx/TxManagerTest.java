94def6885022afd6c818b252c28bd61f3c363635
sanpwc <lapin1702@gmail.com>
2023-10-11 16:17:11 +0300

    IGNITE-20457 Checkstyle

--- modules/transactions/src/test/java/org/apache/ignite/internal/tx/TxManagerTest.java
+++ modules/transactions/src/test/java/org/apache/ignite/internal/tx/TxManagerTest.java
@@ -401,7 +401,7 @@ public class TxManagerTest extends IgniteAbstractTest {
         assertSame(TransactionException.class, throwable.getClass());
         // short cast is useful for better error code readability
         //noinspection NumericCastThatLosesPrecision
-        assertEquals((short)TX_PRIMARY_REPLICA_EXPIRED_ERR, (short) ((TransactionException)throwable).code());
+        assertEquals((short) TX_PRIMARY_REPLICA_EXPIRED_ERR, (short) ((TransactionException) throwable).code());
 
         assertEquals(TxState.ABORTED, txManager.stateMeta(committedTransaction.id()).txState());
     }
