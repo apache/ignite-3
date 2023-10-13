90d813c8e01b58395a7c49c4845b86c7fb66f024
Max Zhuravkov <shhwwa@gmail.com>
2023-10-13 13:58:55 +0300

    IGNITE-20444 Sql. Add restrictions for execution tx related statements with single statement mode.
    Check style.

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItMultistatementTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItMultistatementTest.java
@@ -17,7 +17,6 @@
 
 package org.apache.ignite.internal.sql.engine;
 
-import static org.apache.ignite.internal.sql.engine.util.SqlTestUtils.assertThrowsSqlException;
 import static org.apache.ignite.lang.ErrorGroups.Sql.STMT_VALIDATION_ERR;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
