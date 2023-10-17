257cc8ae28a80050847dc2a0612e9dfd4662fd82
Vladislav Pyatkov <vldpyatkov@gmail.com>
2023-10-17 09:19:01 +0300

    Code style

--- modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseUpdaterTest.java
+++ modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseUpdaterTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.placementdriver;
 
 import static java.util.Collections.emptyMap;
@@ -64,6 +81,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
  */
 @ExtendWith({MockitoExtension.class})
 public class LeaseUpdaterTest extends BaseIgniteAbstractTest {
+    /** Empty leases. */
+    private final Leases leases = new Leases(emptyMap(), BYTE_EMPTY_ARRAY);
+    /** Cluster node. */
+    private final LogicalNode node = new LogicalNode("123", "test-node", NetworkAddress.from("127.0.0.1:10000"));
     @Mock
     private ClusterService clusterService;
     @Mock
@@ -76,10 +97,6 @@ public class LeaseUpdaterTest extends BaseIgniteAbstractTest {
     private Cursor<Entry> mcEntriesCursor;
     /** Lease updater for tests. */
     private LeaseUpdater leaseUpdater;
-    /** Empty leases. */
-    private final Leases leases = new Leases(emptyMap(), BYTE_EMPTY_ARRAY);
-    /** Cluster node. */
-    private final LogicalNode node = new LogicalNode("123", "test-node", NetworkAddress.from("127.0.0.1:10000"));
     /** Closure to get a lease that is passed in Meta storage. */
     private Consumer<Lease> renewLeaseConsumer = null;
 
