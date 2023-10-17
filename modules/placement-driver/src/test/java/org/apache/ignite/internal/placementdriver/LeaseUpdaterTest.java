2d0b0920d55ddb07caecdc71220afc370a4cc9f7
Vladislav Pyatkov <vldpyatkov@gmail.com>
2023-10-17 10:59:02 +0300

    Code style

--- modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseUpdaterTest.java
+++ modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseUpdaterTest.java
@@ -199,8 +199,7 @@ public class LeaseUpdaterTest extends BaseIgniteAbstractTest {
 
                 if (active) {
                     leaseUpdater.activate();
-                }
-                else {
+                } else {
                     leaseUpdater.deactivate();
                 }
             });
