d130326b8473506fb36a464cdc312beacc0702c8
Kirill Gusakov <kgusakov@gmail.com>
2023-10-22 19:39:47 +0300

    fix with causality token

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/TableManager.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/TableManager.java
@@ -2165,9 +2165,9 @@ public class TableManager implements IgniteTablesInternal, IgniteComponent {
                     if (!stableAssignments.equals(ByteUtils.fromBytes(evt.entryEvent().oldEntry().value()))) {
                         LOG.info("Updating raft group {} with the assignments {} to assignments {} thread {}",
                                 tablePartitionId, ByteUtils.fromBytes(evt.entryEvent().oldEntry().value()), stableAssignments, Thread.currentThread().getName());
-                        fut = latestTablesById().get(tableId).internalTable()
+                        fut = tablesById(evt.revision()).thenCompose(t -> t.get(tableId).internalTable()
                                 .partitionRaftGroupService(tablePartitionId.partitionId())
-                                .updateConfiguration(configurationFromAssignments(stableAssignments));
+                                .updateConfiguration(configurationFromAssignments(stableAssignments)));
                     }
 
                     byte[] pendingAssignmentsFromMetaStorage = pendingAssignmentsEntry.value();
