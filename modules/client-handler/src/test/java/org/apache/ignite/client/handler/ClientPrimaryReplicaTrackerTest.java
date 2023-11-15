25237bd5ea1c559b284701b12a9ad363f657d392
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-11-15 08:34:18 +0200

    testInitialAssignmentIsRetrievedFromPlacementDriver done

--- modules/client-handler/src/test/java/org/apache/ignite/client/handler/ClientPrimaryReplicaTrackerTest.java
+++ modules/client-handler/src/test/java/org/apache/ignite/client/handler/ClientPrimaryReplicaTrackerTest.java
@@ -19,25 +19,50 @@ package org.apache.ignite.client.handler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.apache.ignite.client.handler.ClientPrimaryReplicaTracker.ReplicaHolder;
 import org.apache.ignite.internal.event.EventProducer;
 import org.apache.ignite.internal.hlc.HybridClockImpl;
 import org.apache.ignite.internal.table.IgniteTablesInternal;
+import org.apache.ignite.internal.table.InternalTable;
+import org.apache.ignite.internal.table.TableViewInternal;
 import org.junit.jupiter.api.Test;
 
 class ClientPrimaryReplicaTrackerTest {
     // TODO: Test initial retrieval, update by events, table drop, missing table, null replicas
     @Test
-    public void testInitialAssignmentIsRetrievedFromPlacementDriver() {
-        var driver = new FakePlacementDriver(1);
+    public void testInitialAssignmentIsRetrievedFromPlacementDriver() throws Exception {
+        int partitions = 2;
+        int tableId = 123;
+        var driver = new FakePlacementDriver(partitions);
+        driver.setReplicas(List.of("s1", "s2"), tableId);
+
+        InternalTable internalTable = mock(InternalTable.class);
+        when(internalTable.partitions()).thenReturn(partitions);
+
+        TableViewInternal table = mock(TableViewInternal.class);
+        when(table.tableId()).thenReturn(tableId);
+        when(table.internalTable()).thenReturn(internalTable);
+
+        IgniteTablesInternal tables = mock(IgniteTablesInternal.class);
+        when(tables.tableAsync(tableId)).thenReturn(CompletableFuture.completedFuture(table));
+
         var tracker = new ClientPrimaryReplicaTracker(
                 driver,
-                mock(IgniteTablesInternal.class),
+                tables,
                 mock(EventProducer.class),
                 new HybridClockImpl());
 
         tracker.start();
 
         assertEquals(0, tracker.updateCount());
+
+        List<ReplicaHolder> replicas = tracker.primaryReplicasAsync(tableId).join();
+        assertEquals(partitions, replicas.size());
+        assertEquals("s1", replicas.get(0).nodeName());
+        assertEquals("s2", replicas.get(1).nodeName());
     }
 }
