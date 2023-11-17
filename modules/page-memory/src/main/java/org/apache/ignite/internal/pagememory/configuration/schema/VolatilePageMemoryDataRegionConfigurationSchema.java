67092a7f61702c6edd1fc2f791bf1189942e2b31
ibessonov <bessonov.ip@gmail.com>
2023-11-17 12:38:32 +0300

    IGNITE-20818 test fixed
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/configuration/schema/VolatilePageMemoryDataRegionConfigurationSchema.java
+++ modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/configuration/schema/VolatilePageMemoryDataRegionConfigurationSchema.java
@@ -18,6 +18,7 @@
 package org.apache.ignite.internal.pagememory.configuration.schema;
 
 import static org.apache.ignite.internal.util.Constants.MiB;
+import static org.apache.ignite.internal.util.IgniteUtils.getTotalMemoryAvailable;
 
 import org.apache.ignite.configuration.annotation.Config;
 import org.apache.ignite.configuration.annotation.Value;
@@ -29,10 +30,11 @@ import org.apache.ignite.configuration.validation.OneOf;
 @Config
 public class VolatilePageMemoryDataRegionConfigurationSchema extends BasePageMemoryDataRegionConfigurationSchema {
     /** Default initial size. */
-    public static final long DFLT_DATA_REGION_INITIAL_SIZE = 256 * MiB;
+    @SuppressWarnings("NumericCastThatLosesPrecision")
+    public static final long DFLT_DATA_REGION_INITIAL_SIZE = Math.max(256 * MiB, (long) (0.2 * getTotalMemoryAvailable()));
 
     /** Default max size. */
-    public static final long DFLT_DATA_REGION_MAX_SIZE = 256 * MiB;
+    public static final long DFLT_DATA_REGION_MAX_SIZE = DFLT_DATA_REGION_INITIAL_SIZE;
 
     /** Eviction is disabled. */
     public static final String DISABLED_EVICTION_MODE = "DISABLED";
