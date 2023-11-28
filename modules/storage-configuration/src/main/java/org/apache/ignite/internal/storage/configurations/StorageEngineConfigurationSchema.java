baa6028523de20627f9297a1afee893a9378ca40
Kirill Gusakov <kgusakov@gmail.com>
2023-11-29 00:00:59 +0300

    storage-configuration module

---
+++ modules/storage-configuration/src/main/java/org/apache/ignite/internal/storage/configurations/StorageEngineConfigurationSchema.java
@@ -0,0 +1,10 @@
+package org.apache.ignite.internal.storage.configurations;
+
+import org.apache.ignite.configuration.annotation.PolymorphicConfig;
+import org.apache.ignite.configuration.annotation.PolymorphicId;
+
+@PolymorphicConfig
+public class StorageEngineConfigurationSchema {
+    @PolymorphicId
+    public String name;
+}
