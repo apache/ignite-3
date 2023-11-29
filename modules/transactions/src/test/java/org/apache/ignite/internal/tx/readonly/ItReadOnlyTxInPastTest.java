15713d69e05e461d6384095adf1a0fd32f3d4729
denis-chudov <moonglloom@gmail.com>
2023-11-29 18:31:29 +0200

    IGNITE-20961 Extract the Read only TX integration tests from runner module to separate one

--- modules/transactions/src/test/java/org/apache/ignite/internal/tx/readonly/ItReadOnlyTxInPastTest.java
+++ modules/transactions/src/test/java/org/apache/ignite/internal/tx/readonly/ItReadOnlyTxInPastTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.readonly;
+package org.apache.ignite.internal.tx.readonly;
 
 import static org.apache.ignite.internal.SessionUtils.executeUpdate;
 import static org.hamcrest.MatcherAssert.assertThat;
