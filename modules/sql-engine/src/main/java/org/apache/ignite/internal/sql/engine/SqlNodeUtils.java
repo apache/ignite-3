e64d72180af6b46018397d205ba8812ab8ea8a32
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2023-11-30 20:44:40 +0400

    IGNITE-20107 / implement proper table name extraction

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/SqlNodeUtils.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/SqlNodeUtils.java
@@ -21,7 +21,13 @@ import static java.util.stream.Collectors.toSet;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.calcite.sql.SqlBasicCall;
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlDelete;
 import org.apache.calcite.sql.SqlIdentifier;
+import org.apache.calcite.sql.SqlInsert;
+import org.apache.calcite.sql.SqlJoin;
+import org.apache.calcite.sql.SqlMerge;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlSelect;
 import org.apache.calcite.sql.SqlUpdate;
@@ -38,40 +44,183 @@ class SqlNodeUtils {
      * @param rootNode Node representing a query.
      */
     static Set<String> extractTableNames(SqlNode rootNode) {
-        // TODO: IGNITE-20107 - real implementation.
+        CollectTableNames collectTableNames = new CollectTableNames();
 
-        if (rootNode instanceof SqlSelect) {
-            SqlNode from = ((SqlSelect) rootNode).getFrom();
+        rootNode.accept(collectTableNames);
 
-            if (from != null) {
-                return extractIdentifiers(from);
-            }
-        } else if (rootNode instanceof SqlUpdate) {
-            SqlNode targetTable = ((SqlUpdate) rootNode).getTargetTable();
-
-            if (targetTable != null) {
-                return extractIdentifiers(targetTable);
-            }
-        }
-
-        return Set.of();
-    }
-
-    private static Set<String> extractIdentifiers(SqlNode from) {
-        CollectIdentifiers collectIdentifiers = new CollectIdentifiers();
-        from.accept(collectIdentifiers);
-
-        return collectIdentifiers.ids.stream()
+        return collectTableNames.tableIds.stream()
                 .map(sqlId -> Util.last(sqlId.names))
                 .collect(toSet());
     }
 
-    private static class CollectIdentifiers extends SqlBasicVisitor<Void> {
-        private final Set<SqlIdentifier> ids = new HashSet<>();
+    private static class CollectTableNames extends SqlBasicVisitor<Void> {
+        private final Set<SqlIdentifier> tableIds = new HashSet<>();
+
+        private boolean collect;
 
         @Override
         public Void visit(SqlIdentifier identifier) {
-            ids.add(identifier);
+            if (collect) {
+                tableIds.add(identifier);
+            }
+
+            return null;
+        }
+
+        @Override
+        public Void visit(SqlCall call) {
+            switch (call.getKind()) {
+                case SELECT:
+                    return visitSelect((SqlSelect) call);
+                case JOIN:
+                    return visitJoin((SqlJoin) call);
+                case AS:
+                    return visitAs((SqlBasicCall) call);
+                case INSERT:
+                    return visitInsert((SqlInsert) call);
+                case UPDATE:
+                    return visitUpdate((SqlUpdate) call);
+                case DELETE:
+                    return visitDelete((SqlDelete) call);
+                case MERGE:
+                    return visitMerge((SqlMerge) call);
+                default:
+                    return super.visit(call);
+            }
+        }
+
+        private Void visitSelect(SqlSelect select) {
+            boolean oldCollect = collect;
+            collect = false;
+
+            for (SqlNode child : select.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                collect = select.getFrom() == child;
+
+                child.accept(this);
+            }
+
+            collect = oldCollect;
+
+            return null;
+        }
+
+        private Void visitJoin(SqlJoin join) {
+            for (SqlNode child : join.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                if (child == join.getCondition()) {
+                    boolean oldCollect = collect;
+                    collect = false;
+
+                    child.accept(this);
+
+                    collect = oldCollect;
+                } else {
+                    child.accept(this);
+                }
+            }
+
+            return null;
+        }
+
+        private Void visitAs(SqlBasicCall as) {
+            int childIndex = 0;
+
+            for (SqlNode child : as.getOperandList()) {
+                if (child != null) {
+                    if (childIndex == 1) {
+                        boolean oldCollect = collect;
+                        collect = false;
+
+                        child.accept(this);
+
+                        collect = oldCollect;
+                    } else {
+                        child.accept(this);
+                    }
+                }
+
+                childIndex++;
+            }
+
+            return null;
+        }
+
+        private Void visitInsert(SqlInsert insert) {
+            boolean isInFromBackup = collect;
+
+            for (SqlNode child : insert.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                collect = insert.getTargetTable() == child;
+
+                child.accept(this);
+            }
+
+            collect = isInFromBackup;
+
+            return null;
+        }
+
+        private Void visitUpdate(SqlUpdate update) {
+            boolean oldCollect = collect;
+
+            for (SqlNode child : update.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                collect = update.getTargetTable() == child;
+
+                child.accept(this);
+            }
+
+            collect = oldCollect;
+
+            return null;
+        }
+
+        private Void visitDelete(SqlDelete delete) {
+            boolean oldCollect = collect;
+
+            for (SqlNode child : delete.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                collect = delete.getTargetTable() == child;
+
+                child.accept(this);
+            }
+
+            collect = oldCollect;
+
+            return null;
+        }
+
+        private Void visitMerge(SqlMerge merge) {
+            boolean oldCollect = collect;
+
+            for (SqlNode child : merge.getOperandList()) {
+                if (child == null) {
+                    continue;
+                }
+
+                collect = child == merge.getTargetTable()
+                        || child == merge.getSourceTableRef();
+
+                child.accept(this);
+            }
+
+            collect = oldCollect;
 
             return null;
         }
