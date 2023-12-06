b02767d17d6c8b6a955397e65292871e8074a0be
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-12-06 06:53:32 +0200

    wip handle error

--- modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteRequest.java
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteRequest.java
@@ -62,7 +62,10 @@ public class ClientComputeExecuteRequest {
         Object[] args = unpackArgs(in);
 
         compute.executeAsync(Set.of(node), deploymentUnits, jobClassName, args)
-                .thenAccept(res -> notificationSender.sendNotification(w -> w.packObjectAsBinaryTuple(res)));
+                .whenComplete((res, err) -> {
+                    // TODO: Handle error. Send error to the client in the same format.
+                    notificationSender.sendNotification(w -> w.packObjectAsBinaryTuple(res));
+                });
 
         return null;
     }
