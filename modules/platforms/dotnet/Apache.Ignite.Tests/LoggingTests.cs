1e9188fdf6f8b4c16b130b7276e71d70bbd9825f
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-12-06 17:58:43 +0200

    Fix TestBasicLogging

--- modules/platforms/dotnet/Apache.Ignite.Tests/LoggingTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/LoggingTests.cs
@@ -59,7 +59,7 @@ public class LoggingTests
         StringAssert.Contains("ClientFailoverSocket [Debug] Trying to establish secondary connections - awaiting 2 tasks", log);
         StringAssert.Contains("ClientFailoverSocket [Debug] 2 secondary connections established, 0 failed", log);
         StringAssert.Contains("[Trace] Sending request [requestId=1, op=TablesGet, remoteAddress=", log);
-        StringAssert.Contains("[Trace] Received response [requestId=1, remoteAddress=", log);
+        StringAssert.Contains("[Trace] Received response [requestId=1, flags=PartitionAssignmentChanged, remoteAddress=", log);
         StringAssert.Contains("op=SqlExec", log);
         StringAssert.Contains("[Debug] Connection closed gracefully", log);
     }
