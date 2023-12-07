e7010e4cb749aad69399744a703f3529de23e232
Vladislav Pyatkov <vldpyatkov@gmail.com>
2023-12-07 15:12:09 +0300

    IGNITE-20771 Implement tx coordinator liveness check (#2923)

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/TxStateMeta.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/TxStateMeta.java
@@ -17,6 +17,10 @@
 
 package org.apache.ignite.internal.tx;
 
+import static org.apache.ignite.internal.tx.TxState.ABANDONED;
+import static org.apache.ignite.internal.tx.TxState.FINISHING;
+import static org.apache.ignite.internal.tx.TxState.checkTransitionCorrectness;
+
 import java.util.Objects;
 import org.apache.ignite.internal.hlc.HybridTimestamp;
 import org.apache.ignite.internal.replicator.TablePartitionId;
@@ -52,10 +56,51 @@ public class TxStateMeta implements TransactionMeta {
             @Nullable TablePartitionId commitPartitionId,
             @Nullable HybridTimestamp commitTimestamp
     ) {
+        this(txState, txCoordinatorId, commitPartitionId, commitTimestamp, 0);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param txState Transaction state.
+     * @param txCoordinatorId Transaction coordinator id.
+     * @param commitPartitionId Commit partition replication group id.
+     * @param commitTimestamp Commit timestamp.
+     * @param lastAbandonedMarkerTs Timestamp indicates when the transaction is marked as abandoned.
+     */
+    private TxStateMeta(
+            TxState txState,
+            String txCoordinatorId,
+            TablePartitionId commitPartitionId,
+            HybridTimestamp commitTimestamp,
+            long lastAbandonedMarkerTs
+    ) {
         this.txState = txState;
         this.txCoordinatorId = txCoordinatorId;
         this.commitPartitionId = commitPartitionId;
         this.commitTimestamp = commitTimestamp;
+    }
+
+    /**
+     * Creates a transaction state for the same transaction, but this one is marked abandoned.
+     *
+     * @return Transaction state meta.
+     */
+    public TxStateMetaAbandoned abandoned() {
+        assert checkTransitionCorrectness(txState, ABANDONED) : "Transaction state is incorrect [txState=" + txState + "].";
+
+        return new TxStateMetaAbandoned(txCoordinatorId, commitPartitionId);
+    }
+
+    /**
+     * Creates a transaction state for the same transaction, but this one is marked finishing.
+     *
+     * @return Transaction state meta.
+     */
+    public TxStateMetaFinishing finishing() {
+        assert checkTransitionCorrectness(txState, FINISHING) : "Transaction state is incorrect [txState=" + txState + "].";
+
+        return new TxStateMetaFinishing(txCoordinatorId, commitPartitionId);
     }
 
     @Override
@@ -84,12 +129,12 @@ public class TxStateMeta implements TransactionMeta {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-
         TxStateMeta that = (TxStateMeta) o;
 
         if (txState != that.txState) {
             return false;
         }
+
         if (txCoordinatorId != null ? !txCoordinatorId.equals(that.txCoordinatorId) : that.txCoordinatorId != null) {
             return false;
         }
