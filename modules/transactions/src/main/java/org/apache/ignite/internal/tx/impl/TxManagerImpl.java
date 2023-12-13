ebd70a7144f459c920859b7babff74a41e1d6462
Kirill Sizov <sizov.kirill.y@gmail.com>
2023-12-13 22:51:17 +0300

    IGNITE-20735 Support parallel execution of TxManager.finish

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
@@ -351,28 +351,39 @@ public class TxManagerImpl implements TxManager, NetworkMessageHandler {
         // state after the transaction is finished.
         TxStateMetaFinishing finishingStateMeta = updateTxMeta(txId, TxStateMeta::finishing);
 
+        AtomicBoolean performingFinish = new AtomicBoolean();
         TxContext tuple = txCtxMap.compute(txId, (uuid, tuple0) -> {
             if (tuple0 == null) {
                 tuple0 = new TxContext(); // No writes enlisted.
             }
 
-            assert !tuple0.isTxFinishing() : "Transaction is already finished [id=" + uuid + "].";
+            if (!tuple0.isTxFinishing()) {
+                tuple0.finishTx();
+
+                performingFinish.set(true);
+            }
 
             tuple0.finishTx();
 
             return tuple0;
         });
 
-        // Wait for commit acks first, then proceed with the finish request.
-        return tuple.performFinish(commit, ignored ->
-                prepareFinish(
-                        observableTimestampTracker,
-                        commitPartition,
-                        commit,
-                        enlistedGroups,
-                        txId,
-                        finishingStateMeta.txFinishFuture()
-                ));
+        // This is a finishing thread.
+        if (performingFinish.get()) {
+            // Wait for commit acks first, then proceed with the finish request.
+            return tuple.performFinish(commit, ignored ->
+                    prepareFinish(
+                            observableTimestampTracker,
+                            commitPartition,
+                            commit,
+                            enlistedGroups,
+                            txId,
+                            finishingStateMeta.txFinishFuture()
+                    ));
+        }
+        // The method `performFinish` above has a side effect on `finishInProgressFuture` future,
+        // it kicks off another future that will complete it.
+        return tuple.finishInProgressFuture;
     }
 
     private CompletableFuture<Void> prepareFinish(
