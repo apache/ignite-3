30ccc50da50285f8af6ad42ad6f2c2a21edde74c
Pavel Tupitsyn <ptupitsyn@apache.org>
2023-12-21 13:07:35 +0200

    IGNITE-21009 .NET: Add support for nullable columns in KeyValueView with primitive mapping
    https://issues.apache.org/jira/browse/IGNITE-21009

--- modules/platforms/dotnet/Apache.Ignite.Tests/Table/KeyValueViewPrimitiveTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/KeyValueViewPrimitiveTests.cs
@@ -51,6 +51,20 @@ public class KeyValueViewPrimitiveTests : IgniteTestsBase
     }
 
     [Test]
+    public async Task TestPutGetNullable()
+    {
+        // TODO: Refactor this test somehow.
+        await Client.Sql.ExecuteAsync(null, "CREATE TABLE TestPutGetNullable (ID BIGINT PRIMARY KEY, VAL BIGINT)");
+        await Client.Sql.ExecuteAsync(null, "INSERT INTO TestPutGetNullable VALUES (1, 1), (2, NULL)");
+
+        var table = await Client.Tables.GetTableAsync("TestPutGetNullable");
+        var view = table!.GetKeyValueView<long, long>();
+
+        var res1 = await view.GetAsync(null, 1);
+        var res2 = await view.GetAsync(null, 2);
+    }
+
+    [Test]
     public async Task TestGetNonExistentKeyReturnsEmptyOption()
     {
         (string res, bool hasRes) = await KvView.GetAsync(null, -111L);
