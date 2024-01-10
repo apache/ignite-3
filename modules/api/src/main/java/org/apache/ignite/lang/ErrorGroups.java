07e8a6b384cc3c563255128fb36528a80756ab89
Dmitriy Zabotlin <arcolight@users.noreply.github.com>
2024-01-10 17:42:01 +0200

    IGNITE-19944 ODBC 3.0: Propagate SQL errors from engine to driver (#3022)
    Co-authored-by: dzabotlin <dzabotlin@gridgain.com>

--- modules/api/src/main/java/org/apache/ignite/lang/ErrorGroups.java
+++ modules/api/src/main/java/org/apache/ignite/lang/ErrorGroups.java
@@ -20,6 +20,7 @@ package org.apache.ignite.lang;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import java.util.Locale;
+import org.apache.ignite.error.code.annotations.ErrorCodeGroup;
 
 /**
  * Defines error groups and its errors.
@@ -112,6 +113,7 @@ public class ErrorGroups {
     }
 
     /** Common error group. */
+    @ErrorCodeGroup
     public static class Common {
         /** Common error group. */
         public static final ErrorGroup COMMON_ERR_GROUP = registerGroup("CMN", (short) 1);
@@ -139,6 +141,7 @@ public class ErrorGroups {
     }
 
     /** Tables error group. */
+    @ErrorCodeGroup
     public static class Table {
         /** Table error group. */
         public static final ErrorGroup TABLE_ERR_GROUP = registerGroup("TBL", (short) 2);
@@ -166,6 +169,7 @@ public class ErrorGroups {
     }
 
     /** Client error group. */
+    @ErrorCodeGroup
     public static class Client {
         /** Client error group. */
         public static final ErrorGroup CLIENT_ERR_GROUP = registerGroup("CLIENT", (short) 3);
@@ -202,6 +206,7 @@ public class ErrorGroups {
     }
 
     /** SQL error group. */
+    @ErrorCodeGroup
     public static class Sql {
         /** SQL error group. */
         public static final ErrorGroup SQL_ERR_GROUP = registerGroup("SQL", (short) 4);
@@ -272,6 +277,7 @@ public class ErrorGroups {
     }
 
     /** Meta storage error group. */
+    @ErrorCodeGroup
     public static class MetaStorage {
         /** Meta storage error group. */
         public static final ErrorGroup META_STORAGE_ERR_GROUP = registerGroup("META", (short) 5);
@@ -293,6 +299,7 @@ public class ErrorGroups {
     }
 
     /** Index error group. */
+    @ErrorCodeGroup
     public static class Index {
         /** Index error group. */
         public static final ErrorGroup INDEX_ERR_GROUP = registerGroup("IDX", (short) 6);
@@ -308,6 +315,7 @@ public class ErrorGroups {
     }
 
     /** Transactions error group. */
+    @ErrorCodeGroup
     public static class Transactions {
         /** Transactions error group. */
         public static final ErrorGroup TX_ERR_GROUP = registerGroup("TX", (short) 7);
@@ -353,6 +361,7 @@ public class ErrorGroups {
     }
 
     /** Replicator error group. */
+    @ErrorCodeGroup
     public static class Replicator {
         /** Replicator error group. */
         public static final ErrorGroup REPLICATOR_ERR_GROUP = registerGroup("REP", (short) 8);
@@ -386,6 +395,7 @@ public class ErrorGroups {
     }
 
     /** Storage error group. */
+    @ErrorCodeGroup
     public static class Storage {
         /** Storage error group. */
         public static final ErrorGroup STORAGE_ERR_GROUP = registerGroup("STORAGE", (short) 9);
@@ -404,6 +414,7 @@ public class ErrorGroups {
     }
 
     /** Distribution zones error group. */
+    @ErrorCodeGroup
     public static class DistributionZones {
         /** Distribution zones group. */
         public static final ErrorGroup DISTRIBUTION_ZONES_ERR_GROUP = registerGroup("DISTRZONES", (short) 10);
@@ -425,6 +436,7 @@ public class ErrorGroups {
     }
 
     /** Network error group. */
+    @ErrorCodeGroup
     public static class Network {
         /** Network error group. */
         public static final ErrorGroup NETWORK_ERR_GROUP = registerGroup("NETWORK", (short) 11);
@@ -440,6 +452,7 @@ public class ErrorGroups {
     }
 
     /** Node configuration error group. */
+    @ErrorCodeGroup
     public static class NodeConfiguration {
         /** Node configuration error group. */
         public static final ErrorGroup NODE_CONFIGURATION_ERR_GROUP = registerGroup("NODECFG", (short) 12);
@@ -458,6 +471,7 @@ public class ErrorGroups {
     }
 
     /** Code deployment error group. */
+    @ErrorCodeGroup
     public static class CodeDeployment {
         /** Code deployment error group. */
         public static final ErrorGroup CODE_DEPLOYMENT_ERR_GROUP = registerGroup("CODEDEPLOY", (short) 13);
@@ -478,6 +492,7 @@ public class ErrorGroups {
     /**
      * Garbage collector error group.
      */
+    @ErrorCodeGroup
     public static class GarbageCollector {
         /** Garbage collector error group. */
         public static final ErrorGroup GC_ERR_GROUP = registerGroup("GC", (short) 14);
@@ -489,6 +504,7 @@ public class ErrorGroups {
     /**
      * Authentication error group.
      */
+    @ErrorCodeGroup
     public static class Authentication {
         /** Authentication error group. */
         public static final ErrorGroup AUTHENTICATION_ERR_GROUP = registerGroup("AUTHENTICATION", (short) 15);
@@ -506,6 +522,7 @@ public class ErrorGroups {
     /**
      * Compute error group.
      */
+    @ErrorCodeGroup
     public static class Compute {
         /** Compute error group. */
         public static final ErrorGroup COMPUTE_ERR_GROUP = registerGroup("COMPUTE", (short) 16);
@@ -539,6 +556,7 @@ public class ErrorGroups {
     }
 
     /** Catalog error group. */
+    @ErrorCodeGroup
     public static class Catalog {
         /** Catalog error group. */
         public static final ErrorGroup CATALOG_ERR_GROUP = registerGroup("CATALOG", (short) 17);
@@ -548,6 +566,7 @@ public class ErrorGroups {
     }
 
     /** Placement driver error group. */
+    @ErrorCodeGroup
     public static class PlacementDriver {
         /** Placement driver error group. */
         public static final ErrorGroup PLACEMENT_DRIVER_ERR_GROUP = registerGroup("PLACEMENTDRIVER", (short) 18);
