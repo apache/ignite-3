30ac5481551cfce7d9e78025ce272169196c6919
dzabotlin <dzabotlin@gridgain.com>
2024-01-10 12:18:28 +0200

    Fix connection error handling

--- modules/platforms/cpp/ignite/network/detail/linux/linux_async_client_pool.cpp
+++ modules/platforms/cpp/ignite/network/detail/linux/linux_async_client_pool.cpp
@@ -96,13 +96,7 @@ void linux_async_client_pool::close_and_release(uint64_t id, std::optional<ignit
 
     bool closed = client->close();
     if (closed) {
-        ignite_error err0(client->get_close_error());
-        if (err0.get_status_code() == error::code::GENERIC)
-            err0 = ignite_error(error::code::CONNECTION, "Connection closed by server");
-
-        if (!err)
-            err = std::move(err0);
-
+        err = client->get_close_error();
         handle_connection_closed(id, err);
     }
 }
