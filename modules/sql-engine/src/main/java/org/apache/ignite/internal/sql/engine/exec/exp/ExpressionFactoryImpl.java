06ba942e11c4a3ac2a7d877d31ad343865e43b35
korlov42 <korlov@gridgain.com>
2024-01-15 13:35:17 +0200

    review fixes
    - simplify condition in comparator

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/exp/ExpressionFactoryImpl.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/exp/ExpressionFactoryImpl.java
@@ -149,7 +149,7 @@ public class ExpressionFactoryImpl<RowT> implements ExpressionFactory<RowT> {
                 int nullComparison = field.nullDirection.nullComparison;
 
                 if (o1 == null || o2 == null) {
-                    if (o1 == null && o2 == null) {
+                    if (o1 == o2) {
                         return 0;
                     } else if (o1 == null) {
                         return nullComparison;
