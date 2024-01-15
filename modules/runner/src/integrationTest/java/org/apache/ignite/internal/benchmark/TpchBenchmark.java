7da342e73053c395bb650aef10de95adc309625e
korlov42 <korlov@gridgain.com>
2024-01-15 13:49:25 +0200

    baseline

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/TpchBenchmark.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/TpchBenchmark.java
@@ -15,12 +15,13 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.sql.engine.benchmarks;
+package org.apache.ignite.internal.benchmark;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import org.apache.ignite.internal.sql.engine.sql.IgniteSqlParser;
-import org.apache.ignite.internal.sql.engine.sql.StatementParseResult;
 import org.apache.ignite.internal.sql.engine.util.tpch.TpchHelper;
+import org.apache.ignite.sql.Session;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -31,61 +32,83 @@ import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
- * Benchmarks derived from <a href="http://www.tpc.org/tpch/">TPC-H</a>.
+ * Benchmark that runs sql queries from TPC-H suite via embedded client.
  */
-@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+@Fork(1)
+@Threads(1)
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 20, time = 2)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(3)
-@State(Scope.Benchmark)
-public class TpchParseBenchmark {
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class TpchBenchmark extends AbstractMultiNodeBenchmark {
+    private final String[] tablesToInit = {
+            "customer", "lineitem", "nation", "orders",
+            "part", "partsupp", "region", "supplier"
+    };
 
-    /**
-     * Identifiers of TPC-H queries. See {@link TpchHelper#getQuery(String)}.
-     */
     @Param({
             "1", "2", "3", "4", "5", "6", "7", "8", "8v", "9", "10", "11", "12", "12v",
             "13", "14", "14v", "15", "16", "17", "18", "19", "20", "21", "22"
     })
     private String queryId;
 
+    private Session session;
     private String queryString;
 
-    /** Prepares the plan of the query. */
+    /** Initializes a schema and fills tables with data. */
     @Setup
-    public void setUp() {
+    public void setUp() throws IOException {
+        session = clusterNode.sql().createSession();
+
+        System.out.println("Going to create schema...");
+        session.executeScript(TpchHelper.getSchemaDefinitionScript());
+        System.out.println("Done");
+
+        for (String tableName : tablesToInit) {
+            fillTable(session, tableName);
+        }
+
         queryString = TpchHelper.getQuery(queryId);
     }
 
-    /**
-     * Benchmark that measures the time it takes to parse a {@code TPC-H query}.
-     */
+    /** Benchmark that measures performance of queries from TPC-H suite. */
     @Benchmark
-    public void parseQuery(Blackhole bh) {
-        StatementParseResult parseResult = IgniteSqlParser.parse(queryString, StatementParseResult.MODE);
-        bh.consume(parseResult.statement());
+    public void run(Blackhole bh) {
+        try (var rs = session.execute(null, queryString)) {
+            while (rs.hasNext()) {
+                bh.consume(rs.next());
+            }
+        }
     }
 
     /**
-     * Runs the benchmark.
-     *
-     * @param args args
-     * @throws Exception if something goes wrong
+     * Benchmark's entry point.
      */
-    public static void main(String[] args) throws Exception {
-        Options build = new OptionsBuilder()
-                //.addProfiler("gc")
-                .include(TpchParseBenchmark.class.getName())
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + TpchBenchmark.class.getSimpleName() + ".*")
                 .build();
 
-        new Runner(build).run();
+        new Runner(opt).run();
+    }
+
+    private static void fillTable(Session session, String tableName) {
+        System.out.println("Going to fill table \"" + tableName + "\"...");
+        long start = System.nanoTime();
+        session.execute(null, TpchHelper.getDmlForTable(tableName));
+        System.out.println("Done in " + Duration.ofNanos(System.nanoTime() - start));
     }
 }
+
+
