2c7da69d008d8766b81ac0463bac3744c4062166
Dmitry Baranov <nev.dmitry@gmail.com>
2024-01-16 21:48:01 +0300

    Update modules/compute/src/main/java/org/apache/ignite/internal/compute/queue/QueueExecutionImpl.java
    Co-authored-by: Aleksandr Pakhomov <apkhmv@gmail.com>

--- modules/compute/src/main/java/org/apache/ignite/internal/compute/queue/QueueExecutionImpl.java
+++ modules/compute/src/main/java/org/apache/ignite/internal/compute/queue/QueueExecutionImpl.java
@@ -122,7 +122,7 @@ class QueueExecutionImpl<R> implements QueueExecution<R> {
             run();
         } else {
             throw new ComputeException(Compute.CHANGE_JOB_PRIORITY_JOB_EXECUTING_ERR, "Can not change job priority,"
-                    + " job already processing. job id " + jobId);
+                    + " job already processing. [job id = " + jobId);
         }
     }
 
