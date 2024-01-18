d6345324acdaa183825a5ce7ae72f8c0f674b16c
Kirill Gusakov <kgusakov@gmail.com>
2024-01-18 19:47:41 +0300

    code style fix

--- modules/table/src/integrationTest/java/org/apache/ignite/internal/rebalance/ItRebalanceTriggersRecoveryTest.java
+++ modules/table/src/integrationTest/java/org/apache/ignite/internal/rebalance/ItRebalanceTriggersRecoveryTest.java
@@ -170,11 +170,11 @@ public class ItRebalanceTriggersRecoveryTest extends ClusterPerTestIntegrationTe
     void testRebalanceTriggersRecoveryWhenUpdatesWereProcessedByAnotherNodesAlready() throws Exception {
         // The nodes from different regions/zones needed to implement the predictable way of nodes choice.
         startNode(1, US_NODE_BOOTSTRAP_CFG_TEMPLATE);
-        startNode(2, GLOBAL_NODE_BOOTSTRAP_CFG_TEMPLATE);
+        startNode(2, US_NODE_BOOTSTRAP_CFG_TEMPLATE);
         startNode(3, GLOBAL_NODE_BOOTSTRAP_CFG_TEMPLATE);
 
         cluster.doInSession(0, session -> {
-            session.execute(null, "CREATE ZONE TEST_ZONE WITH PARTITIONS=1, REPLICAS=1, DATA_NODES_FILTER='$[?(@.zone == \"global\")]'");
+            session.execute(null, "CREATE ZONE TEST_ZONE WITH PARTITIONS=1, REPLICAS=1, DATA_NODES_FILTER='$[?(@.region == \"US\")]'");
             session.execute(null, "CREATE TABLE TEST (id INT PRIMARY KEY, name INT) WITH PRIMARY_ZONE='TEST_ZONE'");
             session.execute(null, "INSERT INTO TEST VALUES (0, 0)");
         });
@@ -209,16 +209,14 @@ public class ItRebalanceTriggersRecoveryTest extends ClusterPerTestIntegrationTe
                         0
                 );
         long pendingsKeysRevisionBeforeRecovery = node(0).metaStorageManager()
-                .get(pendingPartAssignmentsKey(
-                        new TablePartitionId(getTableId(node(0).catalogManager(), "TEST", new HybridClockImpl().nowLong()), 0)))
+                .get(pendingPartAssignmentsKey(tablePartitionId))
                 .get(10, TimeUnit.SECONDS).revision();
 
 
         startNode(3, GLOBAL_NODE_BOOTSTRAP_CFG_TEMPLATE);
 
         long pendingsKeysRevisionAfterRecovery = node(0).metaStorageManager()
-                .get(pendingPartAssignmentsKey(
-                        new TablePartitionId(getTableId(node(0).catalogManager(), "TEST", new HybridClockImpl().nowLong()), 0)))
+                .get(pendingPartAssignmentsKey(tablePartitionId))
                 .get(10, TimeUnit.SECONDS).revision();
 
         // Check that recovered node doesn't produce new rebalances for already processed triggers.
