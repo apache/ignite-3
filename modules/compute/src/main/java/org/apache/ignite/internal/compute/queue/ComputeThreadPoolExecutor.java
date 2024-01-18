0c830fb7e7b8c4c583ce4857a99b9985f1720f0c
Dmitry Baranov <dbaranov@unison.team>
2024-01-18 09:48:20 +0300

    IGNITE-21066 fix java doc

--- modules/compute/src/main/java/org/apache/ignite/internal/compute/queue/ComputeThreadPoolExecutor.java
+++ modules/compute/src/main/java/org/apache/ignite/internal/compute/queue/ComputeThreadPoolExecutor.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.compute.queue;
 
 import java.util.concurrent.BlockingQueue;
@@ -26,6 +43,7 @@ public class ComputeThreadPoolExecutor {
     }
 
     /**
+     * Executes the given task sometime in the future.
      * {@link ThreadPoolExecutor#execute(Runnable command)}
      */
     public void execute(Runnable command) {
@@ -33,6 +51,7 @@ public class ComputeThreadPoolExecutor {
     }
 
     /**
+     * Removes this task from the executor's internal queue if it is present.
      * {@link ThreadPoolExecutor#remove(Runnable command)}
      */
     public boolean remove(Runnable task) {
@@ -52,7 +71,7 @@ public class ComputeThreadPoolExecutor {
     }
 
     /**
-     * Getter for internal execution service
+     * Getter for internal execution service.
      *
      * @return internal execution service
      */
