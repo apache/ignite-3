4aab8867acf98d957f3787bd55c61232b9c83fdd
apakhomov <apkhmv@gmail.com>
2024-01-24 15:53:23 +0400

    Add one more switch
    Signed-off-by: apakhomov <apkhmv@gmail.com>

--- modules/platforms/cpp/ignite/odbc/common_types.cpp
+++ modules/platforms/cpp/ignite/odbc/common_types.cpp
@@ -274,6 +274,7 @@ sql_state error_code_to_sql_state(error::code code) {
         case error::code::COMPUTE_JOB_FAILED:
         case error::code::CHANGE_JOB_PRIORITY_NO_JOB:
         case error::code::PRIMARY_REPLICA_RESOLVE:
+        case error::code::CHANGE_JOB_PRIORITY_JOB_EXECUTING:
         case error::code::CHANGE_JOB_PRIORITY:
             return sql_state::SHY000_GENERAL_ERROR;
 
