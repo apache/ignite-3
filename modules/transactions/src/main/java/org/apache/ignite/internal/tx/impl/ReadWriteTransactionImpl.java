b2e26bfa0084f784a40751c247aa782605ff3bb2
denis-chudov <moonglloom@gmail.com>
2024-01-25 19:31:33 +0200

    wip

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/ReadWriteTransactionImpl.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/ReadWriteTransactionImpl.java
@@ -60,7 +60,7 @@ public class ReadWriteTransactionImpl extends IgniteAbstractTransactionImpl {
     private final ReentrantReadWriteLock enlistPartitionLock = new ReentrantReadWriteLock();
 
     /** The future is initialized when this transaction starts committing or rolling back and is finished together with the transaction. */
-    private volatile CompletableFuture<Void> finishFuture;
+    private CompletableFuture<Void> finishFuture;
 
     /**
      * Constructs an explicit read-write transaction.
