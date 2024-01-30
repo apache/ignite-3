2edf472a72f0362fbcb4a6b93bd7e92e5d37b157
Mikhail Efremov <jakutenshi@gmail.com>
2024-01-31 01:25:52 +0600

    IGNITE-15889 Revert suggested changes

--- modules/table/src/test/java/org/apache/ignite/internal/table/RecordBinaryViewOperationsTest.java
+++ modules/table/src/test/java/org/apache/ignite/internal/table/RecordBinaryViewOperationsTest.java
@@ -356,10 +356,8 @@ public class RecordBinaryViewOperationsTest extends TableKvOperationsTestBase {
 
         tbl.insert(null, rec);
 
-        assertTrue(tbl.contains(null, rec));
         assertTrue(tbl.contains(null, keyRec));
         assertFalse(tbl.contains(null, Tuple.create().set("id", -1L)));
-        assertFalse(tbl.contains(null, Tuple.create().set("id", -1L).set("val", -1L)));
     }
 
     @Test
