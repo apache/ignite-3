f85458803724bcf96d5b19458da642b35290d247
korlov42 <korlov@gridgain.com>
2024-02-01 15:19:48 +0200

    review fixes
    - replace call to LinkedHashMap constructor with factory method which accounts for fill factor
    - improve javadoc of physicalOrderMatchesLogical

--- modules/schema/src/main/java/org/apache/ignite/internal/schema/SchemaDescriptor.java
+++ modules/schema/src/main/java/org/apache/ignite/internal/schema/SchemaDescriptor.java
@@ -17,11 +17,12 @@
 
 package org.apache.ignite.internal.schema;
 
+import static org.apache.ignite.internal.util.IgniteUtils.newLinkedHashMap;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -91,7 +92,7 @@ public class SchemaDescriptor {
 
         assert this.keyCols.nullMapSize() == 0 : "Primary key cannot contain nullable column [cols=" + this.keyCols + ']';
 
-        colMap = new LinkedHashMap<>(keyCols.length + valCols.length);
+        colMap = newLinkedHashMap(keyCols.length + valCols.length);
         var hasTemporalColumns = new AtomicBoolean(false);
 
         Stream.concat(Arrays.stream(this.keyCols.columns()), Arrays.stream(this.valCols.columns()))
@@ -177,7 +178,7 @@ public class SchemaDescriptor {
         Objects.checkIndex(colIdx, length());
     }
 
-    /** Returns true if physical order matches the logical order, return false otherwise. */
+    /** Returns true if physical order matches the logical order, false otherwise. */
     public boolean physicalOrderMatchesLogical() {
         return physicalOrderMatchesLogical;
     }
