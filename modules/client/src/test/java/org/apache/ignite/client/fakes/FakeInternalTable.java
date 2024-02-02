08c3c9b07bae6c64e229233685b040a4ce9b6a99
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-02-02 19:11:14 +0200

    Fix build

--- modules/client/src/test/java/org/apache/ignite/client/fakes/FakeInternalTable.java
+++ modules/client/src/test/java/org/apache/ignite/client/fakes/FakeInternalTable.java
@@ -45,6 +45,7 @@ import org.apache.ignite.internal.schema.BinaryTuplePrefix;
 import org.apache.ignite.internal.schema.ColumnsExtractor;
 import org.apache.ignite.internal.storage.engine.MvTableStorage;
 import org.apache.ignite.internal.table.InternalTable;
+import org.apache.ignite.internal.table.distributed.replicator.action.RowOpType;
 import org.apache.ignite.internal.tx.InternalTransaction;
 import org.apache.ignite.internal.tx.storage.state.TxStateTableStorage;
 import org.apache.ignite.internal.util.PendingComparableValuesTracker;
@@ -185,7 +186,7 @@ public class FakeInternalTable implements InternalTable {
     }
 
     @Override
-    public CompletableFuture<Void> updateAll(Collection<BinaryRowEx> rows, int partition) {
+    public CompletableFuture<Void> updateAll(Collection<BinaryRowEx> rows, @Nullable List<RowOpType> opTypes, int partition) {
         throw new UnsupportedOperationException();
     }
 
