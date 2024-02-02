c75a72cf341f17ae90c8651022044ad6f083d3c6
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-02-02 19:42:05 +0200

    Fix checkstyle

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/table/ItInternalTableTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/table/ItInternalTableTest.java
@@ -32,7 +32,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
