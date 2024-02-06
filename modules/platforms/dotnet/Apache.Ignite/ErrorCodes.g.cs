4b5e8c6c2c1fe7de3f8579c6b923381eaec27d02
Kirill Tkalenko <tkalkirill@yandex.ru>
2024-02-06 08:18:41 +0300

    IGNITE-21429 Prevent stuck-in-the-wires RW transaction operations from executing in a PartitionReplicaListener (#3153)

--- modules/platforms/dotnet/Apache.Ignite/ErrorCodes.g.cs
+++ modules/platforms/dotnet/Apache.Ignite/ErrorCodes.g.cs
@@ -293,6 +293,9 @@ namespace Apache.Ignite
 
             /// <summary> TxAlreadyFinished error. </summary>
             public const int TxAlreadyFinished = (GroupCode << 16) | (14 & 0xFFFF);
+
+            /// <summary> TxStaleOperation error. </summary>
+            public const int TxStaleOperation = (GroupCode << 16) | (15 & 0xFFFF);
         }
 
         /// <summary> Replicator errors. </summary>
