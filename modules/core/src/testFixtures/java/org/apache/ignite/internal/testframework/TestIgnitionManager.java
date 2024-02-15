21313a008b53cd6da6b2ae7dac257320983e9d28
Kirill Gusakov <kgusakov@gmail.com>
2024-02-15 19:37:04 +0300

    fix defaults configs in tests

--- modules/core/src/testFixtures/java/org/apache/ignite/internal/testframework/TestIgnitionManager.java
+++ modules/core/src/testFixtures/java/org/apache/ignite/internal/testframework/TestIgnitionManager.java
@@ -58,7 +58,9 @@ public class TestIgnitionManager {
     /** Map with default node configuration values. */
     private static final Map<String, String> DEFAULT_NODE_CONFIG = Map.of(
             "network.membership.scaleCube.metadataTimeout", Integer.toString(DEFAULT_SCALECUBE_METADATA_TIMEOUT),
+            "storage.profiles.default_aipersist.engine", "aipersist",
             "storage.profiles.default_aipersist.size", Integer.toString(256 * MiB),
+            "storage.profiles.default_aimem.engine", "aimem",
             "storage.profiles.default_aimem.initSize", Integer.toString(256 * MiB),
             "storage.profiles.default_aimem.maxSize", Integer.toString(256 * MiB),
             "storage.profiles.default.engine", "aipersist"
