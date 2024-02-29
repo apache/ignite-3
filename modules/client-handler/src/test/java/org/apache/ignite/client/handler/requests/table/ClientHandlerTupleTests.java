af4ac9ef89c1c0e5af2d5b4d5a88fff93685d01e
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-02-29 10:33:33 +0200

    wip ClientHandlerTupleTests

--- modules/client-handler/src/test/java/org/apache/ignite/client/handler/requests/table/ClientHandlerTupleTests.java
+++ modules/client-handler/src/test/java/org/apache/ignite/client/handler/requests/table/ClientHandlerTupleTests.java
@@ -64,13 +64,6 @@ public class ClientHandlerTupleTests {
     private static final Instant TIMESTAMP = Instant.now();
 
     /** Schema descriptor. */
-    private final SchemaDescriptor schema = new SchemaDescriptor(
-            42,
-            new Column[]{new Column("id".toUpperCase(), NativeTypes.INT64, false)},
-            new Column[]{new Column("name".toUpperCase(), NativeTypes.STRING, false)}
-    );
-
-    /** Schema descriptor. */
     private final SchemaDescriptor fullSchema = new SchemaDescriptor(42,
             new Column[]{new Column("keyUuidCol".toUpperCase(), NativeTypes.UUID, false)},
             new Column[]{
