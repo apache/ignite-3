e521b46357cb572ae4a758f8746d6485510d0660
denis-chudov <moonglloom@gmail.com>
2024-03-12 18:18:10 +0200

    moved to the correct package

--- modules/transactions/src/integrationTest/java/org/apache/ignite/internal/tx/ItMultipleLocksTest.java
+++ modules/transactions/src/integrationTest/java/org/apache/ignite/internal/tx/ItMultipleLocksTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.tx.readonly;
+package org.apache.ignite.internal.tx;
 
 import static org.apache.ignite.internal.lang.IgniteStringFormatter.format;
 
