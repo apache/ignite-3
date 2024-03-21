59ad98764f32c4ae26e369ab02a6bed4fcb4cba4
Kirill Sizov <sizov.kirill.y@gmail.com>
2024-03-21 10:47:20 +0300

    IGNITE-18617 Consistent naming for tx context removal methods

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TransactionInflights.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TransactionInflights.java
@@ -100,20 +100,15 @@ public class TransactionInflights {
         }
     }
 
-    /**
-     * Cleanup tx inflights on tx finish.
-     *
-     * @param txId Transaction id.
-     */
-    void clearInflights(UUID txId) {
-        txCtxMap.remove(txId);
-    }
-
     Collection<UUID> finishedReadOnlyTransactions() {
         return txCtxMap.entrySet().stream()
                 .filter(e -> e.getValue() instanceof ReadOnlyTxContext && e.getValue().isReadyToFinish())
                 .map(Entry::getKey)
                 .collect(toSet());
+    }
+
+    void removeTxContext(UUID txId) {
+        txCtxMap.remove(txId);
     }
 
     void removeTxContexts(Collection<UUID> txIds) {
