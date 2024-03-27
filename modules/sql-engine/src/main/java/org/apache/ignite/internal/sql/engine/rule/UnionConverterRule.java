e05d6b661168d00a3f6a58cc039c92ccce474ac2
Max Zhuravkov <shhwwa@gmail.com>
2024-03-27 15:23:59 +0200

    IGNITE-21827: Sql. Union returns incorrect result for DECIMAL and INT columns
    Use method introduced in EXCEPT patch.

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/rule/UnionConverterRule.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/rule/UnionConverterRule.java
@@ -17,9 +17,7 @@
 
 package org.apache.ignite.internal.sql.engine.rule;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
@@ -27,13 +25,9 @@ import org.apache.calcite.plan.RelRule;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.logical.LogicalUnion;
-import org.apache.calcite.rel.type.RelDataType;
-import org.apache.calcite.rex.RexBuilder;
-import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.tools.RelBuilder;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.ignite.internal.sql.engine.rel.IgniteConvention;
-import org.apache.ignite.internal.sql.engine.rel.IgniteProject;
 import org.apache.ignite.internal.sql.engine.rel.IgniteUnionAll;
 import org.apache.ignite.internal.sql.engine.util.Commons;
 import org.immutables.value.Value;
@@ -60,7 +54,7 @@ public class UnionConverterRule extends RelRule<UnionConverterRule.Config> {
         RelTraitSet traits = cluster.traitSetOf(IgniteConvention.INSTANCE);
 
         List<RelNode> inputs = Commons.transform(union.getInputs(), input -> convert(input, traits));
-        List<RelNode> convertedInputs = convertInputs(cluster, inputs, traits);
+        List<RelNode> convertedInputs = Commons.castInputsToLeastRestrictiveTypeIfNeeded(inputs, cluster, traits);
 
         RelNode res = new IgniteUnionAll(cluster, traits, convertedInputs);
 
@@ -75,52 +69,6 @@ public class UnionConverterRule extends RelRule<UnionConverterRule.Config> {
         }
 
         call.transformTo(res);
-    }
-
-    private static List<RelNode> convertInputs(RelOptCluster cluster, List<RelNode> inputs, RelTraitSet traits) {
-        List<RelDataType> inputRowTypes = inputs.stream()
-                .map(RelNode::getRowType)
-                .collect(Collectors.toList());
-
-        // Output type of a set operator is equal to leastRestrictive(inputTypes) (see SetOp::deriveRowType)
-
-        RelDataType resultType = cluster.getTypeFactory().leastRestrictive(inputRowTypes);
-        if (resultType == null) {
-            throw new IllegalArgumentException("Cannot compute compatible row type for arguments to set op: " + inputRowTypes);
-        }
-
-        // Check output type of each input, if input's type does not match the result type,
-        // then add a projection with casts for non-matching fields.
-
-        RexBuilder rexBuilder = cluster.getRexBuilder();
-        List<RelNode> actualInputs = new ArrayList<>(inputs.size());
-
-        for (RelNode input : inputs) {
-            RelDataType inputRowType = input.getRowType();
-
-            if (!resultType.equalsSansFieldNames(inputRowType)) {
-                List<RexNode> exprs = new ArrayList<>(inputRowType.getFieldCount());
-
-                for (int i = 0; i < resultType.getFieldCount(); i++) {
-                    RelDataType fieldType = inputRowType.getFieldList().get(i).getType();
-                    RelDataType outFieldType = resultType.getFieldList().get(i).getType();
-                    RexNode ref = rexBuilder.makeInputRef(input, i);
-
-                    if (fieldType.equals(outFieldType)) {
-                        exprs.add(ref);
-                    } else {
-                        RexNode expr = rexBuilder.makeCast(outFieldType, ref, true, false);
-                        exprs.add(expr);
-                    }
-                }
-
-                actualInputs.add(new IgniteProject(cluster, traits, input, exprs, resultType));
-            } else {
-                actualInputs.add(input);
-            }
-        }
-
-        return actualInputs;
     }
 
     /**
