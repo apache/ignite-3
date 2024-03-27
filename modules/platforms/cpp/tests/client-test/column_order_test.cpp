1a7e4a66b01bf1af19003f4e28f98ccab54cd0b1
Igor Sapego <isapego@apache.org>
2024-03-27 01:16:09 +0100

    IGNITE-21527 Add integration test

--- modules/platforms/cpp/tests/client-test/column_order_test.cpp
+++ modules/platforms/cpp/tests/client-test/column_order_test.cpp
@@ -44,14 +44,12 @@ protected:
         client.get_sql().execute(nullptr, {"drop table if exists " TEST_TABLE_NAME}, {});
 
         client.get_sql().execute(nullptr, {
-            "create table " TEST_TABLE_NAME "("
-            "   val1 varchar, key1 integer, val2 bigint, key2 varchar, primary key(key2, key1)"
-            ")"
+            "create table column_order_test(val1 varchar, key1 int, val2 bigint, key2 varchar, primary key(key2, key1))"
         }, {});
 
         for (std::int32_t i = 0; i < test_records; ++i) {
             auto stri = std::to_string(i);
-            client.get_sql().execute(nullptr, {"insert into " TEST_TABLE_NAME " (?, ?, ?, ?)"},
+            client.get_sql().execute(nullptr, {"insert into " TEST_TABLE_NAME " values(?, ?, ?, ?)"},
                 {"test val " + stri, std::int32_t(i), std::int64_t(i*2), "test key " + stri});
         }
     }
@@ -99,17 +97,17 @@ void check_test_tuple(const ignite_tuple &tuple, std::int32_t index, bool key_on
     ASSERT_EQ(key_only ? 2 : 4, tuple.column_count());
 
     auto ref = make_test_full_tuple(index);
-    EXPECT_EQ(ref.get("key1"), tuple.get("key1"));
-    EXPECT_EQ(ref.get("key2"), tuple.get("key2"));
+    EXPECT_EQ(ref.get("key1").get<std::int32_t>(), tuple.get("key1").get<std::int32_t>());
+    EXPECT_EQ(ref.get("key2").get<std::string>(), tuple.get("key2").get<std::string>());
 
     if (!key_only) {
-        EXPECT_EQ(ref.get("val1"), tuple.get("val1"));
-        EXPECT_EQ(ref.get("val2"), tuple.get("val2"));
+        EXPECT_EQ(ref.get("val1").get<std::string>(), tuple.get("val1").get<std::string>());
+        EXPECT_EQ(ref.get("val2").get<std::int64_t>(), tuple.get("val2").get<std::int64_t>());
     }
 }
 
 
-TEST_F(column_order_test, test_key_columns_not_in_beginning) {
+TEST_F(column_order_test, test_key_columns_nondefault_ordering_get) {
     auto tuple_view = m_table.get_record_binary_view();
 
     for (std::int32_t i = 0; i < test_records; ++i) {
@@ -118,4 +116,20 @@ TEST_F(column_order_test, test_key_columns_not_in_beginning) {
         ASSERT_TRUE(res.has_value());
         check_test_tuple(*res, i, false);
     }
+}
+
+TEST_F(column_order_test, test_key_columns_nondefault_ordering_put_get_remove) {
+    auto tuple_view = m_table.get_record_binary_view();
+
+    tuple_view.upsert(nullptr, make_test_full_tuple(13));
+
+    auto res = tuple_view.get(nullptr, make_test_key_tuple(13));
+    ASSERT_TRUE(res.has_value());
+    check_test_tuple(*res, 13, false);
+
+    bool removed = tuple_view.remove(nullptr, make_test_key_tuple(13));
+    ASSERT_TRUE(removed);
+
+    res = tuple_view.get(nullptr, make_test_key_tuple(13));
+    ASSERT_FALSE(res.has_value());
 }
