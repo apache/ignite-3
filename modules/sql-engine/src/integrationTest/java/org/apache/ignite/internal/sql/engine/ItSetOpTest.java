8e37455c9065c7956b5edbab58603ead75731b77
Max Zhuravkov <shhwwa@gmail.com>
2024-03-27 16:44:19 +0200

    IGNITE-21827: Sql. Union returns incorrect result for DECIMAL and INT columns
    Remove unused import.

--- modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItSetOpTest.java
+++ modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/engine/ItSetOpTest.java
@@ -40,7 +40,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Integration test for set op (EXCEPT, INTERSECT).
