0d8c3a150ceff98da25a831df325dc9b6c643345
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2024-03-28 20:27:00 +0400

    IGNITE-21869 Prevent thread hijacking via IgniteSql

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -172,6 +172,7 @@ import org.apache.ignite.internal.schema.configuration.StorageUpdateConfiguratio
 import org.apache.ignite.internal.security.authentication.AuthenticationManager;
 import org.apache.ignite.internal.security.authentication.AuthenticationManagerImpl;
 import org.apache.ignite.internal.security.configuration.SecurityConfiguration;
+import org.apache.ignite.internal.sql.api.AntiHijackIgniteSql;
 import org.apache.ignite.internal.sql.api.IgniteSqlImpl;
 import org.apache.ignite.internal.sql.configuration.distributed.SqlDistributedConfiguration;
 import org.apache.ignite.internal.sql.configuration.local.SqlLocalConfiguration;
@@ -752,7 +753,7 @@ public class IgniteImpl implements Ignite {
                 catalogManager,
                 observableTimestampTracker,
                 placementDriverMgr.placementDriver(),
-                this::sql,
+                this::bareSql,
                 resourcesRegistry,
                 rebalanceScheduler,
                 lowWatermark,
@@ -1193,10 +1194,14 @@ public class IgniteImpl implements Ignite {
         return new AntiHijackIgniteTransactions(transactions, asyncContinuationExecutor);
     }
 
+    private IgniteSql bareSql() {
+        return sql;
+    }
+
     /** {@inheritDoc} */
     @Override
     public IgniteSql sql() {
-        return sql;
+        return new AntiHijackIgniteSql(sql, asyncContinuationExecutor);
     }
 
     /** {@inheritDoc} */
@@ -1231,7 +1236,7 @@ public class IgniteImpl implements Ignite {
 
     @Override
     public IgniteCatalog catalog(Options options) {
-        return new IgniteCatalogSqlImpl(sql(), options);
+        return new IgniteCatalogSqlImpl(sql, options);
     }
 
     /**
