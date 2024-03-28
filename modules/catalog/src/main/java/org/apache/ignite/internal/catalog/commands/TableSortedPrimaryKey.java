37a80743d07e1f8094e9da3a28e5fcd643ec0c28
ygerzhedovich <41903880+ygerzhedovich@users.noreply.github.com>
2024-03-28 13:49:12 +0200

    IGNITE-21673 Do not allow duplicate column names in unique column list of a primary key constraint. (#3501)

--- modules/catalog/src/main/java/org/apache/ignite/internal/catalog/commands/TableSortedPrimaryKey.java
+++ modules/catalog/src/main/java/org/apache/ignite/internal/catalog/commands/TableSortedPrimaryKey.java
@@ -18,7 +18,6 @@
 package org.apache.ignite.internal.catalog.commands;
 
 import java.util.List;
-import java.util.Set;
 import org.apache.ignite.internal.catalog.CatalogValidationException;
 import org.apache.ignite.internal.catalog.descriptors.CatalogColumnCollation;
 import org.apache.ignite.internal.tostring.S;
@@ -46,11 +45,11 @@ public class TableSortedPrimaryKey extends TablePrimaryKey {
 
     /** {@inheritDoc} */
     @Override
-    void validate(Set<String> columns) {
-        super.validate(columns);
+    void validate(List<ColumnParams> allColumns) {
+        super.validate(allColumns);
 
         if (columns().size() != collations.size()) {
-            throw new CatalogValidationException("Number of collations does not match");
+            throw new CatalogValidationException("Number of collations does not match.");
         }
     }
 
