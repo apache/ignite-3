ae77bee3b7f788467bc56174e46220b5a2c38fdd
Aleksandr Polovtsev <alex.polovtcev@gmail.com>
2024-04-03 11:13:33 +0300

    IGNITE-21909 Fix race on getting and destroying an index in SharedRocksDbInstance

--- modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/RocksDbIndexes.java
+++ modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/RocksDbIndexes.java
@@ -73,7 +73,7 @@ class RocksDbIndexes {
                 }
             }
 
-            var indexCfsToDestroy = new ArrayList<IndexColumnFamily>();
+            var indexCfsToDestroy = new ArrayList<ColumnFamily>();
 
             for (IndexColumnFamily indexColumnFamily : rocksDb.sortedIndexes(tableId)) {
                 int indexId = indexColumnFamily.indexId();
@@ -83,9 +83,11 @@ class RocksDbIndexes {
                 var descriptor = (StorageSortedIndexDescriptor) indexDescriptorSupplier.get(indexId);
 
                 if (descriptor == null) {
+                    rocksDb.removeSortedIndex(indexId, cf);
+
                     deleteByPrefix(writeBatch, cf, indexPrefix(tableId, indexId));
 
-                    indexCfsToDestroy.add(indexColumnFamily);
+                    indexCfsToDestroy.add(cf);
                 } else {
                     sortedIndices.put(indexId, SortedIndex.restoreExisting(tableId, cf, descriptor, rocksDb.meta));
                 }
@@ -174,6 +176,8 @@ class RocksDbIndexes {
             }
 
             if (sortedIdx != null) {
+                rocksDb.removeSortedIndex(indexId, sortedIdx.columnFamily());
+
                 sortedIdx.destroy(writeBatch);
             }
 
@@ -181,7 +185,7 @@ class RocksDbIndexes {
         }
 
         if (sortedIdx != null) {
-            rocksDb.scheduleIndexCfsDestroy(List.of(sortedIdx.indexColumnFamily()));
+            rocksDb.scheduleIndexCfsDestroy(List.of(sortedIdx.columnFamily()));
         }
     }
 
