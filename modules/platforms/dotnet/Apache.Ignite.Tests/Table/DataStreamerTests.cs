4f8028af6e08b65cbae28f97e4effc26798b78f3
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-04-04 18:11:26 +0300

    wip tests

--- modules/platforms/dotnet/Apache.Ignite.Tests/Table/DataStreamerTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/DataStreamerTests.cs
@@ -36,7 +36,7 @@ public class DataStreamerTests : IgniteTestsBase
 {
     private const int Count = 100;
 
-    private const int UpdatedKey = Count - 1;
+    private const int UpdatedKey = 0;
     private const int DeletedKey = Count + 1;
 
     [SetUp]
@@ -50,14 +50,19 @@ public class DataStreamerTests : IgniteTestsBase
     [Test]
     public async Task TestBasicStreamingRecordBinaryView()
     {
-        await TupleView.UpsertAsync(null, GetTuple(0, "old"));
-
-        var options = DataStreamerOptions.Default with { PageSize = 10 };
-        var data = Enumerable.Range(0, Count).Select(x => GetTuple(x, "t" + x)).ToList();
-        await TupleView.StreamDataAsync(data.ToAsyncEnumerable(), options);
-        await TupleView.StreamDataAsync(new[] { DataStreamerItem.Create(GetTuple(DeletedKey), DataStreamerOperationType.Remove) }.ToAsyncEnumerable());
-
+        await TupleView.StreamDataAsync(GetData(), DataStreamerOptions.Default with { PageSize = 10 });
         await CheckData();
+
+        static async IAsyncEnumerable<DataStreamerItem<IIgniteTuple>> GetData()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return DataStreamerItem.Create(GetTuple(i, "t" + i));
+            }
+
+            await Task.Yield();
+            yield return DataStreamerItem.Create(GetTuple(DeletedKey), DataStreamerOperationType.Remove);
+        }
     }
 
     [Test]
