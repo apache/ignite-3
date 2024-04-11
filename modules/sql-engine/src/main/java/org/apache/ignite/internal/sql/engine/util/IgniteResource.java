7805eca3d7d7b540f3f6fa5b4e2ba50877960e37
Max Zhuravkov <shhwwa@gmail.com>
2024-04-11 12:56:41 +0300

    IGNITE-22022: Sql. Json object with nested object creation fails

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/IgniteResource.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/util/IgniteResource.java
@@ -75,6 +75,9 @@ public interface IgniteResource {
     @Resources.BaseMessage("Incorrect type of a dynamic parameter. Expected <{0}> but got <{1}>")
     Resources.ExInst<SqlValidatorException> incorrectDynamicParameterType(String expected, String actual);
 
+    @Resources.BaseMessage("Expression is not supported: {0}")
+    Resources.ExInst<SqlValidatorException> unsupportedExpression(String exprType);
+
     /** Constructs a signature string to use in error messages. */
     static String makeSignature(SqlCallBinding binding, RelDataType... operandTypes) {
         return makeSignature(binding, Arrays.asList(operandTypes));
