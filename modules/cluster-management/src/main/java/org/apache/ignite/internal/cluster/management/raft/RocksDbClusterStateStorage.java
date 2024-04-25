48713500874a9b8115f1bece6607abc514cf692a
Alexander Polovtcev <alex.polovtcev@gmail.com>
2024-04-25 16:58:27 +0300

    IGNITE-22105 Add busy lock to RocksDbClusterStateStorage (#3659)

--- modules/cluster-management/src/main/java/org/apache/ignite/internal/cluster/management/raft/RocksDbClusterStateStorage.java
+++ modules/cluster-management/src/main/java/org/apache/ignite/internal/cluster/management/raft/RocksDbClusterStateStorage.java
@@ -20,8 +20,11 @@ package org.apache.ignite.internal.cluster.management.raft;
 import static java.util.concurrent.CompletableFuture.failedFuture;
 import static org.apache.ignite.internal.rocksdb.snapshot.ColumnFamilyRange.fullRange;
 import static org.apache.ignite.internal.util.CompletableFutures.nullCompletedFuture;
+import static org.apache.ignite.internal.util.IgniteUtils.inBusyLock;
+import static org.apache.ignite.internal.util.IgniteUtils.inBusyLockAsync;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -29,15 +32,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiFunction;
 import org.apache.ignite.internal.logger.IgniteLogger;
 import org.apache.ignite.internal.logger.Loggers;
 import org.apache.ignite.internal.rocksdb.ColumnFamily;
-import org.apache.ignite.internal.rocksdb.RocksIteratorAdapter;
 import org.apache.ignite.internal.rocksdb.RocksUtils;
 import org.apache.ignite.internal.rocksdb.snapshot.RocksSnapshotManager;
 import org.apache.ignite.internal.thread.NamedThreadFactory;
-import org.apache.ignite.internal.util.Cursor;
+import org.apache.ignite.internal.util.IgniteSpinBusyLock;
 import org.apache.ignite.internal.util.IgniteUtils;
 import org.jetbrains.annotations.Nullable;
 import org.rocksdb.Options;
@@ -64,6 +67,8 @@ public class RocksDbClusterStateStorage implements ClusterStateStorage {
     /** RockDB options. */
     private final Options options = new Options().setCreateIfMissing(true);
 
+    private final WriteOptions defaultWriteOptions = new WriteOptions().setDisableWAL(true);
+
     /** RocksDb instance. */
     @Nullable
     private volatile RocksDB db;
@@ -71,6 +76,11 @@ public class RocksDbClusterStateStorage implements ClusterStateStorage {
     private volatile RocksSnapshotManager snapshotManager;
 
     private final Object snapshotRestoreLock = new Object();
+
+    private final IgniteSpinBusyLock busyLock = new IgniteSpinBusyLock();
+
+    /** Prevents double stopping of the component. */
+    private final AtomicBoolean stopGuard = new AtomicBoolean();
 
     /**
      * Creates a new instance.
@@ -87,18 +97,18 @@ public class RocksDbClusterStateStorage implements ClusterStateStorage {
 
     @Override
     public CompletableFuture<Void> start() {
-        try {
-            // Delete existing data, relying on log playback.
-            RocksDB.destroyDB(dbPath.toString(), options);
+        return inBusyLockAsync(busyLock, () -> {
+            try {
+                // Delete existing data, relying on log playback.
+                RocksDB.destroyDB(dbPath.toString(), options);
 
-            init();
-        } catch (RocksDBException e) {
-            return failedFuture(new CmgStorageException("Failed to start the storage", e));
-        } catch (CmgStorageException e) {
-            return failedFuture(e);
-        }
+                init();
 
-        return nullCompletedFuture();
+                return nullCompletedFuture();
+            } catch (RocksDBException e) {
+                return failedFuture(new CmgStorageException("Failed to start the storage", e));
+            }
+        });
     }
 
     private void init() {
@@ -117,133 +127,132 @@ public class RocksDbClusterStateStorage implements ClusterStateStorage {
 
     @Override
     public byte @Nullable [] get(byte[] key) {
-        try {
-            return db.get(key);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to get data from Rocks DB", e);
-        }
+        return inBusyLock(busyLock, () -> {
+            try {
+                return db.get(key);
+            } catch (RocksDBException e) {
+                throw new CmgStorageException("Unable to get data from Rocks DB", e);
+            }
+        });
     }
 
     @Override
     public void put(byte[] key, byte[] value) {
-        try {
-            db.put(key, value);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to put data into Rocks DB", e);
-        }
+        inBusyLock(busyLock, () -> {
+            try {
+                db.put(defaultWriteOptions, key, value);
+            } catch (RocksDBException e) {
+                throw new CmgStorageException("Unable to put data into Rocks DB", e);
+            }
+        });
     }
 
     @Override
     public void replaceAll(byte[] prefix, byte[] key, byte[] value) {
-        try (
-                var batch = new WriteBatch();
-                var options = new WriteOptions();
-        ) {
-            byte[] endKey = RocksUtils.incrementPrefix(prefix);
+        inBusyLock(busyLock, () -> {
+            try (var batch = new WriteBatch()) {
+                byte[] endKey = RocksUtils.incrementPrefix(prefix);
 
-            assert endKey != null : Arrays.toString(prefix);
+                assert endKey != null : Arrays.toString(prefix);
 
-            batch.deleteRange(prefix, endKey);
+                batch.deleteRange(prefix, endKey);
 
-            batch.put(key, value);
+                batch.put(key, value);
 
-            db.write(options, batch);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to replace data in Rocks DB", e);
-        }
+                db.write(defaultWriteOptions, batch);
+            } catch (RocksDBException e) {
+                throw new CmgStorageException("Unable to replace data in Rocks DB", e);
+            }
+        });
     }
 
     @Override
     public void remove(byte[] key) {
-        try {
-            db.delete(key);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to remove data from Rocks DB", e);
-        }
+        inBusyLock(busyLock, () -> {
+            try {
+                db.delete(defaultWriteOptions, key);
+            } catch (RocksDBException e) {
+                throw new CmgStorageException("Unable to remove data from Rocks DB", e);
+            }
+        });
     }
 
     @Override
     public void removeAll(Collection<byte[]> keys) {
-        try (
-                var batch = new WriteBatch();
-                var options = new WriteOptions();
-        ) {
-            for (byte[] key : keys) {
-                batch.delete(key);
-            }
+        inBusyLock(busyLock, () -> {
+            try (var batch = new WriteBatch()) {
+                for (byte[] key : keys) {
+                    batch.delete(key);
+                }
 
-            db.write(options, batch);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to remove data from Rocks DB", e);
-        }
+                db.write(defaultWriteOptions, batch);
+            } catch (RocksDBException e) {
+                throw new CmgStorageException("Unable to remove data from Rocks DB", e);
+            }
+        });
     }
 
     @Override
-    public <T> Cursor<T> getWithPrefix(byte[] prefix, BiFunction<byte[], byte[], T> entryTransformer) {
-        byte[] upperBound = RocksUtils.incrementPrefix(prefix);
+    public <T> List<T> getWithPrefix(byte[] prefix, BiFunction<byte[], byte[], T> entryTransformer) {
+        return inBusyLock(busyLock, () -> {
+            byte[] upperBound = RocksUtils.incrementPrefix(prefix);
 
-        Slice upperBoundSlice = upperBound == null ? null : new Slice(upperBound);
+            try (
+                    Slice upperBoundSlice = upperBound == null ? null : new Slice(upperBound);
+                    ReadOptions readOptions = new ReadOptions().setIterateUpperBound(upperBoundSlice);
+                    RocksIterator it = db.newIterator(readOptions)
+            ) {
+                it.seek(prefix);
 
-        ReadOptions readOptions = new ReadOptions().setIterateUpperBound(upperBoundSlice);
+                var result = new ArrayList<T>();
 
-        RocksIterator it = db.newIterator(readOptions);
+                try {
+                    RocksUtils.forEach(it, (key, value) -> result.add(entryTransformer.apply(key, value)));
+                } catch (RocksDBException e) {
+                    throw new CmgStorageException("Unable to get data by prefix", e);
+                }
 
-        it.seek(prefix);
-
-        return new RocksIteratorAdapter<>(it) {
-            @Override
-            protected T decodeEntry(byte[] key, byte[] value) {
-                return entryTransformer.apply(key, value);
+                return result;
             }
-
-            @Override
-            public void close() {
-                super.close();
-
-                RocksUtils.closeAll(readOptions, upperBoundSlice);
-            }
-        };
+        });
     }
 
     @Override
     public CompletableFuture<Void> snapshot(Path snapshotPath) {
-        return snapshotManager.createSnapshot(snapshotPath);
+        return inBusyLockAsync(busyLock, () -> snapshotManager.createSnapshot(snapshotPath));
     }
 
     @Override
     public void restoreSnapshot(Path snapshotPath) {
-        synchronized (snapshotRestoreLock) {
-            destroyDb();
+        inBusyLock(busyLock, () -> {
+            synchronized (snapshotRestoreLock) {
+                db.close();
 
-            init();
+                db = null;
 
-            snapshotManager.restoreSnapshot(snapshotPath);
-        }
-    }
+                try {
+                    RocksDB.destroyDB(dbPath.toString(), options);
+                } catch (RocksDBException e) {
+                    throw new CmgStorageException("Unable to stop the RocksDB instance", e);
+                }
 
-    @Override
-    public void destroy() {
-        IgniteUtils.shutdownAndAwaitTermination(snapshotExecutor, 10, TimeUnit.SECONDS);
+                init();
 
-        destroyDb();
-
-        options.close();
+                snapshotManager.restoreSnapshot(snapshotPath);
+            }
+        });
     }
 
     @Override
     public void stop() {
+        if (!stopGuard.compareAndSet(false, true)) {
+            return;
+        }
+
+        busyLock.block();
+
         IgniteUtils.shutdownAndAwaitTermination(snapshotExecutor, 10, TimeUnit.SECONDS);
 
-        RocksUtils.closeAll(db, options);
-    }
-
-    private void destroyDb() {
-        db.close();
-
-        try {
-            RocksDB.destroyDB(dbPath.toString(), options);
-        } catch (RocksDBException e) {
-            throw new CmgStorageException("Unable to stop the RocksDB instance", e);
-        }
+        RocksUtils.closeAll(db, options, defaultWriteOptions);
     }
 }
