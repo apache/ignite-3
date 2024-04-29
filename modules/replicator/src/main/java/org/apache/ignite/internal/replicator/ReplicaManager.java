bcea969eba8c85fe6dffee1c5416c58b6c80be74
Mikhail Efremov <jakutenshi@gmail.com>
2024-04-29 22:04:47 +0600

    IGNITE-22001 Checkstyle fix

--- modules/replicator/src/main/java/org/apache/ignite/internal/replicator/ReplicaManager.java
+++ modules/replicator/src/main/java/org/apache/ignite/internal/replicator/ReplicaManager.java
@@ -573,13 +573,21 @@ public class ReplicaManager extends AbstractEventProducer<LocalReplicaEvent, Loc
         CompletableFuture<ReplicaListener> newReplicaListenerFut = createRaftClientAsync(replicaGrpId, newConfiguration)
                 .thenApply(createListener);
 
-//        if (shouldSkipReplicaStarting) {
-//            return nullCompletedFuture();
-//        }
+        // if (shouldSkipReplicaStarting) {
+        //     return nullCompletedFuture();
+        // }
 
         return temporalInternalCreateReplica(replicaGrpId, storageIndexTracker, newReplicaListenerFut);
     }
 
+    /**
+     * TODO.
+     *
+     * @param replicaGrpId TODO
+     * @param storageIndexTracker TODO
+     * @param newReplicaListenerFut TODO
+     * @return TODO
+     */
     public CompletableFuture<Replica> temporalInternalCreateReplica(
             ReplicationGroupId replicaGrpId,
             PendingComparableValuesTracker<Long, Void> storageIndexTracker,
