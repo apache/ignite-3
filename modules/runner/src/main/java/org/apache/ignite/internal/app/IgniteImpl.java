70eb42535cddcaf3082fc1811ad99510aebea515
Max Zhuravkov <shhwwa@gmail.com>
2024-04-30 17:07:20 +0300

    [IGNITE-19082] Catalog. Cleanup dead code
    await catalog activation.

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -1112,7 +1112,7 @@ public class IgniteImpl implements Ignite {
                         return cmgMgr.onJoinReady();
                     }, startupExecutor)
                     .thenComposeAsync(ignored -> awaitSelfInLocalLogicalTopology(), startupExecutor)
-                    .thenCompose(c -> systemViewManager.completeRegistration())
+                    .thenCompose(c -> catalogManager.catalogInitializationFuture())
                     .thenRunAsync(() -> {
                         try {
                             // Enable watermark events.
