3e13bacce19ad5f1f19b43b1ec952c8e314f0b34
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2024-05-16 13:04:14 +0300

    IGNITE-22254 Cluster initialization fails when event log is configured

--- modules/eventlog/src/main/java/org/apache/ignite/internal/eventlog/api/IgniteEventType.java
+++ modules/eventlog/src/main/java/org/apache/ignite/internal/eventlog/api/IgniteEventType.java
@@ -28,9 +28,10 @@ public enum IgniteEventType {
     USER_AUTHENTICATED,
     CONNECTION_CLOSED;
 
-    static {
-        // Without the following line, the IgniteEventType enum will not be registered in the EventTypeRegistry
-        // and the EventTypeRegistry will not be able to validate the event types.
+    /**
+     * Registers all event types.
+     */
+    public static void initialize() {
         Arrays.stream(values()).forEach(type -> EventTypeRegistry.register(type.name()));
     }
 }
