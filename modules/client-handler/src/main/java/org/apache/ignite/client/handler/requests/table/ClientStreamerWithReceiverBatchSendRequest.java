fceb1580f7f997af1048dc726922762353ca9c3c
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-05-21 15:47:55 +0300

    wip

--- modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/table/ClientStreamerWithReceiverBatchSendRequest.java
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/table/ClientStreamerWithReceiverBatchSendRequest.java
@@ -22,15 +22,21 @@ import static org.apache.ignite.lang.ErrorGroups.Client.PROTOCOL_ERR;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.compute.ComputeJob;
 import org.apache.ignite.compute.DeploymentUnit;
 import org.apache.ignite.compute.JobExecutionContext;
+import org.apache.ignite.compute.JobExecutionOptions;
+import org.apache.ignite.internal.binarytuple.BinaryTupleReader;
+import org.apache.ignite.internal.client.proto.ClientBinaryTupleUtils;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 import org.apache.ignite.internal.compute.ComputeUtils;
+import org.apache.ignite.internal.compute.IgniteComputeInternal;
 import org.apache.ignite.lang.IgniteException;
+import org.apache.ignite.network.ClusterNode;
 import org.apache.ignite.table.DataStreamerReceiver;
 import org.apache.ignite.table.DataStreamerReceiverContext;
 import org.apache.ignite.table.manager.IgniteTables;
@@ -51,49 +57,71 @@ public class ClientStreamerWithReceiverBatchSendRequest {
             ClientMessageUnpacker in,
             ClientMessagePacker out,
             IgniteTables tables,
-            Ignite ignite
+            IgniteComputeInternal compute
     ) {
         return readTableAsync(in, tables).thenCompose(table -> {
-            int partition = in.unpackInt();
-            List<DeploymentUnit> deploymentUnits = in.unpackDeploymentUnits();
-            boolean returnResults = in.unpackBoolean();
+                    int partition = in.unpackInt();
+                    List<DeploymentUnit> deploymentUnits = in.unpackDeploymentUnits();
+                    boolean returnResults = in.unpackBoolean();
 
-            String receiverClassName = in.unpackString();
-            Object[] receiverArgs = in.unpackObjectArrayFromBinaryTuple();
-            List<Object> items = in.unpackCollectionFromBinaryTuple();
+                    // receiverClassName, receiverArgs, items
+                    int payloadElementCount = in.unpackInt();
+                    byte[] payload = in.readBinary();
+                    Set<ClusterNode> candidateNodes = Set.of(); // TODO
 
-            if (items == null) {
-                throw new IgniteException(PROTOCOL_ERR, "Data streamer items are null.");
-            }
+                    var jobExecution = compute.executeAsyncWithFailover(
+                            candidateNodes,
+                            deploymentUnits,
+                            ReceiverRunnerJob.class.getName(),
+                            JobExecutionOptions.DEFAULT,
+                            payloadFieldCount,
+                            payload);
 
-            // TODO: We need to pass the following further to the compute job.
-            // So we should put all of that stuff into a single object somehow. A single BinaryTuple.
-            List<Object> passThrough = new ArrayList<>();
-            passThrough.add(receiverClassName);
-            passThrough.add(receiverArgs);
-            passThrough.add(items);
-
-            // TODO: Get class loader from units.
-            // TODO: use Compute component to execute receiver on specific node with failover, proper executor, etc.
-            // use this.getClass().classLoader() to get the class loader which Compute has prepared for us.
-            Class<DataStreamerReceiver<Object, Object>> receiverClass = ComputeUtils.receiverClass(
-                    ClassLoader.getSystemClassLoader(), receiverClassName);
-
-            DataStreamerReceiver<Object, Object> receiver = ComputeUtils.instantiateReceiver(receiverClass);
-            DataStreamerReceiverContext context = () -> ignite;
-
-            return receiver.receive(items, context, receiverArgs)
-                    .thenApply(res -> {
-                        out.packCollectionAsBinaryTuple(returnResults ? res : null);
+                    return jobExecution.resultAsync().thenApply(res -> {
+                        // TODO:
+                        // out.packCollectionAsBinaryTuple(returnResults ? res : null);
+                        out.packNil();
 
                         return null;
                     });
-        });
+                });
+
+            // TODO: use Compute component to execute receiver on specific node with failover, proper executor, etc.
+            // use this.getClass().classLoader() to get the class loader which Compute has prepared for us.
+//            Class<DataStreamerReceiver<Object, Object>> receiverClass = ComputeUtils.receiverClass(
+//                    ClassLoader.getSystemClassLoader(), receiverClassName);
+//
+//            DataStreamerReceiver<Object, Object> receiver = ComputeUtils.instantiateReceiver(receiverClass);
+//            DataStreamerReceiverContext context = () -> ignite;
+//
+//            return receiver.receive(items, context, receiverArgs)
+//                    .thenApply(res -> {
+//                        out.packCollectionAsBinaryTuple(returnResults ? res : null);
+//
+//                        return null;
+//                    });
     }
 
     private static class ReceiverRunnerJob implements ComputeJob<List<Object>> {
         @Override
         public List<Object> execute(JobExecutionContext context, Object... args) {
+            int payloadElementCount = (int) args[0];
+            byte[] payload = (byte[]) args[1];
+            BinaryTupleReader reader = new BinaryTupleReader(payloadElementCount, payload);
+
+            String receiverClassName = reader.stringValue(0);
+            int receiverArgsCount = reader.intValue(1);
+
+            List<Object> receiverArgs = new ArrayList<>(receiverArgsCount);
+            for (int i = 0; i < receiverArgsCount; i++) {
+                receiverArgs.add(ClientBinaryTupleUtils.readObject(reader, 2 + i * 3));
+            }
+
+            int itemsCount = reader.intValue(2 + receiverArgsCount * 3);
+
+
+            DataStreamerReceiver<Object, Object> receiver = ComputeUtils.instantiateReceiver(receiverClass);
+            DataStreamerReceiverContext receiverContext = context::ignite;
             return List.of();
         }
     }
