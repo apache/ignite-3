a819beee0097803c002725cf4948c2454c69d96e
dzabotlin <dzabotlin@gridgain.com>
2024-06-05 16:36:48 +0300

    Commit missing file and update doc

--- modules/platforms/cpp/DEVNOTES.md
+++ modules/platforms/cpp/DEVNOTES.md
@@ -15,7 +15,7 @@ The CMake project supports multiple configuration options that can be used to de
 built. You can check the list of available CMake options with the `cmake -LAH` command. 
 Following are the project-specific options:
 
-* USE_LOCAL_DEPS={ON|OFF} - OFF by default. Indicates that build system at first will try to use locally installed 
+* PREFER_LOCAL_DEPS={ON|OFF} - OFF by default. Indicates that build system at first will try to use locally installed 
 libraries and will try to download dependencies only if it will not be found locally.
 * ENABLE_CLIENT={ON|OFF} - ON by default. Indicates whether the C++ client should be built.
 * ENABLE_ODBC={ON|OFF} - OFF by default. Indicates whether the ODBC driver should be built.
@@ -23,6 +23,12 @@ libraries and will try to download dependencies only if it will not be found loc
 * WARNINGS_AS_ERRORS={ON|OFF} - OFF by default. If enabled, the compiler treats warnings as errors. It may be a good 
 idea to enable this option if you are planning on submitting a PR. If you just want to build a project, keep this 
 option disabled.
+
+USE_LOCAL_DEPS option set to OFF implies that dependencies will be downloaded, so you will need an internet connection.
+If you want to build project with your local dependencies you can set option PREFER_LOCAL_DEPS to ON. 
+In this case you have to install: 
+ * msgpack-c development package (for Ubuntu: libmsgpack-dev)
+ * Google test library (for Ubuntu: libgtest-dev) if you want to build tests (ENABLE_TESTS = ON)
 
 You should also specify the general (build type) CMake options. There are two types of build available - `Release` and `Debug`. The choice depends on how are you going to use the resulting artifacts. If you are going to use them in production, use the `Release` build type. If you are planning to just submit a patch for the project, use `Debug`.
 
---
+++ modules/platforms/cpp/cmake/dependencies.cmake
@@ -0,0 +1,59 @@
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+    # Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP:
+    cmake_policy(SET CMP0135 NEW)
+endif()
+
+# Policy about variables override by options
+# https://cmake.org/cmake/help/latest/policy/CMP0077.html
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+set(MSGPACK_BUILD_TESTS OFF)
+set(MSGPACK_GEN_COVERAGE OFF)
+set(MSGPACK_BUILD_EXAMPLES OFF)
+
+set(DOWNLOAD_MSGPACK TRUE)
+set(DOWNLOAD_GTEST TRUE)
+
+if (${PREFER_LOCAL_DEPS})
+    find_package(msgpack QUIET)
+    if (${msgpack_FOUND})
+        # ALIAS for the msgpack-c version lower than 6.0.0
+        add_library(msgpack-c-static ALIAS msgpackc-static)
+        message(STATUS "SYSTEM MSGPACK FOUND: " ${msgpack_VERSION})
+        set(DOWNLOAD_MSGPACK FALSE)
+    endif()
+
+    if (${ENABLE_TESTS})
+        find_package(GTest QUIET)
+        if (${GTest_FOUND})
+            if (TARGET GTest::Main AND NOT TARGET GTest::gtest_main)
+                add_library(GTest::gtest_main ALIAS GTest::Main)
+            endif()
+            message(STATUS "SYSTEM GTEST FOUND: " ${GTest_VERSION})
+            set(DOWNLOAD_GTEST FALSE)
+        endif()
+    endif()
+endif()
+
+include(FetchContent)
+if (${DOWNLOAD_MSGPACK})
+    message(STATUS "DOWNLOAD MSGPACK")
+    FetchContent_Declare(
+            msgpack-c
+            URL https://github.com/msgpack/msgpack-c/releases/download/c-6.0.1/msgpack-c-6.0.1.tar.gz
+            URL_HASH MD5=090df53a59b845767fcfc48221b30ee9
+    )
+    FetchContent_MakeAvailable(msgpack-c)
+endif()
+
+if (${ENABLE_TESTS})
+    if (${DOWNLOAD_GTEST})
+        message(STATUS "DOWNLOAD GTEST")
+        FetchContent_Declare(
+                googletest
+                URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.tar.gz
+                URL_HASH MD5=c8340a482851ef6a3fe618a082304cfc
+        )
+        FetchContent_MakeAvailable(googletest)
+    endif()
+endif()
