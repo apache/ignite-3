cd06b235110d29609ae054054147f29c1c873443
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-06-07 09:45:28 +0300

    wip ExecutionTarget

--- modules/api/src/main/java/org/apache/ignite/compute/ExecutionTarget.java
+++ modules/api/src/main/java/org/apache/ignite/compute/ExecutionTarget.java
@@ -1,6 +1,7 @@
 package org.apache.ignite.compute;
 
 import org.apache.ignite.table.Tuple;
+import org.apache.ignite.table.mapper.Mapper;
 
 public interface ExecutionTarget {
     static ExecutionTarget anyNode() {
@@ -15,7 +16,11 @@ public interface ExecutionTarget {
         return null; // TODO
     }
 
-    static ExecutionTarget fromColocationKey(Tuple key) {
+    static ExecutionTarget fromColocationKey(String tableName, Tuple key) {
+        return null; // TODO
+    }
+
+    static <K> ExecutionTarget fromColocationKey(String tableName, K key, Mapper<K> keyMapper) {
         return null; // TODO
     }
 }
