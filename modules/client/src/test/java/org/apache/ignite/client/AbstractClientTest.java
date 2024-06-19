37829a23e9db8b6d82d0e7b54c3b2d662fc42155
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-06-19 09:32:26 +0300

    fixing client tests

--- modules/client/src/test/java/org/apache/ignite/client/AbstractClientTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/AbstractClientTest.java
@@ -29,6 +29,7 @@ import org.apache.ignite.Ignite;
 import org.apache.ignite.client.fakes.FakeIgnite;
 import org.apache.ignite.client.fakes.FakeIgniteTables;
 import org.apache.ignite.client.fakes.FakeSchemaRegistry;
+import org.apache.ignite.compute.JobTarget;
 import org.apache.ignite.internal.client.ClientClusterNode;
 import org.apache.ignite.internal.testframework.BaseIgniteAbstractTest;
 import org.apache.ignite.network.ClusterNode;
@@ -186,9 +187,9 @@ public abstract class AbstractClientTest extends BaseIgniteAbstractTest {
      * @param names Names.
      * @return Nodes.
      */
-    public static Set<ClusterNode> getClusterNodes(String... names) {
-        return Arrays.stream(names)
+    public static JobTarget getClusterNodes(String... names) {
+        return JobTarget.anyNode(Arrays.stream(names)
                 .map(s -> new ClientClusterNode("id", s, new NetworkAddress("127.0.0.1", 8080)))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toSet()));
     }
 }
