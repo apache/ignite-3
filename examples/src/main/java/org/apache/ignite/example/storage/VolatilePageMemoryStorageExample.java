f07661cf2f55e0a4f6dd6c6e83be7784f5750b4e
Ivan Artiukhov <iartukhov@gridgain.com>
2024-06-24 11:24:32 +0300

    examples: replace instructions with the link to README

--- examples/src/main/java/org/apache/ignite/example/storage/VolatilePageMemoryStorageExample.java
+++ examples/src/main/java/org/apache/ignite/example/storage/VolatilePageMemoryStorageExample.java
@@ -20,41 +20,7 @@ package org.apache.ignite.example.storage;
 /**
  * This example demonstrates a usage of the PageMemory storage engine configured with an in-memory data region.
  *
- * <p>To run the example, please do the following:
- * <ol>
- *     <li>Open the Ignite project in your IDE of choice.</li>
- *     <li>
- *         Download the Ignite ZIP packaging with DB and CLI parts.
- *         Or build them from Ignite sources (see {@code DEVNOTES.md}).
- *         Unpack.
- *     </li>
- *     <li>
- *         Prepare the environment variables:<br>
- *         <code>
- *             export IGNITE_HOME=/path/to/ignite3-db-VERSION<br>
- *             export IGNITE_CLI_HOME=/path/to/ignite3-cli-VERSION<br>
- *             export IGNITE_SRC_HOME=/path/to/ignite/sources
- *         </code>
- *     </li>
- *     <li>
- *         Override the default configuration file:<br>
- *         {@code echo "CONFIG_FILE=$IGNITE_SRC_HOME/examples/config/ignite-config.conf" >> $IGNITE_HOME/etc/vars.env}
- *     </li>
- *     <li>
- *         Start an Ignite node using the startup script from the DB part:<br>
- *         {@code ${IGNITE_HOME}/bin/ignite3db start}
- *     </li>
- *     <li>
- *         Initialize the cluster using Ignite CLI from the CLI part:<br>
- *         {@code $IGNITE_CLI_HOME/bin/ignite3 cluster init --name myCluster1 --metastorage-group defaultNode
- *         --cluster-management-group defaultNode}
- *     </li>
- *     <li>Run the example from the IDE.</li>
- *     <li>
- *         Stop the Ignite node using the startup script:<br>
- *         {@code ${IGNITE_HOME}/bin/ignite3db stop}
- *     </li>
- * </ol>
+ * <p>Find instructions on how to run the example in the {@code README.md}:
  */
 public class VolatilePageMemoryStorageExample {
     /**
