72ba93501d0fbb2d7800e6cdef4ca7b8da37aa49
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2024-07-11 15:47:39 +0400

    IGNITE-22710 / map PrimaryReplicaExpiredException to TransactionException

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/TransactionExceptionMapperProvider.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/TransactionExceptionMapperProvider.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.apache.ignite.internal.lang.IgniteExceptionMapper;
 import org.apache.ignite.internal.lang.IgniteExceptionMappersProvider;
 import org.apache.ignite.internal.replicator.exception.ReplicationException;
+import org.apache.ignite.internal.tx.impl.PrimaryReplicaExpiredException;
 import org.apache.ignite.tx.TransactionException;
 
 /**
@@ -39,6 +40,8 @@ public class TransactionExceptionMapperProvider implements IgniteExceptionMapper
 
         mappers.add(unchecked(LockException.class, err -> new TransactionException(err.traceId(), err.code(), err.getMessage(), err)));
         mappers.add(unchecked(ReplicationException.class,
+                err -> new TransactionException(err.traceId(), err.code(), err.getMessage(), err)));
+        mappers.add(unchecked(PrimaryReplicaExpiredException.class,
                 err -> new TransactionException(err.traceId(), err.code(), err.getMessage(), err)));
 
         return mappers;
