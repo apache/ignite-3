401910751349c1eedf46bab8b032fa408e31e520
Max Zhuravkov <shhwwa@gmail.com>
2024-07-18 18:20:20 +0300

    IGNITE-22655: Sql. incorrect error for quoted SQL
    visitor method can be supported.

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlCreateTableOption.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlCreateTableOption.java
@@ -26,9 +26,6 @@ import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.SqlWriter;
 import org.apache.calcite.sql.parser.SqlParserPos;
-import org.apache.calcite.sql.util.SqlVisitor;
-import org.apache.calcite.sql.validate.SqlValidator;
-import org.apache.calcite.sql.validate.SqlValidatorScope;
 import org.apache.calcite.util.Litmus;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -90,23 +87,6 @@ public class IgniteSqlCreateTableOption extends SqlCall {
         key.unparse(writer, leftPrec, rightPrec);
         writer.keyword("=");
         value.unparse(writer, leftPrec, rightPrec);
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void validate(SqlValidator validator, SqlValidatorScope scope) {
-        for (SqlNode node : getOperandList()) {
-            if (node == null) {
-                continue;
-            }
-            node.validate(validator, scope);
-        }
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public <R> R accept(SqlVisitor<R> visitor) {
-        throw new UnsupportedOperationException();
     }
 
     /** {@inheritDoc} */
