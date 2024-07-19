92d6f213110eafc2f267d1f66daa897439c0f689
Pavel Pereslegin <xxtern@gmail.com>
2024-07-19 21:23:47 +0300

    IGNITE-22636 (minor) Fixed It test package name.

--- modules/catalog-compaction/src/integrationTest/java/org/apache/ignite/internal/catalog/compaction/ItCatalogCompactionCoordinatorChoosingTest.java
+++ modules/catalog-compaction/src/integrationTest/java/org/apache/ignite/internal/catalog/compaction/ItCatalogCompactionCoordinatorChoosingTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.ignite.internal.catalog;
+package org.apache.ignite.internal.catalog.compaction;
 
 import static org.apache.ignite.internal.testframework.IgniteTestUtils.waitForCondition;
 import static org.hamcrest.MatcherAssert.assertThat;
