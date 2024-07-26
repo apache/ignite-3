94c95216ef7f2898952360149618f3fe17a48f12
sanpwc <lapin1702@gmail.com>
2024-07-26 15:24:35 +0300

    IGNITE-22819 Checkstyle.

--- modules/metastorage/src/test/java/org/apache/ignite/internal/metastorage/impl/IdempotentCacheVacuumizerTest.java
+++ modules/metastorage/src/test/java/org/apache/ignite/internal/metastorage/impl/IdempotentCacheVacuumizerTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.metastorage.impl;
 
 import static org.apache.ignite.internal.testframework.IgniteTestUtils.waitForCondition;
@@ -22,6 +39,7 @@ public class IdempotentCacheVacuumizerTest {
      *     <li>Suspend vacuumization triggering and verify that vacuumization action calls were suspended.</li>
      *     <li>Start vacuumization triggering and verify that vacuumization action was called.</li>
      * </ol>
+     *
      * @throws Exception if Thread.sleep() was interrupted.
      */
     @Test
@@ -71,6 +89,7 @@ public class IdempotentCacheVacuumizerTest {
      *     <li>Start the vacuumizer and check that it doesn't take any effect.</li>
      *     <li>Suspend vacuumization triggering and check that it doesn't take any effect.</li>
      * </ol>
+     *
      * @throws Exception if Thread.sleep() was interrupted.
      */
     @Test
@@ -120,7 +139,7 @@ public class IdempotentCacheVacuumizerTest {
 
         Runnable vacuumizationActionStub = () -> {
             touchCounter.incrementAndGet();
-            throw new NullPointerException();
+            throw new IllegalStateException();
         };
 
         IdempotentCacheVacuumizer vacuumizer = new IdempotentCacheVacuumizer(
