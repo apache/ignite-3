a831df504d8e1122a3a6c249c8209d059c83cffc
sanpwc <lapin1702@gmail.com>
2024-07-31 20:16:28 +0300

    IGNITE-22801 Checkstyle.

--- modules/raft/src/integrationTest/java/org/apache/ignite/internal/raft/ItLearnersTest.java
+++ modules/raft/src/integrationTest/java/org/apache/ignite/internal/raft/ItLearnersTest.java
@@ -333,7 +333,8 @@ public class ItLearnersTest extends IgniteAbstractTest {
     }
 
     /**
-     * Tests adding a new learner using {@link RaftGroupService#changePeersAndLearnersAsync} to an Ignite node that is already running a Raft peer.
+     * Tests adding a new learner using {@link RaftGroupService#changePeersAndLearnersAsync} to an Ignite node that is already running a
+     * Raft peer.
      */
     @Test
     void testChangePeersToAddLearnerToSameNodeAsPeer() throws InterruptedException {
