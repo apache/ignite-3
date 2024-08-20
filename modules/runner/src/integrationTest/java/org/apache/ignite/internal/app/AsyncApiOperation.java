b281a3cef7865d6ce068dcee89b2a00ac244439b
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2024-08-20 15:31:33 +0400

    IGNITE-22877 / improve naming of enum constants

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/app/AsyncApiOperation.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/app/AsyncApiOperation.java
@@ -39,23 +39,23 @@ enum AsyncApiOperation {
     TABLES_TABLES(refs -> refs.tables.tablesAsync()),
     TABLES_TABLE(refs -> refs.tables.tableAsync(TEST_TABLE_NAME)),
 
-    KVVIEW_GET(refs -> refs.kvView.getAsync(null, KEY_TUPLE)),
-    KVVIEW_GET_OR_DEFAULT(refs -> refs.kvView.getOrDefaultAsync(null, KEY_TUPLE, null)),
-    KVVIEW_GET_ALL(refs -> refs.kvView.getAllAsync(null, List.of(KEY_TUPLE))),
-    KVVIEW_CONTAINS(refs -> refs.kvView.containsAsync(null, KEY_TUPLE)),
-    KVVIEW_CONTAINS_ALL(refs -> refs.kvView.containsAllAsync(null, List.of(KEY_TUPLE))),
-    KVVIEW_PUT(refs -> refs.kvView.putAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    KVVIEW_PUT_ALL(refs -> refs.kvView.putAllAsync(null, Map.of(KEY_TUPLE, VALUE_TUPLE))),
-    KVVIEW_GET_AND_PUT(refs -> refs.kvView.getAndPutAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    KVVIEW_PUT_IF_ABSENT(refs -> refs.kvView.putIfAbsentAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    KVVIEW_REMOVE(refs -> refs.kvView.removeAsync(null, KEY_TUPLE)),
-    KVVIEW_REMOVE_EXACT(refs -> refs.kvView.removeAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    KVVIEW_REMOVE_ALL(refs -> refs.kvView.removeAllAsync(null, List.of(KEY_TUPLE))),
-    KVVIEW_GET_AND_REMOVE(refs -> refs.kvView.getAndRemoveAsync(null, KEY_TUPLE)),
-    KVVIEW_REPLACE(refs -> refs.kvView.replaceAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    KVVIEW_REPLACE_EXACT(refs -> refs.kvView.replaceAsync(null, KEY_TUPLE, VALUE_TUPLE, VALUE_TUPLE)),
-    KVVIEW_GET_AND_REPLACE(refs -> refs.kvView.getAndReplaceAsync(null, KEY_TUPLE, VALUE_TUPLE)),
-    VKVIEW_STREAM_DATA(refs -> {
+    KV_VIEW_GET(refs -> refs.kvView.getAsync(null, KEY_TUPLE)),
+    KV_VIEW_GET_OR_DEFAULT(refs -> refs.kvView.getOrDefaultAsync(null, KEY_TUPLE, null)),
+    KV_VIEW_GET_ALL(refs -> refs.kvView.getAllAsync(null, List.of(KEY_TUPLE))),
+    KV_VIEW_CONTAINS(refs -> refs.kvView.containsAsync(null, KEY_TUPLE)),
+    KV_VIEW_CONTAINS_ALL(refs -> refs.kvView.containsAllAsync(null, List.of(KEY_TUPLE))),
+    KV_VIEW_PUT(refs -> refs.kvView.putAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_PUT_ALL(refs -> refs.kvView.putAllAsync(null, Map.of(KEY_TUPLE, VALUE_TUPLE))),
+    KV_VIEW_GET_AND_PUT(refs -> refs.kvView.getAndPutAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_PUT_IF_ABSENT(refs -> refs.kvView.putIfAbsentAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_REMOVE(refs -> refs.kvView.removeAsync(null, KEY_TUPLE)),
+    KV_VIEW_REMOVE_EXACT(refs -> refs.kvView.removeAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_REMOVE_ALL(refs -> refs.kvView.removeAllAsync(null, List.of(KEY_TUPLE))),
+    KV_VIEW_GET_AND_REMOVE(refs -> refs.kvView.getAndRemoveAsync(null, KEY_TUPLE)),
+    KV_VIEW_REPLACE(refs -> refs.kvView.replaceAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_REPLACE_EXACT(refs -> refs.kvView.replaceAsync(null, KEY_TUPLE, VALUE_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_GET_AND_REPLACE(refs -> refs.kvView.getAndReplaceAsync(null, KEY_TUPLE, VALUE_TUPLE)),
+    KV_VIEW_STREAM_DATA(refs -> {
         CompletableFuture<?> future;
         try (var publisher = new SimplePublisher<Entry<Tuple, Tuple>>()) {
             future = refs.kvView.streamData(publisher, null);
@@ -63,35 +63,35 @@ enum AsyncApiOperation {
         }
         return future;
     }),
-    KVVIEW_QUERY(refs -> refs.kvView.queryAsync(null, null)),
-    KVVIEW_QUERY_WITH_INDEX(refs -> refs.kvView.queryAsync(null, null, null)),
-    KVVIEW_QUERY_WITH_OPTIONS(refs -> refs.kvView.queryAsync(null, null, null, null)),
+    KV_VIEW_QUERY(refs -> refs.kvView.queryAsync(null, null)),
+    KV_VIEW_QUERY_WITH_INDEX(refs -> refs.kvView.queryAsync(null, null, null)),
+    KV_VIEW_QUERY_WITH_OPTIONS(refs -> refs.kvView.queryAsync(null, null, null, null)),
 
-    TYPED_KVVIEW_GET_NULLABLE(refs -> refs.typedKvView.getNullableAsync(null, 1)),
-    TYPED_KVVIEW_GET_NULLABLE_AND_PUT(refs -> refs.typedKvView.getNullableAndPutAsync(null, 1, "one")),
-    TYPED_KVVIEW_GET_NULLABLE_AND_REMOVE(refs -> refs.typedKvView.getNullableAndRemoveAsync(null, 1)),
-    TYPED_KVVIEW_GET_NULLABLE_AND_REPLACE(refs -> refs.typedKvView.getNullableAndReplaceAsync(null, 1, "one")),
+    TYPED_KV_VIEW_GET_NULLABLE(refs -> refs.typedKvView.getNullableAsync(null, 1)),
+    TYPED_KV_VIEW_GET_NULLABLE_AND_PUT(refs -> refs.typedKvView.getNullableAndPutAsync(null, 1, "one")),
+    TYPED_KV_VIEW_GET_NULLABLE_AND_REMOVE(refs -> refs.typedKvView.getNullableAndRemoveAsync(null, 1)),
+    TYPED_KV_VIEW_GET_NULLABLE_AND_REPLACE(refs -> refs.typedKvView.getNullableAndReplaceAsync(null, 1, "one")),
 
-    MAPPED_KVVIEW_GET(refs -> refs.mappedKvView.getAsync(null, 1)),
+    MAPPED_KV_VIEW_GET(refs -> refs.mappedKvView.getAsync(null, 1)),
 
-    RECORDVIEW_GET(refs -> refs.recordView.getAsync(null, KEY_TUPLE)),
-    RECORDVIEW_GET_ALL(refs -> refs.recordView.getAllAsync(null, List.of(KEY_TUPLE))),
-    RECORDVIEW_CONTAINS(refs -> refs.recordView.containsAsync(null, KEY_TUPLE)),
-    RECORDVIEW_CONTAINS_ALL(refs -> refs.recordView.containsAllAsync(null, List.of(KEY_TUPLE))),
-    RECORDVIEW_UPSERT(refs -> refs.recordView.upsertAsync(null, FULL_TUPLE)),
-    RECORDVIEW_UPSERT_ALL(refs -> refs.recordView.upsertAllAsync(null, List.of(FULL_TUPLE))),
-    RECORDVIEW_GET_AND_UPSERT(refs -> refs.recordView.getAndUpsertAsync(null, FULL_TUPLE)),
-    RECORDVIEW_INSERT(refs -> refs.recordView.insertAsync(null, FULL_TUPLE)),
-    RECORDVIEW_INSERT_ALL(refs -> refs.recordView.insertAllAsync(null, List.of(FULL_TUPLE))),
-    RECORDVIEW_REPLACE(refs -> refs.recordView.replaceAsync(null, FULL_TUPLE)),
-    RECORDVIEW_REPLACE_EXACT(refs -> refs.recordView.replaceAsync(null, FULL_TUPLE, FULL_TUPLE)),
-    RECORDVIEW_GET_AND_REPLACE(refs -> refs.recordView.getAndReplaceAsync(null, FULL_TUPLE)),
-    RECORDVIEW_DELETE(refs -> refs.recordView.deleteAsync(null, KEY_TUPLE)),
-    RECORDVIEW_DELETE_EXACT(refs -> refs.recordView.deleteExactAsync(null, FULL_TUPLE)),
-    RECORDVIEW_GET_AND_DELETE(refs -> refs.recordView.getAndDeleteAsync(null, KEY_TUPLE)),
-    RECORDVIEW_DELETE_ALL(refs -> refs.recordView.deleteAllAsync(null, List.of(KEY_TUPLE))),
-    RECORDVIEW_DELETE_ALL_EXACT(refs -> refs.recordView.deleteAllExactAsync(null, List.of(FULL_TUPLE))),
-    RECORDVIEW_STREAM_DATA(refs -> {
+    RECORD_VIEW_GET(refs -> refs.recordView.getAsync(null, KEY_TUPLE)),
+    RECORD_VIEW_GET_ALL(refs -> refs.recordView.getAllAsync(null, List.of(KEY_TUPLE))),
+    RECORD_VIEW_CONTAINS(refs -> refs.recordView.containsAsync(null, KEY_TUPLE)),
+    RECORD_VIEW_CONTAINS_ALL(refs -> refs.recordView.containsAllAsync(null, List.of(KEY_TUPLE))),
+    RECORD_VIEW_UPSERT(refs -> refs.recordView.upsertAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_UPSERT_ALL(refs -> refs.recordView.upsertAllAsync(null, List.of(FULL_TUPLE))),
+    RECORD_VIEW_GET_AND_UPSERT(refs -> refs.recordView.getAndUpsertAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_INSERT(refs -> refs.recordView.insertAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_INSERT_ALL(refs -> refs.recordView.insertAllAsync(null, List.of(FULL_TUPLE))),
+    RECORD_VIEW_REPLACE(refs -> refs.recordView.replaceAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_REPLACE_EXACT(refs -> refs.recordView.replaceAsync(null, FULL_TUPLE, FULL_TUPLE)),
+    RECORD_VIEW_GET_AND_REPLACE(refs -> refs.recordView.getAndReplaceAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_DELETE(refs -> refs.recordView.deleteAsync(null, KEY_TUPLE)),
+    RECORD_VIEW_DELETE_EXACT(refs -> refs.recordView.deleteExactAsync(null, FULL_TUPLE)),
+    RECORD_VIEW_GET_AND_DELETE(refs -> refs.recordView.getAndDeleteAsync(null, KEY_TUPLE)),
+    RECORD_VIEW_DELETE_ALL(refs -> refs.recordView.deleteAllAsync(null, List.of(KEY_TUPLE))),
+    RECORD_VIEW_DELETE_ALL_EXACT(refs -> refs.recordView.deleteAllExactAsync(null, List.of(FULL_TUPLE))),
+    RECORD_VIEW_STREAM_DATA(refs -> {
         CompletableFuture<?> future;
         try (var publisher = new SimplePublisher<Tuple>()) {
             future = refs.recordView.streamData(publisher, null);
@@ -99,18 +99,18 @@ enum AsyncApiOperation {
         }
         return future;
     }),
-    RECORDVIEW_QUERY(refs -> refs.recordView.queryAsync(null, null)),
-    RECORDVIEW_QUERY_WITH_INDEX(refs -> refs.recordView.queryAsync(null, null, null)),
-    RECORDVIEW_QUERY_WITH_OPTIONS(refs -> refs.recordView.queryAsync(null, null, null, null)),
+    RECORD_VIEW_QUERY(refs -> refs.recordView.queryAsync(null, null)),
+    RECORD_VIEW_QUERY_WITH_INDEX(refs -> refs.recordView.queryAsync(null, null, null)),
+    RECORD_VIEW_QUERY_WITH_OPTIONS(refs -> refs.recordView.queryAsync(null, null, null, null)),
 
-    TYPED_RECORDVIEW_GET(refs -> refs.typedRecordView.getAsync(null, new Record(1, ""))),
+    TYPED_RECORD_VIEW_GET(refs -> refs.typedRecordView.getAsync(null, new Record(1, ""))),
 
-    MAPPED_RECORDVIEW_GET(refs -> refs.mappedRecordView.getAsync(null, new Record(1, ""))),
+    MAPPED_RECORD_VIEW_GET(refs -> refs.mappedRecordView.getAsync(null, new Record(1, ""))),
 
     PARTITION_MANAGER_PRIMARY_REPLICA(refs -> refs.partitionManager.primaryReplicaAsync(new HashPartition(0))),
     PARTITION_MANAGER_PRIMARY_REPLICAS(refs -> refs.partitionManager.primaryReplicasAsync()),
-    PARTITION_MANAGER_PARTITION_ASYNC_BY_KEY(refs -> refs.partitionManager.partitionAsync(1, Mapper.of(Integer.class))),
-    PARTITION_MANAGER_PARTITION_ASYNC_BY_TUPLE(refs -> refs.partitionManager.partitionAsync(KEY_TUPLE));
+    PARTITION_MANAGER_PARTITION_BY_KEY(refs -> refs.partitionManager.partitionAsync(1, Mapper.of(Integer.class))),
+    PARTITION_MANAGER_PARTITION_BY_TUPLE(refs -> refs.partitionManager.partitionAsync(KEY_TUPLE));
 
     private final Function<References, CompletableFuture<?>> action;
 
