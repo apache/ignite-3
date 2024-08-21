d284e9e4befb62dd87c970138d25d85d5752d2af
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2024-08-21 13:43:04 +0300

    Fix test

--- modules/security/src/test/java/org/apache/ignite/internal/security/authentication/SecurityConfigurationModuleTest.java
+++ modules/security/src/test/java/org/apache/ignite/internal/security/authentication/SecurityConfigurationModuleTest.java
@@ -38,6 +38,7 @@ import org.apache.ignite.internal.security.authentication.basic.BasicAuthenticat
 import org.apache.ignite.internal.security.authentication.basic.BasicAuthenticationProviderConfigurationSchema;
 import org.apache.ignite.internal.security.authentication.basic.BasicAuthenticationProviderView;
 import org.apache.ignite.internal.security.authentication.validator.AuthenticationProvidersValidatorImpl;
+import org.apache.ignite.internal.security.configuration.SecurityChange;
 import org.apache.ignite.internal.security.configuration.SecurityExtensionChange;
 import org.apache.ignite.internal.security.configuration.SecurityExtensionConfiguration;
 import org.apache.ignite.internal.security.configuration.SecurityExtensionConfigurationSchema;
@@ -121,15 +122,16 @@ class SecurityConfigurationModuleTest {
 
     @Test
     void doNotSetDefaultUserIfProvidersIsNotEmpty() {
-        SecurityExtensionChange securityExtensionChange = rootChange.changeRoot(SecurityExtensionConfiguration.KEY);
-        securityExtensionChange.changeSecurity().changeAuthentication().changeProviders().create("basic", change -> {
+        SecurityChange securityChange =
+                ((SecurityExtensionChange) rootChange.changeRoot(SecurityExtensionConfiguration.KEY)).changeSecurity();
+        securityChange.changeAuthentication().changeProviders().create("basic", change -> {
             change.convert(BasicAuthenticationProviderChange.class)
                     .changeUsers(users -> users.create("admin", user -> user.changePassword("password")));
         });
 
         module.patchConfigurationWithDynamicDefaults(rootChange);
 
-        SecurityView securityView = rootChange.viewRoot(SecurityExtensionConfiguration.KEY).security();
+        SecurityView securityView = ((SecurityExtensionView) rootChange.viewRoot(SecurityExtensionConfiguration.KEY)).security();
 
         assertThat(securityView.authentication().providers().size(), is(1));
 
