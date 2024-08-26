862d1e8ee539ce5b5dda027a2803a0f510133105
Max Zhuravkov <shhwwa@gmail.com>
2024-08-26 13:50:50 +0300

    IGNITE-22949: Sql. Add for-loop support to SQL logic tests.

--- modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/sqllogic/For.java
+++ modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/sqllogic/For.java
@@ -28,19 +28,24 @@ import java.util.List;
  *     # variable is one or multiple words
  *     for variable first_elem, second_elem, ... last_elem
  *
- *     for var 1, 2, 3
+ *     for var in [1, 2, 3]
  *     ...
  *     endfor
  *
- *     for var TEXT 1, TEXT 2, TEXT 3
+ *     for var in [TEXT 1, TEXT 2, TEXT 3]
  *     ...
  *     endfor
+ *
+ *     # it is possible to escape a comma symbol:
+ *      for var in [DECIMAL(2\,1)]
+ *      ...
+ *      endfor
  * </pre>
  * When an SQL statement or an SQL query is executed inside a for loop, all placeholders {@code ${variable}}
  * in that statement/query including results and error messages are replaced with the current value of that {@code variable}.
  * <pre>
  *     # Executes the query 2 times with i=0 and i=2
- *     for k 0 2
+ *     for k in [0, 2]
  *     query I
  *     SELECT c1 FROM (VALUES (${i})) t(c1);
  *     ----
@@ -78,17 +83,43 @@ final class For extends Command {
 
             // Parses tokens 'a,' 'b,' 'c' into a list of strings ['a', 'b', 'c' ]
             StringBuilder current = new StringBuilder();
+
+            String inTok = cmdTokens[2];
+            if (!"in".equalsIgnoreCase(inTok))  {
+                throw script.reportInvalidCommand("Unexpected for syntax. Expected IN but got " + inTok, cmdTokens);
+            }
+
+            String leftBracket = cmdTokens[3];
+            if (!"[".equalsIgnoreCase(leftBracket) && !leftBracket.startsWith("["))  {
+                throw script.reportInvalidCommand("Unexpected for syntax. Expected [ but got " + leftBracket, cmdTokens);
+            }
+
+            String lastTok = cmdTokens[cmdTokens.length - 1];
+            if (!"]".equalsIgnoreCase(leftBracket) && !lastTok.endsWith("]"))  {
+                throw script.reportInvalidCommand("Unexpected for syntax. Expected ] but got " + lastTok, cmdTokens);
+            }
+
             elements = new ArrayList<>();
 
-            for (int i = 2; i < cmdTokens.length; i++) {
+            for (int i = 3, elem = 0; i < cmdTokens.length; i++, elem++) {
                 String tok = cmdTokens[i];
-                current.append(tok);
+
+                // if a token start with [vv or ends with vv], we should remove them.
+                if (tok.startsWith("[")) {
+                    current.append(tok.substring(1));
+                } else if (tok.endsWith("]")) {
+                    current.append(tok, 0, tok.length() - 1);
+                } else {
+                    current.append(tok);
+                }
 
                 if (tok.endsWith(",")) {
                     String str = current.toString();
                     String e = str.substring(0, str.length() - 1).trim();
                     if (!e.isEmpty()) {
-                        elements.add(e);
+                        elements.add(e.replace("\\,", ","));
+                    } else {
+                        throw script.reportInvalidCommand("Unexpected for syntax. For loop element can not be empty. Index: " + elem, cmdTokens);
                     }
 
                     current.setLength(0);
@@ -103,7 +134,6 @@ final class For extends Command {
                     elements.add(e);
                 }
             }
-
         } catch (Exception e) {
             throw script.reportInvalidCommand("Unexpected loop syntax", cmdTokens, e);
         }
