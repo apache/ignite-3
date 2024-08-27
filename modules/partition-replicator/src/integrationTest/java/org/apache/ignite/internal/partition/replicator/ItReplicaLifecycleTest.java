11a656a8f26c2645b78ea7b51e492c0b62c179d7
Mikhail Efremov <jakutenshi@gmail.com>
2024-08-27 20:38:27 +0600

    IGNITE-23063 enabling `ItReplicaLifecycleTest` after merge

--- modules/partition-replicator/src/integrationTest/java/org/apache/ignite/internal/partition/replicator/ItReplicaLifecycleTest.java
+++ modules/partition-replicator/src/integrationTest/java/org/apache/ignite/internal/partition/replicator/ItReplicaLifecycleTest.java
@@ -209,7 +209,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
  */
 @ExtendWith({WorkDirectoryExtension.class, ConfigurationExtension.class})
 @Timeout(60)
-@Disabled("https://issues.apache.org/jira/browse/IGNITE-23063")
 // TODO: https://issues.apache.org/jira/browse/IGNITE-22522 remove this test after the switching to zone-based replication
 public class ItReplicaLifecycleTest extends BaseIgniteAbstractTest {
     private static final IgniteLogger LOG = Loggers.forClass(ItReplicaLifecycleTest.class);
