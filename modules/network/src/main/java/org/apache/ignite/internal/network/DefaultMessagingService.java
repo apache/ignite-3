08a5e4cb55d5fc124bc3e986117daa107cba755f
Vladislav Pyatkov <vldpyatkov@gmail.com>
2024-08-27 14:56:05 +0300

    Extracted interface for timeout object by comment from P.Tupitsyn

--- modules/network/src/main/java/org/apache/ignite/internal/network/DefaultMessagingService.java
+++ modules/network/src/main/java/org/apache/ignite/internal/network/DefaultMessagingService.java
@@ -106,7 +106,7 @@ public class DefaultMessagingService extends AbstractMessagingService {
     private volatile ConnectionManager connectionManager;
 
     /** Collection that maps correlation id to the future for an invocation request. */
-    private final ConcurrentMap<Long, TimeoutObject<CompletableFuture<NetworkMessage>>> requestsMap = new ConcurrentHashMap<>();
+    private final ConcurrentMap<Long, TimeoutObjectImpl> requestsMap = new ConcurrentHashMap<>();
 
     /** Correlation id generator. */
     private final AtomicLong correlationIdGenerator = new AtomicLong();
@@ -300,7 +300,7 @@ public class DefaultMessagingService extends AbstractMessagingService {
 
         CompletableFuture<NetworkMessage> responseFuture = new CompletableFuture<>();
 
-        requestsMap.put(correlationId, new TimeoutObject<>(timeout > 0 ? coarseCurrentTimeMillis() + timeout : 0, responseFuture));
+        requestsMap.put(correlationId, new TimeoutObjectImpl(timeout > 0 ? coarseCurrentTimeMillis() + timeout : 0, responseFuture));
 
         InetSocketAddress recipientAddress = resolveRecipientAddress(recipient);
 
@@ -570,7 +570,7 @@ public class DefaultMessagingService extends AbstractMessagingService {
      * @param correlationId Request's correlation id.
      */
     private void onInvokeResponse(NetworkMessage response, Long correlationId) {
-        TimeoutObject<CompletableFuture<NetworkMessage>> responseFuture = requestsMap.remove(correlationId);
+        TimeoutObjectImpl responseFuture = requestsMap.remove(correlationId);
 
         if (responseFuture != null) {
             responseFuture.future().complete(response);
@@ -723,6 +723,38 @@ public class DefaultMessagingService extends AbstractMessagingService {
             for (CriticalWorker worker : registeredWorkers) {
                 workerRegistry.unregister(worker);
             }
+        }
+    }
+
+    /**
+     * Timeout object wrapper for the completable future.
+     */
+    private static class TimeoutObjectImpl implements TimeoutObject<CompletableFuture<NetworkMessage>> {
+        /** End time (milliseconds since Unix epoch). */
+        private final long endTime;
+
+        /** Target future. */
+        private final CompletableFuture<NetworkMessage> fut;
+
+        /**
+         * Constructor.
+         *
+         * @param endTime End timestamp in milliseconds.
+         * @param fut Target future.
+         */
+        public TimeoutObjectImpl(long endTime, CompletableFuture<NetworkMessage> fut) {
+            this.endTime = endTime;
+            this.fut = fut;
+        }
+
+        @Override
+        public long endTime() {
+            return endTime;
+        }
+
+        @Override
+        public CompletableFuture<NetworkMessage> future() {
+            return fut;
         }
     }
 
