0723a0fd3e4ae34858fd05d450778fa601333a98
Evgeniy Stanilovskiy <stanilovsky@gmail.com>
2024-09-04 08:21:53 +0300

    IGNITE-23083 Sql. Avoid planner cache pollution with simple insert queries (#4295)

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/mapping/largecluster/AbstractTarget.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/exec/mapping/largecluster/AbstractTarget.java
@@ -197,7 +197,7 @@ abstract class AbstractTarget implements ExecutionTarget {
 
     static ExecutionTarget colocate(PartitionedTarget partitioned, PartitionedTarget otherPartitioned) throws ColocationMappingException {
         if (partitioned.partitionsNodes.length != otherPartitioned.partitionsNodes.length) {
-            throw new ColocationMappingException("Partitioned targets with mot matching numbers of partitioned are not colocated");
+            throw new ColocationMappingException("Partitioned targets with not matching numbers of partitions are not colocated");
         }
 
         boolean changed = false;
