c0ba5da125c2407bc2d53765f25038971dc72120
Kirill Sizov <sizov.kirill.y@gmail.com>
2024-09-11 18:15:32 +0300

    IGNITE-23081 Add lease expiration time to configuration

--- modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseNegotiationTest.java
+++ modules/placement-driver/src/test/java/org/apache/ignite/internal/placementdriver/LeaseNegotiationTest.java
@@ -71,7 +71,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * Test checking exceptional situations on lease negotiation.
