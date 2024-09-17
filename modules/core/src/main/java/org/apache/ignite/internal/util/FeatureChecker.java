79e85211da44d979672c4c3aeaff63135222026f
Mikhail Efremov <jakutenshi@gmail.com>
2024-09-17 18:33:52 +0600

    IGNITE-23221 Add startup parameters temporarily

--- modules/core/src/main/java/org/apache/ignite/internal/util/FeatureChecker.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/FeatureChecker.java
@@ -24,5 +24,18 @@ public class FeatureChecker {
     /** Java specific startup parameters warning to be added in case access failed. */
     public static final String JAVA_STARTUP_PARAMS_WARN = System.lineSeparator()
             + "Please check the required parameters to JVM startup settings and restart the application."
+            + " Parameters: " + System.lineSeparator()
+            + "\t--add-opens java.base/java.lang=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.lang.invoke=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.lang.reflect=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.io=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.nio=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.math=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.util=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/java.time=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/jdk.internal.misc=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/jdk.internal.access=ALL-UNNAMED" + System.lineSeparator()
+            + "\t--add-opens java.base/sun.nio.ch=ALL-UNNAMED" + System.lineSeparator()
+            + "\t-Dio.netty.tryReflectionSetAccessible=true" + System.lineSeparator()
             + " See https://ignite.apache.org/docs/3.0.0-beta/quick-start/getting-started-guide for more information.";
 }
