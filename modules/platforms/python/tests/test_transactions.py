96b1c7d27daffbc4cd4ed59b89f8e50bb253e5ef
Igor Sapego <isapego@apache.org>
2024-10-01 03:06:52 +0200

    IGNITE-22740 Add tests, fix issue

--- modules/platforms/python/tests/test_transactions.py
+++ modules/platforms/python/tests/test_transactions.py
@@ -12,19 +12,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
-
 import pyignite3
+from tests.util import server_addresses_basic
 
 
-@pytest.fixture()
 def create_tx_test_table(cursor, table_name):
     cursor.execute(f'drop table if exists {table_name}')
     cursor.execute(f'create table {table_name}(id int primary key, val int)')
 
 
-def test_commit_rollback(table_name, connection, cursor, create_tx_test_table, drop_table_cleanup):
+def test_commit_rollback_setautocommit(table_name, connection, cursor, drop_table_cleanup):
+    create_tx_test_table(cursor, table_name)
+    assert connection.autocommit is True
+
     connection.setautocommit(False)
+    assert connection.autocommit is False
 
     cursor.execute(f'insert into {table_name} values (42, 10)')
     connection.commit()
@@ -36,4 +38,43 @@ def test_commit_rollback(table_name, connection, cursor, create_tx_test_table, d
     row = cursor.fetchone()
 
     assert row[0] == 10
+
+
+def test_commit_rollback_autocommit_setter(table_name, connection, cursor, drop_table_cleanup):
+    create_tx_test_table(cursor, table_name)
+    assert connection.autocommit is True
+
+    cursor.execute(f'insert into {table_name} values (42, 10)')
+
+    connection.autocommit = False
+    assert connection.autocommit is False
+
+    cursor.execute(f'update {table_name} set val=23 where id=42')
+    connection.rollback()
+
+    cursor.execute(f'select val from {table_name} where id=42')
+    row = cursor.fetchone()
+
+    assert row[0] == 10
+
+
+def test_commit_rollback_autocommit_connection(table_name, drop_table_cleanup):
+    with pyignite3.connect(address=server_addresses_basic, autocommit=True) as conn:
+        with conn.cursor() as cursor:
+            create_tx_test_table(cursor, table_name)
+
+    with pyignite3.connect(address=server_addresses_basic, autocommit=False) as conn_tx:
+        assert conn_tx.autocommit is False
+        with conn_tx.cursor() as cursor:
+            cursor.execute(f'insert into {table_name} values (123, 999)')
+            conn_tx.commit()
+
+            cursor.execute(f'update {table_name} set val=777 where id=123')
+
+    with pyignite3.connect(address=server_addresses_basic, autocommit=True) as conn:
+        assert conn.autocommit is True
+        with conn.cursor() as cursor:
+            cursor.execute(f'select val from {table_name} where id=123')
+            row = cursor.fetchone()
+            assert row[0] == 999
 
