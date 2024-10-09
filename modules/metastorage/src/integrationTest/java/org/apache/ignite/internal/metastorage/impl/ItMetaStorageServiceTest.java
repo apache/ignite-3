e8454cb28799810e59dde30a3d735646bbfd04c6
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2024-10-09 18:51:11 +0400

    IGNITE-22914 / fix a checkstyle violation

--- modules/metastorage/src/integrationTest/java/org/apache/ignite/internal/metastorage/impl/ItMetaStorageServiceTest.java
+++ modules/metastorage/src/integrationTest/java/org/apache/ignite/internal/metastorage/impl/ItMetaStorageServiceTest.java
@@ -57,7 +57,6 @@ import static org.mockito.Mockito.when;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
