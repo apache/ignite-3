9fed017eb614b160745fa6e6bb170a48a44647c2
Igor Sapego <isapego@apache.org>
2024-10-14 23:34:38 +0200

    IGNITE-22744 Add DB API 2 compliance test

---
+++ modules/platforms/python/tests/test_dbapi_compliance.py
@@ -0,0 +1,44 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pyignite3
+import dbapi20
+
+from tests.util import server_addresses_basic
+
+
+class TestPyignite3(dbapi20.DatabaseAPI20Test):
+    driver = pyignite3
+    connect_args = ()
+    connect_kw_args = {'address': server_addresses_basic}
+    lower_func = 'lower'
+
+    def setUp(self):
+        dbapi20.DatabaseAPI20Test.setUp(self)
+        #
+        # try:
+        #     con = self._connect()
+        #     con.close()
+        # except:
+        #     cmd = [ "psql", "-c", "create database dbapi20_test" ]
+        #     if subprocess.call(cmd):
+        #         self.fail("Failed to create databse.")
+
+    def tearDown(self):
+        dbapi20.DatabaseAPI20Test.tearDown(self)
+
+    def test_non_idempotent_close(self): pass
+    def test_nextset(self): pass
+    def test_setoutputsize(self): pass
+
