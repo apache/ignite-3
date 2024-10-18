295701b83dfaae252eae5e698f3690a776ec6b40
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2024-10-18 18:00:24 +0400

    IGNITE-23494 / fix a test

--- modules/partition-distribution/src/main/java/org/apache/ignite/internal/partitiondistribution/AssignmentsSerializer.java
+++ modules/partition-distribution/src/main/java/org/apache/ignite/internal/partitiondistribution/AssignmentsSerializer.java
@@ -29,7 +29,7 @@ import org.apache.ignite.internal.versioned.VersionedSerializer;
  */
 public class AssignmentsSerializer extends VersionedSerializer<Assignments> {
     /** Serializer instance. */
-    static AssignmentsSerializer INSTANCE = new AssignmentsSerializer();
+    public static AssignmentsSerializer INSTANCE = new AssignmentsSerializer();
 
     @Override
     protected void writeExternalData(Assignments assignments, IgniteDataOutput out) throws IOException {
