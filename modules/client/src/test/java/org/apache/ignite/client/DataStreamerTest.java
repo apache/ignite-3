4511cd4e15905b470179723128b26bb2c5f16409
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-10-30 14:49:41 +0200

    Fix testManyItemsWithDisconnectAndRetry

--- modules/client/src/test/java/org/apache/ignite/client/DataStreamerTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/DataStreamerTest.java
@@ -227,10 +227,9 @@ public class DataStreamerTest extends AbstractClientTableTest {
         Function<Integer, Boolean> shouldDropConnection = idx -> idx % 5 == 4;
         var ignite2 = startTestServer2(shouldDropConnection, idx -> 0);
 
-        // Streamer has it's own retry policy, so we can disable retries on the client.
         Builder builder = IgniteClient.builder()
                 .addresses("localhost:" + testServer2.port())
-                .retryPolicy(new RetryLimitPolicy().retryLimit(0))
+                .retryPolicy(new RetryLimitPolicy().retryLimit(8))
                 .reconnectThrottlingPeriod(0)
                 .loggerFactory(new ConsoleLoggerFactory("client-2"));
 
