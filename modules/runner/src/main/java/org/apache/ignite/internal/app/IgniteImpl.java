954c368553187c6dbe5c76a01ac5ab41827c307b
Kirill Tkalenko <tkalkirill@yandex.ru>
2024-10-31 14:31:16 +0300

    IGNITE-23292 wip

--- modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/IgniteImpl.java
@@ -1739,7 +1739,6 @@ public class IgniteImpl implements Ignite {
         return CompletableFuture.allOf(startupConfigurationUpdate, startupRevisionUpdate, startFuture)
                 .thenComposeAsync(t -> {
                     // Deploy all registered watches because all components are ready and have registered their listeners.
-                    // TODO: IGNITE-23292 Run local metastore compaction after start watches for the latest compacted revision
                     return metaStorageMgr.deployWatches();
                 }, startupExecutor);
     }
