1babb410a1da19c6df6dc0bc6b5c02de9ee89a3e
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-11-18 12:52:54 +0200

    IGNITE-23043 .NET: Implement IDistributedCache over Ignite table
    https://issues.apache.org/jira/browse/IGNITE-23043

---
+++ modules/platforms/dotnet/Apache.Extensions.Cache.Ignite/IgniteCacheOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Apache.Extensions.Cache.Ignite;
+
+using Apache.Ignite;
+using Microsoft.Extensions.Options;
+
+public class IgniteCacheOptions : IOptions<IgniteCacheOptions>
+{
+    public IgniteClientConfiguration ClientConfiguration { get; set; } = new("localhost");
+
+    /// <inheritdoc/>
+    public IgniteCacheOptions Value => this; // TODO: Why does RedisCacheOptions have this?
+}
