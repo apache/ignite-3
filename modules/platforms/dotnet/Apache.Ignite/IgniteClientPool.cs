4e87f5b4473f6b02e5e06e88b7fde58cf3a01335
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-11-19 15:49:12 +0200

    IGNITE-23703 .NET: Add client pool

---
+++ modules/platforms/dotnet/Apache.Ignite/IgniteClientPool.cs
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite;
+
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Internal.Common;
+
+/// <summary>
+/// Ignite client pool. Thread safe.
+/// <para>
+/// This pool creates up to <see cref="IgniteClientPoolConfiguration.PoolSize"/> Ignite clients and returns them in a round-robin fashion.
+/// Ignite clients are thread safe, so there is no rent/return semantics.
+/// </para>
+/// </summary>
+public sealed class IgniteClientPool : IDisposable
+{
+    private readonly IIgniteClient?[] _clients;
+
+    private readonly SemaphoreSlim _clientsLock = new(1);
+
+    private int _disposed;
+
+    private int _clientCount;
+
+    private int _clientIndex;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IgniteClientPool"/> class.
+    /// </summary>
+    /// <param name="configuration">Configuration.</param>
+    public IgniteClientPool(IgniteClientPoolConfiguration configuration)
+    {
+        IgniteArgumentCheck.NotNull(configuration);
+        IgniteArgumentCheck.Ensure(configuration.PoolSize > 0, nameof(configuration.PoolSize), "PoolSize > 0");
+
+        Configuration = configuration;
+        _clients = new IIgniteClient[configuration.PoolSize];
+    }
+
+    /// <summary>
+    /// Gets the configuration.
+    /// </summary>
+    public IgniteClientPoolConfiguration Configuration { get; }
+
+    /// <summary>
+    /// Gets an Ignite client from the pool. Creates a new one if necessary.
+    /// Performs round-robin balancing across pooled instances.
+    /// </summary>
+    /// <returns>Ignite client.</returns>
+    [SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Pooled.")]
+    public async Task<IIgniteClient> GetClientAsync()
+    {
+        var index = Interlocked.Increment(ref _clientIndex) % _clients.Length;
+
+        var client = _clients[index];
+        if (client != null)
+        {
+            return client;
+        }
+
+        await _clientsLock.WaitAsync().ConfigureAwait(false);
+
+        try
+        {
+            client = _clients[index];
+            if (client != null)
+            {
+                return client;
+            }
+
+            client = await CreateClientAsync().ConfigureAwait(false);
+            _clients[index] = client;
+        }
+        finally
+        {
+            _clientsLock.Release();
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 1)
+        {
+            return;
+        }
+
+        while (_clients.TryDequeue(out var client))
+        {
+            client.Dispose();
+        }
+    }
+
+    private async Task<IIgniteClient> CreateClientAsync() =>
+        await IgniteClient.StartAsync(Configuration.ClientConfiguration).ConfigureAwait(false);
+}
