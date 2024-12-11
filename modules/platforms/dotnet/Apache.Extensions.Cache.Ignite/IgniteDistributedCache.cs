945ebb268c2c0e8a3e0512537ab5115c56aeb92e
Pavel Tupitsyn <ptupitsyn@apache.org>
2024-12-11 15:07:00 +0200

    wip

--- modules/platforms/dotnet/Apache.Extensions.Cache.Ignite/IgniteDistributedCache.cs
+++ modules/platforms/dotnet/Apache.Extensions.Cache.Ignite/IgniteDistributedCache.cs
@@ -1,67 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace Apache.Extensions.Cache.Ignite;
 
 using System.Buffers;
 using Microsoft.Extensions.Caching.Distributed;
 
+/// <summary>
+/// Ignite-based distributed cache.
+/// </summary>
 public sealed class IgniteDistributedCache : IBufferDistributedCache
 {
+    /// <inheritdoc/>
     public byte[]? Get(string key)
     {
         throw new NotImplementedException();
     }
 
-    public Task<byte[]?> GetAsync(string key, CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public Task<byte[]?> GetAsync(string key, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
     {
         throw new NotImplementedException();
     }
 
-    public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public void Refresh(string key)
     {
         throw new NotImplementedException();
     }
 
-    public Task RefreshAsync(string key, CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public Task RefreshAsync(string key, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public void Remove(string key)
     {
         throw new NotImplementedException();
     }
 
-    public Task RemoveAsync(string key, CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public Task RemoveAsync(string key, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public bool TryGet(string key, IBufferWriter<byte> destination)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask<bool> TryGetAsync(string key, IBufferWriter<byte> destination, CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public ValueTask<bool> TryGetAsync(string key, IBufferWriter<byte> destination, CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public void Set(string key, ReadOnlySequence<byte> value, DistributedCacheEntryOptions options)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask SetAsync(string key, ReadOnlySequence<byte> value, DistributedCacheEntryOptions options,
-        CancellationToken token = new CancellationToken())
+    /// <inheritdoc/>
+    public ValueTask SetAsync(
+        string key,
+        ReadOnlySequence<byte> value,
+        DistributedCacheEntryOptions options,
+        CancellationToken token)
     {
         throw new NotImplementedException();
     }
