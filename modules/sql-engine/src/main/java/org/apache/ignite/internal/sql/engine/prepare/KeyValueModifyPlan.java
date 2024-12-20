c853153476ebef26eeb48af47d14cb977657c18d
Pavel Pereslegin <xxtern@gmail.com>
2024-12-20 12:53:10 +0300

    IGNITE-23604 Fixed rollback of table driven tx.

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/prepare/KeyValueModifyPlan.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/prepare/KeyValueModifyPlan.java
@@ -135,9 +135,15 @@ public class KeyValueModifyPlan implements ExplainablePlan, ExecutablePlan {
 
         UpdatableTable updatableTable = execTable.updatableTable();
 
-        CompletableFuture<Iterator<InternalSqlRow>> result = updatableTable.insert(
-                tx, ctx, rowSupplier.get()
-        ).thenApply(none -> List.<InternalSqlRow>of(new InternalSqlRowSingleLong(1L)).iterator());
+        CompletableFuture<Iterator<InternalSqlRow>> result;
+
+        try {
+            result = updatableTable.insert(
+                    tx, ctx, rowSupplier.get()
+            ).thenApply(none -> List.<InternalSqlRow>of(new InternalSqlRowSingleLong(1L)).iterator());
+        } catch (Throwable t) {
+            result = CompletableFuture.failedFuture(t);
+        }
 
         if (firstPageReadyCallback != null) {
             result.whenComplete((res, err) -> firstPageReadyCallback.onPrefetchComplete(err));
