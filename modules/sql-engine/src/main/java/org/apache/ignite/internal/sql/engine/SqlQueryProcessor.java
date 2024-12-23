2557dd68872bed959199e88d46444259acde48ef
korlov42 <korlov@apache.org>
2024-12-23 10:36:38 +0200

    IGNITE-24045 Sql. Replace UUID.randomUUID() with custom generator for query id (#4935)

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/SqlQueryProcessor.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/SqlQueryProcessor.java
@@ -72,6 +72,7 @@ import org.apache.ignite.internal.sql.engine.exec.ddl.DdlCommandHandler;
 import org.apache.ignite.internal.sql.engine.exec.exp.func.TableFunctionRegistryImpl;
 import org.apache.ignite.internal.sql.engine.exec.fsm.ExecutionPhase;
 import org.apache.ignite.internal.sql.engine.exec.fsm.QueryExecutor;
+import org.apache.ignite.internal.sql.engine.exec.fsm.QueryIdGenerator;
 import org.apache.ignite.internal.sql.engine.exec.fsm.QueryInfo;
 import org.apache.ignite.internal.sql.engine.exec.kill.KillCommandHandler;
 import org.apache.ignite.internal.sql.engine.exec.mapping.ExecutionDistributionProviderImpl;
@@ -354,7 +355,8 @@ public class SqlQueryProcessor implements QueryProcessor, SystemViewProvider {
                 catalogManager,
                 executionSrvc,
                 DEFAULT_PROPERTIES,
-                txTracker
+                txTracker,
+                new QueryIdGenerator(nodeName.hashCode())
         ));
 
         queriesViewProvider.init(queryExecutor);
