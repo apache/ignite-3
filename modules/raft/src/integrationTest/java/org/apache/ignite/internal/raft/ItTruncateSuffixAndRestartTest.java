f8ad4f26d364aa78556d3cc6ed707f1e626c7043
phillippko <phillippko@gmail.com>
2024-12-30 16:11:50 +0700

    fix

--- modules/raft/src/integrationTest/java/org/apache/ignite/internal/raft/ItTruncateSuffixAndRestartTest.java
+++ modules/raft/src/integrationTest/java/org/apache/ignite/internal/raft/ItTruncateSuffixAndRestartTest.java
@@ -406,9 +406,5 @@ public class ItTruncateSuffixAndRestartTest extends BaseIgniteAbstractTest {
         public CompletableFuture<Void> stopAsync(ComponentContext componentContext) {
             return nullCompletedFuture();
         }
-
-        @Override
-        public void sync(){
-        }
     }
 }
