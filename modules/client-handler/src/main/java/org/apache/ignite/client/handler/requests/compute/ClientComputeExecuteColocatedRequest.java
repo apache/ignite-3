6303f4df63b83bcb008982aff1c8aca6ff7a3b08
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-01-09 13:27:58 +0200

    Refactor compute impl to pass around ComputeJobDataHolder - wip - build mostly fixed

--- modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteColocatedRequest.java
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteColocatedRequest.java
@@ -30,6 +30,7 @@ import org.apache.ignite.compute.JobExecutionOptions;
 import org.apache.ignite.deployment.DeploymentUnit;
 import org.apache.ignite.internal.client.proto.ClientMessagePacker;
 import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
+import org.apache.ignite.internal.compute.ComputeJobDataHolder;
 import org.apache.ignite.internal.compute.IgniteComputeInternal;
 import org.apache.ignite.internal.network.ClusterService;
 import org.apache.ignite.table.IgniteTables;
@@ -59,7 +60,7 @@ public class ClientComputeExecuteColocatedRequest {
             List<DeploymentUnit> deploymentUnits = in.unpackDeploymentUnits();
             String jobClassName = in.unpackString();
             JobExecutionOptions options = JobExecutionOptions.builder().priority(in.unpackInt()).maxRetries(in.unpackInt()).build();
-            Object args = unpackJobArgumentWithoutMarshaller(in);
+            ComputeJobDataHolder args = unpackJobArgumentWithoutMarshaller(in);
 
             out.packInt(table.schemaView().lastKnownSchemaVersion());
 
