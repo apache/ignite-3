de4af20ccf7ffbdaf51fe670c2020301012d8ecb
Max Zhuravkov <shhwwa@gmail.com>
2025-01-13 14:15:23 +0200

    IGNITE-23947: Sql. Add a test to check operator signatures
    Add empty line.

--- modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/docs/DocumentedOperators.java
+++ modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/docs/DocumentedOperators.java
@@ -116,7 +116,8 @@ class DocumentedOperators {
                     .append(". Add these operators to as public (add(OperatorTable.MY_OP))")
                     .append(" or include them as internal (call .hide(OperatorTable.MY_OP):")
                     .append(System.lineSeparator())
-                    .append("Review the difference and file an issue to update SQL documentation if necessary.");
+                    .append("Review the difference and file an issue to update SQL documentation if necessary.")
+                    .append(System.lineSeparator());
 
             notDocumented.forEach(o -> sb.append(describeOperator(o)).append(System.lineSeparator()));
 
