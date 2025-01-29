19590cb5aa3091434a67ae6c88caf11594069888
sk0x50 <slava.koptilin@gmail.com>
2025-01-29 16:55:39 +0200

    IGNITE-24266 Add new benchmarks

---
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/AbstractCollocationBenchmark.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.benchmark;
+
+import static org.apache.ignite.catalog.definitions.ColumnDefinition.column;
+import static org.apache.ignite.internal.catalog.CatalogService.DEFAULT_STORAGE_PROFILE;
+import static org.apache.ignite.internal.testframework.TestIgnitionManager.PRODUCTION_CLUSTER_CONFIG_STRING;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.ignite.catalog.ColumnType;
+import org.apache.ignite.catalog.definitions.TableDefinition;
+import org.apache.ignite.catalog.definitions.ZoneDefinition;
+import org.apache.ignite.internal.replicator.TablePartitionId;
+import org.apache.ignite.internal.replicator.ZonePartitionId;
+import org.apache.ignite.internal.tx.message.WriteIntentSwitchReplicaRequest;
+import org.apache.ignite.table.KeyValueView;
+import org.apache.ignite.table.Table;
+import org.apache.ignite.table.Tuple;
+
+/**
+ * Base class that allows to measure basic KeyValue operations for tables that share the same distribution zone.
+ *
+ * TODO: https://issues.apache.org/jira/browse/IGNITE-22522 remove this benchmark.
+ */
+public class AbstractCollocationBenchmark extends AbstractMultiNodeBenchmark {
+    /** Name of shared zone. */
+    private final String SHARED_ZONE_NAME = "shared_zone";
+
+    /** System property that allows to enable/disable collocation feature. */
+    private static final String FEATURE_FLAG_NAME = "IGNITE_ZONE_BASED_REPLICATION";
+
+    /** {@code true} enables collocation feature. */
+    private static final Boolean ENABLE_COLLOCATION_FEATURE = true;
+
+    protected final List<KeyValueView<Tuple, Tuple>> tableViews = new ArrayList<>();
+
+    @Override
+    protected int nodes() {
+        return 1;
+    }
+
+    @Override
+    protected int replicaCount() {
+        return 1;
+    }
+
+    @Override
+    protected String clusterConfiguration() {
+        if (tinySchemaSyncWaits()) {
+            return super.clusterConfiguration();
+        } else {
+            // Return a magic string that explicitly requests production defaults.
+            return PRODUCTION_CLUSTER_CONFIG_STRING;
+        }
+    }
+
+    @Override
+    protected void createDistributionZoneOnStartup() {
+        ZoneDefinition zone = ZoneDefinition.builder(SHARED_ZONE_NAME)
+                .partitions(partitionCount())
+                .replicas(replicaCount())
+                .storageProfiles(DEFAULT_STORAGE_PROFILE)
+                .build();
+
+        publicIgnite.catalog().createZone(zone);
+    }
+
+    @Override
+    protected void createTableOnStartup() {
+        for (int i = 1; i <= tableCount(); ++i) {
+            TableDefinition tableDefinition = TableDefinition.builder("test_table_" + i)
+                    .columns(
+                            column("id", ColumnType.INTEGER),
+                            column("company", ColumnType.varchar(32)))
+                    .primaryKey("id")
+                    .zone(SHARED_ZONE_NAME)
+                    .build();
+
+            Table t = publicIgnite.catalog().createTable(tableDefinition);
+
+            tableViews.add(t.keyValueView());
+        }
+    }
+
+    @Override
+    public void nodeSetUp() throws Exception {
+        // Enable/disable collocation feature.
+        System.setProperty(FEATURE_FLAG_NAME, ENABLE_COLLOCATION_FEATURE.toString());
+
+        // Start the cluster and initialize it.
+        super.nodeSetUp();
+
+        // Patch replica manager to propagate table replication messages to zone replication groups.
+        if (ENABLE_COLLOCATION_FEATURE) {
+            int catalogVersion = igniteImpl
+                    .catalogManager()
+                    .latestCatalogVersion();
+
+            int zoneId = igniteImpl
+                    .catalogManager()
+                    .catalog(catalogVersion)
+                    .zone(SHARED_ZONE_NAME.toUpperCase())
+                    .id();
+
+            igniteImpl.replicaManager().groupIdConverter(request -> {
+                if (!(request instanceof WriteIntentSwitchReplicaRequest)) {
+                    if (request.groupId().asReplicationGroupId() instanceof TablePartitionId) {
+                        TablePartitionId tablePartitionId = (TablePartitionId) request.groupId().asReplicationGroupId();
+
+                        return new ZonePartitionId(zoneId, tablePartitionId.partitionId());
+                    }
+                }
+
+                return request.groupId().asReplicationGroupId();
+            });
+        }
+    }
+
+    protected int tableCount() {
+        return 1;
+    }
+
+    protected boolean tinySchemaSyncWaits() {
+        return true;
+    }
+}
---
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/CollocationSelectBenchmark.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.benchmark;
+
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.openjdk.jmh.annotations.Mode.AverageTime;
+import static org.openjdk.jmh.annotations.Mode.Throughput;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.ignite.table.KeyValueView;
+import org.apache.ignite.table.Tuple;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * This benchmark allows to measure reading key value pairs via KeyValue API for tables that share the same distribution zone.
+ *
+ * TODO: https://issues.apache.org/jira/browse/IGNITE-22522 remove this benchmark.
+ */
+@Fork(1)
+@State(Scope.Benchmark)
+public class CollocationSelectBenchmark extends AbstractCollocationBenchmark {
+    private static final int TABLE_SIZE = 30_000;
+
+    @Param({"1", "32"})
+    private int partitionCount;
+
+    @Param({"1", "32"})
+    private int tableCount;
+
+    @Param({"true"})
+    private boolean tinySchemaSyncWaits;
+
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @Override
+    protected int partitionCount() {
+        return partitionCount;
+    }
+
+    /**
+     * Fills the tables with data.
+     */
+    @Setup
+    public void fillTables() throws IOException {
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            Tuple key = Tuple.create().set("id", i);
+            Tuple value = Tuple.create().set("company", "Apache");
+
+            for (KeyValueView<Tuple, Tuple> view : tableViews) {
+                view.put(null, key, value);
+            }
+        }
+    }
+
+    /**
+     * Measures throughput of key-value api for tables that share the same distribution zone.
+     */
+    @Benchmark
+    @Threads(Threads.MAX)
+    @Warmup(iterations = 5, time = 2)
+    @Measurement(iterations = 10, time = 2)
+    @BenchmarkMode(Throughput)
+    @OutputTimeUnit(SECONDS)
+    public void getKeyValueApiThroughput(Blackhole hole) {
+        doGet(hole);
+    }
+
+    /**
+     * Measures average time of inserting a key-value pair for tables that share the same distribution zone.
+     */
+    @Benchmark
+    @Threads(1)
+    @Warmup(iterations = 5, time = 2)
+    @Measurement(iterations = 10, time = 2)
+    @BenchmarkMode(AverageTime)
+    @OutputTimeUnit(MICROSECONDS)
+    public void getKeyValueApiAverage(Blackhole hole) {
+        doGet(hole);
+    }
+
+    private void doGet(Blackhole hole) {
+        int tableIdx = counter.getAndIncrement() % tableViews.size();
+
+        KeyValueView<Tuple, Tuple> kvView = tableViews.get(tableIdx);
+
+        Tuple value = kvView.get(null, Tuple.create().set("id", random.nextInt(TABLE_SIZE)));
+
+        hole.consume(value);
+    }
+
+    /**
+     * Benchmark's entry point.
+     */
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + CollocationSelectBenchmark.class.getSimpleName() + ".*")
+                .build();
+
+        new Runner(opt).run();
+    }
+}
