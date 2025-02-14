7c425469d8db1c31c00461c5f479c739b3afa009
sk0x50 <slava.koptilin@gmail.com>
2025-02-14 14:33:35 +0200

    IGNITE-24365 Fix review findings

--- modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/ZonePartitionReplicaListener.java
+++ modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/ZonePartitionReplicaListener.java
@@ -26,14 +26,12 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import org.apache.ignite.internal.catalog.CatalogService;
 import org.apache.ignite.internal.hlc.ClockService;
-import org.apache.ignite.internal.hlc.HybridTimestamp;
 import org.apache.ignite.internal.lang.IgniteBiTuple;
 import org.apache.ignite.internal.logger.IgniteLogger;
 import org.apache.ignite.internal.logger.Loggers;
 import org.apache.ignite.internal.partition.replicator.handlers.MinimumActiveTxTimeReplicaRequestHandler;
 import org.apache.ignite.internal.partition.replicator.handlers.TxFinishReplicaRequestHandler;
 import org.apache.ignite.internal.partition.replicator.network.replication.ReadOnlyReplicaRequest;
-import org.apache.ignite.internal.partition.replicator.network.replication.ReadWriteReplicaRequest;
 import org.apache.ignite.internal.partition.replicator.network.replication.UpdateMinimumActiveTxBeginTimeReplicaRequest;
 import org.apache.ignite.internal.partition.replicator.schema.ValidationSchemasSource;
 import org.apache.ignite.internal.raft.service.RaftCommandRunner;
@@ -43,13 +41,10 @@ import org.apache.ignite.internal.replicator.TablePartitionId;
 import org.apache.ignite.internal.replicator.ZonePartitionId;
 import org.apache.ignite.internal.replicator.listener.ReplicaListener;
 import org.apache.ignite.internal.replicator.message.PrimaryReplicaRequest;
-import org.apache.ignite.internal.replicator.message.ReadOnlyDirectReplicaRequest;
 import org.apache.ignite.internal.replicator.message.ReplicaRequest;
 import org.apache.ignite.internal.replicator.message.ReplicaSafeTimeSyncRequest;
-import org.apache.ignite.internal.replicator.message.SchemaVersionAwareReplicaRequest;
 import org.apache.ignite.internal.replicator.message.TableAware;
 import org.apache.ignite.internal.schema.SchemaSyncService;
-import org.apache.ignite.internal.tx.TransactionIds;
 import org.apache.ignite.internal.tx.TxManager;
 import org.apache.ignite.internal.tx.message.TxFinishReplicaRequest;
 import org.apache.ignite.internal.tx.storage.state.TxStatePartitionStorage;
@@ -67,9 +62,6 @@ public class ZonePartitionReplicaListener implements ReplicaListener {
 
     /** Raft client. */
     private final RaftCommandRunner raftClient;
-
-    /** Clock service. */
-    private final ClockService clockService;
 
     private final ReplicationRaftCommandApplicator raftCommandApplicator;
 
@@ -94,7 +86,6 @@ public class ZonePartitionReplicaListener implements ReplicaListener {
             RaftCommandRunner raftClient,
             ZonePartitionId replicationGroupId
     ) {
-        this.clockService = clockService;
         this.raftClient = raftClient;
 
         this.raftCommandApplicator = new ReplicationRaftCommandApplicator(raftClient, replicationGroupId);
@@ -205,32 +196,15 @@ public class ZonePartitionReplicaListener implements ReplicaListener {
             UUID senderId,
             @Nullable Long leaseStartTime
     ) {
-        boolean hasSchemaVersion = request instanceof SchemaVersionAwareReplicaRequest;
-
-        if (hasSchemaVersion) {
-            assert ((SchemaVersionAwareReplicaRequest) request).schemaVersion() > 0 :
-                    "Schema version is not passed [request=" + request + ']';
+        // TODO https://issues.apache.org/jira/browse/IGNITE-24526
+        // Need to move the necessary part of PartitionReplicaListener request processing here
+        if (request instanceof UpdateMinimumActiveTxBeginTimeReplicaRequest) {
+            return minimumActiveTxTimeReplicaRequestHandler.handle((UpdateMinimumActiveTxBeginTimeReplicaRequest) request);
+        } else if (request instanceof ReplicaSafeTimeSyncRequest) {
+            LOG.debug("Non table request is not supported by the zone partition yet " + request);
+        } else {
+            LOG.warn("Non table request is not supported by the zone partition yet " + request);
         }
-
-        @Nullable HybridTimestamp opTs = getTxOpTimestamp(request);
-        @Nullable HybridTimestamp opTsIfDirectRo = (request instanceof ReadOnlyDirectReplicaRequest) ? opTs : null;
-        @Nullable HybridTimestamp txTs = getTxStartTimestamp(request);
-        if (txTs == null) {
-            txTs = opTsIfDirectRo;
-        }
-
-        assert opTs == null || txTs == null || opTs.compareTo(txTs) >= 0 : "Tx started at " + txTs + ", but opTs precedes it: " + opTs
-                + "; request " + request;
-
-        // Don't need to validate schema.
-        if (opTs == null) {
-            assert opTsIfDirectRo == null;
-            return processOperationRequestWithTxOperationManagementLogic(senderId, request, isPrimary, null, leaseStartTime);
-        }
-
-        // Need to copy&paste logic from PartitionReplicaListener to process other messages.
-        LOG.debug("Non table request is not supported by the zone partition yet " + request);
-
         return completedFuture(new ReplicaResult(null, null));
     }
 
@@ -272,86 +246,5 @@ public class ZonePartitionReplicaListener implements ReplicaListener {
                     }
                 }
         );
-    }
-
-    // TODO https://issues.apache.org/jira/browse/IGNITE-22115
-    // Method's name and signature are the same as PartitionReplicaListener,
-    // to keep the same 'structure' and simplify the process of porting the requests processing.
-    private CompletableFuture<?> processOperationRequestWithTxOperationManagementLogic(
-            UUID senderId,
-            ReplicaRequest request,
-            @Nullable Boolean isPrimary,
-            @Nullable HybridTimestamp opStartTsIfDirectRo,
-            @Nullable Long leaseStartTime
-    ) {
-        try {
-            if (request instanceof UpdateMinimumActiveTxBeginTimeReplicaRequest) {
-                return minimumActiveTxTimeReplicaRequestHandler.handle((UpdateMinimumActiveTxBeginTimeReplicaRequest) request);
-            } else if (request instanceof ReplicaSafeTimeSyncRequest) {
-                LOG.debug("Non table request is not supported by the zone partition yet " + request);
-            } else {
-                LOG.warn("Non table request is not supported by the zone partition yet " + request);
-            }
-            return completedFuture(new ReplicaResult(null, null));
-        } catch (Throwable e) {
-            throw e;
-        }
-    }
-
-    /**
-     * Returns the txn operation timestamp.
-     *
-     * <ul>
-     *     <li>For a read/write in an RW transaction, it's 'now'</li>
-     *     <li>For an RO read (with readTimestamp), it's readTimestamp (matches readTimestamp in the transaction)</li>
-     *     <li>For a direct read in an RO implicit transaction, it's the timestamp chosen (as 'now') to process the request</li>
-     * </ul>
-     *
-     * <p>For other requests, op timestamp is not applicable and the validation is skipped.
-     *
-     * @param request The request.
-     * @return The timestamp or {@code null} if not a tx operation request.
-     */
-    private @Nullable HybridTimestamp getTxOpTimestamp(ReplicaRequest request) {
-        HybridTimestamp opStartTs;
-
-        if (request instanceof ReadWriteReplicaRequest) {
-            opStartTs = clockService.current();
-        } else if (request instanceof ReadOnlyReplicaRequest) {
-            opStartTs = ((ReadOnlyReplicaRequest) request).readTimestamp();
-        } else if (request instanceof ReadOnlyDirectReplicaRequest) {
-            opStartTs = clockService.current();
-        } else {
-            opStartTs = null;
-        }
-
-        return opStartTs;
-    }
-
-    /**
-     * Returns timestamp of transaction start (for RW/timestamped RO requests) or @{code null} for other requests.
-     *
-     * @param request Replica request corresponding to the operation.
-     */
-    private static @Nullable HybridTimestamp getTxStartTimestamp(ReplicaRequest request) {
-        HybridTimestamp txStartTimestamp;
-
-        if (request instanceof ReadWriteReplicaRequest) {
-            txStartTimestamp = beginRwTxTs((ReadWriteReplicaRequest) request);
-        } else if (request instanceof ReadOnlyReplicaRequest) {
-            txStartTimestamp = ((ReadOnlyReplicaRequest) request).readTimestamp();
-        } else {
-            txStartTimestamp = null;
-        }
-        return txStartTimestamp;
-    }
-
-    /**
-     * Extracts begin timestamp of a read-write transaction from a request.
-     *
-     * @param request Read-write replica request.
-     */
-    static HybridTimestamp beginRwTxTs(ReadWriteReplicaRequest request) {
-        return TransactionIds.beginTimestamp(request.transactionId());
     }
 }
