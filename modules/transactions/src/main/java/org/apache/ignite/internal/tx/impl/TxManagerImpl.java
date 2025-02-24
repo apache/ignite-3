5d8a8f1be85995a27a70da6b9f9bf0dcc2475ca1
Max Zhuravkov <shhwwa@gmail.com>
2025-02-24 11:04:15 +0200

    IGNITE-24520 Sql. TransactionViewProvider should only return local transactions

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
@@ -880,7 +880,7 @@ public class TxManagerImpl implements TxManager, NetworkMessageHandler, SystemVi
 
             txStateVolatileStorage.start();
 
-            txViewProvider.init(txStateVolatileStorage.statesMap().values());
+            txViewProvider.init(localNodeId, txStateVolatileStorage.statesMap());
 
             orphanDetector.start(txStateVolatileStorage, txConfig.abandonedCheckTs());
 
