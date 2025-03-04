25230cb761386b3eb3f84ecf7679b9c7768bcb8a
Mikhail Efremov <jakutenshi@gmail.com>
2025-03-04 16:10:02 +0600

    IGNITE-22620 ItReplicaLifecycleTest timeout is back

--- modules/partition-replicator/src/integrationTest/java/org/apache/ignite/internal/partition/replicator/ItReplicaLifecycleTest.java
+++ modules/partition-replicator/src/integrationTest/java/org/apache/ignite/internal/partition/replicator/ItReplicaLifecycleTest.java
@@ -83,11 +83,12 @@ import org.apache.ignite.table.KeyValueView;
 import org.apache.ignite.table.Table;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 /**
  * Replica lifecycle test.
  */
-// @Timeout(60)
+@Timeout(60)
 // TODO: https://issues.apache.org/jira/browse/IGNITE-22522 remove this test after the switching to zone-based replication
 public class ItReplicaLifecycleTest extends ItAbstractColocationTest {
     @InjectConfiguration("mock.nodeAttributes: {region = US, storage = SSD}")
