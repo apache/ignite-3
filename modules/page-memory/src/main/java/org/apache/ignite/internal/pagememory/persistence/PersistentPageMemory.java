fed2c78b53729b7683b9221b96bf585f847b5dd7
ibessonov <bessonov.ip@gmail.com>
2025-03-07 11:10:14 +0300

    IGNITE-24547 Port speed-based throttling from Ignite 2
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/persistence/PersistentPageMemory.java
+++ modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/persistence/PersistentPageMemory.java
@@ -1316,6 +1316,23 @@ public class PersistentPageMemory implements PageMemory {
     }
 
     /**
+     * Returns a max dirty pages ratio amongst all segments.
+     */
+    public double dirtyPagesRatio() {
+        if (segments == null) {
+            return 0;
+        }
+
+        double res = 0;
+
+        for (Segment segment : segments) {
+            res = Math.max(res, segment.dirtyPagesRatio());
+        }
+
+        return res;
+    }
+
+    /**
      * Page segment.
      */
     public class Segment extends ReentrantReadWriteLock {
@@ -1423,6 +1440,13 @@ public class PersistentPageMemory implements PageMemory {
             } finally {
                 writeLock().unlock();
             }
+        }
+
+        /**
+         * Returns a dirty pages ratio.
+         */
+        private double dirtyPagesRatio() {
+            return dirtyPagesCntr.doubleValue() / pages();
         }
 
         /**
