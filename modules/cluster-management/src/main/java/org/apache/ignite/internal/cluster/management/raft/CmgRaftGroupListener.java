aeecd297c0c0cb23a79d7b64bf439cb01c36cf26
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2025-03-18 14:57:53 +0300

    IGNITE-24826 Remove unused UpdateClusterStateCommand

--- modules/cluster-management/src/main/java/org/apache/ignite/internal/cluster/management/raft/CmgRaftGroupListener.java
+++ modules/cluster-management/src/main/java/org/apache/ignite/internal/cluster/management/raft/CmgRaftGroupListener.java
@@ -47,7 +47,6 @@ import org.apache.ignite.internal.cluster.management.raft.commands.ReadLogicalTo
 import org.apache.ignite.internal.cluster.management.raft.commands.ReadMetaStorageInfoCommand;
 import org.apache.ignite.internal.cluster.management.raft.commands.ReadStateCommand;
 import org.apache.ignite.internal.cluster.management.raft.commands.ReadValidatedNodesCommand;
-import org.apache.ignite.internal.cluster.management.raft.commands.UpdateClusterStateCommand;
 import org.apache.ignite.internal.cluster.management.raft.responses.LogicalTopologyResponse;
 import org.apache.ignite.internal.cluster.management.raft.responses.ValidationErrorResponse;
 import org.apache.ignite.internal.cluster.management.topology.LogicalTopology;
@@ -174,10 +173,6 @@ public class CmgRaftGroupListener implements RaftGroupListener {
                     Serializable response = initCmgState((InitCmgStateCommand) command);
 
                     clo.result(response);
-                } else if (command instanceof UpdateClusterStateCommand) {
-                    UpdateClusterStateCommand updateClusterStateCommand = (UpdateClusterStateCommand) command;
-                    storageManager.putClusterState(updateClusterStateCommand.clusterState());
-                    clo.result(null);
                 } else if (command instanceof JoinRequestCommand) {
                     ValidationResult response = validateNode((JoinRequestCommand) command);
 
