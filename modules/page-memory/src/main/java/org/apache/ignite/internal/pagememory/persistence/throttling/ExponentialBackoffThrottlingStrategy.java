30890b3a97741c7097d69cc48e65b9891e35eee2
ibessonov <bessonov.ip@gmail.com>
2025-03-25 13:19:45 +0300

    IGNITE-24548 Reduce backoff ratio
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/persistence/throttling/ExponentialBackoffThrottlingStrategy.java
+++ modules/page-memory/src/main/java/org/apache/ignite/internal/pagememory/persistence/throttling/ExponentialBackoffThrottlingStrategy.java
@@ -29,7 +29,7 @@ class ExponentialBackoffThrottlingStrategy {
     /**
      * Backoff ratio. Each next park will be this times longer.
      */
-    private static final double BACKOFF_RATIO = 1.05;
+    private static final double BACKOFF_RATIO = 1.005;
 
     /**
      * Exponential backoff used to throttle threads.
