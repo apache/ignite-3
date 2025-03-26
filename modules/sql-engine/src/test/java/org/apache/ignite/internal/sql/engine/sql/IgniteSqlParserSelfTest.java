35abb81d564d16b0c8839ca6a6904dd985add153
amashenkov <amashenkov@apache.org>
2025-03-26 13:15:38 +0300

    minor

--- modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlParserSelfTest.java
+++ modules/sql-engine/src/test/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlParserSelfTest.java
@@ -134,10 +134,11 @@ public class IgniteSqlParserSelfTest {
     @Test
     public void testIdentifier() {
         IgniteSqlParser.parse("SELECT 1 as _foo", StatementParseResult.MODE);
+        IgniteSqlParser.parse("SELECT 1 as \"$foo\"", StatementParseResult.MODE);
 
         assertThrowsSqlException(
                 Sql.STMT_VALIDATION_ERR,
-                "Malformed identifier. At line 1, column 13",
+                "Malformed identifier at line 1, column 13",
                 () -> IgniteSqlParser.parse("SELECT 1 as $foo", StatementParseResult.MODE));
     }
 
