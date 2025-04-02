e44920d246b36412525c4c81076139c3db4a0d4d
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-04-02 16:53:39 +0300

    wip NativeComputeTests

---
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Compute/Native/NativeComputeTests.cs
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Tests.Compute.Native;
+
+using System.Threading.Tasks;
+using Ignite.Compute;
+using NUnit.Framework;
+
+/// <summary>
+/// Tests for .NET Native Compute API.
+/// </summary>
+public class NativeComputeTests : IgniteTestsBase
+{
+    [Test]
+    public async Task TestSimpleJob()
+    {
+        // TODO: Shortcut method to create a job descriptor from a job type.
+        var jobDesc = new JobDescriptor<object, string>(typeof(ToStringJob).AssemblyQualifiedName!);
+
+        var target = JobTarget.AnyNode(await Client.GetClusterNodesAsync());
+        IJobExecution<string> jobExec = await Client.Compute.SubmitAsync(target, jobDesc, 123);
+        string jobRes = await jobExec.GetResultAsync();
+
+        Assert.AreEqual("123", jobRes);
+    }
+
+    private class ToStringJob : IComputeJob<object, string>
+    {
+        public ValueTask<string> ExecuteAsync(IJobExecutionContext context, object arg) =>
+            ValueTask.FromResult(arg.ToString())!;
+    }
+}
