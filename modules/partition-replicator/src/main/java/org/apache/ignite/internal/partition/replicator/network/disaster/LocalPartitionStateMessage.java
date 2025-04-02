651a75d917ae0e1307233c0e981f00664f8ee46e
Kirill Sizov <sizov.kirill.y@gmail.com>
2025-04-02 11:19:10 +0300

    Fix message

--- modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/network/disaster/LocalPartitionStateMessage.java
+++ modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/network/disaster/LocalPartitionStateMessage.java
@@ -22,6 +22,7 @@ import org.apache.ignite.internal.network.annotations.Transferable;
 import org.apache.ignite.internal.partition.replicator.network.PartitionReplicationMessageGroup.DisasterRecoveryMessages;
 import org.apache.ignite.internal.replicator.message.TablePartitionIdMessage;
 import org.apache.ignite.internal.replicator.message.ZonePartitionIdMessage;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Local partition state message, has partition ID, state and last committed log index.
@@ -29,9 +30,11 @@ import org.apache.ignite.internal.replicator.message.ZonePartitionIdMessage;
 @Transferable(DisasterRecoveryMessages.LOCAL_PARTITION_STATE)
 public interface LocalPartitionStateMessage extends NetworkMessage {
     /** Table Partition ID. */
+    @Nullable
     TablePartitionIdMessage partitionId();
 
     /** Zone Partition ID. */
+    @Nullable
     ZonePartitionIdMessage zonePartitionId();
 
     /** Calculated state of the partition. */
