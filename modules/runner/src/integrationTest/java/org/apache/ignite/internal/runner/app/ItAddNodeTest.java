9e46efa1d93050489ac5ccce336418850ce6cb0c
ibessonov <bessonov.ip@gmail.com>
2025-04-02 17:10:40 +0300

    IGNITE-24985 <review>
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/ItAddNodeTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/ItAddNodeTest.java
@@ -17,19 +17,8 @@
 
 package org.apache.ignite.internal.runner.app;
 
-import static org.apache.ignite.internal.network.configuration.NetworkConfigurationSchema.DEFAULT_PORT;
-import static org.apache.ignite.internal.testframework.IgniteTestUtils.testNodeName;
-import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willCompleteSuccessfully;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteServer;
-import org.apache.ignite.InitParametersBuilder;
-import org.apache.ignite.internal.testframework.IgniteAbstractTest;
-import org.apache.ignite.internal.testframework.TestIgnitionManager;
-import org.apache.ignite.internal.util.IgniteUtils;
+import org.apache.ignite.internal.ClusterPerTestIntegrationTest;
 import org.apache.ignite.sql.ResultSet;
 import org.apache.ignite.sql.SqlRow;
 import org.junit.jupiter.api.Test;
@@ -38,63 +27,29 @@ import org.junit.jupiter.api.TestInfo;
 /**
  * Integration tests for testing simple node join scenarios.
  */
-public class ItAddNodeTest extends IgniteAbstractTest {
-    private static final String NODE_CONFIG_TEMPLATE
-            = "ignite.network.nodeFinder.netClusterNodes = [\"localhost:" + DEFAULT_PORT + "\"]\n"
-            + "ignite.network.port = %d\n"
-            + "ignite.rest.port = %d\n";
+public class ItAddNodeTest extends ClusterPerTestIntegrationTest {
+    @Override
+    protected int initialNodes() {
+        return 0;
+    }
 
     /**
      * Test that shows that we can join a third node to a cluster of two.
      */
     @Test
     public void addOneNodeToTwoNodeCluster(TestInfo testInfo) throws Exception {
-        int numberOfNodesInitially = 2;
+        cluster.startAndInit(2);
 
-        List<IgniteServer> nodes = new ArrayList<>();
+        Ignite ignite0 = cluster.node(0);
 
-        try {
-            for (int i = 0; i < numberOfNodesInitially; i++) {
-                String nodeName = testNodeName(testInfo, i);
-
-                IgniteServer node = TestIgnitionManager.start(
-                        nodeName,
-                        String.format(NODE_CONFIG_TEMPLATE, DEFAULT_PORT + i, 10300 + i),
-                        workDir.resolve(nodeName)
-                );
-
-                nodes.add(node);
-            }
-
-            IgniteServer node0 = nodes.get(0);
-
-            node0.initCluster(new InitParametersBuilder()
-                    .clusterName("clusterName")
-                    .metaStorageNodes(node0)
-                    .build()
-            );
-            assertThat(node0.waitForInitAsync(), willCompleteSuccessfully());
-
-            Ignite ignite0 = node0.api();
-
-            try (ResultSet<SqlRow> unused = ignite0.sql().execute(null, "CREATE TABLE " + "TEST_TABLE" + "(\n"
-                    + "    key int PRIMARY KEY,\n"
-                    + "    field1   int\n"
-                    + ")"
-            )) {
-                // No-op.
-            }
-
-            String nodeName2 = testNodeName(testInfo, numberOfNodesInitially);
-            IgniteServer node2 = TestIgnitionManager.start(
-                    nodeName2,
-                    String.format(NODE_CONFIG_TEMPLATE, DEFAULT_PORT + numberOfNodesInitially, 10300 + numberOfNodesInitially),
-                    workDir.resolve(nodeName2)
-            );
-
-            nodes.add(node2);
-        } finally {
-            IgniteUtils.closeAll(nodes.stream().map(node -> node::shutdown));
+        try (ResultSet<SqlRow> unused = ignite0.sql().execute(null, "CREATE TABLE TEST_TABLE (\n"
+                + "    key int PRIMARY KEY,\n"
+                + "    field1   int\n"
+                + ")"
+        )) {
+            // No-op.
         }
+
+        cluster.startNode(2);
     }
 }
