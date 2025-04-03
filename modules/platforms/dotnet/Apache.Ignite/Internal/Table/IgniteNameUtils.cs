cc41521afa0e054db14ab3d8763276a32b62e2eb
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-04-03 08:02:36 +0300

    wip

---
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Table/IgniteNameUtils.cs
@@ -0,0 +1,184 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Internal.Table;
+
+using System;
+
+/// <summary>
+/// Ignite name utilities.
+/// </summary>
+internal static class IgniteNameUtils
+{
+    /// <summary>
+    /// Separator character between schema and object names.
+    /// </summary>
+    public const char SeparatorChar = '.';
+
+    /// <summary>
+    /// Quote character for identifiers.
+    /// </summary>
+    public const char QuoteChar = '"';
+
+    /// <summary>
+    /// Verifies that the specified identifier is not null or empty.
+    /// </summary>
+    /// <param name="identifier">Identifier.</param>
+    public static void VerifyObjectIdentifier(string identifier) =>
+        ArgumentException.ThrowIfNullOrEmpty(identifier);
+
+    /// <summary>
+    /// Unquotes the specified identifier, or converts it to upper case if it is not quoted.
+    /// </summary>
+    /// <param name="identifier">Identifier.</param>
+    /// <returns>Unquoted or uppercased identifier.</returns>
+    public static string Unquote(ReadOnlyMemory<char> identifier)
+    {
+        if (identifier.IsEmpty)
+        {
+            return string.Empty;
+        }
+
+        return identifier.Span[0] == QuoteChar
+            ? identifier[1..^1].ToString().Replace("\"\"", "\"", StringComparison.Ordinal) // Escaped quotes are rare, don't optimize.
+            : identifier.ToStringUpperInvariant();
+    }
+
+    /// <summary>
+    /// Wraps the given name with double quotes if it is not uppercased non-quoted name,
+    /// e.g. "myColumn" -> "\"myColumn\"", "MYCOLUMN" -> "MYCOLUMN".
+    /// </summary>
+    /// <param name="name">Name.</param>
+    /// <returns>Quoted name.</returns>
+    public static string QuoteIfNeeded(string name)
+    {
+        // TODO: Implement.
+        return name;
+    }
+
+    /// <summary>
+    /// Converts a memory of chars to an uppercase string.
+    /// </summary>
+    /// <param name="chars">Chars.</param>
+    /// <returns>Uppercased string.</returns>
+    public static string ToStringUpperInvariant(this ReadOnlyMemory<char> chars) =>
+        string.Create(chars.Length, chars, (span, args) => args.Span.ToUpperInvariant(span));
+
+    /// <summary>
+    /// Parses the identifier and returns it unquoted.
+    /// </summary>
+    /// <param name="identifier">Identifier.</param>
+    /// <returns>Parsed identifier.</returns>
+    public static string ParseIdentifier(ReadOnlyMemory<char> identifier)
+    {
+        if (IndexOfSeparatorChar(identifier.Span) is var separatorPos && separatorPos != -1)
+        {
+            throw new FormatException($"Unexpected separator at position {separatorPos}: '{identifier}'");
+        }
+
+        return Unquote(identifier);
+    }
+
+    /// <summary>
+    /// Returns a value indicating whether the specified character is a valid identifier start character.
+    /// An identifier start is any character in the Unicode General Category classes “Lu”, “Ll”, “Lt”, “Lm”, “Lo”, or “Nl”.
+    /// </summary>
+    /// <param name="c">Char.</param>
+    /// <returns>Whether the specified character is a valid identifier start character.</returns>
+    public static bool IsIdentifierStart(char c) =>
+        char.IsLetter(c) || c == '_';
+
+    /// <summary>
+    /// Returns a value indicating whether the specified character is a valid identifier extend character.
+    /// An identifier extend is U+00B7, or any character in the Unicode General Category classes “Mn”, “Mc”, “Nd”, “Pc”, or “Cf”.
+    /// </summary>
+    /// <param name="c">Char.</param>
+    /// <returns>Whether the specified character is a valid identifier extend character.</returns>
+    public static bool IsIdentifierExtend(char c)
+    {
+        // Direct port from Java.
+        return c == ('·' & 0xff) /* “Middle Dot” character */
+               || ((((1 << 6)
+                     | (1 << 8)
+                     | (1 << 9)
+                     | (1 << 23)
+                     | (1 << 16)) >> (int)char.GetUnicodeCategory(c)) & 1) != 0;
+    }
+
+    private static int IndexOfSeparatorChar(ReadOnlySpan<char> name)
+    {
+        bool quoted = name[0] == QuoteChar;
+        int pos = quoted ? 1 : 0;
+
+        for (; pos < name.Length; pos++)
+        {
+            char ch = name[pos];
+
+            if (ch == QuoteChar)
+            {
+                if (!quoted)
+                {
+                    throw new FormatException($"Identifier is not quoted, but contains quote character at position {pos}: '{name}'");
+                }
+
+                char? nextCh = pos + 1 < name.Length ? name[pos + 1] : null;
+                if (nextCh == QuoteChar)
+                {
+                    // Escaped quote.
+                    pos++;
+                    continue;
+                }
+
+                if (nextCh == SeparatorChar)
+                {
+                    // End of quoted identifier, separator follows.
+                    return pos + 1;
+                }
+
+                if (nextCh == null)
+                {
+                    // End of quoted identifier, no separator.
+                    return -1;
+                }
+
+                throw new FormatException($"Unexpected character '{ch}' after quote at position {pos}: '{name}'");
+            }
+
+            if (ch == SeparatorChar)
+            {
+                if (quoted)
+                {
+                    continue;
+                }
+
+                return pos;
+            }
+
+            if (!quoted && !IsIdentifierStart(ch) && !IsIdentifierExtend(ch))
+            {
+                throw new FormatException($"Unexpected character '{ch}' at position {pos}: '{name}'");
+            }
+        }
+
+        if (quoted)
+        {
+            throw new FormatException($"Missing closing quote: '{name}");
+        }
+
+        return -1;
+    }
+}
--- modules/platforms/dotnet/Apache.Ignite/Table/QualifiedName.cs
+++ modules/platforms/dotnet/Apache.Ignite/Table/QualifiedName.cs
@@ -18,6 +18,8 @@
 namespace Apache.Ignite.Table;
 
 using System;
+using Internal.Table;
+using static Internal.Table.IgniteNameUtils;
 
 /// <summary>
 /// Represents a qualified name of a database object.
@@ -48,12 +50,12 @@ public readonly record struct QualifiedName
     /// <summary>
     /// Separator character between schema and object names.
     /// </summary>
-    public const char SeparatorChar = '.';
+    public const char SeparatorChar = IgniteNameUtils.SeparatorChar;
 
     /// <summary>
     /// Quote character for identifiers.
     /// </summary>
-    public const char QuoteChar = '"';
+    public const char QuoteChar = IgniteNameUtils.QuoteChar;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="QualifiedName"/> struct.
@@ -125,109 +127,4 @@ public readonly record struct QualifiedName
             Unquote(nameMem[(separatorIndex + 1)..]));
     }
 
-    private static void VerifyObjectIdentifier(string identifier) =>
-        ArgumentException.ThrowIfNullOrEmpty(identifier);
-
-    private static string Unquote(ReadOnlyMemory<char> name) =>
-        name.Span[0] == QuoteChar
-            ? name[1..^1].ToString().Replace("\"\"", "\"", StringComparison.Ordinal) // Escaped quotes are rare, don't optimize.
-            : ToStringUpperInvariant(name);
-
-    private static string QuoteIfNeeded(string name)
-    {
-        // TODO: Implement.
-        return name;
-    }
-
-    private static string ToStringUpperInvariant(ReadOnlyMemory<char> name) =>
-        string.Create(name.Length, name, (span, args) => args.Span.ToUpperInvariant(span));
-
-    private static string ParseIdentifier(ReadOnlyMemory<char> name)
-    {
-        if (IndexOfSeparatorChar(name.Span) is var separatorPos && separatorPos != -1)
-        {
-            throw new FormatException($"Unexpected separator at position {separatorPos}: '{name}'");
-        }
-
-        return Unquote(name);
-    }
-
-    private static int IndexOfSeparatorChar(ReadOnlySpan<char> name)
-    {
-        bool quoted = name[0] == QuoteChar;
-        int pos = quoted ? 1 : 0;
-
-        for (; pos < name.Length; pos++)
-        {
-            char ch = name[pos];
-
-            if (ch == QuoteChar)
-            {
-                if (!quoted)
-                {
-                    throw new FormatException($"Identifier is not quoted, but contains quote character at position {pos}: '{name}'");
-                }
-
-                char? nextCh = pos + 1 < name.Length ? name[pos + 1] : null;
-                if (nextCh == QuoteChar)
-                {
-                    // Escaped quote.
-                    pos++;
-                    continue;
-                }
-
-                if (nextCh == SeparatorChar)
-                {
-                    // End of quoted identifier, separator follows.
-                    return pos + 1;
-                }
-
-                if (nextCh == null)
-                {
-                    // End of quoted identifier, no separator.
-                    return -1;
-                }
-
-                throw new FormatException($"Unexpected character '{ch}' after quote at position {pos}: '{name}'");
-            }
-
-            if (ch == SeparatorChar)
-            {
-                if (quoted)
-                {
-                    continue;
-                }
-
-                return pos;
-            }
-
-            if (!quoted && !IsIdentifierStart(ch) && !IsIdentifierExtend(ch))
-            {
-                throw new FormatException($"Unexpected character '{ch}' at position {pos}: '{name}'");
-            }
-        }
-
-        if (quoted)
-        {
-            throw new FormatException($"Missing closing quote: '{name}");
-        }
-
-        return -1;
-    }
-
-    /** An identifier start is any character in the Unicode General Category classes “Lu”, “Ll”, “Lt”, “Lm”, “Lo”, or “Nl”. */
-    private static bool IsIdentifierStart(char c) =>
-        char.IsLetter(c) || c == '_';
-
-    /** An identifier extend is U+00B7, or any character in the Unicode General Category classes “Mn”, “Mc”, “Nd”, “Pc”, or “Cf”.*/
-    private static bool IsIdentifierExtend(char c)
-    {
-        // Direct port from Java.
-        return c == ('·' & 0xff) /* “Middle Dot” character */
-               || ((((1 << 6)
-                     | (1 << 8)
-                     | (1 << 9)
-                     | (1 << 23)
-                     | (1 << 16)) >> (int)char.GetUnicodeCategory(c)) & 1) != 0;
-    }
 }
