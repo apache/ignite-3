247cd49a20961af328dfc0a5ee19afc1d1de8141
Pavel Pereslegin <xxtern@gmail.com>
2025-04-08 16:46:40 +0300

    IGNITE-24825 (minor) Code cleanup.

--- modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/sqllogic/ItSqlLogicTest.java
+++ modules/sql-engine/src/integrationTest/java/org/apache/ignite/internal/sql/sqllogic/ItSqlLogicTest.java
@@ -149,7 +149,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @WithSystemProperty(key = "IMPLICIT_PK_ENABLED", value = "true")
 // The following is to make sure we unlock LWM on data nodes promptly so that dropped tables are destroyed fast.
 @WithSystemProperty(key = ResourceVacuumManager.RESOURCE_VACUUM_INTERVAL_MILLISECONDS_PROPERTY, value = "1000")
-@SqlLogicTestEnvironment(scriptsRoot = "src/integrationTest/sql/group1", regex = "test_incorrect_dates\\.test")
+@SqlLogicTestEnvironment(scriptsRoot = "src/integrationTest/sql/group1")
 public class ItSqlLogicTest extends BaseIgniteAbstractTest {
     private static final String SQL_LOGIC_TEST_INCLUDE_SLOW = "SQL_LOGIC_TEST_INCLUDE_SLOW";
 
