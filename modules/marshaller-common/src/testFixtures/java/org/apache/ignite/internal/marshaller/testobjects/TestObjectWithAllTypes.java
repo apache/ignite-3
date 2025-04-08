a787e2e3b96ee6632405ddc4d06f775b9cb62a08
Pavel Pereslegin <xxtern@gmail.com>
2025-04-08 15:37:48 +0300

    IGNITE-24825 Review note. Random object generation.

--- modules/marshaller-common/src/testFixtures/java/org/apache/ignite/internal/marshaller/testobjects/TestObjectWithAllTypes.java
+++ modules/marshaller-common/src/testFixtures/java/org/apache/ignite/internal/marshaller/testobjects/TestObjectWithAllTypes.java
@@ -17,6 +17,8 @@
 
 package org.apache.ignite.internal.marshaller.testobjects;
 
+import static org.apache.ignite.internal.schema.SchemaTestUtils.generateRandomValue;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -26,7 +28,6 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 import java.util.UUID;
-import org.apache.ignite.internal.schema.SchemaTestUtils;
 import org.apache.ignite.internal.testframework.IgniteTestUtils;
 import org.apache.ignite.internal.type.NativeTypes;
 
@@ -39,34 +40,34 @@ public class TestObjectWithAllTypes {
      * Creates an object with random data.
      */
     public static TestObjectWithAllTypes randomObject(Random rnd) {
-        final TestObjectWithAllTypes obj = new TestObjectWithAllTypes();
+        TestObjectWithAllTypes obj = new TestObjectWithAllTypes();
 
-        obj.primitiveBooleanCol = rnd.nextBoolean();
-        obj.primitiveByteCol = (byte) rnd.nextInt(255);
-        obj.primitiveShortCol = (short) rnd.nextInt(65535);
-        obj.primitiveIntCol = rnd.nextInt();
-        obj.primitiveLongCol = rnd.nextLong();
-        obj.primitiveFloatCol = rnd.nextFloat();
-        obj.primitiveDoubleCol = rnd.nextDouble();
+        obj.primitiveBooleanCol = (boolean) generateRandomValue(rnd, NativeTypes.BOOLEAN);
+        obj.primitiveByteCol = (byte) generateRandomValue(rnd, NativeTypes.INT8);
+        obj.primitiveShortCol = (short) generateRandomValue(rnd, NativeTypes.INT16);
+        obj.primitiveIntCol = (int) generateRandomValue(rnd, NativeTypes.INT32);
+        obj.primitiveLongCol = (long) generateRandomValue(rnd, NativeTypes.INT64);
+        obj.primitiveFloatCol = (float) generateRandomValue(rnd, NativeTypes.FLOAT);
+        obj.primitiveDoubleCol = (double) generateRandomValue(rnd, NativeTypes.DOUBLE);
 
-        obj.booleanCol = rnd.nextBoolean();
-        obj.byteCol = (byte) rnd.nextInt(255);
-        obj.shortCol = (short) rnd.nextInt(65535);
-        obj.intCol = rnd.nextInt();
-        obj.longCol = rnd.nextLong();
-        obj.floatCol = rnd.nextFloat();
-        obj.doubleCol = rnd.nextDouble();
+        obj.booleanCol = (boolean) generateRandomValue(rnd, NativeTypes.BOOLEAN);
+        obj.byteCol = (byte) generateRandomValue(rnd, NativeTypes.INT8);
+        obj.shortCol = (short) generateRandomValue(rnd, NativeTypes.INT16);
+        obj.intCol = (int) generateRandomValue(rnd, NativeTypes.INT32);
+        obj.longCol = (long) generateRandomValue(rnd, NativeTypes.INT64);
+        obj.floatCol = (float) generateRandomValue(rnd, NativeTypes.FLOAT);
+        obj.doubleCol = (double) generateRandomValue(rnd, NativeTypes.DOUBLE);
 
-        obj.uuidCol = new UUID(rnd.nextLong(), rnd.nextLong());
+        obj.uuidCol = (UUID) generateRandomValue(rnd, NativeTypes.UUID);
 
-        obj.dateCol = LocalDate.ofYearDay(1990 + rnd.nextInt(50), 1 + rnd.nextInt(360));
-        obj.timeCol = LocalTime.of(rnd.nextInt(24), rnd.nextInt(60));
-        obj.dateTimeCol = LocalDateTime.of(obj.dateCol, obj.timeCol);
-        obj.timestampCol = (Instant) SchemaTestUtils.generateRandomValue(rnd, NativeTypes.timestamp(6));
+        obj.dateCol = (LocalDate) generateRandomValue(rnd, NativeTypes.DATE);
+        obj.timeCol = (LocalTime) generateRandomValue(rnd, NativeTypes.time(0));
+        obj.dateTimeCol = (LocalDateTime) generateRandomValue(rnd, NativeTypes.datetime(0));
+        obj.timestampCol = (Instant) generateRandomValue(rnd, NativeTypes.timestamp(6));
 
-        obj.stringCol = IgniteTestUtils.randomString(rnd, rnd.nextInt(255));
-        obj.bytesCol = IgniteTestUtils.randomBytes(rnd, rnd.nextInt(255));
-        obj.decimalCol = BigDecimal.valueOf(rnd.nextLong(), 3);
+        obj.stringCol = (String) generateRandomValue(rnd, NativeTypes.stringOf(rnd.nextInt(255)));
+        obj.bytesCol = (byte[]) generateRandomValue(rnd, NativeTypes.blobOf(255));
+        obj.decimalCol = (BigDecimal) generateRandomValue(rnd, NativeTypes.decimalOf(10, 3));
 
         obj.nullLongCol = null;
         obj.nullBytesCol = null;
