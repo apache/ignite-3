97479d4ea7842ad945bef424650cdfbb6da95262
Tiago Marques Godinho <metaltmg@gmail.com>
2025-04-10 18:27:11 +0100

    IGNITE-24234 Prepare the codebase of MT for donation (#111)

--- migration-tools/modules/migration-tools-persistence/src/main/java/org/apache/ignite/migrationtools/persistence/Ignite2PersistentCacheTools.java
+++ migration-tools/modules/migration-tools-persistence/src/main/java/org/apache/ignite/migrationtools/persistence/Ignite2PersistentCacheTools.java
@@ -1,10 +1,18 @@
 /*
- *  Copyright (C) GridGain Systems. All Rights Reserved.
- *  _________        _____ __________________        _____
- *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
- *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
- *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
- *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.ignite.migrationtools.persistence;
@@ -176,7 +184,7 @@ public class Ignite2PersistentCacheTools {
                 .orElseThrow(() -> new RuntimeException("Could not find the requested cache: " + cacheName));
 
         // TODO: GG-40802 Allow injecting custom aliases/fieldNameForColumn mappings
-        String quotedName = IgniteNameUtils.quote(cacheName);
+        String quotedName = IgniteNameUtils.quoteIfNeeded(cacheName);
         @Nullable ClientTable table = (ClientTable) client.tables().table(quotedName);
         SqlDdlGenerator.GenerateTableResult tableDefinition = sqlGenerator.generate(cacheCfg);
         Map<String, String> columnToFieldMappings = tableDefinition.fieldNameForColumnMappings();
