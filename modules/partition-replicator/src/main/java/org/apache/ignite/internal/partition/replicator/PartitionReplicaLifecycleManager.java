2a7a6f1b2e50793feda6184a48a80d37df984a58
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2025-04-11 19:15:34 +0400

    IGNITE-24981 / reformat code

--- modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/PartitionReplicaLifecycleManager.java
+++ modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/PartitionReplicaLifecycleManager.java
@@ -537,31 +537,33 @@ public class PartitionReplicaLifecycleManager extends
         // Zone nodes might be created in the same catalog version as a table in this zone, so there could be a race
         // between storage creation due to replica start and storage creation due to table addition. To eliminate the race,
         // we acquire a write lock on the zone (table addition acquires a read lock).
-        return inBusyLockAsync(busyLock, () -> executeUnderZoneWriteLock(zoneId, () -> inBusyLockAsync(busyLock, () -> {
-        assert assignments != null : IgniteStringFormatter.format("Zone has empty assignments [id={}].", zoneId);
+        return inBusyLockAsync(busyLock, () -> executeUnderZoneWriteLock(zoneId,
+                () -> inBusyLockAsync(busyLock, () -> {
+                    assert assignments != null : IgniteStringFormatter.format("Zone has empty assignments [id={}].", zoneId);
 
-            var partitionsStartFutures = new CompletableFuture<?>[assignments.size()];
+                    var partitionsStartFutures = new CompletableFuture<?>[assignments.size()];
 
-            for (int partId = 0; partId < assignments.size(); partId++) {
-                Assignments zoneAssignment = assignments.get(partId);
+                    for (int partId = 0; partId < assignments.size(); partId++) {
+                        Assignments zoneAssignment = assignments.get(partId);
 
-                Assignment localMemberAssignment = localMemberAssignment(zoneAssignment);
+                        Assignment localMemberAssignment = localMemberAssignment(zoneAssignment);
 
-                var zonePartitionId = new ZonePartitionId(zoneId, partId);
+                        var zonePartitionId = new ZonePartitionId(zoneId, partId);
 
-                partitionsStartFutures[partId] = createZonePartitionReplicationNode(
-                        zonePartitionId,
-                        localMemberAssignment,
-                        zoneAssignment,
-                        causalityToken,
-                        partitionCount,
-                        isVolatileZoneForCatalogVersion(zoneId, catalogVersion),
-                        true
-                );
-            }
+                        partitionsStartFutures[partId] = createZonePartitionReplicationNode(
+                                zonePartitionId,
+                                localMemberAssignment,
+                                zoneAssignment,
+                                causalityToken,
+                                partitionCount,
+                                isVolatileZoneForCatalogVersion(zoneId, catalogVersion),
+                                true
+                        );
+                    }
 
-            return allOf(partitionsStartFutures);
-        })));
+                    return allOf(partitionsStartFutures);
+                })
+        ));
     }
 
     private boolean isVolatileZoneForCatalogVersion(int zoneId, int catalogVersion) {
