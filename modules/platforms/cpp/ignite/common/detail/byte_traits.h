de595887a27dccac757c885b474d80864573be9b
dzabotlin <dzabotlin@gridgain.com>
2025-04-16 14:58:17 +0300

    Mark static methods of byte_traits as inline

--- modules/platforms/cpp/ignite/common/detail/byte_traits.h
+++ modules/platforms/cpp/ignite/common/detail/byte_traits.h
@@ -26,45 +26,45 @@ namespace ignite::detail {
 struct byte_traits : std::char_traits<char> {
     using char_type = std::byte;
 
-    static void assign(char_type &a, const char_type &b) noexcept { a = b; }
+    static inline void assign(char_type &a, const char_type &b) noexcept { a = b; }
 
-    static bool eq(char_type a, char_type b) noexcept { return a == b; }
+    static inline bool eq(char_type a, char_type b) noexcept { return a == b; }
 
-    static bool lt(char_type a, char_type b) noexcept { return a < b; }
+    static inline bool lt(char_type a, char_type b) noexcept { return a < b; }
 
-    static int compare(const char_type *a, const char_type *b, std::size_t size) noexcept {
+    static inline int compare(const char_type *a, const char_type *b, std::size_t size) noexcept {
         return std::memcmp(a, b, size);
     }
 
     // We cannot determine length of the byte sequence only from the pointer, because there is no 'stop'-byte.
     // So implementing find functions we should not rely upon this implementation of length().
-    static size_t length(const char_type *data) noexcept { return std::strlen(reinterpret_cast<const char *>(data)); }
+    static inline size_t length(const char_type *data) noexcept { return std::strlen(reinterpret_cast<const char *>(data)); }
 
-    static const char_type *find(const char_type *data, std::size_t size, const char_type &value) noexcept {
+    static inline const char_type *find(const char_type *data, std::size_t size, const char_type &value) noexcept {
         return static_cast<const char_type *>(std::memchr(data, static_cast<int>(value), size));
     }
 
-    static char_type *move(char_type *dest, const char_type *src, std::size_t size) noexcept {
+    static inline char_type *move(char_type *dest, const char_type *src, std::size_t size) noexcept {
         return static_cast<char_type *>(std::memmove(dest, src, size));
     }
 
-    static char_type *copy(char_type *dest, const char_type *src, std::size_t size) noexcept {
+    static inline char_type *copy(char_type *dest, const char_type *src, std::size_t size) noexcept {
         return static_cast<char_type *>(std::memcpy(dest, src, size));
     }
 
-    static char_type *assign(char_type *dest, std::size_t size, char_type value) noexcept {
+    static inline char_type *assign(char_type *dest, std::size_t size, char_type value) noexcept {
         return static_cast<char_type *>(std::memset(dest, static_cast<int>(value), size));
     }
 
-    static int_type not_eof(int_type value) noexcept { return eq_int_type(value, eof()) ? ~eof() : value; }
+    static inline int_type not_eof(int_type value) noexcept { return eq_int_type(value, eof()) ? ~eof() : value; }
 
-    static char_type to_char_type(int_type value) noexcept { return char_type(value); }
+    static inline char_type to_char_type(int_type value) noexcept { return char_type(value); }
 
-    static int_type to_int_type(char_type value) noexcept { return int_type(value); }
+    static inline int_type to_int_type(char_type value) noexcept { return int_type(value); }
 
-    static bool eq_int_type(int_type a, int_type b) noexcept { return a == b; }
+    static inline bool eq_int_type(int_type a, int_type b) noexcept { return a == b; }
 
-    static int_type eof() noexcept { return int_type(EOF); }
+    static inline int_type eof() noexcept { return int_type(EOF); }
 };
 
 } // namespace ignite::detail
