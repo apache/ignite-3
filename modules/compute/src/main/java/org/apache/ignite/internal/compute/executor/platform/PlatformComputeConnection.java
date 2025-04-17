cdf7108ea64c47f9f7b3a090fa7db2f0e78ecceb
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-04-17 12:18:04 +0300

    wip PlatformComputeConnection

--- modules/compute/src/main/java/org/apache/ignite/internal/compute/executor/platform/PlatformComputeConnection.java
+++ modules/compute/src/main/java/org/apache/ignite/internal/compute/executor/platform/PlatformComputeConnection.java
@@ -21,8 +21,19 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.internal.compute.ComputeJobDataHolder;
 
-@SuppressWarnings("InterfaceMayBeAnnotatedFunctional")
+/**
+ * Interface for executing platform (non-Java) jobs.
+ */
+@FunctionalInterface
 public interface PlatformComputeConnection {
+    /**
+     * Executes a job asynchronously.
+     *
+     * @param deploymentUnitPaths Paths to deployment units.
+     * @param jobClassName Name of the job class.
+     * @param arg Arguments for the job.
+     * @return A CompletableFuture that will be completed with the result of the job execution.
+     */
     CompletableFuture<ComputeJobDataHolder> executeJobAsync(
             List<String> deploymentUnitPaths, String jobClassName, ComputeJobDataHolder arg);
 }
