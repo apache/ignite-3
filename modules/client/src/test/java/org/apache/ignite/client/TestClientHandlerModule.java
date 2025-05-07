e0322bfea5a0d230bf778ec83b37a610053402ce
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-05-07 17:26:11 +0300

    IGNITE-25331 Fix Compute test flakiness due to random flags in TestClientHandlerModule

--- modules/client/src/test/java/org/apache/ignite/client/TestClientHandlerModule.java
+++ modules/client/src/test/java/org/apache/ignite/client/TestClientHandlerModule.java
@@ -214,6 +214,7 @@ public class TestClientHandlerModule implements IgniteComponent {
 
         BitSet features = BitSet.valueOf(new long[]{ThreadLocalRandom.current().nextLong()});
         features.set(ProtocolBitmaskFeature.TX_DIRECT_MAPPING.featureId());
+        features.set(ProtocolBitmaskFeature.PLATFORM_COMPUTE_JOB.featureId());
 
         bootstrap.childHandler(new ChannelInitializer<>() {
                     @Override
