237c4c2be7e38fa95fe4e387c39306655524d3cf
sk0x50 <slava.koptilin@gmail.com>
2025-05-16 17:58:34 +0300

    IGNITE-21461 Add an ability to dynamically change failure handler configuration

--- modules/failure-handler/src/test/java/org/apache/ignite/internal/failure/FailureProcessorTest.java
+++ modules/failure-handler/src/test/java/org/apache/ignite/internal/failure/FailureProcessorTest.java
@@ -19,10 +19,13 @@ package org.apache.ignite.internal.failure;
 
 import static org.apache.ignite.internal.failure.FailureType.SYSTEM_CRITICAL_OPERATION_TIMEOUT;
 import static org.apache.ignite.internal.failure.FailureType.SYSTEM_WORKER_BLOCKED;
+import static org.apache.ignite.internal.failure.FailureType.SYSTEM_WORKER_TERMINATION;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willSucceedFast;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -30,11 +33,14 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.internal.configuration.testframework.ConfigurationExtension;
 import org.apache.ignite.internal.configuration.testframework.InjectConfiguration;
 import org.apache.ignite.internal.failure.configuration.FailureProcessorConfiguration;
 import org.apache.ignite.internal.failure.handlers.FailureHandler;
 import org.apache.ignite.internal.failure.handlers.NoOpFailureHandler;
+import org.apache.ignite.internal.failure.handlers.StopNodeFailureHandler;
+import org.apache.ignite.internal.failure.handlers.configuration.StopNodeFailureHandlerChange;
 import org.apache.ignite.internal.manager.ComponentContext;
 import org.apache.ignite.internal.testframework.BaseIgniteAbstractTest;
 import org.apache.ignite.internal.testframework.failure.FailureManagerExtension;
@@ -101,6 +107,40 @@ class FailureProcessorTest extends BaseIgniteAbstractTest {
 
             assertTrue(ignoredFailureTypes.contains(SYSTEM_WORKER_BLOCKED));
             assertTrue(ignoredFailureTypes.contains(SYSTEM_CRITICAL_OPERATION_TIMEOUT));
+        } finally {
+            assertThat(failureManager.stopAsync(new ComponentContext()), willSucceedFast());
+        }
+    }
+
+    @Test
+    public void testFailureProcessorReconfiguration() {
+        FailureManager failureManager = new FailureManager(() -> {}, failureProcessorConfiguration);
+
+        try {
+            assertThat(failureManager.startAsync(new ComponentContext()), willSucceedFast());
+
+            // Reconfigure dumpThreadsOnFailure.
+            assertThat(failureManager.dumpThreadsOnFailure(), is(true));
+
+            CompletableFuture<Void> updateFuture = failureProcessorConfiguration.change(c -> {
+                c.changeDumpThreadsOnFailure(false);
+                c.changeDumpThreadsThrottlingTimeoutMillis(3333L);
+            });
+            assertThat(updateFuture, willSucceedFast());
+            assertThat(failureManager.dumpThreadsOnFailure(), is(false));
+            assertThat(failureManager.dumpThreadsThrottlingTimeout(), is(3333L));
+
+            // Reconfigure handler.
+            assertThat(failureManager.handler(), isA(NoOpFailureHandler.class));
+
+            updateFuture = failureProcessorConfiguration.change(c -> {
+                StopNodeFailureHandlerChange ch = c.changeHandler().convert(StopNodeFailureHandlerChange.class);
+
+                ch.changeIgnoredFailureTypes(SYSTEM_WORKER_TERMINATION.typeName());
+            });
+            assertThat(updateFuture, willSucceedFast());
+            assertThat(failureManager.handler(), isA(StopNodeFailureHandler.class));
+            assertThat(failureManager.handler().ignoredFailureTypes(), equalTo(Set.of(SYSTEM_WORKER_TERMINATION)));
         } finally {
             assertThat(failureManager.stopAsync(new ComponentContext()), willSucceedFast());
         }
