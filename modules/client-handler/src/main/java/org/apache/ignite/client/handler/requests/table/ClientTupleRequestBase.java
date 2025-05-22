7f8a24113c350f99f1c5673a01a568799db1c64b
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-05-22 11:10:45 +0300

    wip ClientTupleRequestBase

---
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/table/ClientTupleRequestBase.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.client.handler.requests.table;
+
+import static org.apache.ignite.client.handler.requests.table.ClientTableCommon.readOrStartImplicitTx;
+import static org.apache.ignite.client.handler.requests.table.ClientTableCommon.readTableAsync;
+import static org.apache.ignite.client.handler.requests.table.ClientTableCommon.readTuple;
+
+import java.util.BitSet;
+import java.util.concurrent.CompletableFuture;
+import org.apache.ignite.client.handler.ClientResourceRegistry;
+import org.apache.ignite.client.handler.NotificationSender;
+import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
+import org.apache.ignite.internal.hlc.HybridTimestampTracker;
+import org.apache.ignite.internal.table.TableViewInternal;
+import org.apache.ignite.internal.tx.InternalTransaction;
+import org.apache.ignite.internal.tx.TxManager;
+import org.apache.ignite.table.IgniteTables;
+import org.apache.ignite.table.Tuple;
+
+public class ClientTupleRequestBase {
+    private final InternalTransaction tx;
+    private final TableViewInternal table;
+    private final Tuple tuple;
+
+    private ClientTupleRequestBase(InternalTransaction tx, TableViewInternal table, Tuple tuple) {
+        this.tx = tx;
+        this.table = table;
+        this.tuple = tuple;
+    }
+
+    public InternalTransaction tx() {
+        return tx;
+    }
+
+    public TableViewInternal table() {
+        return table;
+    }
+
+    public Tuple tuple() {
+        return tuple;
+    }
+
+    public static CompletableFuture<ClientTupleRequestBase> readAsync(
+            ClientMessageUnpacker in,
+            IgniteTables tables,
+            ClientResourceRegistry resources,
+            TxManager txManager,
+            boolean txReadOnly,
+            NotificationSender notificationSender,
+            HybridTimestampTracker tsTracker,
+            boolean keyOnly
+    ) {
+        int tableId = in.unpackInt();
+        int schemaId = in.unpackInt();
+
+        var tx = readOrStartImplicitTx(in, tsTracker, resources, txManager, txReadOnly, notificationSender);
+
+        BitSet noValueSet = in.unpackBitSet();
+        byte[] tupleBytes = in.readBinary();
+
+        return readTableAsync(tableId, tables)
+                .thenCompose(table -> readTuple(schemaId, noValueSet, tupleBytes, table, keyOnly)
+                        .thenApply(tuple -> new ClientTupleRequestBase(tx, table, tuple)));
+
+    }
+}
