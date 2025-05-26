0b289ca95e5f3128c34550899fe987d063290106
sanpwc <lapin1702@gmail.com>
2025-05-26 14:03:51 +0300

    IGNITE-25481 Add todo

--- modules/transactions/src/integrationTest/java/org/apache/ignite/internal/disaster/ItDisasterRecoveryReconfigurationTest.java
+++ modules/transactions/src/integrationTest/java/org/apache/ignite/internal/disaster/ItDisasterRecoveryReconfigurationTest.java
@@ -2104,6 +2104,7 @@ public class ItDisasterRecoveryReconfigurationTest extends ClusterPerTestIntegra
 
         logger().info("Pending is {}", pending);
 
+        // TODO Remove pending.value() == null check https://issues.apache.org/jira/browse/IGNITE-25479
         return pending.empty() || pending.value() == null ? null : AssignmentsQueue.fromBytes(pending.value()).poll();
     }
 
