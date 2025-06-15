ea6a4a474c088e03515b6ebc68ea58377409e692
sk0x50 <slava.koptilin@gmail.com>
2025-06-15 15:38:24 +0300

    IGNITE-25639 Use MetricSetBuilder instead of manual creating metrics set

--- modules/metrics/src/main/java/org/apache/ignite/internal/metrics/sources/OsMetricSource.java
+++ modules/metrics/src/main/java/org/apache/ignite/internal/metrics/sources/OsMetricSource.java
@@ -19,10 +19,8 @@ package org.apache.ignite.internal.metrics.sources;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.util.HashMap;
-import org.apache.ignite.internal.metrics.DoubleGauge;
-import org.apache.ignite.internal.metrics.Metric;
 import org.apache.ignite.internal.metrics.MetricSet;
+import org.apache.ignite.internal.metrics.MetricSetBuilder;
 import org.apache.ignite.internal.metrics.MetricSource;
 import org.jetbrains.annotations.Nullable;
 
@@ -64,24 +62,21 @@ public class OsMetricSource implements MetricSource {
             return null;
         }
 
-        var metrics = new HashMap<String, Metric>();
+        var metricSetBuilder = new MetricSetBuilder(SOURCE_NAME);
 
-        metrics.put(
+        metricSetBuilder.doubleGauge(
                 "LoadAverage",
-                new DoubleGauge(
-                        "LoadAverage",
-                        "System load average for the last minute. System load average is the sum of the number of runnable entities "
-                                + "queued to the available processors and the number of runnable entities running on the available "
-                                + "processors averaged over a period of time. The way in which the load average is calculated depends on "
-                                + "the operating system. "
-                                + "If the load average is not available, a negative value is returned.",
-                        operatingSystemMxBean::getSystemLoadAverage
-                )
+                "System load average for the last minute. System load average is the sum of the number of runnable entities "
+                        + "queued to the available processors and the number of runnable entities running on the available "
+                        + "processors averaged over a period of time. The way in which the load average is calculated depends on "
+                        + "the operating system. "
+                        + "If the load average is not available, a negative value is returned.",
+                operatingSystemMxBean::getSystemLoadAverage
         );
 
         enabled = true;
 
-        return new MetricSet(SOURCE_NAME, metrics);
+        return metricSetBuilder.build();
     }
 
     @Override
