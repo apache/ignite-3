e736c4bfd41cb375cb3699f7717adf96fc1fd2dd
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-06-17 13:10:34 +0300

    Log error when failed to send task notification

--- modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteRequest.java
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/requests/compute/ClientComputeExecuteRequest.java
@@ -38,6 +38,8 @@ import org.apache.ignite.internal.client.proto.ClientMessageUnpacker;
 import org.apache.ignite.internal.compute.ComputeJobDataHolder;
 import org.apache.ignite.internal.compute.IgniteComputeInternal;
 import org.apache.ignite.internal.compute.MarshallerProvider;
+import org.apache.ignite.internal.logger.IgniteLogger;
+import org.apache.ignite.internal.logger.Loggers;
 import org.apache.ignite.internal.network.ClusterService;
 import org.apache.ignite.marshalling.Marshaller;
 import org.apache.ignite.network.ClusterNode;
@@ -47,6 +49,8 @@ import org.jetbrains.annotations.Nullable;
  * Compute execute request.
  */
 public class ClientComputeExecuteRequest {
+    private static final IgniteLogger LOG = Loggers.forClass(ClientComputeExecuteRequest.class);
+
     /**
      * Processes the request.
      *
@@ -114,12 +118,21 @@ public class ClientComputeExecuteRequest {
                 return CompletableFuture.<ComputeJobDataHolder>failedFuture(throwable);
             } else {
                 return execution.resultAsync().whenComplete((val, err) ->
-                        execution.stateAsync().whenComplete((state, errState) ->
-                                notificationSender.sendNotification(w -> {
-                                    Marshaller<Object, byte[]> marshaller = extractMarshaller(execution);
-                                    ClientComputeJobPacker.packJobResult(val, marshaller, w);
-                                    packJobState(w, state);
-                                }, err, hybridTimestamp(val))));
+                        execution.stateAsync().whenComplete((state, errState) -> {
+                            try {
+                                notificationSender.sendNotification(
+                                        w -> {
+                                            Marshaller<Object, byte[]> marshaller = extractMarshaller(execution);
+                                            ClientComputeJobPacker.packJobResult(val, marshaller, w);
+                                            packJobState(w, state);
+                                        },
+                                        err,
+                                        hybridTimestamp(val));
+                            }
+                            catch (Throwable e) {
+                                LOG.error("Failed to send job result notification: " + e.getMessage(), e);
+                            }
+                        }));
             }
         }).thenCompose(Function.identity());
     }
