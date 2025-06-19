797cb6b9f96cd9c7ca6e77b569f2d883a59a3b48
Slava Koptilin <slava.koptilin@gmail.com>
2025-06-19 16:05:13 +0300

    IGNITE-25635 Add partitions executor to monitoring (#6077)

--- modules/core/src/main/java/org/apache/ignite/internal/lang/IgniteStringFormatter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/lang/IgniteStringFormatter.java
@@ -108,7 +108,7 @@ public final class IgniteStringFormatter {
                     return sbuf.toString();
                 }
             } else {
-                if (isEscapedDelimeter(messagePattern, j)) {
+                if (isEscapedDelimiter(messagePattern, j)) {
                     if (!isDoubleEscaped(messagePattern, j)) {
                         l--; // DELIM_START was escaped, thus should not be incremented
 
@@ -150,7 +150,7 @@ public final class IgniteStringFormatter {
      * @param delimiterStartIndex Checked position.
      * @return True if the char is delimiter, false otherwise.
      */
-    private static boolean isEscapedDelimeter(String messagePattern, int delimiterStartIndex) {
+    private static boolean isEscapedDelimiter(String messagePattern, int delimiterStartIndex) {
         if (delimiterStartIndex == 0) {
             return false;
         }
