c12209c350f98d8327b145e460e19679159d523c
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-06-20 11:59:12 +0300

    IGNITE-25348 Java thin: add partition-aware batch splitting

--- modules/client-handler/src/integrationTest/java/org/apache/ignite/client/handler/ItClientHandlerTest.java
+++ modules/client-handler/src/integrationTest/java/org/apache/ignite/client/handler/ItClientHandlerTest.java
@@ -479,7 +479,7 @@ public class ItClientHandlerTest extends BaseIgniteAbstractTest {
             packer.packInt(0);
             packer.packInt(0);
             packer.packInt(0);
-            packer.packInt(8); // Size.
+            packer.packInt(9); // Size.
 
             packer.packInt(3); // Major.
             packer.packInt(0); // Minor.
@@ -493,6 +493,7 @@ public class ItClientHandlerTest extends BaseIgniteAbstractTest {
             clientFeatures.set(2);
             clientFeatures.set(6);
             clientFeatures.set(7);
+            clientFeatures.set(8);
             // Unsupported feature
             clientFeatures.set(4);
 
@@ -542,13 +543,14 @@ public class ItClientHandlerTest extends BaseIgniteAbstractTest {
             expected.set(5);
             expected.set(6);
             expected.set(7);
+            expected.set(8);
             assertEquals(expected, supportedFeatures);
 
             var extensionsLen = unpacker.unpackInt();
             unpacker.skipValue(extensionsLen);
 
             assertArrayEquals(MAGIC, magic);
-            assertEquals(98, len);
+            assertEquals(99, len);
             assertEquals(3, major);
             assertEquals(0, minor);
             assertEquals(0, patch);
