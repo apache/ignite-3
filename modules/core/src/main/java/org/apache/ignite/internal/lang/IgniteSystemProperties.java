c1fc8bfd82c9478f56f6527b39e4b5cde4c43a7f
Kirill Sizov <sizov.kirill.y@gmail.com>
2025-06-24 18:20:31 +0300

    return back flag

--- modules/core/src/main/java/org/apache/ignite/internal/lang/IgniteSystemProperties.java
+++ modules/core/src/main/java/org/apache/ignite/internal/lang/IgniteSystemProperties.java
@@ -107,7 +107,7 @@ public final class IgniteSystemProperties {
      */
     @Deprecated
     public static boolean colocationEnabled() {
-        return getBoolean(COLOCATION_FEATURE_FLAG, true);
+        return getBoolean(COLOCATION_FEATURE_FLAG, false);
     }
 
     /**
