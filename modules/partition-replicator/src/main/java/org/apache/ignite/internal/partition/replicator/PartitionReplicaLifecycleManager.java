98bd65296883d1f54ef5bc42b6c34d2c9e639ae5
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2025-07-01 19:17:33 +0400

    IGNITE-24942 Schedule rebalance retries after a delay

--- modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/PartitionReplicaLifecycleManager.java
+++ modules/partition-replicator/src/main/java/org/apache/ignite/internal/partition/replicator/PartitionReplicaLifecycleManager.java
@@ -789,7 +789,7 @@ public class PartitionReplicaLifecycleManager extends
     }
 
     private PartitionMover createPartitionMover(ZonePartitionId replicaGrpId) {
-        return new PartitionMover(busyLock, () -> {
+        return new PartitionMover(busyLock, rebalanceScheduler, () -> {
             CompletableFuture<Replica> replicaFut = replicaMgr.replica(replicaGrpId);
             if (replicaFut == null) {
                 return failedFuture(new IgniteInternalException("No such replica for partition " + replicaGrpId.partitionId()
