e253162cf4510244306853deba19089b92a8a433
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-10 11:58:18 +0300

    IGNITE-25344 .NET: Add custom marshallers to data streamer with receiver

--- modules/platforms/dotnet/Apache.Ignite/Table/ReceiverDescriptor.cs
+++ modules/platforms/dotnet/Apache.Ignite/Table/ReceiverDescriptor.cs
@@ -21,6 +21,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Compute;
+using Marshalling;
 
 /// <summary>
 /// Stream receiver descriptor without results. If the specified receiver returns results, they will be discarded on the server.
@@ -28,12 +29,14 @@ using Compute;
 /// <param name="ReceiverClassName">Name of the streamer receiver class to execute.</param>
 /// <param name="DeploymentUnits">Deployment units.</param>
 /// <param name="Options">Execution options.</param>
+/// <param name="ArgumentMarshaller">Argument marshaller.</param>
 /// <typeparam name="TArg">Argument type.</typeparam>
 [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Reviewed.")]
 public sealed record ReceiverDescriptor<TArg>(
     string ReceiverClassName,
     IEnumerable<DeploymentUnit>? DeploymentUnits = null,
-    ReceiverExecutionOptions? Options = null);
+    ReceiverExecutionOptions? Options = null,
+    IMarshaller<TArg>? ArgumentMarshaller = null);
 
 /// <summary>
 /// Stream receiver descriptor with a result type.
@@ -75,6 +78,8 @@ public sealed record ReceiverDescriptor<TArg, TResult>(
 /// <param name="ReceiverClassName">Name of the streamer receiver class to execute.</param>
 /// <param name="DeploymentUnits">Deployment units.</param>
 /// <param name="Options">Execution options.</param>
+/// <param name="ArgumentMarshaller">Argument marshaller.</param>
+/// <param name="ResultMarshaller">Result marshaller.</param>
 /// <typeparam name="TItem">Streamer item type.</typeparam>
 /// <typeparam name="TArg">Argument type.</typeparam>
 /// <typeparam name="TResult">Result type.</typeparam>
@@ -82,7 +87,9 @@ public sealed record ReceiverDescriptor<TArg, TResult>(
 public sealed record ReceiverDescriptor<TItem, TArg, TResult>(
     string ReceiverClassName,
     IEnumerable<DeploymentUnit>? DeploymentUnits = null,
-    ReceiverExecutionOptions? Options = null)
+    ReceiverExecutionOptions? Options = null,
+    IMarshaller<TArg>? ArgumentMarshaller = null,
+    IMarshaller<TResult>? ResultMarshaller = null)
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="ReceiverDescriptor{TItem,TArg,TResult}"/> class.
@@ -90,14 +97,20 @@ public sealed record ReceiverDescriptor<TItem, TArg, TResult>(
     /// <param name="type">Receiver type.</param>
     /// <param name="deploymentUnits">Deployment units.</param>
     /// <param name="options">Options.</param>
+    /// <param name="argumentMarshaller">Argument marshaller.</param>
+    /// <param name="resultMarshaller">Result marshaller.</param>
     public ReceiverDescriptor(
         Type type,
         IEnumerable<DeploymentUnit>? deploymentUnits = null,
-        ReceiverExecutionOptions? options = null)
+        ReceiverExecutionOptions? options = null,
+        IMarshaller<TArg>? argumentMarshaller = null,
+        IMarshaller<TResult>? resultMarshaller = null)
         : this(
             type.AssemblyQualifiedName ?? throw new ArgumentException("Type has null AssemblyQualifiedName: " + type),
             deploymentUnits,
-            ReceiverDescriptor.EnsureDotNetExecutor(options))
+            ReceiverDescriptor.EnsureDotNetExecutor(options),
+            argumentMarshaller,
+            resultMarshaller)
     {
         // No-op.
     }
@@ -117,7 +130,10 @@ public static class ReceiverDescriptor
     /// <typeparam name="TResult">Result type.</typeparam>
     /// <returns>Receiver descriptor.</returns>
     public static ReceiverDescriptor<TItem, TArg, TResult> Of<TItem, TArg, TResult>(IDataStreamerReceiver<TItem, TArg, TResult> receiver) =>
-        new(receiver.GetType());
+        new(
+            receiver.GetType(),
+            argumentMarshaller: receiver.ArgumentMarshaller,
+            resultMarshaller: receiver.ResultMarshaller);
 
     /// <summary>
     /// Ensures that the provided <see cref="ReceiverExecutionOptions"/> is set to use the .NET executor.
