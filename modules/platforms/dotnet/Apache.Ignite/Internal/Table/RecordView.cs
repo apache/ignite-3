4002beed856ea974d54531243e5a43373397dd99
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-14 15:41:15 +0300

    StreamerReceiverSerializer marshallers - propagate

--- modules/platforms/dotnet/Apache.Ignite/Internal/Table/RecordView.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Table/RecordView.cs
@@ -337,7 +337,7 @@ namespace Apache.Ignite.Internal.Table
                 SingleWriter = false
             });
 
-            // Stream in background.
+            // Stream in the background.
             var streamTask = Stream();
 
             // Result async enumerable is returned immediately. It will be completed when the streaming completes.
@@ -381,6 +381,8 @@ namespace Apache.Ignite.Internal.Table
                         receiver.DeploymentUnits ?? [],
                         receiver.ReceiverClassName,
                         receiver.Options ?? ReceiverExecutionOptions.Default,
+                        receiver.PayloadMarshaller,
+                        receiver.ArgumentMarshaller,
                         receiverArg,
                         cancellationToken).ConfigureAwait(false);
 
@@ -415,6 +417,8 @@ namespace Apache.Ignite.Internal.Table
                 receiver.DeploymentUnits ?? [],
                 receiver.ReceiverClassName,
                 receiver.Options ?? ReceiverExecutionOptions.Default,
+                null,
+                null,
                 receiverArg,
                 cancellationToken).ConfigureAwait(false);
         }
