34e4cc01bd6815ad94ebca55ef16d7787eff61a0
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-17 09:41:41 +0300

    wip

--- modules/compatibility-tests/src/testFixtures/java/org/apache/ignite/internal/PlatformCompatibilityTestNodeRunner.java
+++ modules/compatibility-tests/src/testFixtures/java/org/apache/ignite/internal/PlatformCompatibilityTestNodeRunner.java
@@ -20,7 +20,6 @@ package org.apache.ignite.internal;
 import static org.apache.ignite.internal.TestDefaultProfilesNames.DEFAULT_AIMEM_PROFILE_NAME;
 import static org.apache.ignite.internal.TestDefaultProfilesNames.DEFAULT_AIPERSIST_PROFILE_NAME;
 import static org.apache.ignite.internal.TestDefaultProfilesNames.DEFAULT_ROCKSDB_PROFILE_NAME;
-import static org.apache.ignite.internal.TestWrappers.unwrapIgniteImpl;
 
 import java.lang.reflect.Method;
 import java.nio.file.Path;
