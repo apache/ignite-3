9baa3c24e2bb34f292feb56a212371a3804c03fe
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-24 13:13:32 +0300

    Fix testFailedItems

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/streamer/ItAbstractDataStreamerTest.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/streamer/ItAbstractDataStreamerTest.java
@@ -20,6 +20,7 @@ package org.apache.ignite.internal.streamer;
 import static org.apache.ignite.internal.testframework.IgniteTestUtils.waitForCondition;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willCompleteSuccessfully;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willSucceedIn;
+import static org.apache.ignite.internal.util.ExceptionUtils.unwrapCause;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
@@ -56,6 +57,7 @@ import java.util.stream.IntStream;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.internal.ClusterPerClassIntegrationTest;
 import org.apache.ignite.lang.Cursor;
+import org.apache.ignite.lang.MarshallerException;
 import org.apache.ignite.marshalling.ByteArrayMarshaller;
 import org.apache.ignite.marshalling.Marshaller;
 import org.apache.ignite.network.ClusterNode;
@@ -610,6 +612,15 @@ public abstract class ItAbstractDataStreamerTest extends ClusterPerClassIntegrat
                 } catch (IllegalStateException e) {
                     assertEquals("Streamer is closed, can't add items.", e.getMessage());
                     break;
+                } catch (RuntimeException e) {
+                    if (unwrapCause(e) instanceof MarshallerException) {
+                        // Item was added but failed on flush.
+                        invalidItemsAdded.add(item);
+                        break;
+                    } else {
+                        // Unexpected exception.
+                        throw e;
+                    }
                 }
             }
         }
@@ -618,8 +629,6 @@ public abstract class ItAbstractDataStreamerTest extends ClusterPerClassIntegrat
         DataStreamerException cause = (DataStreamerException) ex.getCause();
         Set<DataStreamerItem<Tuple>> failedItems = (Set<DataStreamerItem<Tuple>>) cause.failedItems();
 
-        assertThat(invalidItemsAdded.size(), is(greaterThan(10)));
-
         for (DataStreamerItem<Tuple> invalidAddedItem : invalidItemsAdded) {
             assertTrue(failedItems.contains(invalidAddedItem), "failedItems item not found: " + invalidAddedItem.get());
         }
@@ -627,6 +636,8 @@ public abstract class ItAbstractDataStreamerTest extends ClusterPerClassIntegrat
         for (DataStreamerItem<Tuple> failedItem : failedItems) {
             assertTrue(invalidItemsAdded.contains(failedItem), "invalidItemsAdded item not found: " + failedItem.get());
         }
+
+        assertThat(invalidItemsAdded.size(), is(greaterThan(10)));
     }
 
     @Test
