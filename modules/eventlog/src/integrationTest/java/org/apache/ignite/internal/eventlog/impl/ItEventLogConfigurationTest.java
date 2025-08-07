4e0f35037d5b145d3f4eaac8633f069063cc650e
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2025-08-07 19:59:04 +0300

    Fix tests

--- modules/eventlog/src/integrationTest/java/org/apache/ignite/internal/eventlog/impl/ItEventLogConfigurationTest.java
+++ modules/eventlog/src/integrationTest/java/org/apache/ignite/internal/eventlog/impl/ItEventLogConfigurationTest.java
@@ -27,11 +27,12 @@ import java.util.UUID;
 import org.apache.ignite.internal.configuration.testframework.ConfigurationExtension;
 import org.apache.ignite.internal.configuration.testframework.InjectConfiguration;
 import org.apache.ignite.internal.eventlog.api.Event;
-import org.apache.ignite.internal.eventlog.api.EventLog;
 import org.apache.ignite.internal.eventlog.config.schema.EventLogConfiguration;
 import org.apache.ignite.internal.eventlog.event.EventUser;
 import org.apache.ignite.internal.eventlog.ser.EventSerializerFactory;
+import org.apache.ignite.internal.manager.ComponentContext;
 import org.apache.ignite.internal.testframework.BaseIgniteAbstractTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -45,7 +46,7 @@ class ItEventLogConfigurationTest extends BaseIgniteAbstractTest {
     @InjectConfiguration(polymorphicExtensions = InMemoryCollectionSinkConfigurationSchema.class)
     private EventLogConfiguration eventLogConfiguration;
 
-    private EventLog eventLog;
+    private EventLogImpl eventLog;
 
     private InMemoryCollectionSink inMemoryCollectionSink;
 
@@ -56,6 +57,12 @@ class ItEventLogConfigurationTest extends BaseIgniteAbstractTest {
                 UUID::randomUUID, "default");
         SinkFactory sinkFactory = new TestSinkFactory(defaultFactory, inMemoryCollectionSink);
         eventLog = new EventLogImpl(eventLogConfiguration, sinkFactory);
+        eventLog.startAsync(new ComponentContext());
+    }
+
+    @AfterEach
+    void tearDown() {
+        eventLog.stopAsync();
     }
 
     @Test
