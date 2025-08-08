009f464e78ddf2b099f3b14992256c2e4d0690d4
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2025-08-08 17:18:25 +0300

    Add args

--- modules/compatibility-tests/src/testFixtures/java/org/apache/ignite/internal/CompatibilityTestBase.java
+++ modules/compatibility-tests/src/testFixtures/java/org/apache/ignite/internal/CompatibilityTestBase.java
@@ -162,16 +162,16 @@ public abstract class CompatibilityTestBase extends BaseIgniteAbstractTest {
 
     protected abstract void setupBaseVersion(Ignite baseIgnite);
 
-    protected List<List<Object>> sql(String query) {
-        return sql(node(0), query);
+    protected List<List<Object>> sql(String query, Object... args) {
+        return sql(node(0), query, args);
     }
 
-    protected static List<List<Object>> sql(Ignite ignite, String query) {
-        return sql(ignite, null, query);
+    protected static List<List<Object>> sql(Ignite ignite, String query, Object... args) {
+        return sql(ignite, null, query, args);
     }
 
-    protected static List<List<Object>> sql(Ignite ignite, @Nullable Transaction tx, String query) {
-        return ClusterPerClassIntegrationTest.sql(ignite, tx, null, null, query);
+    protected static List<List<Object>> sql(Ignite ignite, @Nullable Transaction tx, String query, Object... args) {
+        return ClusterPerClassIntegrationTest.sql(ignite, tx, null, null, query, args);
     }
 
     protected Ignite node(int index) {
