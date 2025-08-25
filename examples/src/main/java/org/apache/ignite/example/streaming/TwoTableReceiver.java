6df858847d8a4d0b294ffab7a10bbc935c917c29
jinxxxoid <jinxxxinthemirror@gmail.com>
2025-08-25 18:30:35 +0400

    IGNITE-26304 Update receiver code in the compute example

--- examples/src/main/java/org/apache/ignite/example/streaming/TwoTableReceiver.java
+++ examples/src/main/java/org/apache/ignite/example/streaming/TwoTableReceiver.java
@@ -1,6 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.example.streaming;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import org.apache.ignite.table.DataStreamerReceiver;
@@ -10,18 +26,11 @@ import org.apache.ignite.table.Tuple;
 
 /** Custom receiver class that extracts data from the provided source and write it into two separate tables: Customers and Addresses */
 
-public class TwoTableReceiver implements DataStreamerReceiver<Tuple, Void, Tuple> {
-
-    public TwoTableReceiver() {
-    }
-
+public class TwoTableReceiver implements DataStreamerReceiver<Tuple, Tuple, Void> {
     @Override
-    public CompletableFuture<List<Tuple>> receive(List<Tuple> page, DataStreamerReceiverContext ctx, Void arg) {
-
+    public CompletableFuture<List<Void>> receive(List<Tuple> page, DataStreamerReceiverContext ctx, Tuple arg) {
         RecordView<Tuple> customersTable = ctx.ignite().tables().table("Customers").recordView();
         RecordView<Tuple> addressesTable = ctx.ignite().tables().table("Addresses").recordView();
-
-        List<Tuple> results = new ArrayList<>(page.size());
 
         for (Tuple sourceItem : page) {
             /* For each source item, receiver extracts customer and address data and upserts it into respective tables */
@@ -39,6 +48,6 @@ public class TwoTableReceiver implements DataStreamerReceiver<Tuple, Void, Tuple
             addressesTable.upsert(null, address);
         }
 
-        return CompletableFuture.completedFuture(results);
+        return CompletableFuture.completedFuture(List.of());
     }
 }
