2ce46be5062e2eceb07eb8a2a0ce29c8ab5000f5
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-09-01 11:14:55 +0300

    wip testAsyncContinuationExecutorException

--- modules/client/src/test/java/org/apache/ignite/client/ConfigurationTest.java
+++ modules/client/src/test/java/org/apache/ignite/client/ConfigurationTest.java
@@ -28,14 +28,13 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import org.apache.ignite.client.IgniteClient.Builder;
 import org.apache.ignite.lang.IgniteException;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -180,14 +179,21 @@ public class ConfigurationTest extends AbstractClientTest {
 
     @Test
     public void testAsyncContinuationExecutorException() {
+        AtomicLong reqId = new AtomicLong();
+
         Builder clientBuilder = IgniteClient.builder()
                 .addresses("localhost:" + serverPort)
                 .asyncContinuationExecutor(command -> {
-                    throw new RuntimeException("bad executor");
+                    if (reqId.incrementAndGet() > 2) {
+                        throw new RuntimeException("bad executor");
+                    }
+
+                    command.run();
                 });
 
         try (var client = clientBuilder.build()) {
-            client.tables().tables();
+            IgniteException ex = assertThrows(IgniteException.class, () -> client.tables().tables());
+            assertThat(ex.getMessage(), containsString("bad executor"));
         }
     }
 }
