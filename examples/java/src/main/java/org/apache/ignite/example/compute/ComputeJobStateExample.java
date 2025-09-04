4ead1e5d18a2711e8b28d0b7a159cf4e00f79e82
jinxxxoid <jinxxxinthemirror@gmail.com>
2025-09-04 16:31:01 +0400

    IGNITE-26375 Update compute examples (#6541)

--- examples/java/src/main/java/org/apache/ignite/example/compute/ComputeJobStateExample.java
+++ examples/java/src/main/java/org/apache/ignite/example/compute/ComputeJobStateExample.java
@@ -18,25 +18,21 @@
 package org.apache.ignite.example.compute;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
-import static org.apache.ignite.compute.BroadcastJobTarget.table;
+import static org.apache.ignite.compute.JobStatus.FAILED;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.compute.BroadcastJobTarget;
 import org.apache.ignite.compute.ComputeJob;
-import org.apache.ignite.compute.IgniteCompute;
 import org.apache.ignite.compute.JobDescriptor;
+import org.apache.ignite.compute.JobExecution;
 import org.apache.ignite.compute.JobExecutionContext;
-import org.apache.ignite.deployment.DeploymentUnit;
-import org.apache.ignite.table.QualifiedName;
+import org.apache.ignite.compute.JobTarget;
 
 /**
- * This example demonstrates the usage of the
- * {@link IgniteCompute#execute(BroadcastJobTarget, JobDescriptor, Object)} API.
+ * This code demonstrates the usage of the {@link JobExecution} interface that allows to get job statuses and, for example, handle failures.
  *
  * <p>Find instructions on how to run the example in the README.md file located in the "examples" directory root.
- *
- * <p>This example is intended to be run on a cluster with more than one node to show that the job is broadcast to each node.
  *
  * <p>The following steps related to code deployment should be additionally executed before running the current example:
  * <ol>
@@ -48,23 +44,18 @@ import org.apache.ignite.table.QualifiedName;
  *         Create a new deployment unit using the CLI tool:<br>
  *         {@code cluster unit deploy computeExampleUnit \
  *          --version 1.0.0 \
- *          --path=$IGNITE_HOME/examples/build/libs/ignite-examples-x.y.z.jar}
+ *          --path=IGNITE_HOME/examples/build/libs/ignite-examples-x.y.z.jar}
  *     </li>
  * </ol>
  */
-public class ComputeBroadcastExample {
-    /** Deployment unit name. */
-    private static final String DEPLOYMENT_UNIT_NAME = "computeExampleUnit";
-
-    /** Deployment unit version. */
-    private static final String DEPLOYMENT_UNIT_VERSION = "1.0.0";
+public class ComputeJobStateExample {
 
     /**
      * Main method of the example.
      *
      * @param args The command line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //--------------------------------------------------------------------------------------
         //
         // Creating a client to connect to the cluster.
@@ -85,51 +76,27 @@ public class ComputeBroadcastExample {
 
             System.out.println("\nConfiguring compute job...");
 
-            JobDescriptor<String, Void> job = JobDescriptor.builder(HelloMessageJob.class)
-                    .units(new DeploymentUnit(DEPLOYMENT_UNIT_NAME, DEPLOYMENT_UNIT_VERSION))
-                    .build();
-
-            BroadcastJobTarget target = table("Person");
-
-            //--------------------------------------------------------------------------------------
-            //
-            // Executing compute job using configured jobTarget.
-            //
-            //--------------------------------------------------------------------------------------
-
-            System.out.println("\nExecuting compute job...");
-
-            client.compute().execute(target, job, "John");
-
-            System.out.println("\nCompute job executed...");
-
-            //--------------------------------------------------------------------------------------
-            //
-            // Executing compute job using a custom by specifying a fully qualified table name .
-            //
-            //
-
-            QualifiedName customSchemaTable = QualifiedName.parse("CUSTOM_SCHEMA.MY_QUALIFIED_TABLE");
-            client.compute().execute(table(customSchemaTable),
-                    JobDescriptor.builder(HelloMessageJob.class).build(), null
+            CompletableFuture<JobExecution<Void>> execution = client.compute().submitAsync(JobTarget.anyNode(client.cluster().nodes()),
+                    JobDescriptor.builder(WordPrintJob.class).build(), null
             );
 
-
-            QualifiedName customSchemaTableName = QualifiedName.of("PUBLIC", "MY_TABLE");
-            client.compute().execute(table(customSchemaTableName),
-                    JobDescriptor.builder(HelloMessageJob.class).build(), null
-            );
+            execution.get().stateAsync().thenApply(state -> {
+                if (state.status() == FAILED) {
+                    System.out.println("\nJob failed...");
+                }
+                return null;
+            });
         }
     }
 
     /**
-     * Job that prints hello message with provided name.
+     * Job that prints provided word.
      */
-    private static class HelloMessageJob implements ComputeJob<String, Void> {
+    private static class WordPrintJob implements ComputeJob<String, Void> {
         /** {@inheritDoc} */
         @Override
         public CompletableFuture<Void> executeAsync(JobExecutionContext context, String arg) {
-            System.out.println("Hello " + arg + "!");
+            System.out.println("\nProcessing word '" + arg + "' at node '" + context.ignite().name() + "'.");
 
             return completedFuture(null);
         }
