75ef86d6e8c30cbfc20ab9c4f693e157e21a7a03
amashenkov <amashenkov@apache.org>
2025-09-05 16:42:28 +0300

    styles

--- modules/cli/src/integrationTest/java/org/apache/ignite/internal/cli/ssl/ItJdbcSslTest.java
+++ modules/cli/src/integrationTest/java/org/apache/ignite/internal/cli/ssl/ItJdbcSslTest.java
@@ -58,7 +58,7 @@ public class ItJdbcSslTest extends CliIntegrationTest {
                 + "&keyStorePassword=" + NodeConfig.keyStorePassword;
 
         // When
-        executeJdbc( jdbcUrl, "select * from person");
+        executeJdbc(jdbcUrl, "select * from person");
 
         // Then the query is executed successfully
         assertAll(
@@ -80,7 +80,7 @@ public class ItJdbcSslTest extends CliIntegrationTest {
                 + "&keyStorePassword=" + NodeConfig.keyStorePassword;
 
         // When
-        executeJdbc( jdbcUrl, "select * from person");
+        executeJdbc(jdbcUrl, "select * from person");
 
         // Then the query is executed successfully
         assertAll(
@@ -104,7 +104,7 @@ public class ItJdbcSslTest extends CliIntegrationTest {
                 + "&clientAuth=require";
 
         // When
-        executeJdbc( jdbcUrl, "select * from person");
+        executeJdbc(jdbcUrl, "select * from person");
 
         // Then the query is executed successfully
         assertAll(
