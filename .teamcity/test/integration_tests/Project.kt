878c71582654645c35c916312019aaed425c5f20
Artem Egorov <artyeshi@gmail.com>
2025-09-10 16:00:29 +0300

    add missing buildtypes

--- .teamcity/test/integration_tests/Project.kt
+++ .teamcity/test/integration_tests/Project.kt
@@ -2,6 +2,7 @@ package test.integration_tests
 
 import jetbrains.buildServer.configs.kotlin.Project
 import org.apache.ignite.teamcity.ApacheIgnite3Teamcity.Companion.INTEGRATION
+import org.apache.ignite.teamcity.ApacheIgnite3Teamcity.Companion.MIGRATION_TOOLS_INTEGRATION
 import org.apache.ignite.teamcity.ApacheIgnite3Teamcity.Companion.SQL_LOGIC
 import org.apache.ignite.teamcity.Teamcity.Companion.getId
 
@@ -10,5 +11,6 @@ object Project : Project({
     name = "[Integration Tests]"
 
     INTEGRATION.buildType().forEach { buildType(it) }
+    MIGRATION_TOOLS_INTEGRATION.buildType().forEach { buildType(it) }
     SQL_LOGIC.forEach { tests -> tests.buildType().forEach { buildType(it) } }
 })
