b0c33b47c61896a25d4d54f766e599bd8daf7c72
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-09-12 17:01:11 +0300

    IGNITE-26183 Bypass apply queue

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TxManagerImpl.java
@@ -994,7 +994,7 @@ public class TxManagerImpl implements TxManager, NetworkMessageHandler, SystemVi
 
         localNodeId = topologyService.localMember().id();
 
-        messagingService.addMessageHandler(ReplicaMessageGroup.class, this);
+        messagingService.addMessageHandler(ReplicaMessageGroup.class, msg -> partitionOperationsExecutor, this);
 
         persistentTxStateVacuumizer = new PersistentTxStateVacuumizer(
                 replicaService,
