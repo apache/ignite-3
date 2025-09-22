25ea514f764c3bc10d48e5e33da6cd89393b289c
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-09-22 10:10:02 +0300

    wip IgniteDbParameterCollectionTests

--- modules/platforms/dotnet/Apache.Ignite.Tests/Sql/IgniteDbParameterCollectionTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Sql/IgniteDbParameterCollectionTests.cs
@@ -17,6 +17,9 @@
 
 namespace Apache.Ignite.Tests.Sql;
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Ignite.Sql;
 using NUnit.Framework;
 
@@ -88,5 +91,115 @@ public class IgniteDbParameterCollectionTests
 
         Assert.AreEqual(0, collection.IndexOf(param1));
         Assert.AreEqual(1, collection.IndexOf("p2"));
+    }
+
+    [Test]
+    public void TestAddRange()
+    {
+        var collection = new IgniteDbParameterCollection();
+        var param1 = new IgniteDbParameter { ParameterName = "p1" };
+        var param2 = new IgniteDbParameter { ParameterName = "p2" };
+        collection.AddRange(new[] { param1, param2 });
+        Assert.AreEqual(2, collection.Count);
+        Assert.AreSame(param1, collection[0]);
+        Assert.AreSame(param2, collection[1]);
+    }
+
+    [Test]
+    public void TestInsertAndRemoveAt()
+    {
+        var collection = new IgniteDbParameterCollection();
+        var param1 = new IgniteDbParameter { ParameterName = "p1" };
+        var param2 = new IgniteDbParameter { ParameterName = "p2" };
+        collection.Add(param1);
+        collection.Insert(0, param2);
+        Assert.AreSame(param2, collection[0]);
+        Assert.AreSame(param1, collection[1]);
+        collection.RemoveAt(0);
+        Assert.AreSame(param1, collection[0]);
+        collection.RemoveAt("p1");
+        Assert.AreEqual(0, collection.Count);
+    }
+
+    [Test]
+    public void TestSetAndGetParameter()
+    {
+        var collection = new IgniteDbParameterCollection();
+        var param1 = new IgniteDbParameter { ParameterName = "p1" };
+        var param2 = new IgniteDbParameter { ParameterName = "p2" };
+        collection.Add(param1);
+
+        // Set by index
+        collection[0] = param2;
+        Assert.AreSame(param2, collection[0]);
+
+        // Set by name (simulate by finding index and setting)
+        collection.Add(param1);
+        var dbParam = new IgniteDbParameter { ParameterName = "p3" };
+        int idx = collection.IndexOf("p2");
+        collection[idx] = dbParam;
+        Assert.AreSame(dbParam, collection[0]);
+
+        // Get by index and name
+        Assert.AreSame(dbParam, collection[0]);
+        Assert.AreSame(dbParam, collection[collection.IndexOf("p3")]);
+    }
+
+    [Test]
+    public void TestCopyTo()
+    {
+        var collection = new IgniteDbParameterCollection();
+        var param1 = new IgniteDbParameter { ParameterName = "p1" };
+        var param2 = new IgniteDbParameter { ParameterName = "p2" };
+        collection.Add(param1);
+        collection.Add(param2);
+        var arr = new IgniteDbParameter[2];
+        collection.CopyTo(arr, 0);
+        Assert.AreSame(param1, arr[0]);
+        Assert.AreSame(param2, arr[1]);
+        var arr2 = new IgniteDbParameter[2];
+        ((ICollection)collection).CopyTo(arr2, 0);
+        Assert.AreSame(param1, arr2[0]);
+        Assert.AreSame(param2, arr2[1]);
+    }
+
+    [Test]
+    public void TestEnumerator()
+    {
+        var collection = new IgniteDbParameterCollection();
+
+        var param1 = new IgniteDbParameter { ParameterName = "p1" };
+        var param2 = new IgniteDbParameter { ParameterName = "p2" };
+
+        collection.Add(param1);
+        collection.Add(param2);
+
+        var list = new List<IgniteDbParameter>();
+        foreach (var p in collection)
+        {
+            list.Add((IgniteDbParameter)p);
+        }
+
+        Assert.AreEqual(2, list.Count);
+        Assert.Contains(param1, list);
+        Assert.Contains(param2, list);
+    }
+
+    [Test]
+    public void TestExceptions()
+    {
+        var collection = new IgniteDbParameterCollection();
+        Assert.Throws<ArgumentOutOfRangeException>(() => { var _ = collection[0]; });
+        Assert.Throws<InvalidOperationException>(() => collection.RemoveAt("notfound"));
+    }
+
+    [Test]
+    public void TestIListCompliance()
+    {
+        var collection = new IgniteDbParameterCollection();
+        var param = new IgniteDbParameter { ParameterName = "p1" };
+        ((IList<IgniteDbParameter>)collection).Add(param);
+        Assert.AreSame(param, ((IReadOnlyList<IgniteDbParameter>)collection)[0]);
+        Assert.IsFalse(((IList<IgniteDbParameter>)collection).IsReadOnly);
     }
 }
