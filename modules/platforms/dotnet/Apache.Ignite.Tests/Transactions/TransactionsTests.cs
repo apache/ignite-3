82079d61290b9c51f7c25be66cb65111c0e216d2
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-10-06 14:55:51 +0300

    Add workaround

--- modules/platforms/dotnet/Apache.Ignite.Tests/Transactions/TransactionsTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Transactions/TransactionsTests.cs
@@ -216,28 +216,30 @@ namespace Apache.Ignite.Tests.Transactions
             cfg.Endpoints.Clear();
             cfg.Endpoints.Add($"127.0.0.1:{ServerPort}");
 
-            using var client = await IgniteClient.StartAsync(GetConfig());
+            using var client = await IgniteClient.StartAsync(cfg);
+            var recordView = (await client.Tables.GetTableAsync(TableName))!.GetRecordView<Poco>();
+
             var key = Random.Shared.NextInt64(1000, long.MaxValue);
             var keyPoco = new Poco { Key = key };
 
-            await PocoView.UpsertAsync(null, new Poco { Key = key, Val = "11" });
+            await recordView.UpsertAsync(null, new Poco { Key = key, Val = "11" });
 
-            await using var roTx = await Client.Transactions.BeginAsync(new TransactionOptions { ReadOnly = true });
+            await using var roTx = await client.Transactions.BeginAsync(new TransactionOptions { ReadOnly = true });
 
             if (readBeforeUpdate)
             {
-                Assert.AreEqual("11", (await PocoView.GetAsync(roTx, keyPoco)).Value.Val);
+                Assert.AreEqual("11", (await recordView.GetAsync(roTx, keyPoco)).Value.Val);
             }
 
             // Update data in a different (implicit) tx.
-            await PocoView.UpsertAsync(transaction: null, new Poco { Key = key, Val = "22" });
+            await recordView.UpsertAsync(transaction: null, new Poco { Key = key, Val = "22" });
 
             // Old read-only tx sees old data.
-            Assert.AreEqual("11", (await PocoView.GetAsync(roTx, keyPoco)).Value.Val);
+            Assert.AreEqual("11", (await recordView.GetAsync(roTx, keyPoco)).Value.Val);
 
             // New tx sees new data
-            await using var tx3 = await Client.Transactions.BeginAsync(new TransactionOptions { ReadOnly = true });
-            Assert.AreEqual("22", (await PocoView.GetAsync(tx3, keyPoco)).Value.Val);
+            await using var tx3 = await client.Transactions.BeginAsync(new TransactionOptions { ReadOnly = true });
+            Assert.AreEqual("22", (await recordView.GetAsync(tx3, keyPoco)).Value.Val);
         }
 
         [Test]
