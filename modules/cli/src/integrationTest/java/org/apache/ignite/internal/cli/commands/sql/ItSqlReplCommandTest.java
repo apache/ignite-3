cd6d4867a312282fc2bbd43d9ed224212a2ea836
Pavel Pereslegin <xxtern@gmail.com>
2025-10-23 11:43:19 +0300

    IGNITE-26143 Sql. Jdbc. Statement.executeBatch using thin client SQL API (#6824)

--- modules/cli/src/integrationTest/java/org/apache/ignite/internal/cli/commands/sql/ItSqlReplCommandTest.java
+++ modules/cli/src/integrationTest/java/org/apache/ignite/internal/cli/commands/sql/ItSqlReplCommandTest.java
@@ -67,7 +67,6 @@ class ItSqlReplCommandTest extends CliIntegrationTest {
         execute("CREATE TABLE MULTILINE_TABLE(K INT PRIMARY KEY); \n INSERT INTO MULTILINE_TABLE VALUES(1);", "--jdbc-url", JDBC_URL);
 
         assertAll(
-                // TODO https://issues.apache.org/jira/browse/IGNITE-26790
                 // The output from CREATE TABLE is: Updated 0 rows.
                 () -> assertOutputContains("Updated 0 rows."),
                 this::assertErrOutputIsEmpty
