820409cf7a2d2b0020f252c42c067a013901fe0d
Mirza Aliev <alievmirza@gmail.com>
2025-10-30 20:00:45 +0400

    IGNITE-26886 change Nullable annotation import library for some SQL classes

--- modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlAlterZoneSet.java
+++ modules/sql-engine/src/main/java/org/apache/ignite/internal/sql/engine/sql/IgniteSqlAlterZoneSet.java
@@ -29,7 +29,7 @@ import org.apache.calcite.sql.SqlWriter;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.util.ImmutableNullableList;
 import org.apache.ignite.internal.sql.engine.exec.fsm.DdlBatchAware;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Parse tree for {@code ALTER ZONE SET} statement.
