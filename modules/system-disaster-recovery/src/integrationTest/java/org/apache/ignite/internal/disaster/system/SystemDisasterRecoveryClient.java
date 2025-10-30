9e9e0ef21e45a30a7f3f9eb3c7ab692c9dcfb068
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2025-10-30 11:58:43 +0300

    Use generated client

--- modules/system-disaster-recovery/src/integrationTest/java/org/apache/ignite/internal/disaster/system/SystemDisasterRecoveryClient.java
+++ modules/system-disaster-recovery/src/integrationTest/java/org/apache/ignite/internal/disaster/system/SystemDisasterRecoveryClient.java
@@ -17,24 +17,17 @@
 
 package org.apache.ignite.internal.disaster.system;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublisher;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpRequest.Builder;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.function.Consumer;
 import org.apache.ignite.internal.logger.IgniteLogger;
 import org.apache.ignite.internal.logger.Loggers;
-import org.apache.ignite.internal.rest.api.cluster.ClusterState;
-import org.apache.ignite.internal.rest.api.recovery.system.MigrateRequest;
-import org.apache.ignite.internal.rest.api.recovery.system.ResetClusterRequest;
+import org.apache.ignite.rest.client.api.ClusterManagementApi;
+import org.apache.ignite.rest.client.api.RecoveryApi;
+import org.apache.ignite.rest.client.invoker.ApiClient;
+import org.apache.ignite.rest.client.invoker.ApiException;
+import org.apache.ignite.rest.client.model.ClusterState;
+import org.apache.ignite.rest.client.model.MigrateRequest;
+import org.apache.ignite.rest.client.model.ResetClusterRequest;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -50,19 +43,26 @@ class SystemDisasterRecoveryClient {
             String... newCmgNodeNames
     ) {
         LOG.info(
-                "Initiating cluster reset via {}:{}, new CMG {}, metastorage replication Factor {}",
+                "Initiating cluster reset via {}:{}, new CMG {}, metastorage replication factor {}",
                 httpHost,
                 httpPort,
                 List.of(newCmgNodeNames),
                 metastorageReplicationFactor
         );
 
-        ResetClusterRequest resetClusterRequest = new ResetClusterRequest(List.of(newCmgNodeNames), metastorageReplicationFactor);
-
         try {
-            sendPostRequest(httpHost, httpPort, "/management/v1/recovery/cluster/reset", resetClusterRequest);
-        } catch (IOException e) {
-            LOG.info("Node has gone, this most probably means that cluster repair is initiated and the node restarts.");
+            RecoveryApi recoveryApi = new RecoveryApi(createApiClient(httpHost, httpPort));
+            ResetClusterRequest resetRequest = new ResetClusterRequest()
+                    .cmgNodeNames(List.of(newCmgNodeNames))
+                    .metastorageReplicationFactor(metastorageReplicationFactor);
+
+            recoveryApi.resetCluster(resetRequest);
+        } catch (ApiException e) {
+            if (e.getCause() instanceof IOException) {
+                LOG.info("Node has gone, this most probably means that cluster repair is initiated and the node restarts.");
+            } else {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -70,59 +70,29 @@ class SystemDisasterRecoveryClient {
         LOG.info("Initiating migration, old {}:{}, new {}:{}", oldHttpHost, oldHttpPort, newHttpHost, newHttpPort);
 
         try {
-            HttpResponse<String> response = sendGetRequest(newHttpHost, newHttpPort, "/management/v1/cluster/state");
-            ObjectMapper objectMapper = new ObjectMapper();
-            ClusterState clusterState = objectMapper.readValue(response.body(), ClusterState.class);
+            ClusterManagementApi clusterManagementApi = new ClusterManagementApi(createApiClient(newHttpHost, newHttpPort));
+            ClusterState clusterState = clusterManagementApi.clusterState();
 
-            MigrateRequest migrateRequest = new MigrateRequest(
-                    List.copyOf(clusterState.cmgNodes()),
-                    List.copyOf(clusterState.msNodes()),
-                    clusterState.igniteVersion(),
-                    clusterState.clusterTag().clusterId(),
-                    clusterState.clusterTag().clusterName(),
-                    clusterState.formerClusterIds()
-            );
+            RecoveryApi recoveryApi = new RecoveryApi(createApiClient(oldHttpHost, oldHttpPort));
+            MigrateRequest migrateRequest = new MigrateRequest()
+                    .cmgNodes(clusterState.getCmgNodes())
+                    .metaStorageNodes(clusterState.getMsNodes())
+                    .version(clusterState.getIgniteVersion())
+                    .clusterId(clusterState.getClusterTag().getClusterId())
+                    .clusterName(clusterState.getClusterTag().getClusterName())
+                    .formerClusterIds(clusterState.getFormerClusterIds());
 
-            sendPostRequest(oldHttpHost, oldHttpPort, "/management/v1/recovery/cluster/migrate", migrateRequest);
-        } catch (IOException e) {
-            LOG.info("Node has gone, this most probably means that migration is initiated and the node restarts.");
-        }
-    }
-
-    private static HttpResponse<String> sendGetRequest(String httpHost, int httpPort, String path) throws IOException {
-        return sendHttpRequest(httpHost, httpPort, path, Builder::GET);
-    }
-
-    private static void sendPostRequest(String httpHost, int httpPort, String path, Object arg) throws IOException {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            BodyPublisher bodyPublisher = BodyPublishers.ofString(objectMapper.writeValueAsString(arg));
-            sendHttpRequest(httpHost, httpPort, path, builder -> builder.POST(bodyPublisher));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static HttpResponse<String> sendHttpRequest(
-            String httpHost,
-            int httpPort,
-            String path,
-            Consumer<Builder> builderConsumer
-    ) throws IOException {
-        Builder builder = HttpRequest.newBuilder(URI.create("http://" + httpHost + ":" + httpPort + path))
-                .header("content-type", "application/json");
-        builderConsumer.accept(builder);
-        HttpRequest request = builder.build();
-
-        try {
-            HttpClient client = HttpClient.newBuilder().build();
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            if (response.statusCode() != 200) {
-                throw new RuntimeException(response.body());
+            recoveryApi.migrate(migrateRequest);
+        } catch (ApiException e) {
+            if (e.getCause() instanceof IOException) {
+                LOG.info("Node has gone, this most probably means that migration is initiated and the node restarts.");
+            } else {
+                throw new RuntimeException(e);
             }
-            return response;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    private static ApiClient createApiClient(String httpHost, int httpPort) {
+        return new ApiClient().setBasePath("http://" + httpHost + ":" + httpPort);
     }
 }
