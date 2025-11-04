c482358d34a9bc3ecc01d1fee93a1999d9204a4a
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-11-04 11:39:58 +0200

    Disable heavy tests on Windows

--- modules/platforms/dotnet/Apache.Ignite.Tests/Table/RecordViewBinaryTests.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Table/RecordViewBinaryTests.cs
@@ -602,6 +602,7 @@ namespace Apache.Ignite.Tests.Table
         }
 
         [Test]
+        [Platform("Linux")]
         public async Task TestUpsertAllMany()
         {
             int count = 25_000;
