e1ea9bddd69e5178de4c5376081e781b63bf808a
ibessonov <bessonov.ip@gmail.com>
2025-11-12 19:17:19 +0300

    IGNITE-27035 <comment>
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/metastorage/src/main/java/org/apache/ignite/internal/metastorage/server/raft/MetaStorageWriteHandler.java
+++ modules/metastorage/src/main/java/org/apache/ignite/internal/metastorage/server/raft/MetaStorageWriteHandler.java
@@ -384,6 +384,8 @@ public class MetaStorageWriteHandler {
         if (command instanceof MetaStorageWriteCommand) {
             // Initiator sends us a timestamp to adjust to.
             // Alter command by setting safe time based on the adjusted clock.
+            // We need to clone the original command, because we have no control over its lifecycle. For example, raft client might send it
+            // second time during the retry, this would lead to changing an already processing command in such a case, which is a data race.
             MetaStorageWriteCommand writeCommand = (MetaStorageWriteCommand) command.clone();
 
             clusterTime.adjustClock(writeCommand.initiatorTime());
