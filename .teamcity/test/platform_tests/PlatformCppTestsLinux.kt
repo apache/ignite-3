ff24df9ad052fb5decda02b8f909c008ee879b7c
Viktor Lozmanov <lozmanov.viktor@gmail.com>
2025-11-13 10:35:52 +0300

    IGNITE-27036 fix artifact path for C++ tests

--- .teamcity/test/platform_tests/PlatformCppTestsLinux.kt
+++ .teamcity/test/platform_tests/PlatformCppTestsLinux.kt
@@ -39,7 +39,7 @@ object PlatformCppTestsLinux : BuildType({
             scriptContent = """
                 gcc --version || exit 0
                 g++ --version || exit 0
-                
+
                 odbcinst -j || exit 0
                 cat /etc/odbcinst.ini || exit 0
             """.trimIndent()
@@ -49,7 +49,7 @@ object PlatformCppTestsLinux : BuildType({
             scriptContent = """
                 mkdir %PATH__CMAKE_BUILD_DIRECTORY%  || exit 2
                 cd %PATH__CMAKE_BUILD_DIRECTORY%  || exit 3
-                
+
                 cmake .. -DENABLE_TESTS=ON -DENABLE_ODBC=ON -DWARNINGS_AS_ERRORS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%env.CPP_STAGING% || (echo 'CMake configuration failed' && exit 5)
                 cmake --build . -j8  || (echo 'CMake build failed' && exit 6)
                 cmake --install . || (echo 'CMake install failed' && exit 7)
@@ -70,14 +70,22 @@ object PlatformCppTestsLinux : BuildType({
             workingDir = "%PATH__CMAKE_BUILD_DIRECTORY%"
             scriptContent = """
                 ctest --version
-                ctest -E '(IgniteClientTest|IgniteOdbcTest)' --output-junit %PATH__UNIT_TESTS_RESULT%  || exit 0
+                ctest -E '(IgniteClientTest|IgniteOdbcTest)' --output-junit %PATH__UNIT_TESTS_RESULT%  || true
+                pwd
+                ls -lah
+                find . -name cpp_unit_test_results.xml
             """.trimIndent()
             formatStderrAsError = true
         }
         script {
             name = "C++ Client integration tests"
             workingDir = "%PATH__CMAKE_BUILD_DIRECTORY%"
-            scriptContent = "./bin/ignite-client-test --gtest_output=xml:%PATH__CLIENT_TEST_RESULTS%"
+            scriptContent = """
+                ./bin/ignite-client-test --gtest_output=xml:%PATH__CLIENT_TEST_RESULTS% || true
+                pwd
+                ls -lah
+                find . -name cpp_client_tests_results.xml
+            """
             formatStderrAsError = true
         }
         customScript(type = "bash") {
