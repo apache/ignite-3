28ad899014c7c5689646003eebd8782b3a9a623e
ashishm07 <ashish.mishra.2019@gmail.com>
2025-11-27 14:49:38 +0530

    IGNITE-27167: Fixed SerializationExample.java example

--- examples/java/src/main/java/org/apache/ignite/example/serialization/SerializationExample.java
+++ examples/java/src/main/java/org/apache/ignite/example/serialization/SerializationExample.java
@@ -17,19 +17,86 @@
 
 package org.apache.ignite.example.serialization;
 
+import static org.apache.ignite.example.util.DeployComputeUnit.deployUnit;
+import static org.apache.ignite.example.util.DeployComputeUnit.deploymentExists;
+import static org.apache.ignite.example.util.DeployComputeUnit.undeployUnit;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.example.util.DeployComputeUnit;
+
 
 public class SerializationExample {
+
+    private static final String DEPLOYMENT_UNIT_NATIVE = "nativeSerializationExampleUnit";
+    private static final String DEPLOYMENT_UNIT_CUSTOM = "customPojoSerializationExampleUnit";
+    private static final String DEPLOYMENT_UNIT_AUTO = "pojoAutoSerializationExampleUnit";
+    private static final String DEPLOYMENT_UNIT_TUPLE = "tupleSerializationExampleUnit";
+    private static final String VERSION = "1.0.0";
+    private static final Path projectRoot = Paths.get("").toAbsolutePath(); // This resolves ignite-examples/
+    private static final Path CLASSES_DIR = projectRoot.resolve("examples/java/build/classes/java/main"); // Compiled output
+    private static final Path JAR_PATH = Path.of("build/libs/serialization-example-1.0.0.jar"); // Output jar
 
     public static void main(String[] args) throws Exception {
         try (IgniteClient client = IgniteClient.builder()
                 .addresses("127.0.0.1:10800")
                 .build()) {
 
+            DeployComputeUnit.buildJar(CLASSES_DIR, JAR_PATH);
+
+//            // 1) Check if deployment unit already exists
+            if (deploymentExists(DEPLOYMENT_UNIT_NATIVE, VERSION)) {
+                System.out.println("Deployment unit already exists. Skip deploy.");
+            } else {
+                System.out.println("Deployment unit not found. Deploying...");
+                deployUnit(DEPLOYMENT_UNIT_NATIVE, VERSION, JAR_PATH);
+                System.out.println(" Deployment completed runNativeSerialization.");
+            }
+
             NativeTypeSerializationExample.runNativeSerialization(client);
+
+//            // 2) Check if deployment unit already exists
+            if (deploymentExists(DEPLOYMENT_UNIT_TUPLE, VERSION)) {
+                System.out.println("Deployment unit already exists. Skip deploy.");
+            } else {
+                System.out.println("Deployment unit not found. Deploying...");
+                deployUnit(DEPLOYMENT_UNIT_TUPLE, VERSION, JAR_PATH);
+                System.out.println(" Deployment completed runTupleSerialization.");
+            }
+
             TupleSerializationExample.runTupleSerialization(client);
+
+//            // 3) Check if deployment unit already exists
+            if (deploymentExists(DEPLOYMENT_UNIT_AUTO, VERSION)) {
+                System.out.println("Deployment unit already exists. Skip deploy.");
+            } else {
+                System.out.println("Deployment unit not found. Deploying...");
+                deployUnit(DEPLOYMENT_UNIT_AUTO, VERSION, JAR_PATH);
+                System.out.println(" Deployment completed runPojoAutoSerialization.");
+            }
+
             PojoAutoSerializationExample.runPojoAutoSerialization(client);
+
+            // 4) Check if deployment unit already exists
+            if (deploymentExists(DEPLOYMENT_UNIT_CUSTOM, VERSION)) {
+                System.out.println("Deployment unit already exists. Skip deploy.");
+            } else {
+                System.out.println("Deployment unit not found. Deploying...");
+                deployUnit(DEPLOYMENT_UNIT_CUSTOM, VERSION, JAR_PATH);
+                System.out.println(" Deployment completed runPojoCustomJsonSerialization.");
+            }
             CustomPojoSerializationExample.runPojoCustomJsonSerialization(client);
+
+        } finally {
+
+            System.out.println("Cleaning up resources...");
+            undeployUnit(DEPLOYMENT_UNIT_CUSTOM, VERSION);
+            undeployUnit(DEPLOYMENT_UNIT_AUTO, VERSION);
+            undeployUnit(DEPLOYMENT_UNIT_NATIVE, VERSION);
+            undeployUnit(DEPLOYMENT_UNIT_TUPLE, VERSION);
+
+
         }
     }
 }
