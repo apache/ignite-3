b5ecf2a16282d7765fe843e9ba70b1da5ceb37c3
ashishm07 <ashish.mishra.2019@gmail.com>
2025-11-27 14:39:25 +0530

    IGNITE-27169: Fixed DistributedComputeWithReceiverExample failure

--- examples/java/src/main/java/org/apache/ignite/example/streaming/DistributedComputeWithReceiverExample.java
+++ examples/java/src/main/java/org/apache/ignite/example/streaming/DistributedComputeWithReceiverExample.java
@@ -17,8 +17,17 @@
 
 package org.apache.ignite.example.streaming;
 
+import static java.sql.DriverManager.getConnection;
 import static org.apache.ignite.catalog.definitions.ColumnDefinition.column;
+import static org.apache.ignite.example.util.DeployComputeUnit.deployUnit;
+import static org.apache.ignite.example.util.DeployComputeUnit.deploymentExists;
+import static org.apache.ignite.example.util.DeployComputeUnit.undeployUnit;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -30,98 +39,141 @@ import java.util.stream.IntStream;
 import org.apache.ignite.catalog.ColumnType;
 import org.apache.ignite.catalog.definitions.TableDefinition;
 import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.deployment.DeploymentUnit;
+import org.apache.ignite.example.util.DeployComputeUnit;
 import org.apache.ignite.table.DataStreamerReceiver;
 import org.apache.ignite.table.DataStreamerReceiverContext;
 import org.apache.ignite.table.DataStreamerReceiverDescriptor;
 import org.apache.ignite.table.Table;
 import org.apache.ignite.table.Tuple;
 
-/** This example demonstrates how to use the streaming API to simulate a fraud detection process,
- * which typically involves intensive processing of each transaction using ML models.
+/**
+ * This example demonstrates how to use the streaming API to simulate a fraud detection process, which typically involves intensive
+ * processing of each transaction using ML models.
  */
 
 public class DistributedComputeWithReceiverExample {
 
-    public static void main(String[] arg) {
+    private static final String DEPLOYMENT_UNIT_NAME = "streamerReceiverExampleUnit";
+
+    /** Deployment unit version. */
+    private static final String DEPLOYMENT_UNIT_VERSION = "1.0.0";
+
+    private static final Path projectRoot = Paths.get("").toAbsolutePath(); // This resolves ignite-examples/
+    private static final Path CLASSES_DIR = projectRoot.resolve("examples/java/build/classes/java/main"); // Compiled output
+    private static final Path JAR_PATH = Path.of("build/libs/serialization-example-1.0.0.jar"); // Output jar
+
+    public static void main(String[] arg) throws Exception {
 
         try (IgniteClient client = IgniteClient.builder()
                 .addresses("127.0.0.1:10800")
                 .build()) {
 
-        /* Source data is a list of financial transactions */
-        /* We distribute this processing across the cluster, then gather and return results */
-        List<Tuple> sourceData = IntStream.range(1, 10)
-                .mapToObj(i -> Tuple.create()
-                        .set("txId", i)
-                        .set("txData", "{some-json-data}"))
-                .collect(Collectors.toList());
+            DeployComputeUnit.buildJar(CLASSES_DIR, JAR_PATH);
 
-        DataStreamerReceiverDescriptor<Tuple, Void, Tuple> desc = DataStreamerReceiverDescriptor
-                .builder(FraudDetectorReceiver.class)
-                .build();
+//            // 1) Check if deployment unit already exists
+            if (deploymentExists(DEPLOYMENT_UNIT_NAME, DEPLOYMENT_UNIT_VERSION)) {
+                System.out.println("Deployment unit already exists. Skip deploy.");
+            } else {
+                System.out.println("Deployment unit not found. Deploying...");
+                deployUnit(DEPLOYMENT_UNIT_NAME, DEPLOYMENT_UNIT_VERSION, JAR_PATH);
+                System.out.println(" Deployment completed " + DEPLOYMENT_UNIT_NAME + ".");
+            }
 
-        CompletableFuture<Void> streamerFut;
+            /* Source data is a list of financial transactions */
+            /* We distribute this processing across the cluster, then gather and return results */
+            List<Tuple> sourceData = IntStream.range(1, 10)
+                    .mapToObj(i -> Tuple.create()
+                            .set("txId", i)
+                            .set("txData", "{some-json-data}"))
+                    .collect(Collectors.toList());
+
+            DataStreamerReceiverDescriptor<Tuple, Void, Tuple> desc = DataStreamerReceiverDescriptor
+                    .builder(FraudDetectorReceiver.class)
+                    .units(new DeploymentUnit(DEPLOYMENT_UNIT_NAME, DEPLOYMENT_UNIT_VERSION))
+                    .build();
+
+            CompletableFuture<Void> streamerFut;
 
         /* Streaming requires a target table to partition data.
         /* Use a dummy table for this scenario, because we are not going to store any data */
-        TableDefinition txDummyTableDef = TableDefinition.builder("tx_dummy")
-                .columns(column("id", ColumnType.INTEGER))
-                .primaryKey("id")
-                .build();
+            TableDefinition txDummyTableDef = TableDefinition.builder("tx_dummy")
+                    .columns(column("id", ColumnType.INTEGER))
+                    .primaryKey("id")
+                    .build();
 
-        Table dummyTable = client.catalog().createTable(txDummyTableDef);
+            Table dummyTable = client.catalog().createTable(txDummyTableDef);
 
-        /* Source data has "txId" field, but target dummy table has "id" column, so keyFunc maps "txId" to "id" */
-        Function<Tuple, Tuple> keyFunc = sourceItem -> Tuple.create().set("id", sourceItem.value("txId"));
+
+            /* Source data has "txId" field, but target dummy table has "id" column, so keyFunc maps "txId" to "id" */
+            Function<Tuple, Tuple> keyFunc = sourceItem -> Tuple.create().set("id", sourceItem.value("txId"));
 
         /* Payload function is used to extract the payload (data that goes to the receiver) from the source item.
         /* In our case, we want to use the whole source item as the payload */
-        Function<Tuple, Tuple> payloadFunc = Function.identity();
+            Function<Tuple, Tuple> payloadFunc = Function.identity();
 
-        Flow.Subscriber<Tuple> resultSubscriber = new Flow.Subscriber<>() {
-            @Override
-            public void onSubscribe(Flow.Subscription subscription) {
-                subscription.request(Long.MAX_VALUE);
+            Flow.Subscriber<Tuple> resultSubscriber = new Flow.Subscriber<>() {
+                @Override
+                public void onSubscribe(Flow.Subscription subscription) {
+                    subscription.request(Long.MAX_VALUE);
+                }
+
+                @Override
+                public void onNext(Tuple item) {
+                    System.out.println("Transaction processed: " + item);
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    System.err.println("Error during streaming: " + throwable.getMessage());
+                }
+
+                @Override
+                public void onComplete() {
+                    System.out.println("Streaming completed.");
+                }
+            };
+
+            try (var publisher = new SubmissionPublisher<Tuple>()) {
+                streamerFut = dummyTable.recordView().streamData(
+                        publisher,
+                        desc,
+                        keyFunc,
+                        payloadFunc,
+                        null, /* Optional Receiver arguments*/
+                        resultSubscriber,
+                        null /* DataStreamer options */
+                );
+
+                for (Tuple item : sourceData) {
+                    publisher.submit(item);
+                }
             }
 
-            @Override
-            public void onNext(Tuple item) {
-                System.out.println("Transaction processed: " + item);
+            streamerFut.join();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+
+            System.out.println("Cleaning up resources");
+            undeployUnit(DEPLOYMENT_UNIT_NAME, DEPLOYMENT_UNIT_VERSION);
+
+            /* Drop table */
+            System.out.println("\nDropping the table...");
+            try (
+                    Connection conn = getConnection("jdbc:ignite:thin://127.0.0.1:10800/");
+                    Statement stmt = conn.createStatement()
+            ) {
+                stmt.executeUpdate("DROP TABLE IF EXISTS tx_dummy");
             }
 
-            @Override
-            public void onError(Throwable throwable) {
-                System.err.println("Error during streaming: " + throwable.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("Streaming completed.");
-            }
-        };
-
-        try (var publisher = new SubmissionPublisher<Tuple>()) {
-            streamerFut = dummyTable.recordView().streamData(
-                    publisher,
-                    desc,
-                    keyFunc,
-                    payloadFunc,
-                    null, /* Optional Receiver arguments*/
-                    resultSubscriber,
-                    null /* DataStreamer options */
-            );
-
-            for (Tuple item : sourceData) {
-                publisher.submit(item);
-            }
         }
-
-        streamerFut.join();
     }
-}
 
 
-    private static class FraudDetectorReceiver implements DataStreamerReceiver<Tuple, Void, Tuple> {
+    public static class FraudDetectorReceiver implements DataStreamerReceiver<Tuple, Void, Tuple> {
         @Override
         public CompletableFuture<List<Tuple>> receive(List<Tuple> page, DataStreamerReceiverContext ctx, Void arg) {
             List<Tuple> results = new ArrayList<>(page.size());
