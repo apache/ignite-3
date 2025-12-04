3a6472d163240c32769a86b4e4035d302caa8059
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-04 09:02:08 +0200

    wip MapperReader struct

--- modules/platforms/dotnet/Apache.Ignite/Internal/Proto/BinaryTuple/BinaryTupleReader.cs
+++ modules/platforms/dotnet/Apache.Ignite/Internal/Proto/BinaryTuple/BinaryTupleReader.cs
@@ -24,13 +24,12 @@ namespace Apache.Ignite.Internal.Proto.BinaryTuple
     using System.Diagnostics.CodeAnalysis;
     using System.Numerics;
     using Ignite.Sql;
-    using Ignite.Table.Mapper;
     using NodaTime;
 
     /// <summary>
     /// Binary tuple reader.
     /// </summary>
-    internal readonly ref struct BinaryTupleReader : IMapperReader
+    internal readonly ref struct BinaryTupleReader
     {
         /** Buffer. */
         private readonly ReadOnlySpan<byte> _buffer;
@@ -521,9 +520,6 @@ namespace Apache.Ignite.Internal.Proto.BinaryTuple
                 throw;
             }
         }
-
-        /// <inheritdoc/>
-        public T? Read<T>(IMapperColumn column) => (T?)GetObject(column.);
 
         private static LocalDate ReadDate(ReadOnlySpan<byte> span)
         {
---
+++ modules/platforms/dotnet/Apache.Ignite/Table/Mapper/MapperReader.cs
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache.Ignite.Table.Mapper;
+
+using System;
+using Internal.Proto.BinaryTuple;
+using Internal.Table;
+
+/// <summary>
+/// Mapper reader.
+/// </summary>
+public ref struct MapperReader
+{
+    private readonly BinaryTupleReader _reader;
+
+    private readonly Schema _schema;
+
+    private int _position;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MapperReader"/> struct.
+    /// </summary>
+    /// <param name="reader">Reader.</param>
+    /// <param name="schema">Schema.</param>
+    internal MapperReader(BinaryTupleReader reader, Schema schema)
+    {
+        _reader = reader;
+        _schema = schema;
+    }
+
+    /// <summary>
+    /// Returns the value of the next column.
+    /// </summary>
+    /// <typeparam name="T">Value type.</typeparam>
+    /// <returns>Column value.</returns>
+    public T? Read<T>()
+    {
+        var pos = _position++;
+
+        if (pos >= _schema.Columns.Length)
+        {
+            throw new InvalidOperationException($"No more columns to read. Total columns: {_schema.Columns.Length}.");
+        }
+
+        var col = _schema.Columns[pos];
+
+        return (T?)_reader.GetObject(_position++, col.Type, col.Scale);
+    }
+}
