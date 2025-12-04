0c41e5963b7bc7f2d3f9346769b1126ce45296b4
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-04 09:12:45 +0200

    wip MapperWriter

--- modules/platforms/dotnet/Apache.Ignite/Table/Mapper/IMapper.cs
+++ modules/platforms/dotnet/Apache.Ignite/Table/Mapper/IMapper.cs
@@ -29,7 +29,7 @@ public interface IMapper<T>
     /// <param name="obj">Object.</param>
     /// <param name="writer">Row writer.</param>
     /// <param name="schema">Row schema.</param>
-    void Write(T obj, MapperWriter writer, IMapperSchema schema);
+    void Write(T obj, ref MapperWriter writer, IMapperSchema schema);
 
     /// <summary>
     /// Reads an object of type <typeparamref name="T"/> from an Ignite table row.
