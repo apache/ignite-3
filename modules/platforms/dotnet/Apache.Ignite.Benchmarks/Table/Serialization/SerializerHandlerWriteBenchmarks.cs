d6db3c6192aa35636f322e8ea415e078596cb57f
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-05 13:00:57 +0200

    wip WriteObjectWithMapper bench

--- modules/platforms/dotnet/Apache.Ignite.Benchmarks/Table/Serialization/SerializerHandlerWriteBenchmarks.cs
+++ modules/platforms/dotnet/Apache.Ignite.Benchmarks/Table/Serialization/SerializerHandlerWriteBenchmarks.cs
@@ -26,13 +26,14 @@ namespace Apache.Ignite.Benchmarks.Table.Serialization
     /// <summary>
     /// Benchmarks for <see cref="IRecordSerializerHandler{T}"/> write methods.
     ///
-    /// Results on i9-12900H, .NET SDK 6.0.419, Ubuntu 22.04:
+    /// Results on i9-12900H, .NET SDK 8.0.416, Ubuntu 22.04:
     ///
-    /// |            Method |     Mean |   Error |  StdDev | Ratio | RatioSD |  Gen 0 | Allocated |
-    /// |------------------ |---------:|--------:|--------:|------:|--------:|-------:|----------:|
-    /// | WriteObjectManual | 116.4 ns | 1.33 ns | 1.11 ns |  1.00 |    0.00 | 0.0002 |      80 B |
-    /// |       WriteObject | 137.5 ns | 1.26 ns | 1.18 ns |  1.18 |    0.01 | 0.0002 |      80 B |
-    /// |        WriteTuple | 213.3 ns | 2.76 ns | 2.59 ns |  1.83 |    0.02 | 0.0007 |     184 B |.
+    /// | Method                | Mean     | Error   | StdDev  | Ratio | Gen0   | Allocated |
+    /// |---------------------- |---------:|--------:|--------:|------:|-------:|----------:|
+    /// | WriteObjectManual     | 118.9 ns | 0.57 ns | 0.50 ns |  1.00 | 0.0002 |      80 B |
+    /// | WriteObject           | 134.8 ns | 0.49 ns | 0.43 ns |  1.13 | 0.0002 |      80 B |
+    /// | WriteObjectWithMapper | 130.2 ns | 0.92 ns | 0.81 ns |  1.10 | 0.0002 |     112 B |
+    /// | WriteTuple            | 186.3 ns | 0.68 ns | 0.60 ns |  1.57 | 0.0005 |     184 B |.
     /// </summary>
     [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Benchmarks.")]
     [MemoryDiagnoser]
