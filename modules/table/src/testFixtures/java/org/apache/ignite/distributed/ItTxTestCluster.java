4d6551531c317857b83d11ac3c749c2e2ab2aaa4
sk0x50 <slava.koptilin@gmail.com>
2025-12-08 14:32:50 +0200

    IGNITE-27175 Remove unused import

--- modules/table/src/testFixtures/java/org/apache/ignite/distributed/ItTxTestCluster.java
+++ modules/table/src/testFixtures/java/org/apache/ignite/distributed/ItTxTestCluster.java
@@ -76,7 +76,6 @@ import org.apache.ignite.internal.cluster.management.topology.api.LogicalNode;
 import org.apache.ignite.internal.cluster.management.topology.api.LogicalTopologyEventListener;
 import org.apache.ignite.internal.cluster.management.topology.api.LogicalTopologyService;
 import org.apache.ignite.internal.cluster.management.topology.api.LogicalTopologySnapshot;
-import org.apache.ignite.internal.components.SystemPropertiesNodeProperties;
 import org.apache.ignite.internal.configuration.RaftGroupOptionsConfigHelper;
 import org.apache.ignite.internal.configuration.SystemDistributedConfiguration;
 import org.apache.ignite.internal.configuration.SystemLocalConfiguration;
