b09355dbd1d28f009f6d8f5158b9eb063fcbaf1f
Mikhail Efremov <jakutenshi@gmail.com>
2025-12-09 19:55:31 +0600

    IGNITE-21500 Fix is done

--- modules/table/src/integrationTest/java/org/apache/ignite/internal/table/ItOperationRetryTest.java
+++ modules/table/src/integrationTest/java/org/apache/ignite/internal/table/ItOperationRetryTest.java
@@ -19,27 +19,35 @@ package org.apache.ignite.internal.table;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.ignite.internal.TestWrappers.unwrapIgniteImpl;
-import static org.apache.ignite.internal.TestWrappers.unwrapTableImpl;
+import static org.apache.ignite.internal.distributionzones.DistributionZonesTestUtil.zoneId;
+import static org.apache.ignite.internal.lang.IgniteStringFormatter.format;
 import static org.apache.ignite.internal.sql.engine.util.SqlTestUtils.executeUpdate;
 import static org.apache.ignite.internal.storage.pagememory.configuration.PageMemoryStorageEngineLocalConfigurationModule.DEFAULT_PROFILE_NAME;
 import static org.apache.ignite.internal.testframework.matchers.CompletableFutureMatcher.willCompleteSuccessfully;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.Duration;
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.apache.ignite.internal.ClusterPerTestIntegrationTest;
 import org.apache.ignite.internal.TestWrappers;
 import org.apache.ignite.internal.app.IgniteImpl;
 import org.apache.ignite.internal.network.DefaultMessagingService;
 import org.apache.ignite.internal.placementdriver.ReplicaMeta;
+import org.apache.ignite.internal.placementdriver.message.StopLeaseProlongationMessage;
 import org.apache.ignite.internal.replicator.ZonePartitionId;
+import org.apache.ignite.internal.replicator.message.PrimaryReplicaRequest;
 import org.apache.ignite.internal.replicator.message.ReplicaResponse;
 import org.apache.ignite.table.RecordView;
 import org.apache.ignite.table.Tuple;
 import org.apache.ignite.tx.Transaction;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -50,29 +58,31 @@ public class ItOperationRetryTest extends ClusterPerTestIntegrationTest {
     /** Table name. */
     private static final String TABLE_NAME = "test_table";
 
+    private static final String ZONE_NAME = "TEST_ZONE";
+
     private static final int PART_ID = 0;
 
     @BeforeEach
     public void setup() {
-        String zoneSql = "create zone test_zone (partitions 1, replicas 3) storage profiles ['" + DEFAULT_PROFILE_NAME + "']";
-        String sql = "create table " + TABLE_NAME + " (key int primary key, val varchar(20)) zone TEST_ZONE";
+        String createZoneSql = format("create zone {} (partitions 1, replicas 3) storage profiles ['{}']", ZONE_NAME, DEFAULT_PROFILE_NAME);
+        String createTableQuery = format("create table {} (key int primary key, val varchar(20)) zone {}", TABLE_NAME,  ZONE_NAME);
 
         cluster.doInSession(0, session -> {
-            executeUpdate(zoneSql, session);
-            executeUpdate(sql, session);
+            executeUpdate(createZoneSql, session);
+            executeUpdate(createTableQuery, session);
         });
     }
 
     @Test
     public void testLockExceptionRetry() {
-        TableImpl tbl = unwrapTableImpl(node(0).tables().table(TABLE_NAME));
+        IgniteImpl node0 = unwrapIgniteImpl(node(0));
 
-        ZonePartitionId partitionGroupId = new ZonePartitionId(tbl.zoneId(), PART_ID);
+        ZonePartitionId partitionGroupId = extractPartitionGroupId(node0, ZONE_NAME, PART_ID);
 
-        String leaseholder = waitAndGetPrimaryReplica(unwrapIgniteImpl(node(0)), partitionGroupId).getLeaseholder();
+        String leaseholder = waitAndGetPrimaryReplica(node0, partitionGroupId).getLeaseholder();
 
         IgniteImpl leaseholderNode = findNodeByName(leaseholder);
-        IgniteImpl otherNode = findNode(0, initialNodes(), ignite -> !leaseholderNode.name().equals(ignite.name()));
+        IgniteImpl otherNode = findNonLeaseholderNode(leaseholderNode);
 
         log.info("Transactions are executed from a non-primary node [node={}, primary={}].", otherNode.name(), leaseholderNode.name());
 
@@ -104,8 +114,50 @@ public class ItOperationRetryTest extends ClusterPerTestIntegrationTest {
         assertEquals("new value", view.get(null, Tuple.create().set("key", 1)).value("val"));
     }
 
-    private IgniteImpl findNodeByName(String leaseholder) {
-        return findNode(0, initialNodes(), n -> leaseholder.equals(n.name()));
+    @Test
+    public void retryImplicitTransactionsTest() throws InterruptedException {
+        IgniteImpl node0 = unwrapIgniteImpl(node(0));
+
+        ZonePartitionId partitionGroupId = extractPartitionGroupId(node0, ZONE_NAME, PART_ID);
+
+        String leaseholderNodeName = waitAndGetPrimaryReplica(node0, partitionGroupId).getLeaseholder();
+
+        IgniteImpl leaseholderNode = findNodeByName(leaseholderNodeName);
+
+        IgniteImpl transactionCoordinatorNode = findNonLeaseholderNode(leaseholderNode);
+
+        IgniteImpl futureLeaseholderNode = findNodeByFilter(n -> {
+            String nodeName = n.name();
+
+            return !(
+                    nodeName.equals(leaseholderNode.name())
+                            || nodeName.equals(transactionCoordinatorNode.name())
+            );
+        });
+
+        DefaultMessagingService messagingService = (DefaultMessagingService) leaseholderNode.clusterService().messagingService();
+        messagingService.dropMessages((nodeName, msg) -> msg instanceof PrimaryReplicaRequest);
+
+        RecordView<Tuple> view = transactionCoordinatorNode.tables().table(TABLE_NAME).recordView();
+        view.upsert(null, Tuple.create().set("key", 1).set("val", "new value"));
+
+        Collection<IgniteImpl> nodes = runningNodes().map(TestWrappers::unwrapIgniteImpl).collect(Collectors.toList());
+
+        NodeUtils.transferPrimary(nodes, partitionGroupId, futureLeaseholderNode.name());
+
+        assertEquals("new value", view.get(null, Tuple.create().set("key", 1)).value("val"));
+    }
+
+    private static ZonePartitionId extractPartitionGroupId(IgniteImpl node, String zoneName, int partitionId) {
+        return new ZonePartitionId(zoneId(node.catalogManager(), zoneName), partitionId);
+    }
+
+    private IgniteImpl findNodeByName(String leaseholderNodeName) {
+        return findNodeByFilter(n -> leaseholderNodeName.equals(n.name()));
+    }
+
+    private IgniteImpl findNodeByFilter(Predicate<IgniteImpl> filter) {
+        return findNode(0, initialNodes(),  filter);
     }
 
     private IgniteImpl findNode(int startRange, int endRange, Predicate<IgniteImpl> filter) {
@@ -117,9 +169,13 @@ public class ItOperationRetryTest extends ClusterPerTestIntegrationTest {
                 .get();
     }
 
-    private ReplicaMeta waitAndGetPrimaryReplica(IgniteImpl node, ZonePartitionId tblReplicationGrp) {
+    private IgniteImpl findNonLeaseholderNode(IgniteImpl leaseholderNode) {
+        return findNodeByFilter(ignite -> !leaseholderNode.name().equals(ignite.name()));
+    }
+
+    private ReplicaMeta waitAndGetPrimaryReplica(IgniteImpl node, ZonePartitionId replicationGroup) {
         CompletableFuture<ReplicaMeta> primaryReplicaFut = node.placementDriver().awaitPrimaryReplica(
-                tblReplicationGrp,
+                replicationGroup,
                 node.clock().now(),
                 10,
                 SECONDS
@@ -128,5 +184,29 @@ public class ItOperationRetryTest extends ClusterPerTestIntegrationTest {
         assertThat(primaryReplicaFut, willCompleteSuccessfully());
 
         return primaryReplicaFut.join();
+    }
+
+    private ReplicaMeta movePrimaryReplicaToAnyNodeFrom(IgniteImpl leaseholderNode, ZonePartitionId replicationGroup) {
+        log.info("Move lease from {}",  leaseholderNode.name());
+
+        DefaultMessagingService messagingService = (DefaultMessagingService) leaseholderNode.clusterService().messagingService();
+
+        messagingService.dropMessages((nodeName, networkMessage) -> networkMessage instanceof StopLeaseProlongationMessage);
+
+        IgniteImpl nonLeaseholderNode = findNonLeaseholderNode(leaseholderNode);
+
+        ReplicaMeta newPrimaryReplicaMeta = Awaitility.waitAtMost(Duration.ofSeconds(20)).until(
+                () -> waitAndGetPrimaryReplica(nonLeaseholderNode , replicationGroup),
+                replicaMeta ->
+                        replicaMeta != null && !replicaMeta.getLeaseholder().equals(leaseholderNode.name())
+        );
+
+        messagingService.stopDroppingMessages();
+
+        assertNotNull(newPrimaryReplicaMeta);
+
+        log.info("New lease is on {}",  newPrimaryReplicaMeta.getLeaseholder());
+
+        return newPrimaryReplicaMeta;
     }
 }
--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/storage/InternalTableImpl.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/storage/InternalTableImpl.java
@@ -53,6 +53,7 @@ import static org.apache.ignite.internal.util.ExceptionUtils.unwrapCause;
 import static org.apache.ignite.internal.util.ExceptionUtils.withCause;
 import static org.apache.ignite.internal.util.FastTimestamps.coarseCurrentTimeMillis;
 import static org.apache.ignite.lang.ErrorGroups.Common.INTERNAL_ERR;
+import static org.apache.ignite.lang.ErrorGroups.PlacementDriver.PRIMARY_REPLICA_AWAIT_ERR;
 import static org.apache.ignite.lang.ErrorGroups.Replicator.GROUP_OVERLOADED_ERR;
 import static org.apache.ignite.lang.ErrorGroups.Replicator.REPLICA_MISS_ERR;
 import static org.apache.ignite.lang.ErrorGroups.Replicator.REPLICA_UNAVAILABLE_ERR;
@@ -2343,7 +2344,14 @@ public class InternalTableImpl implements InternalTable {
      * @return True if retrying is possible, false otherwise.
      */
     private static boolean exceptionAllowsImplicitTxRetry(Throwable e) {
-        return matchAny(unwrapCause(e), ACQUIRE_LOCK_ERR, REPLICA_MISS_ERR, GROUP_OVERLOADED_ERR);
+        return matchAny(
+                unwrapCause(e),
+                ACQUIRE_LOCK_ERR,
+                GROUP_OVERLOADED_ERR,
+                REPLICA_MISS_ERR,
+                REPLICA_UNAVAILABLE_ERR,
+                PRIMARY_REPLICA_AWAIT_ERR
+        );
     }
 
     private CompletableFuture<ReplicaMeta> awaitPrimaryReplica(ReplicationGroupId replicationGroupId, HybridTimestamp timestamp) {
