ab42f2946d5e1e892a5b4e25ab5002eed6ffc7fe
ashishm07 <ashish.mishra.2019@gmail.com>
2025-12-09 15:19:05 +0530

    IGNITE-27212 : Removing IF NOT EXISTS implementation and overcome jdbc thin client incompatibility

--- examples/java/src/main/java/org/apache/ignite/example/table/MapperExample.java
+++ examples/java/src/main/java/org/apache/ignite/example/table/MapperExample.java
@@ -17,9 +17,6 @@
 
 package org.apache.ignite.example.table;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import org.apache.ignite.client.IgniteClient;
 import org.apache.ignite.table.RecordView;
 import org.apache.ignite.table.mapper.Mapper;
@@ -41,12 +38,13 @@ public class MapperExample {
 
     public static void main(String[] args) throws Exception {
 
-        try (Connection conn = DriverManager.getConnection("jdbc:ignite:thin://127.0.0.1:10800/")) {
-
-            try (Statement stmt = conn.createStatement()) {
-
-                stmt.executeUpdate(
-                        "CREATE TABLE IF NOT EXISTS Person ("
+        try (IgniteClient client = IgniteClient.builder()
+                .addresses("127.0.0.1:10800")
+                .build()
+        ) {
+            try {
+                client.sql().executeScript(
+                        "CREATE TABLE Person ("
                                 + "id int primary key, "
                                 + "city varchar, "
                                 + "name varchar, "
@@ -55,21 +53,16 @@ public class MapperExample {
                                 + "city_id int)"
                 );
 
-                stmt.executeUpdate(
+                client.sql().executeScript(
                         "INSERT INTO Person (id, city, name, age, company, city_id) VALUES (1, 'London', 'John Doe', 42, 'Apache', 101)");
-                stmt.executeUpdate(
+                client.sql().executeScript(
                         "INSERT INTO Person (id, city, name, age, company, city_id) VALUES (2, 'New York', 'Jane Doe', 36, 'Apache', 102)");
 
-            }
-            var mapper = Mapper.builder(Person.class)
-                    .automap()
-                    .map("cityId", "city_id", new CityIdConverter())
-                    .build();
+                var mapper = Mapper.builder(Person.class)
+                        .automap()
+                        .map("cityId", "city_id", new CityIdConverter())
+                        .build();
 
-            try (IgniteClient client = IgniteClient.builder()
-                    .addresses("127.0.0.1:10800")
-                    .build()
-            ) {
                 RecordView<Person> view = client.tables()
                         .table("person")
                         .recordView(mapper);
@@ -81,10 +74,9 @@ public class MapperExample {
 
                 System.out.println("Dropping the table...");
 
-                try (Statement stmt = conn.createStatement()) {
-                    stmt.executeUpdate(
-                            "DROP TABLE Person;");
-                }
+                client.sql().executeScript(
+                        "DROP TABLE Person;");
+
             }
         }
     }
