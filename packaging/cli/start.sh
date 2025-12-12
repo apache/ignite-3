b50eac3a0be20250a38cd5d04bd9118f7639831a
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2025-12-12 19:53:25 +0300

    IGNITE-27299 Limit maximum heap size in CLI (#7197)

--- packaging/cli/start.sh
+++ packaging/cli/start.sh
@@ -25,6 +25,7 @@ MAIN_CLASS="@MAIN_CLASS@"
 
 DEFAULT_JVM_OPTS="-Dfile.encoding=UTF-8 \
     --add-opens=java.base/java.lang=ALL-UNNAMED \
+    -Xmx256m \
     -XX:+HeapDumpOnOutOfMemoryError \
     -XX:+ExitOnOutOfMemoryError \
     -XX:HeapDumpPath=${LOG_DIR}"
