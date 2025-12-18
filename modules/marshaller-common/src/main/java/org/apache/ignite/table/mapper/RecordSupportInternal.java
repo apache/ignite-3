23596c6612c04c9971d5dcc20d0e7f45d19daf37
Tiago Marques Godinho <metaltmg@gmail.com>
2025-12-18 11:03:54 +0000

    IGNITE-27361 Extract methods from Creator into a new RecordSupportInternal utility class.
    ** Class is placed in org.apache.ignite.table.mapper to access the original package-private utility class from the API.

---
+++ modules/marshaller-common/src/main/java/org/apache/ignite/table/mapper/RecordSupportInternal.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.table.mapper;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/** Enables access to {@link RecordSupport} without reflection to provides utility methods to support records in Java 11. */
+public final class RecordSupportInternal {
+    private RecordSupportInternal() {
+        // No-op.
+    }
+
+    /**
+     * Java 11 compatible, reflection based alternative, that returns true if and only if this class is a record class.
+     *
+     * <p>Without reflection:
+     *
+     * {@snippet lang="java" :
+     * Class<?> clazz;
+     * clazz.isRecord();
+     * }
+     */
+    public static boolean isRecord(Class<?> clazz) throws IllegalAccessException {
+        return RecordSupport.isRecord(clazz);
+    }
+
+    /**
+     * Java 11 compatible, reflection based alternative to find the record canonical constructor.
+     *
+     * <p>Without reflection:
+     *
+     * {@snippet lang="java" :
+     * static <T extends Record> Constructor<T> getCanonicalConstructor(Class<T> cls)
+     *     throws NoSuchMethodException {
+     *   Class<?>[] paramTypes =
+     *     Arrays.stream(cls.getRecordComponents())
+     *           .map(RecordComponent::getType)
+     *           .toArray(Class<?>[]::new);
+     *   return cls.getDeclaredConstructor(paramTypes);
+     * }}
+     */
+    public static <T> Constructor<T> getCanonicalConstructor(Class<T> clazz)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+        Method getRecordComponentsMtd = Class.class.getDeclaredMethod("getRecordComponents");
+
+        Object[] recordComponents = (Object[]) getRecordComponentsMtd.invoke(clazz);
+
+        Method getTypeMtd = Class.forName("java.lang.reflect.RecordComponent")
+                .getDeclaredMethod("getType");
+
+        Class<?>[] types = new Class<?>[recordComponents.length];
+        for (int i = 0; i < recordComponents.length; i++) {
+            types[i] = (Class<?>) getTypeMtd.invoke(recordComponents[i]);
+        }
+
+        return clazz.getDeclaredConstructor(types);
+    }
+}
