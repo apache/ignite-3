358b269a3b92553522e21d46fd1ae88cf0f53295
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-01-12 12:40:07 +0200

    IGNITE-27180 Java client: Partition awareness request fails with "connection refused" after node shutdown gracefully
    https://issues.apache.org/jira/browse/IGNITE-27180

---
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/runner/app/client/ItThinConnectionFailoverTest.java
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.runner.app.client;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.stream.IntStream;
+import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.internal.ClusterPerTestIntegrationTest;
+import org.apache.ignite.sql.IgniteSql;
+import org.apache.ignite.table.KeyValueView;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+public class ItThinConnectionFailoverTest extends ClusterPerTestIntegrationTest {
+    @Override
+    protected int initialNodes() {
+        return 0;
+    }
+
+    // KV fails
+    @Test
+    void testStopNodePartitionAwarenessKeyValue() {
+        int nodesCount = 3;
+        long reconnectInterval = 1000;
+        cluster.startAndInit(nodesCount, new int[]{2});
+
+        String[] addresses = IntStream.range(0, nodesCount)
+                .mapToObj(i -> "127.0.0.1:" + (10800 + i))
+                .toArray(String[]::new);
+
+        try (IgniteClient client = IgniteClient.builder()
+                .backgroundReconnectInterval(reconnectInterval)
+                .addresses(addresses).build()) {
+
+            client.sql().executeScript("CREATE ZONE zone1 (REPLICAS 3) STORAGE PROFILES ['default'];"
+                    + "CREATE TABLE t(id INT PRIMARY KEY, val INT) ZONE zone1");
+
+            Awaitility.await().until(() -> client.connections().size(), is(3));
+
+            KeyValueView<Integer, Integer> kvView = client.tables().table("t")
+                    .keyValueView(Integer.class, Integer.class);
+
+            for (int i = 0; i < 100; i++) {
+                kvView.put(null, i, i);
+            }
+
+            cluster.stopNode(0);
+            assertThat(client.connections().size(), is(2));
+
+            for (int i = 100; i < 200; i++) {
+                kvView.put(null, i, i);
+            }
+        }
+    }
+
+    // SQL PA query fails
+    @Test
+    void testStopNodePartitionAwarenessQuery() {
+        int nodesCount = 3;
+        long reconnectInterval = 1000;
+        cluster.startAndInit(nodesCount, new int[]{2});
+
+        String[] addresses = IntStream.range(0, nodesCount)
+                .mapToObj(i -> "127.0.0.1:" + (10800 + i))
+                .toArray(String[]::new);
+
+        try (IgniteClient client = IgniteClient.builder()
+                .backgroundReconnectInterval(reconnectInterval)
+                .addresses(addresses).build()) {
+            IgniteSql sql = client.sql();
+
+            sql.executeScript("CREATE ZONE zone1 (REPLICAS 3) STORAGE PROFILES ['default'];"
+                    + "CREATE TABLE t(id INT PRIMARY KEY, val INT) ZONE zone1");
+
+            for (int i = 0; i < 100; i++) {
+                sql.execute(null, "INSERT INTO t VALUES (?, ?)", i, i).close();
+            }
+
+            cluster.stopNode(0);
+
+            assertThat(client.connections().size(), is(2));
+
+            for (int i = 100; i < 200; i++) {
+                sql.execute(null, "INSERT INTO t VALUES (?, ?)", i, i).close();
+            }
+        }
+    }
+
+    // SQL non-PA Works fine
+    @Test
+    void testStopNode() {
+        int nodesCount = 3;
+        cluster.startAndInit(nodesCount, new int[]{2});
+
+        String[] addresses = IntStream.range(0, nodesCount)
+                .mapToObj(i -> "127.0.0.1:" + (10800 + i))
+                .toArray(String[]::new);
+
+        try (IgniteClient client = IgniteClient.builder().addresses(addresses).build()) {
+            IgniteSql sql = client.sql();
+
+            Awaitility.await().until(() -> client.connections().size(), is(3));
+
+            for (int i = 0; i < 100; i++) {
+                sql.execute(null, "SELECT " + i).close();
+            }
+
+            cluster.stopNode(0);
+
+            assertThat(client.connections().size(), is(2));
+
+            for (int i = 100; i < 200; i++) {
+                sql.execute(null, "SELECT " + i).close();
+            }
+        }
+    }
+}
