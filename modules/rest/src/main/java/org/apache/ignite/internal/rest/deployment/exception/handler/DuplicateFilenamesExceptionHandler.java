2738c3926867f76dd0146555f16892bd308faf9a
Vadim Pakhnushev <8614891+valepakh@users.noreply.github.com>
2026-01-14 16:40:05 +0300

    IGNITE-27350 Verify deployment contents (#7310)

---
+++ modules/rest/src/main/java/org/apache/ignite/internal/rest/deployment/exception/handler/DuplicateFilenamesExceptionHandler.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.rest.deployment.exception.handler;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import jakarta.inject.Singleton;
+import org.apache.ignite.internal.rest.api.Problem;
+import org.apache.ignite.internal.rest.constants.HttpCode;
+import org.apache.ignite.internal.rest.deployment.DuplicateFilenamesException;
+import org.apache.ignite.internal.rest.problem.HttpProblemResponse;
+
+/**
+ * REST exception handler for {@link DuplicateFilenamesException}.
+ */
+@Singleton
+@Requires(classes = {DuplicateFilenamesException.class, ExceptionHandler.class})
+public class DuplicateFilenamesExceptionHandler
+        implements ExceptionHandler<DuplicateFilenamesException, HttpResponse<? extends Problem>> {
+    @Override
+    public HttpResponse<? extends Problem> handle(HttpRequest request, DuplicateFilenamesException exception) {
+        return HttpProblemResponse.from(
+                Problem.fromHttpCode(HttpCode.BAD_REQUEST)
+                        .detail(exception.getMessage()).build()
+        );
+    }
+}
