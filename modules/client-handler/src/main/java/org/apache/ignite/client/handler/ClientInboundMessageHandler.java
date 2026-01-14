01a72b41162f93811c638b8374354b14c4cd1672
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-01-14 14:29:22 +0200

    fix feature set

--- modules/client-handler/src/main/java/org/apache/ignite/client/handler/ClientInboundMessageHandler.java
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/ClientInboundMessageHandler.java
@@ -1414,7 +1414,7 @@ public class ClientInboundMessageHandler
                         packer.packString(jobClassName);
                         packDeploymentUnitPaths(ctx.deploymentUnits(), packer);
                         packer.packBoolean(false); // Retain deployment units in cache.
-                        ClientComputeJobPacker.packJobArgument(arg, null, packer);
+                        ClientComputeJobPacker.packJobArgument(arg, null, packer, null);
                     })
                     .thenApply(unpacker -> {
                         try (unpacker) {
