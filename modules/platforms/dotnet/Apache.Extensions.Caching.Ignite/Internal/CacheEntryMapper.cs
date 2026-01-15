2290ab36c99c68694417b942a1016d64321005a5
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-01-15 11:22:59 +0200

    cleanup

--- modules/platforms/dotnet/Apache.Extensions.Caching.Ignite/Internal/CacheEntryMapper.cs
+++ modules/platforms/dotnet/Apache.Extensions.Caching.Ignite/Internal/CacheEntryMapper.cs
@@ -84,9 +84,9 @@ internal sealed class CacheEntryMapper : IMapper<KeyValuePair<string, CacheEntry
             throw new InvalidOperationException("Value column is missing.");
         }
 
-        if (_options.CacheKeyPrefix != null && key.StartsWith(_options.CacheKeyPrefix, StringComparison.Ordinal))
+        if (_options.CacheKeyPrefix is { } prefix && key.StartsWith(prefix, StringComparison.Ordinal))
         {
-            key = key[_options.CacheKeyPrefix.Length..];
+            key = key[prefix.Length..];
         }
 
         return KeyValuePair.Create(key, new CacheEntry(value));
