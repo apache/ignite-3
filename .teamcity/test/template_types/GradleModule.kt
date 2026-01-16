bbc0bbdb26ec70a6d387954c688dacdaaa808382
Vadim Kolodin <vkolodin@gridgain.com>
2026-01-16 15:29:57 +0400

    IGNITE-27589 Add parallelTests feature to gradle modules

--- .teamcity/test/template_types/GradleModule.kt
+++ .teamcity/test/template_types/GradleModule.kt
@@ -1,6 +1,10 @@
 package test.template_types
 
-data class GradleModule(val displayName: String, val moduleName: String, val jvmArgs: String = "", val dependencies: List<String> = emptyList()) {
+/**
+ * @param parallelTestsEnabled split tests of a build into several batches, by test classes and run them in parallel on suitable build agents.
+ *        See [jetbrains.buildServer.configs.kotlin.buildFeatures.ParallelTestsFeature].
+ */
+data class GradleModule(val displayName: String, val moduleName: String, val jvmArgs: String = "", val dependencies: List<String> = emptyList(), val parallelTestsEnabled: Boolean = false) {
     fun buildTask(taskName: String): String {
         val dependencyTasks = dependencies.joinToString(" ")
         val mainTask = ":$moduleName:$taskName"
