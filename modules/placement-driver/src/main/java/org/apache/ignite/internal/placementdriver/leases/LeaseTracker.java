2c6d512bb4dd3b49922f9b06b0e497873e3136d6
Mirza Aliev <alievmirza@gmail.com>
2026-02-05 12:40:18 +0400

    IGNITE-27725 Add some logs to Placement Driver

--- modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/leases/LeaseTracker.java
+++ modules/placement-driver/src/main/java/org/apache/ignite/internal/placementdriver/leases/LeaseTracker.java
@@ -135,6 +135,8 @@ public class LeaseTracker extends AbstractEventProducer<PrimaryReplicaEvent, Pri
      */
     public void startTrack(long recoveryRevision) {
         inBusyLock(busyLock, () -> {
+            LOG.info("Starting lease tracker recovery [revision={}].", recoveryRevision);
+
             msManager.registerExactWatch(PLACEMENTDRIVER_LEASES_KEY, updateListener);
 
             loadLeasesBusyAsync(recoveryRevision);
@@ -148,6 +150,8 @@ public class LeaseTracker extends AbstractEventProducer<PrimaryReplicaEvent, Pri
         }
 
         busyLock.block();
+
+        LOG.info("Stopping lease tracker.");
 
         primaryReplicaWaiters.values().forEach(PendingComparableValuesTracker::close);
         primaryReplicaWaiters.clear();
@@ -456,7 +460,7 @@ public class LeaseTracker extends AbstractEventProducer<PrimaryReplicaEvent, Pri
             leases = new Leases(leasesMap, leasesBytes);
         }
 
-        LOG.info("Leases cache recovered [leases={}]", leases);
+        LOG.info("Leases cache recovered [revision={}, leases={}]", recoveryRevision, leases);
     }
 
     /**
