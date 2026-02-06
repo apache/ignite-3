0b005b33d08ce808b03176bf0370a5af5a07fd89
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2026-02-06 12:01:46 +0400

    IGNITE-27766 Reduce log level of 'Fail to connect' in Raft client

--- modules/raft/src/main/java/org/apache/ignite/raft/jraft/rpc/impl/AbstractClientService.java
+++ modules/raft/src/main/java/org/apache/ignite/raft/jraft/rpc/impl/AbstractClientService.java
@@ -129,7 +129,7 @@ public abstract class AbstractClientService implements ClientService, TopologyEv
             if (!deadPeers.contains(peerId)) {
                 deadPeers.add(peerId);
 
-                LOG.error("Fail to connect {}, exception: {}.", peerId, e.getMessage());
+                LOG.warn("Fail to connect {}, exception: {}.", peerId, e.getMessage());
             }
         }
 
