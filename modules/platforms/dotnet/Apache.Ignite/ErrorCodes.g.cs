17cf0956839bc7d3d1ff5f7767c83f21094a7198
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-02-18 18:12:46 +0200

    Update generated code

--- modules/platforms/dotnet/Apache.Ignite/ErrorCodes.g.cs
+++ modules/platforms/dotnet/Apache.Ignite/ErrorCodes.g.cs
@@ -54,7 +54,7 @@ namespace Apache.Ignite
             Rest.GroupCode => Rest.GroupName,
             CommonConfiguration.GroupCode => CommonConfiguration.GroupName,
 
-            _ => UnknownGroupName
+            _ => UnknownGroupName + groupCode
         };
 
         /// <summary>
@@ -89,7 +89,7 @@ namespace Apache.Ignite
             Rest.GroupCode => Rest.ErrorPrefix,
             CommonConfiguration.GroupCode => CommonConfiguration.ErrorPrefix,
 
-            _ => UnknownGroupName
+            _ => UnknownGroupName + groupCode
         };
 
         /// <summary> Common errors. </summary>
