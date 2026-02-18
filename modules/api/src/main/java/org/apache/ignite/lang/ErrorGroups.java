4d0ad12c11bc0a1f899d382c6cb99542529ea19a
Pochatkin Mikhail <m.a.pochatkin@gmail.com>
2026-02-18 21:18:34 +0300

    IGNITE-27789 Remove file transfer module

--- modules/api/src/main/java/org/apache/ignite/lang/ErrorGroups.java
+++ modules/api/src/main/java/org/apache/ignite/lang/ErrorGroups.java
@@ -539,11 +539,8 @@ public class ErrorGroups {
         /** Address or port bind error. */
         public static final int BIND_ERR = NETWORK_ERR_GROUP.registerErrorCode((short) 2);
 
-        /** File transfer error. */
-        public static final int FILE_TRANSFER_ERR = NETWORK_ERR_GROUP.registerErrorCode((short) 3);
-
-        /** File validation error. */
-        public static final int FILE_VALIDATION_ERR = NETWORK_ERR_GROUP.registerErrorCode((short) 4);
+        // Error codes 3 (FILE_TRANSFER_ERR) and 4 (FILE_VALIDATION_ERR) were removed along with the file-transfer module.
+        // These codes are reserved forever and must not be reused.
 
         /** Recipient node has left the physical topology. */
         public static final int RECIPIENT_LEFT_ERR = NETWORK_ERR_GROUP.registerErrorCode((short) 5);
