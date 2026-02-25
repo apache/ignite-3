e0af1ddc88976e715322d4dc76e009c7055f6c4a
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-02-25 15:16:39 +0200

    IGNITE-27861 .NET: Fix TestSlidingExpiration flakiness

--- modules/platforms/dotnet/Apache.Extensions.Caching.Ignite.Tests/IgniteDistributedCacheTests.cs
+++ modules/platforms/dotnet/Apache.Extensions.Caching.Ignite.Tests/IgniteDistributedCacheTests.cs
@@ -276,7 +276,7 @@ public class IgniteDistributedCacheTests(string keyPrefix) : IgniteTestsBase
 
         var entryOptions = new DistributedCacheEntryOptions
         {
-            SlidingExpiration = TimeSpan.FromSeconds(0.5)
+            SlidingExpiration = TimeSpan.FromSeconds(1)
         };
 
         await cache.SetAsync("x", [1], entryOptions);
@@ -285,12 +285,12 @@ public class IgniteDistributedCacheTests(string keyPrefix) : IgniteTestsBase
         // Access before expiration to reset the timer.
         for (int i = 0; i < 7; i++)
         {
-            await Task.Delay(TimeSpan.FromSeconds(0.1));
+            await Task.Delay(TimeSpan.FromSeconds(0.2));
             Assert.IsNotNull(await cache.GetAsync("x"));
         }
 
         // Wait for expiration without accessing.
-        await Task.Delay(TimeSpan.FromSeconds(0.7));
+        await Task.Delay(TimeSpan.FromSeconds(1.5));
         Assert.IsNull(await cache.GetAsync("x"));
     }
 
