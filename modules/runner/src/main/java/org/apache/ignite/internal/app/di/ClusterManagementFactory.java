abe4454575f89224cc14939084f98b3b70dfbb6e
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2026-03-05 09:21:02 +0400

    Step6

---
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/di/ClusterManagementFactory.java
@@ -0,0 +1,246 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.app.di;
+
+import io.micronaut.context.annotation.Factory;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import org.apache.ignite.configuration.ConfigurationDynamicDefaultsPatcher;
+import org.apache.ignite.internal.cluster.management.ClusterInitializer;
+import org.apache.ignite.internal.cluster.management.ClusterManagementGroupManager;
+import org.apache.ignite.internal.cluster.management.NodeAttributesCollector;
+import org.apache.ignite.internal.cluster.management.configuration.NodeAttributesConfiguration;
+import org.apache.ignite.internal.cluster.management.configuration.NodeAttributesExtensionConfiguration;
+import org.apache.ignite.internal.cluster.management.raft.ClusterStateStorageManager;
+import org.apache.ignite.internal.cluster.management.raft.RocksDbClusterStateStorage;
+import org.apache.ignite.internal.cluster.management.raft.ValidationManager;
+import org.apache.ignite.internal.cluster.management.topology.LogicalTopologyImpl;
+import org.apache.ignite.internal.cluster.management.topology.LogicalTopologyServiceImpl;
+import org.apache.ignite.internal.configuration.ComponentWorkingDir;
+import org.apache.ignite.internal.configuration.ConfigurationDynamicDefaultsPatcherImpl;
+import org.apache.ignite.internal.configuration.ConfigurationModules;
+import org.apache.ignite.internal.configuration.ConfigurationRegistry;
+import org.apache.ignite.internal.configuration.ConfigurationTreeGenerator;
+import org.apache.ignite.internal.configuration.validation.ConfigurationValidator;
+import org.apache.ignite.internal.configuration.validation.ConfigurationValidatorImpl;
+import org.apache.ignite.internal.di.IgniteStartupPhase;
+import org.apache.ignite.internal.di.StartupPhase;
+import org.apache.ignite.internal.disaster.system.ClusterIdService;
+import org.apache.ignite.internal.disaster.system.SystemDisasterRecoveryStorage;
+import org.apache.ignite.internal.failure.FailureManager;
+import org.apache.ignite.internal.metrics.MetricManager;
+import org.apache.ignite.internal.network.ChannelTypeRegistryProvider;
+import org.apache.ignite.internal.network.ClusterService;
+import org.apache.ignite.internal.network.MessageSerializationRegistryImpl;
+import org.apache.ignite.internal.network.NettyBootstrapFactory;
+import org.apache.ignite.internal.network.configuration.NetworkConfiguration;
+import org.apache.ignite.internal.network.recovery.InMemoryStaleIds;
+import org.apache.ignite.internal.network.scalecube.ScaleCubeClusterService;
+import org.apache.ignite.internal.network.serialization.MessageSerializationRegistry;
+import org.apache.ignite.internal.raft.RaftGroupOptionsConfigurer;
+import org.apache.ignite.internal.raft.RaftManager;
+import org.apache.ignite.internal.storage.configurations.StorageConfiguration;
+import org.apache.ignite.internal.storage.configurations.StorageExtensionConfiguration;
+import org.apache.ignite.internal.vault.VaultManager;
+import org.apache.ignite.internal.version.DefaultIgniteProductVersionSource;
+import org.apache.ignite.internal.worker.CriticalWorkerRegistry;
+
+/**
+ * Micronaut factory for cluster management components.
+ */
+@Factory
+public class ClusterManagementFactory {
+    /** Creates the message serialization registry. */
+    @Singleton
+    public MessageSerializationRegistry messageSerializationRegistry() {
+        return new MessageSerializationRegistryImpl();
+    }
+
+    /** Creates the cluster service. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public ClusterService clusterService(
+            NodeSeedParams seedParams,
+            NetworkConfiguration networkConfiguration,
+            NettyBootstrapFactory nettyBootstrapFactory,
+            MessageSerializationRegistry serializationRegistry,
+            ClusterIdService clusterIdService,
+            CriticalWorkerRegistry criticalWorkerRegistry,
+            FailureManager failureManager,
+            MetricManager metricManager
+    ) {
+        return new ScaleCubeClusterService(
+                seedParams.nodeName(),
+                networkConfiguration,
+                nettyBootstrapFactory,
+                serializationRegistry,
+                new InMemoryStaleIds(),
+                clusterIdService,
+                criticalWorkerRegistry,
+                failureManager,
+                ChannelTypeRegistryProvider.loadByServiceLoader(seedParams.serviceProviderClassLoader()),
+                new DefaultIgniteProductVersionSource(),
+                metricManager
+        );
+    }
+
+    /** Creates the node attributes configuration from the node config registry. */
+    @Singleton
+    public NodeAttributesConfiguration nodeAttributesConfiguration(
+            @Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry
+    ) {
+        return nodeConfigRegistry.getConfiguration(NodeAttributesExtensionConfiguration.KEY).nodeAttributes();
+    }
+
+    /** Creates the storage profiles configuration from the node config registry. */
+    @Singleton
+    public StorageConfiguration storageConfiguration(@Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry) {
+        return nodeConfigRegistry.getConfiguration(StorageExtensionConfiguration.KEY).storage();
+    }
+
+    /** Creates the cluster state storage backed by RocksDB. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public RocksDbClusterStateStorage clusterStateStorage(@Named("cmg") ComponentWorkingDir cmgWorkDir, NodeSeedParams seedParams) {
+        return new RocksDbClusterStateStorage(cmgWorkDir.dbPath(), seedParams.nodeName());
+    }
+
+    /** Creates the cluster state storage manager. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public ClusterStateStorageManager clusterStateStorageManager(RocksDbClusterStateStorage clusterStateStorage) {
+        return new ClusterStateStorageManager(clusterStateStorage);
+    }
+
+    /** Creates the logical topology implementation. */
+    @Singleton
+    public LogicalTopologyImpl logicalTopology(RocksDbClusterStateStorage clusterStateStorage, FailureManager failureManager) {
+        return new LogicalTopologyImpl(clusterStateStorage, failureManager);
+    }
+
+    /** Creates the validation manager. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public ValidationManager validationManager(
+            ClusterStateStorageManager clusterStateStorageManager,
+            LogicalTopologyImpl logicalTopology
+    ) {
+        return new ValidationManager(clusterStateStorageManager, logicalTopology);
+    }
+
+    /** Creates the distributed configuration tree generator. */
+    @Singleton
+    @Named("distributed")
+    public ConfigurationTreeGenerator distributedConfigurationTreeGenerator(ConfigurationModules modules) {
+        return new ConfigurationTreeGenerator(
+                modules.distributed().rootKeys(),
+                modules.distributed().schemaExtensions(),
+                modules.distributed().polymorphicSchemaExtensions()
+        );
+    }
+
+    /** Creates the distributed configuration validator. */
+    @Singleton
+    @Named("distributed")
+    public ConfigurationValidator distributedConfigurationValidator(
+            @Named("distributed") ConfigurationTreeGenerator generator,
+            ConfigurationModules modules
+    ) {
+        return ConfigurationValidatorImpl.withDefaultValidators(generator, modules.distributed().validators());
+    }
+
+    /** Creates the configuration dynamic defaults patcher. */
+    @Singleton
+    public ConfigurationDynamicDefaultsPatcher configurationDynamicDefaultsPatcher(
+            ConfigurationModules modules,
+            @Named("distributed") ConfigurationTreeGenerator generator
+    ) {
+        return new ConfigurationDynamicDefaultsPatcherImpl(modules.distributed(), generator);
+    }
+
+    /** Creates the cluster initializer. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public ClusterInitializer clusterInitializer(
+            ClusterService clusterService,
+            ConfigurationDynamicDefaultsPatcher configurationDynamicDefaultsPatcher,
+            @Named("distributed") ConfigurationValidator distributedCfgValidator
+    ) {
+        return new ClusterInitializer(clusterService, configurationDynamicDefaultsPatcher, distributedCfgValidator);
+    }
+
+    /** Creates the node attributes collector. */
+    @Singleton
+    public NodeAttributesCollector nodeAttributesCollector(
+            NodeAttributesConfiguration nodeAttributesConfiguration,
+            StorageConfiguration storageConfiguration
+    ) {
+        return new NodeAttributesCollector(nodeAttributesConfiguration, storageConfiguration);
+    }
+
+    /** Creates the system disaster recovery storage. */
+    @Singleton
+    public SystemDisasterRecoveryStorage systemDisasterRecoveryStorage(VaultManager vaultManager) {
+        return new SystemDisasterRecoveryStorage(vaultManager);
+    }
+
+    /** Creates the cluster management group manager. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public ClusterManagementGroupManager clusterManagementGroupManager(
+            VaultManager vaultManager,
+            SystemDisasterRecoveryStorage systemDisasterRecoveryStorage,
+            ClusterService clusterService,
+            ClusterInitializer clusterInitializer,
+            RaftManager raftManager,
+            ClusterStateStorageManager clusterStateStorageManager,
+            LogicalTopologyImpl logicalTopology,
+            ValidationManager validationManager,
+            NodeAttributesCollector nodeAttributesCollector,
+            FailureManager failureManager,
+            ClusterIdService clusterIdService,
+            @Named("cmg") RaftGroupOptionsConfigurer cmgRaftConfigurer,
+            MetricManager metricManager
+    ) {
+        return new ClusterManagementGroupManager(
+                vaultManager,
+                systemDisasterRecoveryStorage,
+                clusterService,
+                clusterInitializer,
+                raftManager,
+                clusterStateStorageManager,
+                logicalTopology,
+                validationManager,
+                nodeAttributesCollector,
+                failureManager,
+                clusterIdService,
+                cmgRaftConfigurer,
+                metricManager
+        );
+    }
+
+    /** Creates the logical topology service. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public LogicalTopologyServiceImpl logicalTopologyService(
+            LogicalTopologyImpl logicalTopology,
+            ClusterManagementGroupManager cmgManager
+    ) {
+        return new LogicalTopologyServiceImpl(logicalTopology, cmgManager);
+    }
+}
---
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/di/RaftFactory.java
@@ -0,0 +1,236 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.app.di;
+
+import static org.apache.ignite.internal.configuration.IgnitePaths.cmgPath;
+import static org.apache.ignite.internal.configuration.IgnitePaths.metastoragePath;
+import static org.apache.ignite.internal.configuration.IgnitePaths.partitionsPath;
+
+import io.micronaut.context.annotation.Factory;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import java.nio.file.Path;
+import java.util.Map;
+import org.apache.ignite.internal.cluster.management.CmgGroupId;
+import org.apache.ignite.internal.configuration.ComponentWorkingDir;
+import org.apache.ignite.internal.configuration.ConfigurationRegistry;
+import org.apache.ignite.internal.configuration.RaftGroupOptionsConfigHelper;
+import org.apache.ignite.internal.configuration.SystemLocalConfiguration;
+import org.apache.ignite.internal.di.IgniteStartupPhase;
+import org.apache.ignite.internal.di.StartupPhase;
+import org.apache.ignite.internal.failure.FailureManager;
+import org.apache.ignite.internal.hlc.HybridClock;
+import org.apache.ignite.internal.metastorage.server.raft.MetastorageGroupId;
+import org.apache.ignite.internal.metrics.MetricManager;
+import org.apache.ignite.internal.network.ClusterService;
+import org.apache.ignite.internal.raft.Loza;
+import org.apache.ignite.internal.raft.RaftGroupOptionsConfigurer;
+import org.apache.ignite.internal.raft.configuration.RaftConfiguration;
+import org.apache.ignite.internal.raft.configuration.RaftExtensionConfiguration;
+import org.apache.ignite.internal.raft.server.impl.GroupStoragesContextResolver;
+import org.apache.ignite.internal.raft.storage.GroupStoragesDestructionIntents;
+import org.apache.ignite.internal.raft.storage.LogStorageManager;
+import org.apache.ignite.internal.raft.storage.impl.VaultGroupStoragesDestructionIntents;
+import org.apache.ignite.internal.raft.util.SharedLogStorageManagerUtils;
+import org.apache.ignite.internal.replicator.PartitionGroupId;
+import org.apache.ignite.internal.vault.VaultManager;
+import org.apache.ignite.raft.jraft.rpc.impl.RaftGroupEventsClientListener;
+
+/**
+ * Micronaut factory for Raft infrastructure components.
+ */
+@Factory
+public class RaftFactory {
+    /** Used for durable destruction purposes. */
+    private static final String PARTITION_GROUP_NAME = "partition";
+
+    /** Creates the Raft configuration from the node config registry. */
+    @Singleton
+    public RaftConfiguration raftConfiguration(@Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry) {
+        return nodeConfigRegistry.getConfiguration(RaftExtensionConfiguration.KEY).raft();
+    }
+
+    /** Creates the Raft group events client listener. */
+    @Singleton
+    public RaftGroupEventsClientListener raftGroupEventsClientListener() {
+        return new RaftGroupEventsClientListener();
+    }
+
+    /** Creates the partitions working directory. */
+    @Singleton
+    @Named("partitions")
+    public ComponentWorkingDir partitionsWorkDir(SystemLocalConfiguration systemConfiguration, NodeSeedParams seedParams) {
+        return partitionsPath(systemConfiguration, seedParams.workDir());
+    }
+
+    /** Creates the metastorage working directory. */
+    @Singleton
+    @Named("metastorage")
+    public ComponentWorkingDir metastorageWorkDir(SystemLocalConfiguration systemConfiguration, NodeSeedParams seedParams) {
+        return metastoragePath(systemConfiguration, seedParams.workDir());
+    }
+
+    /** Creates the CMG working directory. */
+    @Singleton
+    @Named("cmg")
+    public ComponentWorkingDir cmgWorkDir(SystemLocalConfiguration systemConfiguration, NodeSeedParams seedParams) {
+        return cmgPath(systemConfiguration, seedParams.workDir());
+    }
+
+    /** Creates the partitions log storage manager. */
+    @Singleton
+    @Named("partitions")
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public LogStorageManager partitionsLogStorageManager(
+            NodeSeedParams seedParams,
+            RaftConfiguration raftConfiguration,
+            @Named("partitions") ComponentWorkingDir partitionsWorkDir
+    ) {
+        return SharedLogStorageManagerUtils.create(
+                "table data log",
+                seedParams.nodeName(),
+                partitionsWorkDir.raftLogPath(),
+                raftConfiguration.fsync().value()
+        );
+    }
+
+    /** Creates the metastorage log storage manager. */
+    @Singleton
+    @Named("metastorage")
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public LogStorageManager msLogStorageManager(
+            NodeSeedParams seedParams,
+            @Named("metastorage") ComponentWorkingDir metastorageWorkDir
+    ) {
+        return SharedLogStorageManagerUtils.create(
+                "meta-storage log",
+                seedParams.nodeName(),
+                metastorageWorkDir.raftLogPath(),
+                true
+        );
+    }
+
+    /** Creates the CMG log storage manager. */
+    @Singleton
+    @Named("cmg")
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public LogStorageManager cmgLogStorageManager(
+            NodeSeedParams seedParams,
+            @Named("cmg") ComponentWorkingDir cmgWorkDir
+    ) {
+        return SharedLogStorageManagerUtils.create(
+                "cluster-management-group log",
+                seedParams.nodeName(),
+                cmgWorkDir.raftLogPath(),
+                true
+        );
+    }
+
+    /** Creates the partitions Raft group options configurer. */
+    @Singleton
+    @Named("partitions")
+    public RaftGroupOptionsConfigurer partitionRaftConfigurer(
+            @Named("partitions") LogStorageManager partitionsLogStorageManager,
+            @Named("partitions") ComponentWorkingDir partitionsWorkDir
+    ) {
+        return RaftGroupOptionsConfigHelper.configureProperties(partitionsLogStorageManager, partitionsWorkDir.metaPath());
+    }
+
+    /** Creates the metastorage Raft group options configurer. */
+    @Singleton
+    @Named("metastorage")
+    public RaftGroupOptionsConfigurer msRaftConfigurer(
+            @Named("metastorage") LogStorageManager msLogStorageManager,
+            @Named("metastorage") ComponentWorkingDir metastorageWorkDir
+    ) {
+        return RaftGroupOptionsConfigHelper.configureProperties(msLogStorageManager, metastorageWorkDir.metaPath());
+    }
+
+    /** Creates the CMG Raft group options configurer. */
+    @Singleton
+    @Named("cmg")
+    public RaftGroupOptionsConfigurer cmgRaftConfigurer(
+            @Named("cmg") LogStorageManager cmgLogStorageManager,
+            @Named("cmg") ComponentWorkingDir cmgWorkDir
+    ) {
+        return RaftGroupOptionsConfigHelper.configureProperties(cmgLogStorageManager, cmgWorkDir.metaPath());
+    }
+
+    /** Creates the group storages context resolver. */
+    @Singleton
+    public GroupStoragesContextResolver groupStoragesContextResolver(
+            @Named("partitions") LogStorageManager partitionsLogStorageManager,
+            @Named("metastorage") LogStorageManager msLogStorageManager,
+            @Named("cmg") LogStorageManager cmgLogStorageManager,
+            @Named("partitions") ComponentWorkingDir partitionsWorkDir,
+            @Named("metastorage") ComponentWorkingDir metastorageWorkDir,
+            @Named("cmg") ComponentWorkingDir cmgWorkDir
+    ) {
+        Map<String, LogStorageManager> logStorageManagerByGroupName = Map.of(
+                PARTITION_GROUP_NAME, partitionsLogStorageManager,
+                MetastorageGroupId.INSTANCE.toString(), msLogStorageManager,
+                CmgGroupId.INSTANCE.toString(), cmgLogStorageManager
+        );
+
+        Map<String, Path> serverDataPathByGroupName = Map.of(
+                PARTITION_GROUP_NAME, partitionsWorkDir.metaPath(),
+                MetastorageGroupId.INSTANCE.toString(), metastorageWorkDir.metaPath(),
+                CmgGroupId.INSTANCE.toString(), cmgWorkDir.metaPath()
+        );
+
+        return new GroupStoragesContextResolver(
+                replicationGroupId -> replicationGroupId instanceof PartitionGroupId
+                        ? PARTITION_GROUP_NAME : replicationGroupId.toString(),
+                serverDataPathByGroupName,
+                logStorageManagerByGroupName
+        );
+    }
+
+    /** Creates the group storages destruction intents backed by vault. */
+    @Singleton
+    public GroupStoragesDestructionIntents groupStoragesDestructionIntents(VaultManager vaultManager) {
+        return new VaultGroupStoragesDestructionIntents(vaultManager);
+    }
+
+    /** Creates the Raft manager (Loza). */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_1)
+    public Loza loza(
+            ClusterService clusterService,
+            MetricManager metricManager,
+            RaftConfiguration raftConfiguration,
+            SystemLocalConfiguration systemConfiguration,
+            HybridClock clock,
+            RaftGroupEventsClientListener raftGroupEventsClientListener,
+            FailureManager failureManager,
+            GroupStoragesDestructionIntents groupStoragesDestructionIntents,
+            GroupStoragesContextResolver groupStoragesContextResolver
+    ) {
+        return new Loza(
+                clusterService,
+                metricManager,
+                raftConfiguration,
+                systemConfiguration,
+                clock,
+                raftGroupEventsClientListener,
+                failureManager,
+                groupStoragesDestructionIntents,
+                groupStoragesContextResolver
+        );
+    }
+}
