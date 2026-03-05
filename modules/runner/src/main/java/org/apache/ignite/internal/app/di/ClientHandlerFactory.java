4b9a6466001c51180a5e2d2cf57e32ed3f89c341
Ivan Zlenko <241953+ivanzlenko@users.noreply.github.com>
2026-03-05 10:04:47 +0400

    Step9

---
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/di/ClientHandlerFactory.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.app.di;
+
+import io.micronaut.context.annotation.Factory;
+import jakarta.inject.Named;
+import jakarta.inject.Provider;
+import jakarta.inject.Singleton;
+import org.apache.ignite.client.handler.ClientHandlerMetricSource;
+import org.apache.ignite.client.handler.ClientHandlerModule;
+import org.apache.ignite.client.handler.ClusterInfo;
+import org.apache.ignite.client.handler.configuration.ClientConnectorConfiguration;
+import org.apache.ignite.client.handler.configuration.ClientConnectorExtensionConfiguration;
+import org.apache.ignite.internal.app.ThreadPoolsManager;
+import org.apache.ignite.internal.catalog.CatalogManagerImpl;
+import org.apache.ignite.internal.cluster.management.ClusterManagementGroupManager;
+import org.apache.ignite.internal.cluster.management.ClusterState;
+import org.apache.ignite.internal.compute.IgniteComputeInternal;
+import org.apache.ignite.internal.configuration.ConfigurationRegistry;
+import org.apache.ignite.internal.configuration.SuggestionsClusterExtensionConfiguration;
+import org.apache.ignite.internal.configuration.SuggestionsConfiguration;
+import org.apache.ignite.internal.di.IgniteStartupPhase;
+import org.apache.ignite.internal.di.StartupPhase;
+import org.apache.ignite.internal.eventlog.impl.EventLogImpl;
+import org.apache.ignite.internal.hlc.ClockServiceImpl;
+import org.apache.ignite.internal.lowwatermark.LowWatermarkImpl;
+import org.apache.ignite.internal.metrics.MetricManager;
+import org.apache.ignite.internal.network.ClusterService;
+import org.apache.ignite.internal.network.NettyBootstrapFactory;
+import org.apache.ignite.internal.placementdriver.PlacementDriverManager;
+import org.apache.ignite.internal.security.authentication.AuthenticationManager;
+import org.apache.ignite.internal.security.authentication.AuthenticationManagerImpl;
+import org.apache.ignite.internal.security.configuration.SecurityConfiguration;
+import org.apache.ignite.internal.security.configuration.SecurityExtensionConfiguration;
+import org.apache.ignite.internal.sql.engine.SqlQueryProcessor;
+import org.apache.ignite.internal.table.distributed.TableManager;
+import org.apache.ignite.internal.table.distributed.schema.SchemaSyncServiceImpl;
+import org.apache.ignite.internal.tx.TxManager;
+
+/**
+ * Micronaut factory for client handler and authentication components.
+ */
+@Factory
+public class ClientHandlerFactory {
+    /** Creates the security configuration from the cluster config registry. */
+    @Singleton
+    public SecurityConfiguration securityConfiguration(
+            @Named("clusterConfig") ConfigurationRegistry clusterConfigRegistry
+    ) {
+        return clusterConfigRegistry.getConfiguration(SecurityExtensionConfiguration.KEY).security();
+    }
+
+    /** Creates the authentication manager. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public AuthenticationManager authenticationManager(
+            SecurityConfiguration securityConfiguration,
+            EventLogImpl eventLog
+    ) {
+        return new AuthenticationManagerImpl(securityConfiguration, eventLog);
+    }
+
+    /** Creates the client connector configuration from the node config registry. */
+    @Singleton
+    public ClientConnectorConfiguration clientConnectorConfiguration(
+            @Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry
+    ) {
+        return nodeConfigRegistry.getConfiguration(ClientConnectorExtensionConfiguration.KEY).clientConnector();
+    }
+
+    /** Creates the suggestions configuration from the cluster config registry. */
+    @Singleton
+    public SuggestionsConfiguration suggestionsConfiguration(
+            @Named("clusterConfig") ConfigurationRegistry clusterConfigRegistry
+    ) {
+        return clusterConfigRegistry.getConfiguration(SuggestionsClusterExtensionConfiguration.KEY).suggestions();
+    }
+
+    /** Creates the client handler module. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public ClientHandlerModule clientHandlerModule(
+            SqlQueryProcessor sqlQueryProcessor,
+            TableManager tableManager,
+            TxManager txManager,
+            IgniteComputeInternal compute,
+            ClusterService clusterService,
+            NettyBootstrapFactory nettyBootstrapFactory,
+            Provider<ClusterManagementGroupManager> cmgManagerProvider,
+            MetricManager metricManager,
+            AuthenticationManager authenticationManager,
+            ClockServiceImpl clockService,
+            SchemaSyncServiceImpl schemaSyncService,
+            CatalogManagerImpl catalogManager,
+            PlacementDriverManager placementDriverManager,
+            ClientConnectorConfiguration clientConnectorConfiguration,
+            EventLogImpl eventLog,
+            LowWatermarkImpl lowWatermark,
+            ThreadPoolsManager threadPoolsManager,
+            SuggestionsConfiguration suggestionsConfiguration
+    ) {
+        return new ClientHandlerModule(
+                sqlQueryProcessor,
+                tableManager,
+                txManager,
+                compute,
+                clusterService,
+                nettyBootstrapFactory,
+                () -> {
+                    ClusterState clusterState = cmgManagerProvider.get().clusterState().join();
+
+                    return new ClusterInfo(clusterState.clusterTag(), clusterState.clusterIdHistory());
+                },
+                metricManager,
+                new ClientHandlerMetricSource(),
+                authenticationManager,
+                clockService,
+                schemaSyncService,
+                catalogManager,
+                placementDriverManager.placementDriver(),
+                clientConnectorConfiguration,
+                eventLog,
+                lowWatermark,
+                threadPoolsManager.partitionOperationsExecutor(),
+                () -> suggestionsConfiguration.sequentialDdlExecution().enabled().value()
+        );
+    }
+}
---
+++ modules/runner/src/main/java/org/apache/ignite/internal/app/di/ComputeFactory.java
@@ -0,0 +1,179 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.app.di;
+
+import io.micronaut.context.annotation.Factory;
+import jakarta.inject.Named;
+import jakarta.inject.Provider;
+import jakarta.inject.Singleton;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.internal.cluster.management.ClusterManagementGroupManager;
+import org.apache.ignite.internal.cluster.management.topology.api.LogicalTopologyService;
+import org.apache.ignite.internal.compute.ComputeComponentImpl;
+import org.apache.ignite.internal.compute.IgniteComputeImpl;
+import org.apache.ignite.internal.compute.IgniteComputeInternal;
+import org.apache.ignite.internal.compute.configuration.ComputeConfiguration;
+import org.apache.ignite.internal.compute.configuration.ComputeExtensionConfiguration;
+import org.apache.ignite.internal.compute.executor.ComputeExecutorImpl;
+import org.apache.ignite.internal.compute.state.InMemoryComputeStateMachine;
+import org.apache.ignite.internal.configuration.ConfigurationRegistry;
+import org.apache.ignite.internal.deployunit.DeploymentManagerImpl;
+import org.apache.ignite.internal.deployunit.configuration.DeploymentConfiguration;
+import org.apache.ignite.internal.deployunit.configuration.DeploymentExtensionConfiguration;
+import org.apache.ignite.internal.deployunit.loader.UnitsClassLoaderFactory;
+import org.apache.ignite.internal.deployunit.loader.UnitsContextManager;
+import org.apache.ignite.internal.deployunit.metastore.DeploymentUnitStoreImpl;
+import org.apache.ignite.internal.di.IgniteStartupPhase;
+import org.apache.ignite.internal.di.StartupPhase;
+import org.apache.ignite.internal.eventlog.impl.EventLogImpl;
+import org.apache.ignite.internal.hlc.ClockServiceImpl;
+import org.apache.ignite.internal.hlc.HybridClock;
+import org.apache.ignite.internal.hlc.HybridTimestampTracker;
+import org.apache.ignite.internal.metastorage.impl.MetaStorageManagerImpl;
+import org.apache.ignite.internal.network.ClusterService;
+import org.apache.ignite.internal.placementdriver.PlacementDriverManager;
+import org.apache.ignite.internal.table.distributed.TableManager;
+
+/**
+ * Micronaut factory for compute and deployment components.
+ */
+@Factory
+public class ComputeFactory {
+    /** Creates the compute configuration from the node config registry. */
+    @Singleton
+    public ComputeConfiguration computeConfiguration(
+            @Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry
+    ) {
+        return nodeConfigRegistry.getConfiguration(ComputeExtensionConfiguration.KEY).compute();
+    }
+
+    /** Creates the deployment configuration from the node config registry. */
+    @Singleton
+    public DeploymentConfiguration deploymentConfiguration(
+            @Named("nodeConfig") ConfigurationRegistry nodeConfigRegistry
+    ) {
+        return nodeConfigRegistry.getConfiguration(DeploymentExtensionConfiguration.KEY).deployment();
+    }
+
+    /** Creates the in-memory compute state machine. */
+    @Singleton
+    public InMemoryComputeStateMachine inMemoryComputeStateMachine(
+            ComputeConfiguration computeConfiguration,
+            NodeSeedParams seedParams
+    ) {
+        return new InMemoryComputeStateMachine(computeConfiguration, seedParams.nodeName());
+    }
+
+    /** Creates the compute executor. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public ComputeExecutorImpl computeExecutor(
+            Provider<Ignite> igniteProvider,
+            InMemoryComputeStateMachine stateMachine,
+            ComputeConfiguration computeConfiguration,
+            ClusterService clusterService,
+            ClockServiceImpl clockService,
+            EventLogImpl eventLog
+    ) {
+        return new ComputeExecutorImpl(
+                igniteProvider.get(),
+                stateMachine,
+                computeConfiguration,
+                clusterService.topologyService(),
+                clockService,
+                eventLog
+        );
+    }
+
+    /** Creates the deployment manager. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public DeploymentManagerImpl deploymentManager(
+            ClusterService clusterService,
+            MetaStorageManagerImpl metaStorageManager,
+            LogicalTopologyService logicalTopologyService,
+            NodeSeedParams seedParams,
+            DeploymentConfiguration deploymentConfiguration,
+            ClusterManagementGroupManager cmgManager,
+            ComputeExecutorImpl computeExecutor
+    ) {
+        return new DeploymentManagerImpl(
+                clusterService,
+                new DeploymentUnitStoreImpl(metaStorageManager),
+                logicalTopologyService,
+                seedParams.workDir(),
+                deploymentConfiguration,
+                cmgManager,
+                seedParams.nodeName(),
+                computeExecutor::onUnitRemoving
+        );
+    }
+
+    /** Creates the compute component. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public ComputeComponentImpl computeComponent(
+            NodeSeedParams seedParams,
+            ClusterService clusterService,
+            LogicalTopologyService logicalTopologyService,
+            DeploymentManagerImpl deploymentManager,
+            ComputeExecutorImpl computeExecutor,
+            ComputeConfiguration computeConfiguration,
+            EventLogImpl eventLog,
+            HybridTimestampTracker observableTimestampTracker
+    ) {
+        return new ComputeComponentImpl(
+                seedParams.nodeName(),
+                clusterService.messagingService(),
+                clusterService.topologyService(),
+                logicalTopologyService,
+                new UnitsContextManager(
+                        deploymentManager,
+                        deploymentManager.deploymentUnitAccessor(),
+                        new UnitsClassLoaderFactory()
+                ),
+                computeExecutor,
+                computeConfiguration,
+                eventLog,
+                observableTimestampTracker
+        );
+    }
+
+    /** Creates the internal compute implementation. */
+    @Singleton
+    @IgniteStartupPhase(StartupPhase.PHASE_2)
+    public IgniteComputeInternal igniteCompute(
+            NodeSeedParams seedParams,
+            PlacementDriverManager placementDriverManager,
+            ClusterService clusterService,
+            TableManager tableManager,
+            ComputeComponentImpl computeComponent,
+            HybridClock clock,
+            HybridTimestampTracker observableTimestampTracker
+    ) {
+        return new IgniteComputeImpl(
+                seedParams.nodeName(),
+                placementDriverManager.placementDriver(),
+                clusterService.topologyService(),
+                tableManager,
+                computeComponent,
+                clock,
+                observableTimestampTracker
+        );
+    }
+}
