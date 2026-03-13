8643df4d7daa43188b1f71c1dbf3c273642f5ee4
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-03-13 15:24:41 +0200

    Fix testTransactionCannotBeUsedAfterNodeRestart

--- modules/jdbc/src/integrationTest/java/org/apache/ignite/jdbc/ItJdbcConnectionFailoverTest.java
+++ modules/jdbc/src/integrationTest/java/org/apache/ignite/jdbc/ItJdbcConnectionFailoverTest.java
@@ -219,8 +219,7 @@ public class ItJdbcConnectionFailoverTest extends ClusterPerTestIntegrationTest 
                 cluster.startNode(1);
 
                 //noinspection ThrowableNotThrown
-                assertThrowsSqlException("Transaction context has been lost due to connection errors",
-                        () -> stmt.execute(dummyQuery));
+                assertThrowsSqlException("Transaction is already finished", () -> stmt.execute(dummyQuery));
             }
         }
     }
