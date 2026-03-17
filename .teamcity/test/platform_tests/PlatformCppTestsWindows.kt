dcef106cc663f4018a1d79c6861eb59ab1b379a1
Igor Sapego <isapego@apache.org>
2026-03-17 21:21:17 +0100

    IGNITE-27304: Improve logic to reduce artifact size

--- .teamcity/test/platform_tests/PlatformCppTestsWindows.kt
+++ .teamcity/test/platform_tests/PlatformCppTestsWindows.kt
@@ -108,28 +108,47 @@ object PlatformCppTestsWindows : BuildType({
             platform = PowerShellStep.Platform.x64
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
             scriptMode = script {
-                content = """ 
+                content = """
                     ${'$'}dumpsDir = "%PATH__CRASH_DUMPS%"
 
-                    Write-Host "CMake build dir: %PATH__CMAKE_BUILD_DIRECTORY%"
-                    Write-Host "Working dir: %PATH__WORKING_DIR%"
-                    Write-Host "Dumps dir: %PATH__CRASH_DUMPS%"
-    
                     if (-not (Test-Path ${'$'}dumpsDir)) {
                         Write-Host "Dumps directory '${'$'}dumpsDir' does not exist, skipping."
                         exit 0
                     }
-    
-                    ${'$'}dumps = Get-ChildItem -Path ${'$'}dumpsDir -File
+
+                    ${'$'}dumps = Get-ChildItem -Path ${'$'}dumpsDir -File -Filter "*.dmp"
                     if (${'$'}dumps.Count -eq 0) {
                         Write-Host "Dumps directory '${'$'}dumpsDir' is empty, skipping."
                         exit 0
                     }
-    
+
                     Write-Host "Found ${'$'}(${'$'}dumps.Count) dump file(s), collecting binaries from CMake build directory."
-    
-                    ${'$'}binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"    
-                    Get-ChildItem -Path "${'$'}binDir\*" -File -Include "*.exe", "*.dll", "*.pdb" | ForEach-Object {
+
+                    ${'$'}binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"
+                    if (-not (Test-Path ${'$'}binDir)) {
+                        Write-Error "Bin directory '${'$'}binDir' does not exist."
+                        exit 1
+                    }
+
+                    ${'$'}dumpNames = ${'$'}dumps | ForEach-Object { ${'$'}_.BaseName.Split('_')[0] }
+
+                    ${'$'}exes = Get-ChildItem -Path "${'$'}binDir\*" -File -Include "*.exe" |
+                        Where-Object { ${'$'}dumpNames -contains ${'$'}_.BaseName }
+
+                    ${'$'}dlls = Get-ChildItem -Path "${'$'}binDir\*" -File -Include "*.dll"
+
+                    ${'$'}binaryNames = (${'$'}exes + ${'$'}dlls) | ForEach-Object { ${'$'}_.BaseName }
+
+                    ${'$'}pdbs = Get-ChildItem -Path "${'$'}binDir\*" -File -Include "*.pdb" |
+                        Where-Object { ${'$'}binaryNames -contains ${'$'}_.BaseName }
+
+                    ${'$'}filesToCopy = ${'$'}exes + ${'$'}dlls + ${'$'}pdbs
+                    if (${'$'}filesToCopy.Count -eq 0) {
+                        Write-Host "Warning: no matching binaries found in '${'$'}binDir'."
+                        exit 0
+                    }
+
+                    ${'$'}filesToCopy | ForEach-Object {
                         Copy-Item -Path ${'$'}_.FullName -Destination ${'$'}dumpsDir -Force
                         Write-Host "Copied: ${'$'}(${'$'}_.Name)"
                     }
