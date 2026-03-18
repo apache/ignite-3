22f905487ca72e9c57af67213b62c27f483eaac1
Igor Sapego <isapego@apache.org>
2026-03-19 00:15:53 +0100

    IGNITE-27304: Debug

--- .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
+++ .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
@@ -23,10 +23,22 @@ foreach ($dump in $dumps) {
     Write-Host "##teamcity[blockClosed name='PDB Debug 1: $($dump.Name)']"
 
     Write-Host "##teamcity[blockOpened name='PDB Debug 2: $($dump.Name)']"
-    & $cdb -z $dump.FullName -y $symPath -c "lm v m ignite_client_test; q"
+    & $cdb -z $dump.FullName -y $symPath -c ".reload /f; lm v m ignite_client_test; q"
     Write-Host "##teamcity[blockClosed name='PDB Debug 2: $($dump.Name)']"
 
     Write-Host "##teamcity[blockOpened name='Crash analysis: $($dump.Name)']"
     & $cdb -z $dump.FullName -y $symPath -c ".symopt+ 0x80000; .lines; !analyze -v; .ecxr; kn /f; q"
     Write-Host "##teamcity[blockClosed name='Crash analysis: $($dump.Name)']"
+}
+
+$pdbs = @(Get-ChildItem -Path $dumpsDir -File -Filter "*.pdb")
+if ($pdbs.Count -eq 0) {
+    Write-Host "No PDB files found in '$dumpsDir', skipping."
+    exit 0
+}
+
+foreach ($pdb in $pdbs) {
+    Write-Host "##teamcity[blockOpened name='PDB Debug 1: $($pdb.Name)']"
+    & $pdbcopy $pdb.FullName -p
+    Write-Host "##teamcity[blockClosed name='PDB Debug 1: $($pdb.Name)']"
 }
