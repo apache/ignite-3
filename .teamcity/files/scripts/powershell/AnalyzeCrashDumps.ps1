45c14f60c11333b31b2e7c71497682f0009a3df8
Igor Sapego <isapego@apache.org>
2026-03-18 23:12:49 +0100

    IGNITE-27304: Debug

--- .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
+++ .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
@@ -1,6 +1,7 @@
 $dumpsDir = "%PATH__CRASH_DUMPS%"
 $binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"
 $cdb = "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe"
+$pdbcopy = "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\pdbcopy.exe"
 $symPath = "srv*%PATH__DEBUG_SYMBOLS_DIR%*https://msdl.microsoft.com/download/symbols;$binDir"
 
 if (-not (Test-Path $dumpsDir)) {
@@ -16,9 +17,16 @@ if ($dumps.Count -eq 0) {
 
 foreach ($dump in $dumps) {
     Write-Host "##teamcity[buildProblem description='Crash dump detected: $($dump.Name)']"
+
+    Write-Host "##teamcity[blockOpened name='PDB Debug 1: $($dump.Name)']"
+    & $pdbcopy $dump.FullName -p
+    Write-Host "##teamcity[blockClosed name='PDB Debug 1: $($dump.Name)']"
+
+    Write-Host "##teamcity[blockOpened name='PDB Debug 2: $($dump.Name)']"
+    & $cdb -z $dump.FullName -y $symPath -c "lm v m ignite_client_test; q"
+    Write-Host "##teamcity[blockClosed name='PDB Debug 2: $($dump.Name)']"
+
     Write-Host "##teamcity[blockOpened name='Crash analysis: $($dump.Name)']"
-
-    & $cdb -z $dump.FullName -y $symPath -c ".symopt+ 0x40000; .lines; !analyze -v; .ecxr; kL; q"
-
+    & $cdb -z $dump.FullName -y $symPath -c ".symopt+ 0x80000; .lines; !analyze -v; .ecxr; kn /f; q"
     Write-Host "##teamcity[blockClosed name='Crash analysis: $($dump.Name)']"
 }
