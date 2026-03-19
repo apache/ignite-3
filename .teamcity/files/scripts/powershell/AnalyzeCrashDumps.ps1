11ed3c78cdc3a93a3083b2fdcdee245dcb4ea35f
Igor Sapego <isapego@apache.org>
2026-03-19 09:12:35 +0100

    IGNITE-27304: Debug

--- .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
+++ .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
@@ -2,6 +2,7 @@ $dumpsDir = "%PATH__CRASH_DUMPS%"
 $binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"
 $cdb = "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe"
 $symPath = "srv*%PATH__DEBUG_SYMBOLS_DIR%*https://msdl.microsoft.com/download/symbols;$binDir"
+$srcPath = "%PATH__WORKING_DIR%"
 
 if (-not (Test-Path $dumpsDir)) {
     Write-Host "Dumps directory '$dumpsDir' does not exist, skipping."
@@ -18,6 +19,6 @@ foreach ($dump in $dumps) {
     Write-Host "##teamcity[buildProblem description='Crash dump detected: $($dump.Name)']"
 
     Write-Host "##teamcity[blockOpened name='Crash analysis: $($dump.Name)']"
-    & $cdb -z $dump.FullName -y $symPath -c ".symopt+ 0x80000; .symopt- 0x4; .reload /f; .lines; .ecxr; kL; q"
+    & $cdb -z $dump.FullName -y $symPath -srcpath $srcPath -c ".symopt+ 0x80000; .symopt- 0x4; .reload /f; .lines; .ecxr; kL; q"
     Write-Host "##teamcity[blockClosed name='Crash analysis: $($dump.Name)']"
 }
