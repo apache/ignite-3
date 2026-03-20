bc8e96cd1447ed509a0069b7c73a097995df4cfe
Pavel Tupitsyn <ptupitsyn@apache.org>
2026-03-20 10:44:12 +0100

    IGNITE-28215 Fix transaction context loss in DirectTxUtils (#7814)
    Use tx.channel() when possible and don't resolve by name to avoid channel mismatch.

--- modules/client/src/main/java/org/apache/ignite/internal/client/tx/ClientTransaction.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/tx/ClientTransaction.java
@@ -710,4 +710,15 @@ public class ClientTransaction implements Transaction {
     public boolean killed() {
         return state.get() == STATE_KILLED;
     }
+
+    /**
+     * Validates transaction ownership.
+     *
+     * @param clientCh Client channel.
+     */
+    void validateOwnership(ReliableChannel clientCh) {
+        if (clientCh != reliableChannel) {
+            throw new IllegalArgumentException("Transaction belongs to a different client instance");
+        }
+    }
 }
