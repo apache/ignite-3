d4f39a3e91c99353ed35abdbc82ca7335e03e882
Igor Sapego <isapego@apache.org>
2026-03-20 20:20:20 +0100

    IGNITE-27304: Debug

--- .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
+++ .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
@@ -1,7 +1,7 @@
 $dumpsDir = "%PATH__CRASH_DUMPS%"
 $binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"
 $cdb = "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe"
-$symPath = "srv*%PATH__DEBUG_SYMBOLS_DIR%*https://msdl.microsoft.com/download/symbols;$binDir"
+$symPath = "$dumpsDir;$binDir"
 $srcPath = "%PATH__WORKING_DIR%"
 
 if (-not (Test-Path $dumpsDir)) {
@@ -19,18 +19,7 @@ foreach ($dump in $dumps) {
     Write-Host "##teamcity[buildProblem description='Crash dump detected: $($dump.Name)']"
     Write-Host "##teamcity[blockOpened name='Crash analysis: $($dump.Name)']"
 
-    $stackCommands = @(
-        "kn",
-        "kPL",
-        "kv",
-        "dds esp",
-        "k /f"
-    )
-
-    foreach ($cmd in $stackCommands) {
-        Write-Host "--- Stack trace variant: $cmd ---"
-        & $cdb -z $dump.FullName -y $symPath -srcpath $srcPath -c ".symopt+ 0x80000; .reload /f; .lines; .ecxr; $cmd; q"
-    }
+    & $cdb -z $dump.FullName -y $symPath -srcpath $srcPath -c ".symopt+ 0x80000; .reload /f; .lines; .ecxr; knPL; q"
 
     Write-Host "##teamcity[blockClosed name='Crash analysis: $($dump.Name)']"
 }
