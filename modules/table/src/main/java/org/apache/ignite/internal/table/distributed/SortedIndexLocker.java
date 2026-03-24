9965d0b8a04c881aae33ef98772cd54215390246
ibessonov <bessonov.ip@gmail.com>
2026-03-24 17:41:10 +0300

    IGNITE-28330 <review>
    Signed-off-by: ibessonov <bessonov.ip@gmail.com>

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/SortedIndexLocker.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/SortedIndexLocker.java
@@ -229,6 +229,10 @@ public class SortedIndexLocker implements IndexLocker {
 
         @Override
         public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
