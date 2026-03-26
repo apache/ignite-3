47f51d5baf8868112b7fafd73054800adf8df4f2
Igor Sapego <isapego@apache.org>
2026-03-26 10:37:29 +0100

    IGNITE-27304 C++ Client: Add Windows TC suite (#7875)

---
+++ .teamcity/files/scripts/powershell/AnalyzeCrashDumps.ps1
@@ -0,0 +1,31 @@
+$dumpsDir = "%PATH__CRASH_DUMPS%"
+$binDir = "%PATH__CMAKE_BUILD_DIRECTORY%\Debug\bin"
+$cdb = "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe"
+$symPath = "$dumpsDir;$binDir"
+$srcPath = "%PATH__WORKING_DIR%"
+
+if (-not (Test-Path $dumpsDir)) {
+    Write-Host "Dumps directory '$dumpsDir' does not exist, skipping."
+    exit 0
+}
+
+$dumps = @(Get-ChildItem -Path $dumpsDir -File -Filter "*.dmp")
+if ($dumps.Count -eq 0) {
+    Write-Host "No dump files found in '$dumpsDir', skipping."
+    exit 0
+}
+
+if (-not (Test-Path $cdb)) {
+    Write-Error "Crash dump analyzer requires cdb.exe, but it was not found at the expected path: '$cdb'. " +
+                "Ensure the Windows SDK Debuggers are installed and the path is correct."
+    exit 1
+}
+
+foreach ($dump in $dumps) {
+    Write-Host "##teamcity[buildProblem description='Crash dump detected: $($dump.Name)']"
+    Write-Host "##teamcity[blockOpened name='Crash analysis: $($dump.Name)']"
+
+    & $cdb -z $dump.FullName -y $symPath -srcpath $srcPath -t 0 -c ".symopt+ 0x80000; .reload /f; .lines; .ecxr; ~*kp; q"
+
+    Write-Host "##teamcity[blockClosed name='Crash analysis: $($dump.Name)']"
+}
