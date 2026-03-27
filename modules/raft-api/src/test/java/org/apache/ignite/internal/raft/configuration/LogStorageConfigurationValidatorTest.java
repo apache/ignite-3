f1949d5811df7b0b5977fe20370850a71719a6a7
Aleksandr Polovtsev <alex.polovtcev@gmail.com>
2026-03-27 11:15:08 +0200

    review fix

--- modules/raft-api/src/test/java/org/apache/ignite/internal/raft/configuration/LogStorageConfigurationValidatorTest.java
+++ modules/raft-api/src/test/java/org/apache/ignite/internal/raft/configuration/LogStorageConfigurationValidatorTest.java
@@ -18,12 +18,12 @@
 package org.apache.ignite.internal.raft.configuration;
 
 import static org.apache.ignite.internal.configuration.validation.TestValidationUtil.mockValidationContext;
-import static org.apache.ignite.internal.raft.configuration.LogStorageConfigurationSchema.DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE;
 import static org.apache.ignite.internal.raft.configuration.LogStorageConfigurationSchema.DEFAULT_SEGMENT_FILE_SIZE_BYTES;
-import static org.apache.ignite.internal.raft.configuration.LogStorageConfigurationSchema.DEFAULT_SOFT_LOG_SIZE_LIMIT_BYTES;
 import static org.apache.ignite.internal.raft.configuration.LogStorageConfigurationSchema.UNSPECIFIED_MAX_LOG_ENTRY_SIZE;
 import static org.apache.ignite.internal.util.ArrayUtils.STRING_EMPTY_ARRAY;
 
+import org.apache.ignite.internal.configuration.testframework.ConfigurationExtension;
+import org.apache.ignite.internal.configuration.testframework.InjectConfiguration;
 import org.apache.ignite.internal.configuration.validation.TestValidationUtil;
 import org.apache.ignite.internal.testframework.BaseIgniteAbstractTest;
 import org.jetbrains.annotations.Nullable;
@@ -33,6 +33,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(ConfigurationExtension.class)
 class LogStorageConfigurationValidatorTest extends BaseIgniteAbstractTest {
     @Mock
     private static ValidLogStorageConfiguration VALID_LOG_STORAGE_CONFIGURATION;
@@ -40,125 +41,63 @@ class LogStorageConfigurationValidatorTest extends BaseIgniteAbstractTest {
     private final LogStorageConfigurationValidator validator = new LogStorageConfigurationValidator();
 
     @Test
-    void unspecifiedLogEntrySizeIsValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                DEFAULT_SEGMENT_FILE_SIZE_BYTES,
-                UNSPECIFIED_MAX_LOG_ENTRY_SIZE,
-                DEFAULT_SOFT_LOG_SIZE_LIMIT_BYTES
-        );
-
+    void unspecifiedLogEntrySizeIsValid(
+            @InjectConfiguration(value = "mock.maxLogEntrySizeBytes=" + UNSPECIFIED_MAX_LOG_ENTRY_SIZE, validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config);
     }
 
     @Test
-    void correctLogEntrySizeIsValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                DEFAULT_SEGMENT_FILE_SIZE_BYTES,
-                (int) (DEFAULT_SEGMENT_FILE_SIZE_BYTES * 0.9),
-                DEFAULT_SOFT_LOG_SIZE_LIMIT_BYTES
-        );
-
+    void correctLogEntrySizeIsValid(
+            @InjectConfiguration(value = "mock.maxLogEntrySizeBytes=" + (int) (DEFAULT_SEGMENT_FILE_SIZE_BYTES * 0.9), validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config);
     }
 
     @Test
-    void zeroLogEntrySizeIsNotValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                DEFAULT_SEGMENT_FILE_SIZE_BYTES,
-                0,
-                DEFAULT_SOFT_LOG_SIZE_LIMIT_BYTES
-        );
-
+    void zeroLogEntrySizeIsNotValid(
+            @InjectConfiguration(value = "mock.maxLogEntrySizeBytes=0", validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config, "Maximum log entry size must be positive [maxEntrySize=0 bytes].");
     }
 
     @Test
-    void logEntrySizeEqualToSegmentFileSizeIsNotValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                10,
-                10,
-                DEFAULT_SOFT_LOG_SIZE_LIMIT_BYTES
-        );
-
+    void logEntrySizeEqualToSegmentFileSizeIsNotValid(
+            @InjectConfiguration(value = "mock { maxLogEntrySizeBytes=10, segmentFileSizeBytes=10 }", validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config, "Maximum log entry size is too big [maxEntrySize=10 bytes, maxAllowedEntrySize=9 bytes].");
     }
 
     @Test
-    void softLimitLessThanSegmentFileSizeIsNotValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                1000,
-                UNSPECIFIED_MAX_LOG_ENTRY_SIZE,
-                500
-        );
-
+    void softLimitLessThanSegmentFileSizeIsNotValid(
+            @InjectConfiguration(value = "mock { softLogSizeLimitBytes=500, segmentFileSizeBytes=1000 }", validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config, "Soft log size limit must be at least the segment file size [softLimit=500 bytes, segmentFileSize=1000 bytes].");
     }
 
     @Test
-    void softLimitEqualToSegmentFileSizeIsValid() {
-        var config = new MockLogStorageView(
-                DEFAULT_MAX_CHECKPOINT_QUEUE_SIZE,
-                1000,
-                UNSPECIFIED_MAX_LOG_ENTRY_SIZE,
-                1000
-        );
-
+    void softLimitEqualToSegmentFileSizeIsValid(
+            @InjectConfiguration(value = "mock { softLogSizeLimitBytes=1000, segmentFileSizeBytes=1000 }", validate = false)
+            LogStorageConfiguration config
+    ) {
         validate(config);
     }
 
-    private void validate(LogStorageView config) {
+    private void validate(LogStorageConfiguration config) {
         validate(config, STRING_EMPTY_ARRAY);
     }
 
-    private void validate(LogStorageView config, String @Nullable ... errorMessagePrefixes) {
+    private void validate(LogStorageConfiguration config, String @Nullable ... errorMessagePrefixes) {
         TestValidationUtil.validate(
                 validator,
                 VALID_LOG_STORAGE_CONFIGURATION,
-                mockValidationContext(null, config),
+                mockValidationContext(null, config.value()),
                 errorMessagePrefixes
         );
-    }
-
-    private static class MockLogStorageView implements LogStorageView {
-        private final int maxCheckpointQueueSize;
-
-        private final long segmentFileSizeBytes;
-
-        private final int maxLogEntrySizeBytes;
-
-        private final long softLogSizeLimitBytes;
-
-        MockLogStorageView(int maxCheckpointQueueSize, long segmentFileSizeBytes, int maxLogEntrySizeBytes,
-                long softLogSizeLimitBytes) {
-            this.maxCheckpointQueueSize = maxCheckpointQueueSize;
-            this.segmentFileSizeBytes = segmentFileSizeBytes;
-            this.maxLogEntrySizeBytes = maxLogEntrySizeBytes;
-            this.softLogSizeLimitBytes = softLogSizeLimitBytes;
-        }
-
-        @Override
-        public int maxCheckpointQueueSize() {
-            return maxCheckpointQueueSize;
-        }
-
-        @Override
-        public long segmentFileSizeBytes() {
-            return segmentFileSizeBytes;
-        }
-
-        @Override
-        public int maxLogEntrySizeBytes() {
-            return maxLogEntrySizeBytes;
-        }
-
-        @Override
-        public long softLogSizeLimitBytes() {
-            return softLogSizeLimitBytes;
-        }
     }
 }
